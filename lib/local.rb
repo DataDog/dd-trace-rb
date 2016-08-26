d9da15c3cf63b515582726f83bba7c80f3ccb08e
Matt Perpick <matthewperpick@gmail.com>
2016-08-26 18:22:57 +0000

    comments

--- lib/local.rb
+++ lib/local.rb
@@ -6,14 +6,17 @@ module Datadog
 
   class SpanBuffer
 
+    # Set the current active span.
     def set(span)
       Thread.current[:datadog_span] = span
     end
 
+    # Return the current active span or nil.
     def get()
       return Thread.current[:datadog_span]
     end
 
+    # Pop the current active span.
     def pop()
       s = self.get()
       self.set(nil)
