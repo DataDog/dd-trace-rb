035f7d6d75a518a489b0dc386187852cd5a2ef78
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2016-10-04 09:28:53 +0200

    [trace] linting lib

--- lib/ddtrace/local.rb
+++ lib/ddtrace/local.rb
@@ -1,28 +1,22 @@
-
-
 require 'thread'
 
 module Datadog
-
   class SpanBuffer
-
     # Set the current active span.
     def set(span)
       Thread.current[:datadog_span] = span
     end
 
     # Return the current active span or nil.
-    def get()
-      return Thread.current[:datadog_span]
+    def get
+      Thread.current[:datadog_span]
     end
 
     # Pop the current active span.
-    def pop()
-      s = self.get()
-      self.set(nil)
-      return s
+    def pop
+      s = get
+      set(nil)
+      s
     end
-
   end
-
 end
--- lib/ddtrace/writer.rb
+++ lib/ddtrace/writer.rb
@@ -1,37 +1,33 @@
-
-
 require 'ddtrace/transport'
 
 module Datadog
-
   # Writer buffer and periodically sends traces to the server.
   class Writer
-
-    def initialize(options={})
-      @transport = options[:transport] || Datadog::Transport.new("localhost", "7777")
+    def initialize(options = {})
+      @transport = options[:transport] || Datadog::Transport.new('localhost', '7777')
       @trace_buffer = TraceBuffer.new(100)
 
       @flush_interval = 1
       @traces_flushed = 0
 
-      self.spawn(@flush_interval)
+      spawn(@flush_interval)
     end
 
     # spawn will spawn a thread that will periodically flush to the server.
     def spawn(interval)
-      Thread.new {
+      Thread.new do
         loop do
           sleep(interval)
-          self.flush()
+          flush
         end
-      }
+      end
     end
 
     # flush will trigger a flush to the server.
-    def flush()
-      traces = @trace_buffer.pop()
-      if 0 < traces.length
-        spans = traces.flatten()
+    def flush
+      traces = @trace_buffer.pop
+      unless traces.empty?
+        spans = traces.flatten
         # FIXME[matt] submit as an array of traces or a flat array of spans?
         #
         @transport.write(spans)          # FIXME matt: if there's an error, requeue
@@ -45,19 +41,17 @@ module Datadog
     end
 
     # stats returns a dictionary of stats about the writer.
-    def stats()
-      return {
-        :traces_flushed => @traces_flushed,
-        :traces_buffered => @trace_buffer.length()
+    def stats
+      {
+        traces_flushed: @traces_flushed,
+        traces_buffered: @trace_buffer.length
       }
     end
-
   end
 
   # TraceBuffer buffers a maximum number of traces waiting to be sent to the
   # app.
   class TraceBuffer
-
     def initialize(max_size)
       @mutex = Mutex.new
       @traces = []
@@ -65,31 +59,29 @@ module Datadog
     end
 
     def push(trace)
-      @mutex.synchronize {
+      @mutex.synchronize do
         len = @traces.length
         if len < @max_size
           @traces << trace
         else
-          @traces[rand(len)] = trace    # drop a random one
+          @traces[rand(len)] = trace # drop a random one
         end
-      }
+      end
     end
 
-    def length()
-      @mutex.synchronize {
+    def length
+      @mutex.synchronize do
         return @traces.length
-      }
+      end
     end
 
-    def pop()
-      @mutex.synchronize {
+    def pop
+      @mutex.synchronize do
         # FIXME: reuse array?
         t = @traces
         @traces = []
         return t
-      }
+      end
     end
-
   end
-
 end
