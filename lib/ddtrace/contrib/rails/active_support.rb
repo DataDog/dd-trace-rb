f29f80400c40ae5040be5190e58495024b890ffb
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2016-10-07 18:54:55 +0200

    [rails] active_support refactoring to prevent duplication

--- lib/ddtrace/contrib/rails/active_support.rb
+++ lib/ddtrace/contrib/rails/active_support.rb
@@ -1,3 +1,5 @@
+require 'ddtrace/ext/cache'
+
 module Datadog
   module Contrib
     module Rails
@@ -6,25 +8,27 @@ module Datadog
         def self.instrument
           # subscribe when a cache read has been processed
           ::ActiveSupport::Notifications.subscribe('cache_read.active_support') do |*args|
-            cache_read(*args)
+            trace_cache('GET', *args)
           end
 
           # subscribe when a cache write has been processed
           ::ActiveSupport::Notifications.subscribe('cache_write.active_support') do |*args|
-            cache_write(*args)
+            trace_cache('SET', *args)
           end
 
           # subscribe when a cache delete has been processed
           ::ActiveSupport::Notifications.subscribe('cache_delete.active_support') do |*args|
-            cache_delete(*args)
+            trace_cache('DELETE', *args)
           end
         end
 
-        def self.cache_read(_name, start, finish, _id, payload)
+        def self.trace_cache(resource, _name, start, finish, _id, payload)
           tracer = ::Rails.configuration.datadog_trace.fetch(:tracer)
+          span_type = Datadog::Ext::CACHE::TYPE
+
           span = tracer.trace('rails.cache')
-          span.span_type = 'cache'
-          span.resource = 'GET'
+          span.span_type = span_type
+          span.resource = resource
           span.set_tag('rails.cache.backend', ::Rails.configuration.cache_store)
           span.set_tag('rails.cache.key', payload.fetch(:key))
           span.start_time = start
@@ -34,33 +38,7 @@ module Datadog
           puts e
         end
 
-        def self.cache_write(_name, start, finish, _id, payload)
-          tracer = ::Rails.configuration.datadog_trace.fetch(:tracer)
-          span = tracer.trace('rails.cache')
-          span.span_type = 'cache'
-          span.resource = 'SET'
-          span.set_tag('rails.cache.backend', ::Rails.configuration.cache_store)
-          span.set_tag('rails.cache.key', payload.fetch(:key))
-          span.start_time = start
-          span.finish_at(finish)
-        rescue StandardError => e
-          # TODO[manu]: better error handling
-          puts e
-        end
-
-        def self.cache_delete(_name, start, finish, _id, payload)
-          tracer = ::Rails.configuration.datadog_trace.fetch(:tracer)
-          span = tracer.trace('rails.cache')
-          span.span_type = 'cache'
-          span.resource = 'DELETE'
-          span.set_tag('rails.cache.backend', ::Rails.configuration.cache_store)
-          span.set_tag('rails.cache.key', payload.fetch(:key))
-          span.start_time = start
-          span.finish_at(finish)
-        rescue StandardError => e
-          # TODO[manu]: better error handling
-          puts e
-        end
+        private_class_method :trace_cache
       end
     end
   end
