7e686362bb86b1b326714a3c149ca7113d6bf2bc
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2016-10-11 10:57:50 +0200

    [trace] improved Transport API; making Net::HTTP thread-safe

--- lib/ddtrace/transport.rb
+++ lib/ddtrace/transport.rb
@@ -1,23 +1,63 @@
+require 'thread'
 require 'net/http'
-require 'ddtrace/encoding'
+require 'ddtrace'
 
 module Datadog
   # Transport class that handles the spans delivery to the
-  # local trace-agent
-  class Transport
+  # local trace-agent. The class wraps a Net:HTTP instance
+  # so that the Transport is thread-safe.
+  class HTTPTransport
     def initialize(host, port)
-      @http = Net::HTTP.new(host, port)
       @headers = { 'Content-Type' => 'text/json' }
+
+      @mutex = Mutex.new
+      @http = Net::HTTP.new(host, port)
     end
 
-    def write(spans)
-      out = Datadog::Encoding.encode_spans(spans)
+    # send data to the trace-agent; the method is thread-safe
+    def send(url, data)
+      request = Net::HTTP::Post.new(url, @headers)
+      request.body = data
 
-      request = Net::HTTP::Post.new('/spans', @headers)
-      request.body = out
+      @mutex.synchronize do
+        response = @http.request(request)
+        handle_response(response)
+      end
+    end
 
-      response = @http.request(request)
-      puts response
+    def informational?(code)
+      code.between?(100, 199)
+    end
+
+    def success?(code)
+      code.between?(200, 299)
+    end
+
+    def redirect?(code)
+      code.between?(300, 399)
+    end
+
+    def client_error?(code)
+      code.between?(400, 499)
+    end
+
+    def server_error?(code)
+      code.between?(500, 599)
+    end
+
+    # handles the server response; here you can log the trace-agent response
+    # or do something more complex to recover from a possible error. This
+    # function is handled within the HTTP mutex.synchronize so it's thread-safe.
+    def handle_response(response)
+      status_code = response.code
+
+      if success?(status_code)
+        Datadog::Tracer.log.debug('Payload correctly sent to the trace agent.')
+      elsif client_error?(status_code)
+        Datadog::Tracer.log.error("Client error: #{response.message}")
+      elsif server_error?(status_code)
+        Datadog::Tracer.log.error("Server error: #{response.message}")
+      end
     end
   end
 end
