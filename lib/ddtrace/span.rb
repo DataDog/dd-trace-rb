9ea8ede69af7a90534a3335ff7f53ed1bd932769
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2016-10-14 14:14:23 +0200

    [docs] documentation for Span

--- lib/ddtrace/span.rb
+++ lib/ddtrace/span.rb
@@ -4,17 +4,29 @@ require 'ddtrace/utils'
 require 'ddtrace/ext/errors'
 
 module Datadog
-  # Span model that defines a logical unit of work that
-  # composes a complete trace.
+  # Represents a logical unit of work in the system. Each trace consists of one or more spans.
+  # Each span consists of a start time and a duration. For example, a span can describe the time
+  # spent on a distributed call on a separate machine, or the time spent in a small component
+  # within a larger operation. Spans can be nested within each other, and in those instances
+  # will have a parent-child relationship.
   class Span
+    # The max value for a \Span identifier
     MAX_ID = 2**64 - 1
 
     attr_accessor :name, :service, :resource, :span_type,
                   :start_time, :end_time,
                   :span_id, :trace_id, :parent_id,
-                  :meta, :status, :parent
+                  :status, :parent
 
-    # Create a new span linked to the given tracer.
+    # Create a new span linked to the given tracer. Call the <tt>finish()</tt> method once the
+    # tracer operation is over or use the <tt>finish_at(time)</tt> helper to close the span with the
+    # given +time+. Available options are:
+    #
+    # * +service+: the service name for this span
+    # * +resource+: the resource this span refers, or +name+ if it's missing
+    # * +span_type+: the type of the span (such as +http+, +db+ and so on)
+    # * +parent_id+: the identifier of the parent span
+    # * +trace_id+: the identifier of the root span for this trace
     def initialize(tracer, name, options = {})
       @tracer = tracer
 
@@ -36,6 +48,10 @@ module Datadog
       @end_time = nil
     end
 
+    # Set the given key / value tag pair on the span. Keys and values
+    # must be strings (or have a proper +to_s+). A valid example is
+    #
+    #   span.set_tag('http.method', request.method)
     def set_tag(key, value)
       @meta[key] = value
     end
@@ -88,6 +104,7 @@ module Datadog
       end
     end
 
+    # Return the hash representation of the current span.
     def to_hash
       h = {
         span_id: @span_id,
