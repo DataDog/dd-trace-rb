c91314fa30e503487601bf752e336c50cf086124
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2016-10-18 12:52:42 +0200

    [rails] instead of creating an initializer, bootstrap the instrumentation after initializers are loaded

--- test/contrib/rails/tracer_test.rb
+++ test/contrib/rails/tracer_test.rb
@@ -3,22 +3,16 @@ require 'helper'
 require 'contrib/rails/test_helper'
 
 class TracerTest < ActionController::TestCase
-  setup do
-    # reinitialize the tracer with default settings
-    Datadog::Contrib::Rails::Framework.configure({})
-  end
-
-  test 'correct initializer for ddtrace' do
-    initializer = Rails.application.initializers.detect { |i| i.name == 'ddtrace.instrument' }
-    assert initializer
-  end
-
-  test 'a tracer is available in the Rails config' do
-    tracer = Rails.configuration.datadog_trace[:tracer]
-    assert tracer
+  test 'the configuration is correctly called' do
+    assert Rails.configuration.datadog_trace[:enabled]
+    assert Rails.configuration.datadog_trace[:auto_instrument]
+    assert_equal(Rails.configuration.datadog_trace[:default_service], 'rails-app')
+    assert_equal(Rails.configuration.datadog_trace[:template_base_path], 'views/')
+    assert_equal(Rails.configuration.datadog_trace[:tracer], Datadog.tracer)
   end
 
   test 'a default service is properly set' do
+    Datadog::Contrib::Rails::Framework.configure({})
     tracer = Rails.configuration.datadog_trace[:tracer]
     assert_equal(tracer.services, 'rails-app' => { 'app' => 'rails', 'app_type' => 'web' })
   end
