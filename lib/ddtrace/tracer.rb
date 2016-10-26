aa3efe722f4c71b8f3ef8819361264afca66873c
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2016-10-26 10:55:48 +0200

    [tracer] refactoring how threads are used; Defining an AsyncTransport that owns the sending buffer (thread-safety is granted)

--- lib/ddtrace/tracer.rb
+++ lib/ddtrace/tracer.rb
@@ -49,6 +49,8 @@ module Datadog
       @mutex = Mutex.new
       @spans = []
       @services = {}
+
+      @writer.start()
     end
 
     # Updates the current \Tracer instance, so that the tracer can be configured after the
