0a783bd460e596c9e652bf34ce27941e8e01d885
Christian Mauduit (DataDog) <christian.mauduit@datadoghq.com>
2016-12-12 15:01:51 +0100

    [redis] factorized testing code so that all drivers do the same

--- test/contrib/redis/redis_test.rb
+++ test/contrib/redis/redis_test.rb
@@ -8,35 +8,43 @@ class RedisSetGetTest < Minitest::Test
   def setup
     @tracer = get_test_tracer
 
-    @redis = Redis.new(host: REDIS_HOST, port: REDIS_PORT, driver: :ruby)
-    pin = Datadog::Pin.get_from(@redis)
-    pin.tracer = @tracer
+    @drivers = {}
+    [:ruby, :hiredis].each do |d|
+      @drivers[d] = Redis.new(host: REDIS_HOST, port: REDIS_PORT, driver: d)
+      pin = Datadog::Pin.get_from(@drivers[d])
+      pin.tracer = @tracer
+    end
   end
 
-  def check_connect_span(span)
+  def check_connect_span(d, span)
     # we don't know when connection is going to happen, when
     # there's a suspicion it happened (typically, one extra span)
     # check the instrumentation is OK with this func.
     assert_equal('redis.connect', span.name)
     assert_equal('redis', span.service)
     assert_equal('127.0.0.1:46379', span.resource)
-    assert_equal('Redis::Connection::Ruby', span.get_tag('redis.driver'))
+    case d
+    when :ruby
+      assert_equal('Redis::Connection::Ruby', span.get_tag('redis.driver'))
+    when :hiredis
+      assert_equal('Redis::Connection::Hiredis', span.get_tag('redis.driver'))
+    end
   end
 
-  def roundtrip_set
-    set_response = @redis.set 'FOO', 'bar'
+  def roundtrip_set(d, driver)
+    set_response = driver.set 'FOO', 'bar'
     assert_equal 'OK', set_response
     spans = @tracer.writer.spans()
     assert_operator(1, :<=, spans.length)
-    check_connect_span(spans[0]) if spans.length >= 2
+    check_connect_span(d, spans[0]) if spans.length >= 2
     span = spans[-1]
     assert_equal('redis.command', span.name)
     assert_equal('redis', span.service)
     assert_equal('set FOO bar', span.resource)
   end
 
-  def roundtrip_get
-    get_response = @redis.get 'FOO'
+  def roundtrip_get(_d, driver)
+    get_response = driver.get 'FOO'
     assert_equal 'bar', get_response
     spans = @tracer.writer.spans()
     assert_equal(1, spans.length)
@@ -47,26 +55,30 @@ class RedisSetGetTest < Minitest::Test
   end
 
   def test_roundtrip
-    roundtrip_set
-    roundtrip_get
+    @drivers.each do |d, driver|
+      roundtrip_set d, driver
+      roundtrip_get d, driver
+    end
   end
 
   def test_pipeline
-    responses = []
-    @redis.pipelined do
-      responses << @redis.set('v1', '0')
-      responses << @redis.set('v2', '0')
-      responses << @redis.incr('v1')
-      responses << @redis.incr('v2')
-      responses << @redis.incr('v2')
+    @drivers.each do |d, driver|
+      responses = []
+      driver.pipelined do
+        responses << driver.set('v1', '0')
+        responses << driver.set('v2', '0')
+        responses << driver.incr('v1')
+        responses << driver.incr('v2')
+        responses << driver.incr('v2')
+      end
+      assert_equal(['OK', 'OK', 1, 1, 2], responses.map(&:value))
+      spans = @tracer.writer.spans()
+      assert_operator(1, :<=, spans.length)
+      check_connect_span(d, spans[0]) if spans.length >= 2
+      span = spans[-1]
+      assert_equal('redis.pipeline', span.name)
+      assert_equal('redis', span.service)
+      assert_equal("set v1 0\nset v2 0\nincr v1\nincr v2\nincr v2", span.resource)
     end
-    assert_equal(['OK', 'OK', 1, 1, 2], responses.map(&:value))
-    spans = @tracer.writer.spans()
-    assert_operator(1, :<=, spans.length)
-    check_connect_span(spans[0]) if spans.length >= 2
-    span = spans[-1]
-    assert_equal('redis.pipeline', span.name)
-    assert_equal('redis', span.service)
-    assert_equal("set v1 0\nset v2 0\nincr v1\nincr v2\nincr v2", span.resource)
   end
 end
