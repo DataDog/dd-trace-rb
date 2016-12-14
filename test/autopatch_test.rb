9010fd5740638dbad3969f531717eaba9bd0ab68
Christian Mauduit (DataDog) <christian.mauduit@datadoghq.com>
2016-12-14 11:16:48 +0100

    [autopatch] comments nitpicks

--- test/autopatch_test.rb
+++ test/autopatch_test.rb
@@ -5,17 +5,19 @@ require 'redis'
 
 # AutopatchTest checks that autopatching works when using the
 # env var DATADOG_TRACE_AUTOPATCH is set to true (the 'true' string).
-# This env var ^ MUST BE SET for the test to pass.
 class AutopatchTest < Minitest::Test
   def test_autopatch
+    # Importing 'ddtrace/monkey' for the sake of testing and access
+    # the list of patched modules, but you don't need this in the real world.
     require 'ddtrace/monkey'
     patched_modules = Datadog::Monkey.get_patched_modules
     assert_equal(false, patched_modules[:elasticsearch])
     assert_equal(false, patched_modules[:redis])
+
     ENV['DATADOG_TRACE_AUTOPATCH'] = 'true'
-    # Note, when using autopatching, requirements order does matter,
+    # When using autopatching, requirements order does matter,
     # you need to import 3rd party libs *before* ddtrace.
-    require 'ddtrace'
+    require 'ddtrace' # here, patching happens
     patched_modules = Datadog::Monkey.get_patched_modules
     assert_equal(true, patched_modules[:elasticsearch])
     assert_equal(true, patched_modules[:redis])
