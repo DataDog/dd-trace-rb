d98ad92808452e2b7f8d0935a675b42521f3cf13
Christian Mauduit (DataDog) <christian.mauduit@datadoghq.com>
2016-12-19 15:37:35 +0100

    [rails/redis] bulletproofing rails+redis integration

--- lib/ddtrace/contrib/rails/active_support.rb
+++ lib/ddtrace/contrib/rails/active_support.rb
@@ -1,3 +1,4 @@
+require 'thread'
 require 'ddtrace/ext/cache'
 
 module Datadog
@@ -37,20 +38,44 @@ module Datadog
           end
         end
 
-        def self.start_trace_cache(_resource, *_args)
-          tracer = ::Rails.configuration.datadog_trace.fetch(:tracer)
+        def self.create_span(tracer)
           service = ::Rails.configuration.datadog_trace.fetch(:default_cache_service)
           type = Datadog::Ext::CACHE::TYPE
           tracer.trace('rails.cache', service: service, span_type: type)
+        end
+
+        def self.get_key(resource)
+          'datadog_activesupport_' + resource
+        end
+
+        def self.start_trace_cache(resource, *_args)
+          create_span(::Rails.configuration.datadog_trace.fetch(:tracer))
+          Thread.current[get_key(resource)] = true
         rescue StandardError => e
           Datadog::Tracer.log.error(e.message)
         end
 
         def self.trace_cache(resource, _name, start, finish, _id, payload)
-          # finish the tracing and update the execution time
           tracer = ::Rails.configuration.datadog_trace.fetch(:tracer)
+          key = get_key(resource)
+          if Thread.current[key]
+            # span was created by start_trace_cache, plan to re-use this one
+            Thread.current[key] = false
+          else
+            # Create a span now, as start_trace_cache was not called.
+            #
+            # This could typically happen if, for some reason the monkey-patching
+            # of the cache class did not work as expected. Doing this, we might
+            # loose some interesting parentship between some spans, because this
+            # span is created too late, and children won't "find" their parent.
+            # But, it's better than no span at all, and it case there is no child
+            # at all, it will work just as expected. In practice, it's required to
+            # have standard file cache work together with redis cache.
+            create_span(tracer)
+          end
           span = tracer.active_span()
           return unless span
+          # finish the tracing and update the execution time
           span.resource = resource
           span.set_tag('rails.cache.backend', ::Rails.configuration.cache_store)
           span.set_tag('rails.cache.key', payload.fetch(:key))
@@ -68,7 +93,7 @@ module Datadog
           Datadog::Tracer.log.error(e.message)
         end
 
-        private_class_method :trace_cache
+        private_class_method :create_span, :get_key, :start_trace_cache, :trace_cache
       end
     end
   end
