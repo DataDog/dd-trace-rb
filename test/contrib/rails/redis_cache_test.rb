5fd02222274fbd190ae19d96f5a3d4f2d4a53f37
Christian Mauduit (DataDog) <christian.mauduit@datadoghq.com>
2016-12-19 14:43:08 +0100

    [rails/redis] added proper for the rails+redis case

--- test/contrib/rails/redis_cache_test.rb
+++ test/contrib/rails/redis_cache_test.rb
@@ -6,11 +6,15 @@ require 'redis-activesupport'
 require 'helper'
 require 'contrib/rails/test_helper'
 
+# require 'redis-activesupport'
+
 class RedisCacheTracingTest < ActionController::TestCase
   setup do
     @original_tracer = Rails.configuration.datadog_trace[:tracer]
     @tracer = get_test_tracer
     Rails.configuration.datadog_trace[:tracer] = @tracer
+    pin = Datadog::Pin.get_from(Rails.cache.data)
+    pin.tracer = @tracer
   end
 
   teardown do
@@ -24,7 +28,7 @@ class RedisCacheTracingTest < ActionController::TestCase
     assert_equal(50, value)
 
     spans = @tracer.writer.spans()
-    assert_equal(spans.length, 2)
+    assert_equal(spans.length, 4)
     span = spans[-1]
     assert_equal(span.name, 'rails.cache')
     assert_equal(span.span_type, 'cache')
@@ -32,48 +36,57 @@ class RedisCacheTracingTest < ActionController::TestCase
     assert_equal(span.service, 'rails-cache')
     assert_equal(span.get_tag('rails.cache.backend').to_s, '[:redis_store, {:host=>"127.0.0.1", :port=>"46379"}]')
     assert_equal(span.get_tag('rails.cache.key'), 'custom-key')
+    span = spans[-2]
+    assert_equal(span.name, 'redis.command')
+    assert_equal(span.span_type, 'redis')
+    assert_equal(span.resource, 'get custom-key')
+    assert_equal(span.service, 'redis')
+    # the following ensures span will be correctly displayed (parent/child of the same trace)
+    assert_equal(spans[-1].trace_id, spans[-2].trace_id)
+    assert_equal(spans[-1].span_id, spans[-2].parent_id)
   end
 
   test 'cache.write() is properly traced' do
     # use the cache and assert the proper span
     Rails.cache.write('custom-key', 50)
     spans = @tracer.writer.spans()
-    assert_equal(spans.length, 1)
-    span = spans[0]
+    assert_equal(spans.length, 2)
+    span = spans[-1]
     assert_equal(span.name, 'rails.cache')
     assert_equal(span.span_type, 'cache')
     assert_equal(span.resource, 'SET')
     assert_equal(span.service, 'rails-cache')
     assert_equal(span.get_tag('rails.cache.backend').to_s, '[:redis_store, {:host=>"127.0.0.1", :port=>"46379"}]')
     assert_equal(span.get_tag('rails.cache.key'), 'custom-key')
+    span = spans[-2]
+    assert_equal(span.name, 'redis.command')
+    assert_equal(span.span_type, 'redis')
+    assert_match(/set custom-key .*ActiveSupport.*/, span.resource)
+    assert_equal(span.service, 'redis')
+    # the following ensures span will be correctly displayed (parent/child of the same trace)
+    assert_equal(spans[-1].trace_id, spans[-2].trace_id)
+    assert_equal(spans[-1].span_id, spans[-2].parent_id)
   end
 
   test 'cache.delete() is properly traced' do
     # use the cache and assert the proper span
     Rails.cache.delete('custom-key')
     spans = @tracer.writer.spans()
-    assert_equal(spans.length, 1)
-    span = spans[0]
+    assert_equal(spans.length, 2)
+    span = spans[-1]
     assert_equal(span.name, 'rails.cache')
     assert_equal(span.span_type, 'cache')
     assert_equal(span.resource, 'DELETE')
     assert_equal(span.service, 'rails-cache')
     assert_equal(span.get_tag('rails.cache.backend').to_s, '[:redis_store, {:host=>"127.0.0.1", :port=>"46379"}]')
     assert_equal(span.get_tag('rails.cache.key'), 'custom-key')
-  end
-
-  test 'doing a cache call uses the proper service name if it is changed' do
-    # update database configuration
-    update_config(:default_cache_service, 'service-cache')
-
-    # make the cache write and assert the proper spans
-    Rails.cache.write('custom-key', 50)
-    spans = @tracer.writer.spans()
-    assert_equal(spans.length, 1)
-    span = spans.first
-    assert_equal(span.service, 'service-cache')
-
-    # reset the original configuration
-    reset_config()
+    span = spans[-2]
+    assert_equal(span.name, 'redis.command')
+    assert_equal(span.span_type, 'redis')
+    assert_equal(span.resource, 'del custom-key')
+    assert_equal(span.service, 'redis')
+    # the following ensures span will be correctly displayed (parent/child of the same trace)
+    assert_equal(spans[-1].trace_id, spans[-2].trace_id)
+    assert_equal(spans[-1].span_id, spans[-2].parent_id)
   end
 end
