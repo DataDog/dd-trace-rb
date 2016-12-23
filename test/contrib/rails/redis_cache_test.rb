5b07ccac8b35bb1117ceb535abe63d8cc2a053d6
Christian Mauduit (DataDog) <christian.mauduit@datadoghq.com>
2016-12-23 14:23:43 +0100

    [redis/rails] write test to check fetch works as expected

--- test/contrib/rails/redis_cache_test.rb
+++ test/contrib/rails/redis_cache_test.rb
@@ -25,29 +25,33 @@ class RedisCacheTracingTest < ActionController::TestCase
     Rails.configuration.datadog_trace[:tracer] = @original_tracer
   end
 
-  test 'cache.read() is properly traced' do
-    # use the cache and assert the proper span
-    Rails.cache.write('custom-key', 50)
-    value = Rails.cache.read('custom-key')
-    assert_equal(50, value)
+  test 'cache.read() and cache.fetch() are properly traced' do
+    # read and fetch should behave exactly the same, and we shall
+    # never see a read() having a fetch() as parent.
+    [:read, :fetch].each do |f|
+      # use the cache and assert the proper span
+      Rails.cache.write('custom-key', 50)
+      value = Rails.cache.send(f, 'custom-key')
+      assert_equal(50, value)
 
-    spans = @tracer.writer.spans()
-    assert_equal(spans.length, 4)
-    span = spans[-1]
-    assert_equal(span.name, 'rails.cache')
-    assert_equal(span.span_type, 'cache')
-    assert_equal(span.resource, 'GET')
-    assert_equal(span.service, 'rails-cache')
-    assert_equal(span.get_tag('rails.cache.backend').to_s, '[:redis_store, {:host=>"127.0.0.1", :port=>"46379"}]')
-    assert_equal(span.get_tag('rails.cache.key'), 'custom-key')
-    span = spans[-2]
-    assert_equal(span.name, 'redis.command')
-    assert_equal(span.span_type, 'redis')
-    assert_equal(span.resource, 'get custom-key')
-    assert_equal(span.service, 'redis')
-    # the following ensures span will be correctly displayed (parent/child of the same trace)
-    assert_equal(spans[-1].trace_id, spans[-2].trace_id)
-    assert_equal(spans[-1].span_id, spans[-2].parent_id)
+      spans = @tracer.writer.spans()
+      assert_equal(spans.length, 4)
+      span = spans[-1]
+      assert_equal(span.name, 'rails.cache')
+      assert_equal(span.span_type, 'cache')
+      assert_equal(span.resource, 'GET')
+      assert_equal(span.service, 'rails-cache')
+      assert_equal(span.get_tag('rails.cache.backend').to_s, '[:redis_store, {:host=>"127.0.0.1", :port=>"46379"}]')
+      assert_equal(span.get_tag('rails.cache.key'), 'custom-key')
+      span = spans[-2]
+      assert_equal(span.name, 'redis.command')
+      assert_equal(span.span_type, 'redis')
+      assert_equal(span.resource, 'get custom-key')
+      assert_equal(span.service, 'redis')
+      # the following ensures span will be correctly displayed (parent/child of the same trace)
+      assert_equal(spans[-1].trace_id, spans[-2].trace_id)
+      assert_equal(spans[-1].span_id, spans[-2].parent_id)
+    end
   end
 
   test 'cache.write() is properly traced' do
