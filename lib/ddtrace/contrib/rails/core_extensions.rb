b2af1e36dba623a73e30020015af4201a37376d7
Christian Mauduit (DataDog) <christian.mauduit@datadoghq.com>
2017-03-07 10:30:50 +0100

    [rails] rewrote monkey patching to be Ruby 1.9 compatible

--- lib/ddtrace/contrib/rails/core_extensions.rb
+++ lib/ddtrace/contrib/rails/core_extensions.rb
@@ -1,75 +1,24 @@
 module Datadog
-  # TODO[manu]: write docs
-  module RendererExtension
-    def render_template(*args)
-      ActiveSupport::Notifications.instrument('start_render_template.action_view')
-      super(*args)
-    end
-  end
-
-  # TODO[manu]: write docs
-  module PartialRendererExtension
-    def render_partial(*args)
-      ActiveSupport::Notifications.instrument('start_render_partial.action_view')
-      super(*args)
-    end
-  end
-
-  # CacheStoreReadExtension contains a new read function that notifies
-  # the framework of a read, then calls read.
-  module CacheStoreReadExtension
-    def read(*args)
-      ActiveSupport::Notifications.instrument('start_cache_read.active_support')
-      super(*args)
-    end
-  end
-
-  # CacheStoreFetchExtension contains a new fetch function that notifies
-  # the framework of a fetch, then calls fetch.
-  module CacheStoreFetchExtension
-    # It might seem redundant to instrument both read and fetch since
-    # fetch very often calls read. But there's no garantee of this, in
-    # some cases fetch can call directly read_entry without calling read.
-    def fetch(*args)
-      ActiveSupport::Notifications.instrument('start_cache_fetch.active_support')
-      super(*args)
-    end
-  end
-
-  # CacheWriteReadExtension contains a new read function that notifies
-  # the framework of a write, then calls write.
-  module CacheStoreWriteExtension
-    def write(*args)
-      ActiveSupport::Notifications.instrument('start_cache_write.active_support')
-      super(*args)
-    end
-  end
-
-  # CacheDeleteExtension contains a new read function that notifies
-  # the framework of a delete, then calls delete.
-  module CacheStoreDeleteExtension
-    def delete(*args)
-      ActiveSupport::Notifications.instrument('start_cache_delete.active_support')
-      super(*args)
-    end
-  end
-
-  # CacheInstrumentExtension prepends the instrument function that Rails 3.x uses
-  # to know if the underlying cache should be instrumented or not. By default,
-  # we force that instrumentation if the Rails application is auto instrumented.
-  module CacheInstrumentExtension
-    def instrument
-      true
-    end
-  end
-
   # RailsPatcher contains function to patch the Rails libraries.
   module RailsPatcher
     module_function
 
     def patch_renderer
-      ::ActionView::Renderer.prepend Datadog::RendererExtension
-      ::ActionView::PartialRenderer.prepend Datadog::PartialRendererExtension
+      ::ActionView::Renderer.class_eval do
+        alias_method :render_template_without_datadog, :render_template
+        def render_template(*args)
+          ActiveSupport::Notifications.instrument('start_render_template.action_view')
+          render_template_without_datadog(*args)
+        end
+      end
+
+      ::ActionView::PartialRenderer.class_eval do
+        alias_method :render_partial_without_datadog, :render_partial
+        def render_partial(*args)
+          ActiveSupport::Notifications.instrument('start_render_partial.action_view')
+          render_partial_without_datadog(*args)
+        end
+      end
     end
 
     def patch_cache_store
@@ -80,18 +29,25 @@ module Datadog
       # redefines write but leaves untouched read and delete:
       # https://github.com/redis-store/redis-activesupport/blob/master/lib/active_support/cache/redis_store.rb
 
-      { read: Datadog::CacheStoreReadExtension,
-        fetch: Datadog::CacheStoreFetchExtension,
-        write: Datadog::CacheStoreWriteExtension,
-        delete: Datadog::CacheStoreDeleteExtension }.each do |k, v|
+      { read: 'start_cache_read.active_support',
+        fetch: 'start_cache_fetch.active_support',
+        write: 'start_cache_write.active_support',
+        delete: 'start_cache_delete.active_support' }.each do |k, v|
         c = if defined?(::ActiveSupport::Cache::RedisStore) &&
                ::ActiveSupport::Cache::RedisStore.instance_methods(false).include?(k)
               ::ActiveSupport::Cache::RedisStore
             else
               ::ActiveSupport::Cache::Store
             end
-        Datadog::Tracer.log.debug("monkey patching #{c}.#{k} with #{v}.#{k}")
-        c.prepend v
+        Datadog::Tracer.log.debug("monkey patching #{c}.#{k} triggering #{v}")
+        c.class_eval do
+          k_without_datadog = "#{k}_without_datadog".to_sym
+          alias_method k_without_datadog, k
+          define_method k do |*args|
+            ActiveSupport::Notifications.instrument(v)
+            send(k_without_datadog, *args)
+          end
+        end
       end
 
       # by default, Rails 3 doesn't instrument the cache system so we should turn it on
@@ -100,7 +56,14 @@ module Datadog
       # application servers like Passenger.
       # More details: https://github.com/rails/rails/blob/v3.2.22.5/activesupport/lib/active_support/cache.rb#L175-L177
       return unless ::Rails::VERSION::MAJOR.to_i == 3
-      ::ActiveSupport::Cache::Store.singleton_class.prepend Datadog::CacheInstrumentExtension
+      ::ActiveSupport::Cache::Store.singleton_class.class_eval do
+        # Add the instrument function that Rails 3.x uses
+        # to know if the underlying cache should be instrumented or not. By default,
+        # we force that instrumentation if the Rails application is auto instrumented.
+        def instrument
+          true
+        end
+      end
     end
   end
 end
