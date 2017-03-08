27155e0e31ae7dbdf33c0c056dfbc785b8b63a22
Danilo Cabello <danilo.cabello@gmail.com>
2017-03-08 12:35:59 -0500

    Implement Active Record support for Sinatra apps

---
+++ lib/ddtrace/contrib/active_record/patcher.rb
@@ -0,0 +1,79 @@
+module Datadog
+  module Contrib
+    module ActiveRecord
+      module Patcher
+        @patched = false
+
+        module_function
+
+        # patched? tells whether patch has been successfully applied
+        def patched?
+          @patched
+        end
+
+        def patch
+          if !@patched && defined?(::ActiveRecord)
+            begin
+              require 'ddtrace/contrib/rails/utils'
+              require 'ddtrace/ext/sql'
+
+              patch_active_record()
+
+              @patched = true
+            rescue
+              Datadog::Tracer.log.error("Unable to apply Active Record integration: #{e}")
+            end
+          end
+
+          @patched
+        end
+
+        def patch_active_record
+          # subscribe when the active record query has been processed
+          ::ActiveSupport::Notifications.subscribe('sql.active_record') do |*args|
+            sql(*args)
+          end
+        end
+
+        def self.datadog_trace
+          # TODO: Consider using patcher for Rails as well.
+          # @tracer ||= defined?(::Rails) && ::Rails.configuration.datadog_trace
+          @datadog_trace ||= defined?(::Sinatra) && ::Sinatra::Application.settings.datadog_tracer.cfg
+        end
+
+        def self.adapter_name
+          @adapter_name ||= Datadog::Contrib::Rails::Utils.normalize_vendor(::ActiveRecord::Base.connection_config[:adapter])
+        end
+
+        def self.tracer
+          @tracer ||= datadog_trace.fetch(:tracer)
+        end
+
+        def self.database_service
+          @database_service ||= datadog_trace.fetch(:default_database_service, adapter_name)
+        end
+
+        def self.sql(_name, start, finish, _id, payload)
+          span_type = Datadog::Ext::SQL::TYPE
+
+          span = tracer.trace(
+            "#{adapter_name}.query",
+            resource: payload.fetch(:sql),
+            service: database_service,
+            span_type: span_type
+          )
+
+          # the span should have the query ONLY in the Resource attribute,
+          # so that the ``sql.query`` tag will be set in the agent with an
+          # obfuscated version
+          span.span_type = Datadog::Ext::SQL::TYPE
+          span.set_tag('active_record.db.vendor', adapter_name)
+          span.start_time = start
+          span.finish_at(finish)
+        rescue StandardError => e
+          Datadog::Tracer.log.error(e.message)
+        end
+      end
+    end
+  end
+end
--- lib/ddtrace/monkey.rb
+++ lib/ddtrace/monkey.rb
@@ -3,15 +3,17 @@ require 'thread'
 # We import all patchers for every module we support, but this is fine
 # because patchers do not include any 3rd party module nor even our
 # patching code, which is required on demand, when patching.
+require 'ddtrace/contrib/active_record/patcher'
 require 'ddtrace/contrib/elasticsearch/patcher'
 require 'ddtrace/contrib/http/patcher'
 require 'ddtrace/contrib/redis/patcher'
+
 
 module Datadog
   # Monkey is used for monkey-patching 3rd party libs.
   module Monkey
     @patched = []
-    @autopatch_modules = { elasticsearch: true, http: true, redis: true }
+    @autopatch_modules = { elasticsearch: true, http: true, redis: true, active_record: false }
     # Patchers should expose 2 methods:
     # - patch, which applies our patch if needed. Should be idempotent,
     #   can be call twice but should just do nothing the second time.
@@ -19,7 +21,8 @@ module Datadog
     #   patched (patching might have failed if requirements were not here)
     @patchers = { elasticsearch: Datadog::Contrib::Elasticsearch::Patcher,
                   http: Datadog::Contrib::HTTP::Patcher,
-                  redis: Datadog::Contrib::Redis::Patcher }
+                  redis: Datadog::Contrib::Redis::Patcher,
+                  active_record: Datadog::Contrib::ActiveRecord::Patcher }
     @mutex = Mutex.new
 
     module_function
