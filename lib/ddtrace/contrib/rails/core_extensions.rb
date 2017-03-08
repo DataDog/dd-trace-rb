c8092eca1bb0cb75c1c229d366d9ac9937d4e017
Christian Mauduit (DataDog) <christian.mauduit@datadoghq.com>
2017-03-08 11:24:58 +0100

    [rails] monkey patch using direct class_evel and less meta-programming

--- lib/ddtrace/contrib/rails/core_extensions.rb
+++ lib/ddtrace/contrib/rails/core_extensions.rb
@@ -4,6 +4,19 @@ module Datadog
     module_function
 
     def patch_renderer
+      patch_renderer_render_template
+      patch_renderer_render_partial
+    end
+
+    def patch_cache_store
+      patch_cache_store_read
+      patch_cache_store_fetch
+      patch_cache_store_write
+      patch_cache_store_delete
+      patch_cache_store_instrument
+    end
+
+    def patch_renderer_render_template
       ::ActionView::Renderer.class_eval do
         alias_method :render_template_without_datadog, :render_template
         def render_template(*args)
@@ -11,7 +24,9 @@ module Datadog
           render_template_without_datadog(*args)
         end
       end
+    end
 
+    def patch_renderer_render_partial
       ::ActionView::PartialRenderer.class_eval do
         alias_method :render_partial_without_datadog, :render_partial
         def render_partial(*args)
@@ -21,35 +36,63 @@ module Datadog
       end
     end
 
-    def patch_cache_store
+    def cache_store_class(k)
       # When Redis is used, we can't only patch Cache::Store as it is
       # Cache::RedisStore, a sub-class of it that is used, in practice.
       # We need to do a per-method monkey patching as some of them might
       # be redefined, and some of them not. The latest version of redis-activesupport
       # redefines write but leaves untouched read and delete:
       # https://github.com/redis-store/redis-activesupport/blob/master/lib/active_support/cache/redis_store.rb
-
-      { read: 'start_cache_read.active_support',
-        fetch: 'start_cache_fetch.active_support',
-        write: 'start_cache_write.active_support',
-        delete: 'start_cache_delete.active_support' }.each do |k, v|
-        c = if defined?(::ActiveSupport::Cache::RedisStore) &&
-               ::ActiveSupport::Cache::RedisStore.instance_methods(false).include?(k)
-              ::ActiveSupport::Cache::RedisStore
-            else
-              ::ActiveSupport::Cache::Store
-            end
-        Datadog::Tracer.log.debug("monkey patching #{c}.#{k} triggering #{v}")
-        c.class_eval do
-          k_without_datadog = "#{k}_without_datadog".to_sym
-          alias_method k_without_datadog, k
-          define_method k do |*args|
-            ActiveSupport::Notifications.instrument(v)
-            send(k_without_datadog, *args)
+      c = if defined?(::ActiveSupport::Cache::RedisStore) &&
+             ::ActiveSupport::Cache::RedisStore.instance_methods(false).include?(k)
+            ::ActiveSupport::Cache::RedisStore
+          else
+            ::ActiveSupport::Cache::Store
           end
+      c
+    end
+
+    def patch_cache_store_read
+      cache_store_class(:read).class_eval do
+        alias_method :read_without_datadog, :read
+        def read(*args)
+          ActiveSupport::Notifications.instrument('start_cache_read.active_support')
+          read_without_datadog(*args)
         end
       end
+    end
 
+    def patch_cache_store_fetch
+      cache_store_class(:fetch).class_eval do
+        alias_method :fetch_without_datadog, :fetch
+        def fetch(*args)
+          ActiveSupport::Notifications.instrument('start_cache_fetch.active_support')
+          fetch_without_datadog(*args)
+        end
+      end
+    end
+
+    def patch_cache_store_write
+      cache_store_class(:write).class_eval do
+        alias_method :write_without_datadog, :write
+        def write(*args)
+          ActiveSupport::Notifications.instrument('start_cache_write.active_support')
+          write_without_datadog(*args)
+        end
+      end
+    end
+
+    def patch_cache_store_delete
+      cache_store_class(:delete).class_eval do
+        alias_method :delete_without_datadog, :delete
+        def delete(*args)
+          ActiveSupport::Notifications.instrument('start_cache_delete.active_support')
+          delete_without_datadog(*args)
+        end
+      end
+    end
+
+    def patch_cache_store_instrument
       # by default, Rails 3 doesn't instrument the cache system so we should turn it on
       # using the ActiveSupport::Cache::Store.instrument= function. Unfortunately, early
       # versions of Rails use a Thread.current store that is not compatible with some
