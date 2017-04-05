78d7da1840d473786a42b81c2629a6c2ef0cb1e2
Nicolas Martyanoff <khaelin@gmail.com>
2017-04-05 14:56:30 +0200

    configure sinatra tests during setup
    The sinatra tracer configuration is part of the sinatra module. When
    configured in the sinatra app, the configuration is applied during the
    creation of the app, not when the test starts. This can cause
    interferences between multiple tests.

--- test/contrib/sinatra/tracer_disabled_test.rb
+++ test/contrib/sinatra/tracer_disabled_test.rb
@@ -3,16 +3,6 @@ require 'contrib/sinatra/tracer_test_base'
 
 class DisabledTracerTest < ::TracerTestBase
   class DisabledTracerTestApp < Sinatra::Application
-    configure do
-      writer = FauxWriter.new()
-      tracer = Datadog::Tracer.new(writer: writer)
-
-      settings.datadog_tracer.configure(tracer: tracer, enabled: false)
-
-      set :datadog_test_writer, writer
-      set :datadog_test_tracer, tracer
-    end
-
     get '/request' do
       'hello world'
     end
@@ -23,8 +13,12 @@ class DisabledTracerTest < ::TracerTestBase
   end
 
   def setup
-    @writer = app().settings.datadog_test_writer
-    @writer.spans() # clear trace buffer
+    @writer = FauxWriter.new()
+    app().set :datadog_test_writer, @writer
+
+    tracer = Datadog::Tracer.new(writer: @writer)
+    app().settings.datadog_tracer.configure(tracer: tracer, enabled: false)
+
     super
   end
 
