54fff5b750605ad515549eef8e88b1cce986ac45
Christian Mauduit (DataDog) <christian.mauduit@datadoghq.com>
2017-08-08 10:49:49 +0200

    [tracer:stacktrace] refined test

--- test/contrib/rails/rack_middleware_test.rb
+++ test/contrib/rails/rack_middleware_test.rb
@@ -98,6 +98,7 @@ class FullStackTest < ActionDispatch::IntegrationTest
     assert_equal(request_span.status, 1, 'span should be flagged as an error')
     assert_not_nil(request_span.get_tag('error.stack')) # error stack is in rack span
     assert_match(/controllers\.rb.*error/, request_span.get_tag('error.stack'))
+    assert_match(/\n/, request_span.get_tag('error.stack'))
   end
 
   test 'the rack.request span has the Rails exception, soft error version' do
@@ -149,11 +150,14 @@ class FullStackTest < ActionDispatch::IntegrationTest
     assert_equal(request_span.get_tag('http.method'), 'GET')
     assert_equal(request_span.get_tag('http.status_code'), '500')
     assert_equal(request_span.status, 1, 'span should be flagged as an error')
+    assert_equal(controller_span.get_tag('error.type'), 'ZeroDivisionError')
+    assert_equal(controller_span.get_tag('error.msg'), 'divided by 0')
     assert_not_nil(request_span.get_tag('error.stack')) # error stack is in rack span
     assert_match(/controllers\.rb.*error/, request_span.get_tag('error.stack'))
     assert_match(/controllers\.rb.*another_nested_error_call/, request_span.get_tag('error.stack'))
     assert_match(/controllers\.rb.*a_nested_error_call/, request_span.get_tag('error.stack'))
     assert_match(/controllers\.rb.*sub_error/, request_span.get_tag('error.stack'))
+    assert_match(/\n/, request_span.get_tag('error.stack'))
   end
 
   test 'the status code is properly set if Rails controller is bypassed' do
