495c8080991050548177cee0fb8346d83d0a4f37
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2017-08-11 10:59:42 +0200

    [mongodb] add simple query normalizer to strip out values with high cardinality

--- lib/ddtrace/contrib/mongodb/subscribers.rb
+++ lib/ddtrace/contrib/mongodb/subscribers.rb
@@ -11,17 +11,45 @@ module Datadog
           @active_spans = {}
         end
 
+        # TODO: move in the parser.rb
+        # return a Command from the given MongoDB query
+        def normalize_query(query)
+          # remove the _id value
+          query.each do |command|
+            command.delete(:_id)
+            command.each do |key, _|
+              command[key] = '?'
+            end
+          end
+          query
+        end
+
         def started(event)
           pin = Datadog::Pin.get_from(event.address)
           return unless pin || !pin.enabled?
 
+          # TODO: make all safe
           # start a trace and store it in the active span manager; using the `operation_id`
           # is safe since it's a unique id used to link events together. Reference:
           # https://github.com/mongodb/mongo-ruby-driver/blob/master/lib/mongo/monitoring.rb#L70
+          command_name = event.command_name
+          collection = event.command[command_name]
           span = pin.tracer.trace('mongo.cmd', service: pin.service, span_type: 'mongodb')
           span.set_tag('mongodb.db', event.database_name)
+          span.set_tag('mongodb.ordered', event.command['ordered'])
+          span.set_tag('mongodb.collection', collection)
           span.set_tag('out.host', event.address.host)
           span.set_tag('out.port', event.address.port)
+
+          # get and normalize documents list; some commands don't have documents
+          documents = event.command['documents']
+          span.set_tag('mongodb.documents', normalize_query(documents)) unless documents.nil?
+          documents ||= ''
+
+          # set the resource
+          span.resource = "#{command_name} #{collection} #{documents}"
+
+          # TODO: missing rows number
           @active_spans[event.operation_id] = span
         end
 
---
+++ test/contrib/mongodb/client_test.rb
@@ -0,0 +1,57 @@
+require 'byebug'
+require 'contrib/mongodb/test_helper'
+require 'helper'
+
+class MongoDBTest < Minitest::Test
+  MONGO_HOST = '127.0.0.1'.freeze
+  MONGO_PORT = 57017
+  MONGO_DB = 'test'.freeze
+
+  def setup
+    # initialize the client and overwrite the default tracer
+    @client = Mongo::Client.new(["#{MONGO_HOST}:#{MONGO_PORT}"], :database => MONGO_DB)
+    @tracer = get_test_tracer()
+    pin = Datadog::Pin.get_from(@client)
+    pin.tracer = @tracer
+  end
+
+  def teardown
+    # clear intermediate data
+    @client.database.drop
+  end
+
+  def test_pin_attributes
+    # the client must have a PIN set
+    pin = Datadog::Pin.get_from(@client)
+    assert_equal('mongodb', pin.service)
+    assert_equal('mongodb', pin.app)
+    assert_equal('db', pin.app_type)
+  end
+
+  def test_pin_service_change
+    pin = Datadog::Pin.get_from(@client)
+    pin.service = 'mongodb-primary'
+    @client[:artists].insert_one({ :name => 'FKA Twigs' })
+    spans = @tracer.writer.spans()
+    assert_equal(1, spans.length)
+    span = spans[0]
+    # check fields
+    assert_equal('mongodb-primary', span.service)
+  end
+
+  def test_insert_operation
+    @client[:artists].insert_one({ :name => 'FKA Twigs' })
+    spans = @tracer.writer.spans()
+    assert_equal(1, spans.length)
+    span = spans[0]
+    # check fields
+    assert_equal('insert artists [{:name=>"?"}]', span.resource)
+    assert_equal('mongodb', span.service)
+    assert_equal('mongodb', span.span_type)
+    assert_equal('test', span.get_tag('mongodb.db'))
+    assert_equal('artists', span.get_tag('mongodb.collection'))
+    assert_equal('[{:name=>"?"}]', span.get_tag('mongodb.documents'))
+    assert_equal('127.0.0.1', span.get_tag('out.host'))
+    assert_equal('57017', span.get_tag('out.port'))
+  end
+end
