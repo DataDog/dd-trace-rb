4ec71d93fb72e5db665cba1fb6be231dd6a87e42
Pedro Lambert <pedrolambert@gmail.com>
2017-09-05 10:59:09 -0400

    Add `faraday` integration

---
+++ lib/ddtrace/contrib/faraday/middleware.rb
@@ -0,0 +1,75 @@
+require 'faraday'
+require 'ddtrace/ext/http'
+require 'ddtrace/ext/net'
+require 'ddtrace/ext/distributed'
+
+module Datadog
+  module Contrib
+    module Faraday
+      # Middleware implements a faraday-middleware for ddtrace instrumentation
+      class Middleware < ::Faraday::Middleware
+        DEFAULT_ERROR_HANDLER = lambda do |env|
+          Ext::HTTP::ERROR_RANGE.cover?(env[:status])
+        end
+
+        DEFAULT_OPTIONS = {
+          distributed_tracing: false,
+          split_by_domain: false,
+          error_handler: DEFAULT_ERROR_HANDLER
+        }.freeze
+
+        def initialize(app, options = {})
+          super(app)
+          @options = DEFAULT_OPTIONS.merge(options)
+        end
+
+        def call(env)
+          dd_pin.tracer.trace(SERVICE) do |span|
+            annotate!(span, env)
+            propagate!(span, env) if options[:distributed_tracing]
+            app.call(env).on_complete { |resp| handle_response(span, resp) }
+          end
+        end
+
+        private
+
+        attr_reader :app, :options
+
+        def annotate!(span, env)
+          span.resource = env[:method].to_s.upcase
+          span.service = service_name(env)
+          span.span_type = Ext::HTTP::TYPE
+          span.set_tag(Ext::HTTP::URL, env[:url].path)
+          span.set_tag(Ext::HTTP::METHOD, env[:method].to_s.upcase)
+          span.set_tag(Ext::NET::TARGET_HOST, env[:url].host)
+          span.set_tag(Ext::NET::TARGET_PORT, env[:url].port)
+        end
+
+        def handle_response(span, env)
+          if options.fetch(:error_handler).call(env)
+            span.set_error(["Error #{env[:status]}", env[:body]])
+          end
+
+          span.set_tag(Ext::HTTP::STATUS_CODE, env[:status])
+        end
+
+        def propagate!(span, env)
+          env[:request_headers].merge!(
+            Ext::DistributedTracing::HTTP_HEADER_TRACE_ID => span.trace_id,
+            Ext::DistributedTracing::HTTP_HEADER_PARENT_ID => span.parent_id
+          )
+        end
+
+        def dd_pin
+          Pin.get_from(::Faraday)
+        end
+
+        def service_name(env)
+          return env[:url].host if options[:split_by_domain]
+
+          dd_pin.service
+        end
+      end
+    end
+  end
+end
--- test/monkey_test.rb
+++ test/monkey_test.rb
@@ -4,11 +4,12 @@ require 'ddtrace'
 require 'active_record'
 require 'elasticsearch/transport'
 require 'redis'
+require 'faraday'
 
 class MonkeyTest < Minitest::Test
   def test_autopatch_modules
     assert_equal(
-      { elasticsearch: true, http: true, redis: true, grape: true, active_record: false },
+      { elasticsearch: true, http: true, redis: true, grape: true, faraday: true, active_record: false },
       Datadog::Monkey.autopatch_modules
     )
   end
@@ -23,16 +24,18 @@ class MonkeyTest < Minitest::Test
     assert_equal(false, Datadog::Contrib::HTTP::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::Redis::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::Grape::Patcher.patched?)
+    refute(Datadog::Contrib::Faraday::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::ActiveRecord::Patcher.patched?)
-    assert_equal({ elasticsearch: false, http: false, redis: false, grape: false, active_record: false }, Datadog::Monkey.get_patched_modules())
+    assert_equal({ elasticsearch: false, http: false, redis: false, grape: false, faraday: false, active_record: false }, Datadog::Monkey.get_patched_modules())
 
     Datadog::Monkey.patch_module(:redis)
     assert_equal(false, Datadog::Contrib::Elasticsearch::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::HTTP::Patcher.patched?)
     assert_equal(true, Datadog::Contrib::Redis::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::Grape::Patcher.patched?)
+    refute(Datadog::Contrib::Faraday::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::ActiveRecord::Patcher.patched?)
-    assert_equal({ elasticsearch: false, http: false, redis: true, grape: false, active_record: false }, Datadog::Monkey.get_patched_modules())
+    assert_equal({ elasticsearch: false, http: false, redis: true, grape: false, faraday: false, active_record: false }, Datadog::Monkey.get_patched_modules())
 
     # now do it again to check it's idempotent
     Datadog::Monkey.patch_module(:redis)
@@ -40,16 +43,18 @@ class MonkeyTest < Minitest::Test
     assert_equal(false, Datadog::Contrib::HTTP::Patcher.patched?)
     assert_equal(true, Datadog::Contrib::Redis::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::Grape::Patcher.patched?)
+    refute(Datadog::Contrib::Faraday::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::ActiveRecord::Patcher.patched?)
-    assert_equal({ elasticsearch: false, http: false, redis: true, grape: false, active_record: false }, Datadog::Monkey.get_patched_modules())
+    assert_equal({ elasticsearch: false, http: false, redis: true, grape: false, faraday: false, active_record: false }, Datadog::Monkey.get_patched_modules())
 
     Datadog::Monkey.patch(elasticsearch: true, redis: true)
     assert_equal(true, Datadog::Contrib::Elasticsearch::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::HTTP::Patcher.patched?)
     assert_equal(true, Datadog::Contrib::Redis::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::Grape::Patcher.patched?)
+    refute(Datadog::Contrib::Faraday::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::ActiveRecord::Patcher.patched?)
-    assert_equal({ elasticsearch: true, http: false, redis: true, grape: false, active_record: false }, Datadog::Monkey.get_patched_modules())
+    assert_equal({ elasticsearch: true, http: false, redis: true, grape: false, faraday: false, active_record: false }, Datadog::Monkey.get_patched_modules())
 
     # verify that active_record is not auto patched by default
     Datadog::Monkey.patch_all()
@@ -57,15 +62,17 @@ class MonkeyTest < Minitest::Test
     assert_equal(true, Datadog::Contrib::HTTP::Patcher.patched?)
     assert_equal(true, Datadog::Contrib::Redis::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::Grape::Patcher.patched?)
+    assert(Datadog::Contrib::Faraday::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::ActiveRecord::Patcher.patched?)
-    assert_equal({ elasticsearch: true, http: true, redis: true, grape: false, active_record: false }, Datadog::Monkey.get_patched_modules())
+    assert_equal({ elasticsearch: true, http: true, redis: true, grape: false, faraday: true, active_record: false }, Datadog::Monkey.get_patched_modules())
 
     Datadog::Monkey.patch_module(:active_record)
     assert_equal(true, Datadog::Contrib::Elasticsearch::Patcher.patched?)
     assert_equal(true, Datadog::Contrib::HTTP::Patcher.patched?)
     assert_equal(true, Datadog::Contrib::Redis::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::Grape::Patcher.patched?)
+    assert(Datadog::Contrib::Faraday::Patcher.patched?)
     assert_equal(true, Datadog::Contrib::ActiveRecord::Patcher.patched?)
-    assert_equal({ elasticsearch: true, http: true, redis: true, grape: false, active_record: true }, Datadog::Monkey.get_patched_modules())
+    assert_equal({ elasticsearch: true, http: true, redis: true, grape: false, faraday: true, active_record: true }, Datadog::Monkey.get_patched_modules())
   end
 end
