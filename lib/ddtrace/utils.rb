6d0803118be11f14326399ed2f7d1b0f1b249a87
Pedro Lambert <pedrolambert@gmail.com>
2017-09-18 16:24:48 -0400

    Fix ID collision for forked processes

--- lib/ddtrace/utils.rb
+++ lib/ddtrace/utils.rb
@@ -1,16 +1,28 @@
-require 'thread'
-
 module Datadog
   # Utils contains low-level utilities, typically to provide pseudo-random trace IDs.
   module Utils
     # We use a custom random number generator because we want no interference
     # with the default one. Using the default prng, we could break code that
     # would rely on srand/rand sequences.
-    @rnd = Random.new
 
     # Return a span id
     def self.next_id
+      reset! if was_forked?
+
       @rnd.rand(Datadog::Span::MAX_ID)
     end
+
+    def self.reset!
+      @pid = Process.pid
+      @rnd = Random.new
+    end
+
+    def self.was_forked?
+      Process.pid != @pid
+    end
+
+    private_class_method :reset!, :was_forked?
+
+    reset!
   end
 end
