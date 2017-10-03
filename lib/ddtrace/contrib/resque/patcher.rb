282cb004879fe248d1d00bdda6c17cfe9d00d11e
Rachel Lo <rachel.lo@datadoghq.com>
2017-10-03 14:36:28 -0400

    [resque] adds patcher - sets pin in patcher;

---
+++ lib/ddtrace/contrib/resque/patcher.rb
@@ -0,0 +1,38 @@
+module Datadog
+  module Contrib
+    module Resque
+      SERVICE = 'resque'.freeze
+
+      module Patcher
+        @patched = false
+
+        class << self
+          def patch
+            return @patched if patched?
+            add_pin
+            @patched = true
+          rescue => e
+            Tracer.log.error("Unable to add Resque pin: #{e}")
+            @patched
+          end
+
+          def patched?
+            @patched
+          end
+
+          private
+          
+          def add_pin
+            Pin.new(SERVICE, app_type: Ext::AppTypes::WORKER).tap do |pin| 
+              pin.onto(::Resque)
+            end
+          end    
+
+          def to_s
+            "Pin(service:#{@service},app:#{@app},app_type:#{@app_type},name:#{@name})"
+          end
+        end
+      end
+    end
+  end
+end
--- lib/ddtrace/contrib/resque/resque_job.rb
+++ lib/ddtrace/contrib/resque/resque_job.rb
@@ -3,26 +3,21 @@ require 'ddtrace/ext/app_types'
 module Datadog
   module Contrib
     # Uses Resque job hooks to create traces
-    module ResqueJob
-      SERVICE = 'resque'.freeze
-
-      def self.extended(base)
-        Datadog::Pin.new(SERVICE, app_type: Ext::AppTypes::WORKER).onto(base)
-      end
-
-      def around_perform(*args)
-        pin = datadog_pin
-        pin.tracer.set_service_info(SERVICE, 'resque', Ext::AppTypes::WORKER)
-        pin.tracer.trace('resque.job', service: SERVICE) do |span|
-          span.resource = name
-          span.span_type = pin.app_type
-          yield
+    module Resque
+      module ResqueJob
+        def around_perform(*args)
+          pin = Pin.get_from(::Resque)
+          pin.tracer.trace('resque.job', service: pin.service) do |span|
+            span.resource = name
+            span.span_type = pin.app_type
+            yield
+          end
         end
-      end
 
-      def after_perform(*args)
-        pin = datadog_pin
-        pin.tracer.shutdown!
+        def after_perform(*args)
+          pin = Pin.get_from(::Resque)
+          pin.tracer.shutdown!
+        end
       end
     end
   end
