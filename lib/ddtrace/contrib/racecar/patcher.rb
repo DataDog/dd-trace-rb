90aafddb72022d47d9ef137eb0d3d4792a4af68c
Pedro Lambert <pedrolambert@gmail.com>
2017-12-08 16:22:58 -0500

    Use `ActiveSupport::Notification` signals as instrumentation hooks

--- lib/ddtrace/contrib/racecar/patcher.rb
+++ lib/ddtrace/contrib/racecar/patcher.rb
@@ -1,31 +1,60 @@
 module Datadog
   module Contrib
     module Racecar
+      # Provides instrumentation for `racecar` through ActiveSupport instrumentation signals
       module Patcher
         include Base
+        NAME = 'racecar.consumer'.freeze
         register_as :racecar
+        option :tracer, default: Datadog.tracer
         option :service_name, default: 'racecar'
 
-        module_function
+        class << self
+          def patch
+            return patched? if patched? || !compatible?
 
-        def patch
-          return patched? if patched? || !defined?(::Racecar)
+            ::ActiveSupport::Notifications.subscribe('start_process_batch.racecar', &method(:start))
+            ::ActiveSupport::Notifications.subscribe('start_process_message.racecar', &method(:start))
+            ::ActiveSupport::Notifications.subscribe('process_batch.racecar', &method(:finish))
+            ::ActiveSupport::Notifications.subscribe('process_message.racecar', &method(:finish))
 
-          require_relative 'tracer'
-
-          ::Racecar.singleton_class.class_eval do
-            alias_method :__instrumenter, :instrumenter
-
-            def instrumenter
-              @instrumenter ||= Datadog::Contrib::Racecar::Tracer.new(__instrumenter)
-            end
+            @patched = true
           end
 
-          @patched = true
-        end
+          def patched?
+            @patched ||= false
+          end
 
-        def patched?
-          @patched ||= false
+          private
+
+          def configuration
+            Datadog.configuration[:racecar]
+          end
+
+          def compatible?
+            defined?(::Racecar) && defined?(::ActiveSupport::Notifications)
+          end
+
+          def start(*_, payload)
+            span = configuration[:tracer].trace(NAME)
+            span.service = configuration[:service_name]
+            span.resource = payload[:consumer_class]
+            span.set_tag('kafka.topic', payload[:topic])
+            span.set_tag('kafka.consumer', payload[:consumer_class])
+            span.set_tag('kafka.partition', payload[:partition])
+            span.set_tag('kafka.offset', payload[:offset]) if payload.key?(:offset)
+            span.set_tag('kafka.first_offset', payload[:first_offset]) if payload.key?(:first_offset)
+
+            payload.merge!(trace_span: span)
+          end
+
+          def finish(*_, payload)
+            return unless payload[:trace_span]
+
+            span = payload[:trace_span]
+            span.set_error(payload[:exception_object]) if payload[:exception_object]
+            span.finish
+          end
         end
       end
     end
