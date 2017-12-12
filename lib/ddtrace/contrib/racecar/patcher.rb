57d4a20c5f02bd3ff575cffc849f631effa6ecce
Pedro Lambert <pedrolambert@gmail.com>
2017-12-12 18:18:49 -0500

    Add safe guards to the `racecar` instrumentation

--- lib/ddtrace/contrib/racecar/patcher.rb
+++ lib/ddtrace/contrib/racecar/patcher.rb
@@ -1,3 +1,5 @@
+require 'ddtrace/ext/app_types'
+
 module Datadog
   module Contrib
     module Racecar
@@ -18,11 +20,18 @@ module Datadog
             ::ActiveSupport::Notifications.subscribe('process_batch.racecar', &method(:finish))
             ::ActiveSupport::Notifications.subscribe('process_message.racecar', &method(:finish))
 
+            configuration[:tracer].set_service_info(
+              configuration[:service_name],
+              'racecar',
+              Ext::AppTypes::WORKER
+            )
+
             @patched = true
           end
 
           def patched?
-            @patched ||= false
+            return @patched if defined?(@patched)
+            @patched = false
           end
 
           private
@@ -36,6 +45,8 @@ module Datadog
           end
 
           def start(*_, payload)
+            ensure_clean_context!
+
             span = configuration[:tracer].trace(NAME)
             span.service = configuration[:service_name]
             span.resource = payload[:consumer_class]
@@ -44,16 +55,21 @@ module Datadog
             span.set_tag('kafka.partition', payload[:partition])
             span.set_tag('kafka.offset', payload[:offset]) if payload.key?(:offset)
             span.set_tag('kafka.first_offset', payload[:first_offset]) if payload.key?(:first_offset)
-
-            payload.merge!(trace_span: span)
           end
 
           def finish(*_, payload)
-            return unless payload[:trace_span]
+            current_span = configuration[:tracer].call_context.current_span
 
-            span = payload[:trace_span]
-            span.set_error(payload[:exception_object]) if payload[:exception_object]
-            span.finish
+            return unless current_span
+
+            current_span.set_error(payload[:exception_object]) if payload[:exception_object]
+            current_span.finish
+          end
+
+          def ensure_clean_context!
+            return unless configuration[:tracer].call_context.current_span
+
+            configuration[:tracer].provider.context = Context.new
           end
         end
       end
