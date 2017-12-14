f2ed64d8552735f7f28521e6e598ad725daacb79
David Elner <david.elner@datadoghq.com>
2017-12-14 13:36:19 -0500

    Added: RSpec test suite initial setup.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'pry'
+require 'rspec/collection_matchers'
+
+require 'ddtrace'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.disable_monkey_patching!
+  config.warnings = true
+  config.order = :random
+end
