a82e4de283c071033d707fafb68b46652d2a9b84
David Elner <david.elner@datadoghq.com>
2017-12-14 14:06:37 -0500

    Added: ddtrace and DataDog::Registry specs.

---
+++ spec/ddtrace/registry_spec.rb
@@ -0,0 +1,94 @@
+require 'spec_helper'
+
+RSpec.describe Datadog::Registry do
+  describe 'instance' do
+    subject(:registry) { described_class.new }
+
+    describe 'behavior' do
+      it { is_expected.to be_a_kind_of(Enumerable) }
+
+      shared_context 'an existing entry' do
+        before(:each) { registry.add(name, klass, auto_patch) }
+        let(:name) { :foo }
+        let(:klass) { double('class') }
+        let(:auto_patch) { false }
+      end
+
+      describe '#add' do
+        subject(:add) { registry.add(name, klass, auto_patch) }
+
+        let(:name) { :foo }
+        let(:auto_patch) { false }
+        
+        context 'when given an class' do
+          let(:klass) { double('class') }
+
+          context 'and then the registry is accessed by the same name' do
+            subject(:entry) { registry[name] }
+            before(:each) { add }
+            it { is_expected.to be(klass) }
+          end
+        end
+      end
+
+      describe '#map' do
+        subject { registry.map(&:klass) }
+
+        context 'when a class has been registered' do
+          include_context 'an existing entry'
+
+          it { is_expected.to match a_collection_including(klass) }
+        end
+      end
+
+      describe '#to_h' do
+        subject { registry.to_h }
+
+        context 'when no class has been registered' do
+          it { is_expected.to eq({}) }
+        end
+
+        context 'when a class has been registered' do
+          include_context 'an existing entry'
+
+          it do
+            is_expected.to match a_hash_including(
+              name => auto_patch
+            )
+          end
+        end
+      end
+    end
+  end
+
+  describe Datadog::Registry::Entry do
+    describe 'instance' do
+      subject(:entry) { described_class.new(name, klass, auto_patch) }
+
+      let(:name) { :foo }
+      let(:klass) { double('class') }
+      let(:auto_patch) { true }
+
+      describe 'behavior' do
+        describe '#initialize' do
+          describe 'returns an entry with' do
+            describe '#name' do
+              subject { entry.name }
+              it { is_expected.to eq(name) }
+            end
+
+            describe '#klass' do
+              subject { entry.klass }
+              it { is_expected.to be(klass) }
+            end
+
+            describe '#auto_patch' do
+              subject { entry.auto_patch }
+              it { is_expected.to eq(auto_patch) }
+            end
+          end
+        end
+      end
+    end
+  end
+end
