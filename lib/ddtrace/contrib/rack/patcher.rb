f1e0eeabe3c65c66b4fb119859010785c9a4c68c
Pedro Lambert <pedrolambert@gmail.com>
2017-12-20 11:53:02 -0500

    Provide better resource names for `rack`

--- lib/ddtrace/contrib/rack/patcher.rb
+++ lib/ddtrace/contrib/rack/patcher.rb
@@ -1,19 +1,56 @@
 module Datadog
   module Contrib
     module Rack
+      # Provides instrumentation for `rack`
       module Patcher
         include Base
         register_as :rack
-
         option :tracer, default: Datadog.tracer
         option :distributed_tracing, default: false
+        option :middleware_names, default: false
+        option :application
         option :service_name, default: 'rack', depends_on: [:tracer] do |value|
           get_option(:tracer).set_service_info(value, 'rack', Ext::AppTypes::WEB)
           value
         end
 
-        def self.patch
+        module_function
+
+        def patch
+          return true if patched?
+
           require_relative 'middlewares'
+          @patched = true
+
+          return unless get_option(:middleware_names)
+
+          top = get_option(:application) || rails_app
+          retain_middleware_name(top)
+        end
+
+        def patched?
+          @patched ||= false
+        end
+
+        def rails_app
+          return unless Datadog.registry[:rails].compatible?
+          ::Rails.application.app
+        end
+
+        def retain_middleware_name(middleware)
+          return unless middleware && middleware.respond_to?(:call)
+
+          middleware.singleton_class.class_eval do
+            alias_method :__call, :call
+
+            def call(env)
+              env['RESPONSE_MIDDLEWARE'] = self.class.to_s
+              __call(env)
+            end
+          end
+
+          following = middleware.instance_variable_get('@app')
+          retain_middleware_name(following)
         end
       end
     end
