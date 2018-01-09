e232e91a98942e361b2b5a1c142ff92a1d2a0bb8
David Elner <david.elner@datadoghq.com>
2018-01-09 17:13:15 -0500

    Added: Dalli specs.

---
+++ spec/ddtrace/contrib/dalli/quantize_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+require 'dalli'
+require 'ddtrace'
+require 'ddtrace/contrib/dalli/quantize'
+
+RSpec.describe Datadog::Contrib::Dalli::Quantize do
+  describe '#format_command' do
+    subject { described_class.format_command(op, args) }
+
+    context 'output' do
+      let(:op) { :set }
+      let(:args) { [123, 'foo', nil] }
+
+      it { is_expected.to eq('set 123 foo') }
+    end
+
+    context 'truncation' do
+      let(:op) { :set }
+      let(:args) { ['foo', 'A' * 100] }
+
+      it { expect(subject.size).to eq(described_class::MAX_CMD_LENGTH) }
+      it { is_expected.to end_with('...') }
+      it { is_expected.to eq('set foo ' + 'A' * 89 + '...') }
+    end
+
+    context 'different encodings' do
+      let(:op) { :set }
+      let(:args) { ["\xa1".force_encoding('iso-8859-1'), "\xa1\xa1".force_encoding('euc-jp')] }
+
+      it { is_expected.to match(/BLOB \(OMITTED\)/) }
+    end
+  end
+end
