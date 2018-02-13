f099128c50d660a946ad5a62c0ed07e82a7dedb2
David Elner <david.elner@datadoghq.com>
2018-02-13 11:48:18 -0500

    Changed: Rails::ExceptionMiddleware to catch more middleware errors.

--- lib/ddtrace/contrib/rails/railtie.rb
+++ lib/ddtrace/contrib/rails/railtie.rb
@@ -6,7 +6,10 @@ module Datadog
   # Railtie class initializes
   class Railtie < Rails::Railtie
     config.app_middleware.insert_before(0, Datadog::Contrib::Rack::TraceMiddleware)
-    config.app_middleware.use(Datadog::Contrib::Rails::ExceptionMiddleware)
+    # Insert right after Rails exception handling middleware, because if it's before,
+    # it catches and swallows the error. If it's too far after, custom middleware can find itself
+    # between, and raise exceptions that don't end up getting tagged on the request properly (e.g lost stack trace.)
+    config.app_middleware.insert_after(ActionDispatch::ShowExceptions, Datadog::Contrib::Rails::ExceptionMiddleware)
 
     config.after_initialize do
       Datadog::Contrib::Rails::Framework.setup
