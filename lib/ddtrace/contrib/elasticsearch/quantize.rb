a39d0076d76b3c1218076516416b36828592e7df
David Elner <david.elner@datadoghq.com>
2018-03-02 11:27:46 -0500

    Changed: Allow options to be passed to ElasticSearch quantization.

--- lib/ddtrace/contrib/elasticsearch/quantize.rb
+++ lib/ddtrace/contrib/elasticsearch/quantize.rb
@@ -3,9 +3,10 @@ module Datadog
     module Elasticsearch
       # Quantize contains ES-specific resource quantization tools.
       module Quantize
+        PLACEHOLDER = '?'.freeze
         EXCLUDE_KEYS = [].freeze
         SHOW_KEYS = [:_index, :_type, :_id].freeze
-        PLACEHOLDER = '?'.freeze
+        DEFAULT_OPTIONS = { exclude: EXCLUDE_KEYS, show: SHOW_KEYS }.freeze
 
         ID_REGEXP = %r{\/([0-9]+)([\/\?]|$)}
         ID_PLACEHOLDER = '/?\2'.freeze
@@ -21,44 +22,81 @@ module Datadog
           quantized_url.gsub(INDEX_REGEXP, INDEX_PLACEHOLDER)
         end
 
-        def format_body(body, exclude = [], show = [])
+        def format_body(body, options = {})
+          options = merge_options(DEFAULT_OPTIONS, options)
+
           # Determine if bulk query or not, based on content
           statements = body.end_with?("\n") ? body.split("\n") : [body]
 
-          # Attempt to parse each
-          statements.collect do |s|
-            begin
-              JSON.dump(format_statement(JSON.parse(s), EXCLUDE_KEYS, SHOW_KEYS))
-            rescue JSON::ParserError
-              # If it can't parse/dump, don't raise an error.
-              PLACEHOLDER
+          # Parse each statement and quantize them.
+          statements.collect do |string|
+            reserialize_json(string) do |obj|
+              format_statement(obj, options)
             end
           end.join("\n")
         end
 
-        def format_statement(statement, exclude = [], show = [])
+        def format_statement(statement, options = {})
+          return statement if options[:show] == :all
+
           case statement
           when Hash
             statement.each_with_object({}) do |(key, value), quantized|
-              if show == :all || show.include?(key.to_sym)
+              if options[:show].include?(key.to_sym)
                 quantized[key] = value
-              elsif !exclude.include?(key.to_sym)
-                quantized[key] = format_value(value, exclude, show)
+              elsif !options[:exclude].include?(key.to_sym)
+                quantized[key] = format_value(value, options)
               end
             end
           else
-            format_value(statement, exclude, show)
+            format_value(statement, options)
           end
         end
 
-        def format_value(value, exclude = [], show = [])
+        def format_value(value, options = {})
+          return value if options[:show] == :all
+
           case value
           when Hash
-            format_statement(value, exclude, show)
+            format_statement(value, options)
           when Array
-            format_value(value.first, exclude, show)
+            # If any are objects, format them.
+            if value.any? { |v| v.class <= Hash || v.class <= Array }
+              value.collect { |i| format_value(i, options) }
+            # Otherwise short-circuit and return single placeholder
+            else
+              PLACEHOLDER
+            end
           else
-            show == :all ? value : PLACEHOLDER
+            PLACEHOLDER
+          end
+        end
+
+        def merge_options(original, additional)
+          {}.tap do |options|
+            # Show
+            # If either is :all, value becomes :all
+            options[:show] = if original[:show] == :all || additional[:show] == :all
+                               :all
+                             else
+                               (original[:show] || []).dup.concat(additional[:show] || []).uniq
+                             end
+
+            # Exclude
+            options[:exclude] = (original[:exclude] || []).dup.concat(additional[:exclude] || []).uniq
+          end
+        end
+
+        # Parses a JSON object from a string, passes its value
+        # to the block provided, and dumps its result back to JSON.
+        # If JSON parsing fails, it prints fail_value.
+        def reserialize_json(string, fail_value = PLACEHOLDER)
+          return string unless block_given?
+          begin
+            JSON.dump(yield(JSON.parse(string)))
+          rescue JSON::ParserError
+            # If it can't parse/dump, don't raise an error.
+            fail_value
           end
         end
       end
--- test/contrib/elasticsearch/quantize_test.rb
+++ test/contrib/elasticsearch/quantize_test.rb
@@ -22,6 +22,8 @@ class ESQuantizeTest < Minitest::Test
     assert_equal('/my?/thing/?', Datadog::Contrib::Elasticsearch::Quantize.format_url('/my123/thing/456789'))
   end
 
+  # rubocop:disable Metrics/LineLength
+  # rubocop:disable Style/StringLiterals
   def test_body
     # MGet format
     body = "{\"ids\":[\"1\",\"2\",\"3\"]}"
@@ -42,5 +44,29 @@ class ESQuantizeTest < Minitest::Test
     body = "{\"index\":{\"_index\":\"myindex\",\"_type\":\"mytype\",\"_id\":1}}\n{\"title\":\"foo\"}\n{\"index\":{\"_index\":\"myindex\",\"_type\":\"mytype\",\"_id\":2}}\n{\"title\":\"foo\"}\n"
     quantized_body = "{\"index\":{\"_index\":\"myindex\",\"_type\":\"mytype\",\"_id\":1}}\n{\"title\":\"?\"}\n{\"index\":{\"_index\":\"myindex\",\"_type\":\"mytype\",\"_id\":2}}\n{\"title\":\"?\"}"
     assert_equal(quantized_body, Datadog::Contrib::Elasticsearch::Quantize.format_body(body))
+  end
+
+  def test_body_show
+    body = "{\"query\":{\"match\":{\"title\":\"test\",\"subtitle\":\"test\"}}}"
+    quantized_body = "{\"query\":{\"match\":{\"title\":\"test\",\"subtitle\":\"?\"}}}"
+    assert_equal(quantized_body, Datadog::Contrib::Elasticsearch::Quantize.format_body(body, show: [:title]))
+
+    body = "{\"query\":{\"match\":{\"title\":\"test\",\"subtitle\":\"test\"}}}"
+    quantized_body = "{\"query\":{\"match\":{\"title\":\"test\",\"subtitle\":\"test\"}}}"
+    assert_equal(quantized_body, Datadog::Contrib::Elasticsearch::Quantize.format_body(body, show: :all))
+
+    body = "[{\"foo\":\"foo\"},{\"bar\":\"bar\"}]"
+    quantized_body = "[{\"foo\":\"foo\"},{\"bar\":\"bar\"}]"
+    assert_equal(quantized_body, Datadog::Contrib::Elasticsearch::Quantize.format_body(body, show: :all))
+
+    body = "[\"foo\",\"bar\"]"
+    quantized_body = "[\"foo\",\"bar\"]"
+    assert_equal(quantized_body, Datadog::Contrib::Elasticsearch::Quantize.format_body(body, show: :all))
+  end
+
+  def test_body_exclude
+    body = "{\"query\":{\"match\":{\"title\":\"test\",\"subtitle\":\"test\"}}}"
+    quantized_body = "{\"query\":{\"match\":{\"subtitle\":\"?\"}}}"
+    assert_equal(quantized_body, Datadog::Contrib::Elasticsearch::Quantize.format_body(body, exclude: [:title]))
   end
 end
