35a7f2c806f7d7b84e277a0931cc87aad6d929f0
Jared Davis <jared@haiq.us>
2018-04-12 09:16:17 -0500

    gRPC Distributed TracingThis alternate implementation patches the GRPC::Interceptor registry.This leads to cleaner separation between client side and server sidetracing implementation and testing.

---
+++ spec/ddtrace/contrib/grpc/datadog_interceptor/server_spec.rb
@@ -0,0 +1,98 @@
+require 'spec_helper'
+require 'grpc'
+require 'ddtrace'
+
+RSpec.describe 'tracing on the server connection' do
+  subject { Datadog::Contrib::GRPC::DatadogInterceptor::Server.new }
+
+  before do
+    Datadog.configure do |c|
+      c.use :grpc, tracer: get_test_tracer
+    end
+  end
+
+  let(:span) { Datadog::Pin.get_from(::GRPC).tracer.writer.spans.first }
+
+  describe '#request_response' do
+    let(:keywords) do
+      { request: instance_double(Object),
+        call: instance_double('GRPC::ActiveCall', metadata: { some: 'datum' }),
+        method: instance_double(Method, name: 'endpoint') }
+    end
+
+    before do
+      subject.request_response(keywords) { }
+    end
+
+    specify do
+      expect(span.name).to eq 'grpc.service'
+      expect(span.span_type).to eq 'grpc'
+      expect(span.service).to eq 'grpc.service'
+      expect(span.resource).to eq 'server.endpoint'
+      expect(span.get_tag('error.stack')).to be_nil
+      expect(span.get_tag(:some)).to eq 'datum'
+    end
+  end
+
+  describe '#client_streamer' do
+    let(:keywords) do
+      { call: instance_double('GRPC::ActiveCall', metadata: { some: 'datum'}),
+        method: instance_double(Method, name: 'endpoint') }
+    end
+
+    before do
+      subject.client_streamer(keywords) { }
+    end
+
+    specify do
+      expect(span.name).to eq 'grpc.service'
+      expect(span.span_type).to eq 'grpc'
+      expect(span.service).to eq 'grpc.service'
+      expect(span.resource).to eq 'server.endpoint'
+      expect(span.get_tag('error.stack')).to be_nil
+      expect(span.get_tag(:some)).to eq 'datum'
+    end
+  end
+
+  describe '#server_streamer' do
+    let(:keywords) do
+      { request: instance_double(Object),
+        call: instance_double('GRPC::ActiveCall', metadata: { some: 'datum'}),
+        method: instance_double(Method, name: 'endpoint') }
+    end
+
+    before do
+      subject.server_streamer(keywords) {  }
+    end
+
+    specify do
+      expect(span.name).to eq 'grpc.service'
+      expect(span.span_type).to eq 'grpc'
+      expect(span.service).to eq 'grpc.service'
+      expect(span.resource).to eq 'server.endpoint'
+      expect(span.get_tag('error.stack')).to be_nil
+      expect(span.get_tag(:some)).to eq 'datum'
+    end
+  end
+
+  describe '#bidi_streamer' do
+    let(:keywords) do
+      { requests: instance_double(Array),
+        call: instance_double('GRPC::ActiveCall', metadata: { some: 'datum'}),
+        method: instance_double(Method, name: 'endpoint') }
+    end
+
+    before do
+      subject.bidi_streamer(keywords) {  }
+    end
+
+    specify do
+      expect(span.name).to eq 'grpc.service'
+      expect(span.span_type).to eq 'grpc'
+      expect(span.service).to eq 'grpc.service'
+      expect(span.resource).to eq 'server.endpoint'
+      expect(span.get_tag('error.stack')).to be_nil
+      expect(span.get_tag(:some)).to eq 'datum'
+    end
+  end
+end
---
+++ spec/ddtrace/contrib/grpc/interception_context_spec.rb
@@ -0,0 +1,170 @@
+require 'spec_helper'
+require 'grpc'
+require 'ddtrace'
+
+RSpec.describe GRPC::InterceptionContext do
+  subject { described_class.new }
+
+  describe '#intercept!' do
+    let(:span) { Datadog::Pin.get_from(::GRPC).tracer.writer.spans.first }
+
+    before do
+      Datadog.configure do |c|
+        c.use :grpc, tracer: get_test_tracer
+      end
+
+      subject.intercept!(type, keywords) { }
+    end
+
+    context 'when intercepting on the client' do
+      context 'request response call type' do
+        let(:type) { :request_response }
+        let(:keywords) do
+          { request: instance_double(Object),
+            call: instance_double('GRPC::ActiveCall'),
+            method: 'MyService.Endpoint',
+            metadata: { some: 'datum' } }
+        end
+
+        specify do
+          expect(span.name).to eq 'grpc.client'
+          expect(span.span_type).to eq 'grpc'
+          expect(span.service).to eq 'grpc.client'
+          expect(span.resource).to eq 'myservice.endpoint'
+          expect(span.get_tag('error.stack')).to be_nil
+          expect(span.get_tag(:some)).to eq 'datum'
+        end
+      end
+
+      context 'client streaming call type' do
+        let(:type) { :client_streamer }
+        let(:keywords) do
+          { call: instance_double('GRPC::ActiveCall'),
+            method: 'MyService.Endpoint',
+            metadata: { some: 'datum'}}
+        end
+
+        specify do
+          expect(span.name).to eq 'grpc.client'
+          expect(span.span_type).to eq 'grpc'
+          expect(span.service).to eq 'grpc.client'
+          expect(span.resource).to eq 'myservice.endpoint'
+          expect(span.get_tag('error.stack')).to be_nil
+          expect(span.get_tag(:some)).to eq 'datum'
+        end
+      end
+
+      context 'server streaming call type' do
+        let(:type) { :server_streamer }
+        let(:keywords) do
+          { request: instance_double(Object),
+            call: instance_double('GRPC::ActiveCall'),
+            method: 'MyService.Endpoint',
+            metadata: { some: 'datum'}}
+        end
+
+        specify do
+          expect(span.name).to eq 'grpc.client'
+          expect(span.span_type).to eq 'grpc'
+          expect(span.service).to eq 'grpc.client'
+          expect(span.resource).to eq 'myservice.endpoint'
+          expect(span.get_tag('error.stack')).to be_nil
+          expect(span.get_tag(:some)).to eq 'datum'
+        end
+      end
+
+      context 'bidirectional streaming call type' do
+        let(:type) { :bidi_streamer }
+        let(:keywords) do
+          { requests: instance_double(Array),
+            call: instance_double('GRPC::ActiveCall'),
+            method: 'MyService.Endpoint',
+            metadata: { some: 'datum'} }
+        end
+
+        specify do
+          expect(span.name).to eq 'grpc.client'
+          expect(span.span_type).to eq 'grpc'
+          expect(span.service).to eq 'grpc.client'
+          expect(span.resource).to eq 'myservice.endpoint'
+          expect(span.get_tag('error.stack')).to be_nil
+          expect(span.get_tag(:some)).to eq 'datum'
+        end
+      end
+    end
+
+
+    context 'when intercepting on the server' do
+      context 'request response call type' do
+        let(:type) { :request_response }
+        let(:keywords) do
+          { request: instance_double(Object),
+            call: instance_double('GRPC::ActiveCall', metadata: { some: 'datum' }),
+            method: instance_double(Method, name: 'endpoint') }
+        end
+
+        specify do
+          expect(span.name).to eq 'grpc.service'
+          expect(span.span_type).to eq 'grpc'
+          expect(span.service).to eq 'grpc.service'
+          expect(span.resource).to eq 'server.endpoint'
+          expect(span.get_tag('error.stack')).to be_nil
+          expect(span.get_tag(:some)).to eq 'datum'
+        end
+      end
+
+      context 'client streaming call type' do
+        let(:type) { :client_streamer }
+        let(:keywords) do
+          { call: instance_double('GRPC::ActiveCall', metadata: { some: 'datum'}),
+            method: instance_double(Method, name: 'endpoint') }
+        end
+
+        specify do
+          expect(span.name).to eq 'grpc.service'
+          expect(span.span_type).to eq 'grpc'
+          expect(span.service).to eq 'grpc.service'
+          expect(span.resource).to eq 'server.endpoint'
+          expect(span.get_tag('error.stack')).to be_nil
+          expect(span.get_tag(:some)).to eq 'datum'
+        end
+      end
+
+      context 'server streaming call type' do
+        let(:type) { :server_streamer }
+        let(:keywords) do
+          { request: instance_double(Object),
+            call: instance_double('GRPC::ActiveCall', metadata: { some: 'datum'}),
+            method: instance_double(Method, name: 'endpoint') }
+        end
+
+        specify do
+          expect(span.name).to eq 'grpc.service'
+          expect(span.span_type).to eq 'grpc'
+          expect(span.service).to eq 'grpc.service'
+          expect(span.resource).to eq 'server.endpoint'
+          expect(span.get_tag('error.stack')).to be_nil
+          expect(span.get_tag(:some)).to eq 'datum'
+        end
+      end
+
+      context 'bidirectional streaming call type do' do
+        let(:type) { :bidi_streamer }
+        let(:keywords) do
+          { requests: instance_double(Array),
+            call: instance_double('GRPC::ActiveCall', metadata: { some: 'datum'}),
+            method: instance_double(Method, name: 'endpoint') }
+        end
+
+        specify do
+          expect(span.name).to eq 'grpc.service'
+          expect(span.span_type).to eq 'grpc'
+          expect(span.service).to eq 'grpc.service'
+          expect(span.resource).to eq 'server.endpoint'
+          expect(span.get_tag('error.stack')).to be_nil
+          expect(span.get_tag(:some)).to eq 'datum'
+        end
+      end
+    end
+  end
+end
