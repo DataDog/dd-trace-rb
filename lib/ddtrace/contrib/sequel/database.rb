28d62e6919e042724449a7be3fa1c8969f6ee822
David Elner <david.elner@datadoghq.com>
2018-04-18 17:20:23 -0400

    Removed: Sequel patching methods in favor of prepended modules.

--- lib/ddtrace/contrib/sequel/database.rb
+++ lib/ddtrace/contrib/sequel/database.rb
@@ -9,47 +9,7 @@ module Datadog
       module Database
         def self.included(base)
           base.send(:include, Utils)
-
-          # If prepending is supported
-          if base.respond_to?(:prepend)
-            base.send(:prepend, InstanceMethods)
-          else
-            patch(base)
-          end
-        end
-
-        def self.patch(base)
-          Datadog::Patcher.without_warnings do
-            # Database#initialize
-            base.send(:alias_method, :initialize_without_datadog, :initialize)
-            base.send(:remove_method, :initialize)
-            base.send(:define_method, :initialize) do |*args|
-              pin = Datadog::Pin.new(Patcher::SERVICE, app: Patcher::APP, app_type: Datadog::Ext::AppTypes::DB)
-              pin.onto(self)
-              initialize_without_datadog(*args)
-            end
-
-            # Database#run
-            base.send(:alias_method, :run_without_datadog, :run)
-            base.send(:remove_method, :run)
-            base.send(:define_method, :run) do |sql, options = ::Sequel::OPTS|
-              pin = Datadog::Pin.get_from(self)
-              return run_without_datadog(sql, options) unless pin && pin.tracer
-
-              opts = parse_opts(sql, options)
-
-              response = nil
-
-              pin.tracer.trace('sequel.query') do |span|
-                span.service = pin.service
-                span.resource = opts[:query]
-                span.span_type = Datadog::Ext::SQL::TYPE
-                span.set_tag('sequel.db.vendor', adapter_name)
-                response = run_without_datadog(sql, options)
-              end
-              response
-            end
-          end
+          base.send(:prepend, InstanceMethods)
         end
 
         # Instance methods for instrumenting Sequel::Database
