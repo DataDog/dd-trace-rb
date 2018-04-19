b48bd0e0d508b203c7c928e7ab3af77cc8cd13cd
David Elner <david.elner@datadoghq.com>
2018-04-19 15:20:18 -0400

    Changed: Sequel to use adapter name as default service name.

--- lib/ddtrace/contrib/sequel/utils.rb
+++ lib/ddtrace/contrib/sequel/utils.rb
@@ -3,19 +3,13 @@ module Datadog
     module Sequel
       # General purpose functions for Sequel
       module Utils
-        def adapter_name
-          Datadog::Utils::Database.normalize_vendor(db.adapter_scheme.to_s)
+        module_function
+
+        def adapter_name(database)
+          Datadog::Utils::Database.normalize_vendor(database.adapter_scheme.to_s)
         end
 
-        def parse_opts(sql, opts)
-          db_opts = if ::Sequel::VERSION < '3.41.0' && self.class.to_s !~ /Dataset$/
-                      @opts
-                    elsif instance_variable_defined?(:@pool) && @pool
-                      @pool.db.opts
-                    else
-                      @db.opts
-                    end
-
+        def parse_opts(sql, opts, db_opts)
           if ::Sequel::VERSION > '4.36.0' && !sql.is_a?(String)
             # In 4.37.0, sql was converted to a prepared statement object
             sql = sql.prepared_sql unless sql.is_a?(Symbol)
