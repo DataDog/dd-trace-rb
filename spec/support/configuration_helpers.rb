c7d69e1c1473ab8a26185115c87d1170c2397c86
David Elner <david.elner@datadoghq.com>
2018-04-19 17:08:29 -0400

    Added: Resque RSpec examples.

--- spec/support/configuration_helpers.rb
+++ spec/support/configuration_helpers.rb
@@ -1,24 +1,4 @@
 module ConfigurationHelpers
-  # update Datadog user configuration; you should pass:
-  #
-  # * +key+: the key that should be updated
-  # * +value+: the value of the key
-  def update_config(key, value)
-    Datadog.configuration[:rails][key] = value
-    Datadog::Contrib::Rails::Framework.setup
-  end
-
-  # reset default configuration and replace any dummy tracer
-  # with the global one
-  def reset_config
-    Datadog.configure do |c|
-      c.use :rails
-      c.use :redis
-    end
-
-    Datadog::Contrib::Rails::Framework.setup
-  end
-
   def remove_patch!(integration)
     Datadog
       .registry[integration]
