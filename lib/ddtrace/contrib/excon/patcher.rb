906426782c60727fedb849ec161af7042cd2bf06
David Elner <david.elner@datadoghq.com>
2018-04-23 15:12:20 -0400

    Refactored: Excon instrumentation to use new style.

--- lib/ddtrace/contrib/excon/patcher.rb
+++ lib/ddtrace/contrib/excon/patcher.rb
@@ -1,56 +1,48 @@
+require 'ddtrace/ext/app_types'
+
 module Datadog
   module Contrib
     module Excon
-      COMPATIBLE_UNTIL = Gem::Version.new('1.0.0')
-      SERVICE = 'excon-request'.freeze
-
-      # Responsible for hooking the instrumentation into excon
+      # Responsible for hooking the instrumentation into Excon
       module Patcher
         include Base
-        register_as :excon, auto_patch: true
-        option :distributed_tracing_enabled, default: false
+
+        DEFAULT_SERVICE = 'excon'.freeze
+
+        register_as :excon
+        option :tracer, default: Datadog.tracer
+        option :service_name, default: DEFAULT_SERVICE
+        option :distributed_tracing, default: false
         option :split_by_domain, default: false
         option :error_handler, default: nil
 
         @patched = false
 
-        class << self
-          def patch
-            return @patched if patched? || !compatible?
+        module_function
 
-            require 'ddtrace/ext/app_types'
-            require 'ddtrace/contrib/excon/middleware'
+        def patch
+          return @patched if patched? || !compatible?
 
-            add_pin
-            add_middleware
+          require 'ddtrace/contrib/excon/middleware'
 
-            @patched = true
-          rescue => e
-            Tracer.log.error("Unable to apply Excon integration: #{e}")
-            @patched
-          end
+          add_middleware
 
-          def patched?
-            @patched
-          end
+          @patched = true
+        rescue => e
+          Tracer.log.error("Unable to apply Excon integration: #{e}")
+          @patched
+        end
 
-          private
+        def patched?
+          @patched
+        end
 
-          def compatible?
-            return unless defined?(::Excon::VERSION)
+        def compatible?
+          defined?(::Excon)
+        end
 
-            Gem::Version.new(::Excon::VERSION) < COMPATIBLE_UNTIL
-          end
-
-          def add_pin
-            Pin.new(SERVICE, app_type: Ext::AppTypes::WEB).tap do |pin|
-              pin.onto(::Excon)
-            end
-          end
-
-          def add_middleware
-            ::Excon.defaults[:middlewares].append(Middleware)
-          end
+        def add_middleware
+          ::Excon.defaults[:middlewares] = Middleware.around_default_stack
         end
       end
     end
