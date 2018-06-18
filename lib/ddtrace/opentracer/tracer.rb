363a9555e5b16b8085575218744c974a48ec2094
David Elner <david@davidelner.com>
2018-06-18 13:48:38 -0400

    [OpenTracing] Add OpenTracing configuration (#464)
    * Added: Datadog::OpenTracer::Tracer#datadog_tracer.
    
    * Added: Patch for OpenTracing#global_tracer=.

--- lib/ddtrace/opentracer/tracer.rb
+++ lib/ddtrace/opentracer/tracer.rb
@@ -1,6 +1,22 @@
+require 'ddtrace/tracer'
+
 module Datadog
   module OpenTracer
+    # OpenTracing adapter for Datadog::Tracer
     class Tracer < ::OpenTracing::Tracer
+      extend Forwardable
+
+      attr_reader \
+        :datadog_tracer
+
+      def_delegators \
+        :datadog_tracer,
+        :configure
+
+      def initialize(options = {})
+        super()
+        @datadog_tracer = Datadog::Tracer.new(options)
+      end
     end
   end
 end
