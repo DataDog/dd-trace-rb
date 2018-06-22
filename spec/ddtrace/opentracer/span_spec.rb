406812848360ecd16d6f615d42f576a7d0bc05f4
David Elner <david@davidelner.com>
2018-06-22 16:44:19 -0400

    [OpenTracing] Span and SpanContext implementation (#472)
    * Added: Datadog::OpenTracer::SpanContext implementation.
    
    * Added: Datadog::OpenTracer::Span implementation.
    
    * Changed: Bump required Ruby version for OpenTracing to 2.1.
    
    * Added: Datadog::OpenTracer::SpanContextFactory.
    
    * Changed: OpenTracer::Span#set_baggage_item to create new SpanContexts.
    
    * Changed: Datadog::OpenTracer::SpanContext#baggage to be immutable.
    
    * Added: span_id, trace_id, parent_id to Datadog::OpenTracer::SpanContext.
    
    * Changed: Replaced span_id, trace_id, parent_id with reference to Datadog::Context.

--- spec/ddtrace/opentracer/span_spec.rb
+++ spec/ddtrace/opentracer/span_spec.rb
@@ -7,24 +7,28 @@ if Datadog::OpenTracer.supported?
   RSpec.describe Datadog::OpenTracer::Span do
     include_context 'OpenTracing helpers'
 
-    subject(:span) { described_class.new }
+    subject(:span) { described_class.new(datadog_span: datadog_span, span_context: span_context) }
+    let(:datadog_span) { instance_double(Datadog::Span) }
+    let(:span_context) { instance_double(Datadog::OpenTracer::SpanContext) }
 
     describe '#operation_name=' do
       subject(:result) { span.operation_name = name }
       let(:name) { 'execute_job' }
 
+      before(:each) { expect(datadog_span).to receive(:name=).with(name).and_return(name) }
       it { expect(result).to eq(name) }
     end
 
     describe '#context' do
       subject(:context) { span.context }
-      it { is_expected.to be(OpenTracing::SpanContext::NOOP_INSTANCE) }
+      it { is_expected.to be(span_context) }
     end
 
     describe '#set_tag' do
       subject(:result) { span.set_tag(key, value) }
       let(:key) { 'account_id' }
       let(:value) { '1234' }
+      before(:each) { expect(datadog_span).to receive(:set_tag).with(key, value) }
       it { is_expected.to be(span) }
     end
 
@@ -32,13 +36,25 @@ if Datadog::OpenTracer.supported?
       subject(:result) { span.set_baggage_item(key, value) }
       let(:key) { 'account_id' }
       let(:value) { '1234' }
-      it { is_expected.to be(span) }
+      let(:new_span_context) { instance_double(Datadog::OpenTracer::SpanContext) }
+
+      it 'creates a new SpanContext with the baggage item' do
+        expect(Datadog::OpenTracer::SpanContextFactory).to receive(:clone)
+          .with(span_context: span_context, baggage: hash_including(key => value))
+          .and_return(new_span_context)
+
+        is_expected.to be(span)
+        expect(span.context).to be(new_span_context)
+      end
     end
 
     describe '#get_baggage_item' do
       subject(:result) { span.get_baggage_item(key) }
       let(:key) { 'account_id' }
-      it { is_expected.to be nil }
+      let(:value) { '1234' }
+      let(:baggage) { { key => value } }
+      before(:each) { allow(span_context).to receive(:baggage).and_return(baggage) }
+      it { is_expected.to be(value) }
     end
 
     describe '#log' do
@@ -47,23 +63,30 @@ if Datadog::OpenTracer.supported?
       let(:timestamp) { Time.now }
       let(:fields) { { time_started: Time.now, account_id: '1234' } }
 
-      before(:each) do
+      # Expect a deprecation warning to be output.
+      it do
         expect { log }.to output("Span#log is deprecated.  Please use Span#log_kv instead.\n").to_stderr
       end
-
-      it { is_expected.to be nil }
     end
 
     describe '#log_kv' do
       subject(:log_kv) { span.log_kv(timestamp: timestamp, **fields) }
       let(:timestamp) { Time.now }
-      let(:fields) { { time_started: Time.now, account_id: '1234' } }
 
-      before(:each) do
-        expect { log_kv }.to_not output.to_stderr
+      context 'when given arbitrary key/value pairs' do
+        let(:fields) { { time_started: Time.now, account_id: '1234' } }
+        # We don't expect this to do anything right now.
+        it { is_expected.to be nil }
       end
 
-      it { is_expected.to be nil }
+      context 'when given an \'error.object\'' do
+        let(:fields) { { :'error.object' => error_object } }
+        let(:error_object) { instance_double(StandardError) }
+
+        before(:each) { expect(datadog_span).to receive(:set_error).with(error_object) }
+
+        it { is_expected.to be nil }
+      end
     end
   end
 end
