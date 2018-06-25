ea75490e6c4fe5693f649e64bbf640c3fc406b72
David Elner <david.elner@datadoghq.com>
2018-06-25 17:29:24 -0400

    Added: Contrib::Configuration specs.

---
+++ spec/ddtrace/contrib/configuration/option_definition_spec.rb
@@ -0,0 +1,104 @@
+require 'spec_helper'
+
+require 'ddtrace'
+
+RSpec.describe Datadog::Contrib::Configuration::OptionDefinition do
+  subject(:definition) { described_class.new(name, meta, &block) }
+
+  let(:name) { :enabled }
+  let(:meta) { {} }
+  let(:block) { nil }
+
+  describe '#default' do
+    subject(:default) { definition.default }
+
+    context 'when not initialized with a value' do
+      it { is_expected.to be nil }
+    end
+
+    context 'when initialized with a value' do
+      let(:meta) { { default: default_value } }
+      let(:default_value) { double('default') }
+      it { is_expected.to be default_value }
+    end
+  end
+
+  describe '#depends_on' do
+    subject(:default) { definition.depends_on }
+
+    context 'when not initialized with a value' do
+      it { is_expected.to eq([]) }
+    end
+
+    context 'when initialized with a value' do
+      let(:meta) { { depends_on: depends_on_value } }
+      let(:depends_on_value) { double('depends_on') }
+      it { is_expected.to be depends_on_value }
+    end
+  end
+
+  describe '#lazy' do
+    subject(:lazy) { definition.lazy }
+
+    context 'when not initialized with a value' do
+      it { is_expected.to be false }
+    end
+
+    context 'when initialized with a value' do
+      let(:meta) { { lazy: lazy_value } }
+      let(:lazy_value) { double('lazy') }
+      it { is_expected.to be lazy_value }
+    end
+  end
+
+  describe '#name' do
+    subject(:result) { definition.name }
+
+    context 'when given a String' do
+      let(:name) { 'enabled' }
+      it { is_expected.to be name.to_sym }
+    end
+
+    context 'when given a Symbol' do
+      let(:name) { :enabled }
+      it { is_expected.to be name }
+    end
+  end
+
+  describe '#setter' do
+    subject(:setter) { definition.setter }
+
+    context 'when given a value' do
+      let(:meta) { { setter: setter_value } }
+      let(:setter_value) { double('setter') }
+      it { is_expected.to be setter_value }
+    end
+
+    context 'when initialized with a block' do
+      let(:block) { proc {} }
+      it { is_expected.to be block }
+    end
+
+    context 'when not initialized' do
+      it { is_expected.to be described_class::IDENTITY }
+    end
+  end
+
+  describe '#default_value' do
+    subject(:result) { definition.default_value }
+    let(:meta) { { default: default } }
+    let(:default) { double('default') }
+
+    context 'when lazy is true' do
+      let(:meta) { super().merge(lazy: true) }
+      let(:default_value) { double('default_value') }
+      before(:each) { expect(default).to receive(:call).and_return(default_value) }
+      it { is_expected.to be default_value }
+    end
+
+    context 'when lazy is false' do
+      let(:meta) { super().merge(lazy: false) }
+      it { is_expected.to be default }
+    end
+  end
+end
---
+++ spec/ddtrace/contrib/configuration/option_spec.rb
@@ -0,0 +1,68 @@
+require 'spec_helper'
+
+require 'ddtrace'
+
+RSpec.describe Datadog::Contrib::Configuration::Option do
+  subject(:option) { described_class.new(definition, context) }
+  let(:definition) do
+    instance_double(
+      Datadog::Contrib::Configuration::OptionDefinition,
+      default_value: default_value,
+      setter: setter
+    )
+  end
+  let(:default_value) { double('default value') }
+  let(:setter) { proc { setter_value } }
+  let(:setter_value) { double('setter_value') }
+  let(:context) { double('configuration object') }
+
+  describe '#initialize' do
+    it { expect(option.definition).to be(definition) }
+  end
+
+  describe '#set' do
+    subject(:set) { option.set(value) }
+    let(:value) { double('value') }
+
+    before(:each) { expect(context).to receive(:instance_exec).with(value, &setter) }
+
+    it { is_expected.to be(setter_value) }
+  end
+
+  describe '#get' do
+    subject(:get) { option.get }
+
+    context 'when #set' do
+      context 'hasn\'t been called' do
+        it { is_expected.to be(default_value) }
+      end
+
+      context 'has been called' do
+        let(:value) { double('value') }
+
+        before(:each) do
+          allow(context).to receive(:instance_exec).with(value, &setter)
+          option.set(value)
+        end
+
+        it { is_expected.to be(setter_value) }
+      end
+    end
+  end
+
+  describe '#reset' do
+    subject(:reset) { option.reset }
+
+    context 'when a value has been set' do
+      let(:value) { double('value') }
+
+      before(:each) do
+        allow(context).to receive(:instance_exec).with(value, &setter)
+        allow(context).to receive(:instance_exec).with(default_value, &setter).and_return(default_value)
+        option.set(value)
+      end
+
+      it { is_expected.to be(default_value) }
+    end
+  end
+end
