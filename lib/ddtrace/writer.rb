79dc281cc24b9a4d78d932db00bbcf091478148a
Pawel Chojnacki <pawel.chcki@gmail.com>
2018-06-26 13:41:05 +0200

    Avoid using mutex when Pids are the same, reduces lock contention 2-3x

--- lib/ddtrace/writer.rb
+++ lib/ddtrace/writer.rb
@@ -15,7 +15,6 @@ module Datadog
       @buff_size = options.fetch(:buffer_size, 100)
       @flush_interval = options.fetch(:flush_interval, 1)
       transport_options = options.fetch(:transport_options, {})
-
       # priority sampling
       if options[:priority_sampler]
         @priority_sampler = options[:priority_sampler]
@@ -95,10 +94,12 @@ module Datadog
       # This check ensures that if a process doesn't own the current +Writer+, async workers
       # will be initialized again (but only once for each process).
       pid = Process.pid
-      @mutex_after_fork.synchronize do
-        if pid != @pid
-          # we should start threads because the worker doesn't own this
-          start()
+      if pid != @pid
+        @mutex_after_fork.synchronize do
+          if pid != @pid
+            # we should start threads because the worker doesn't own this
+            start()
+          end
         end
       end
 
