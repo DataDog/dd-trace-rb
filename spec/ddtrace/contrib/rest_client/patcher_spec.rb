db0edfebe263b0cc1e2fd9166bba832a8aafbdf6
Pawel Chojnacki <pawel.chcki@gmail.com>
2018-06-28 16:12:58 +0200

    Better test names

--- spec/ddtrace/contrib/rest_client/patcher_spec.rb
+++ spec/ddtrace/contrib/rest_client/patcher_spec.rb
@@ -14,16 +14,16 @@ RSpec.describe Datadog::Contrib::RestClient::Patcher do
         hide_const('RestClient::Request')
       end
 
-      it "shouldn't patch the code" do
+      it 'does not patch the code' do
         expect { described_class.patch }.not_to(change { described_class.patched? })
       end
     end
 
-    it 'should patch the code' do
+    it 'patches the code' do
       expect { described_class.patch }.to change { described_class.patched? }.from(false).to(true)
     end
 
-    it 'should add RequestPatch to ancestors of Request class' do
+    it 'adds RequestPatch to ancestors of Request class' do
       expect { described_class.patch }
         .to change { rest_client_request_class.ancestors }.to include(Datadog::Contrib::RestClient::RequestPatch)
     end
