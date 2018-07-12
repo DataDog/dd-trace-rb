a92e249f8a7a35aab0fefef85a1ddd822be88f5b
Pawel Chojnacki <pawel.chcki@gmail.com>
2018-07-12 17:19:07 -0400

    aws specs working

--- spec4/test/contrib/aws/parsed_context_spec.rb
+++ spec4/test/contrib/aws/parsed_context_spec.rb
@@ -1,13 +1,14 @@
-require('helper')
+require('spec_helper')
 require('aws-sdk')
 require('ddtrace/contrib/aws/parsed_context')
-class ParsedContextTest < Minitest::Test
+
+RSpec.describe Datadog::Contrib::Aws::ParsedContext do
   before do
     @client = Aws::S3::Client.new(region: 'us-west-2', stub_responses: true)
   end
   it('context parsing') do
     response = @client.list_buckets
-    context = Datadog::Contrib::Aws::ParsedContext.new(response.context)
+    context = described_class.new(response.context)
     expect(context.resource).to(eq('s3.list_buckets'))
     expect(context.operation).to(eq(:list_buckets))
     expect(context.status_code).to(eq(200))
@@ -15,14 +16,13 @@ class ParsedContextTest < Minitest::Test
     expect(context.region).to(eq('us-west-2'))
     expect(context.retry_attempts).to(eq(0))
     expect(context.path).to(eq('/'))
-    assert_includes(context.host, 'us-west-2.amazonaws.com')
+    expect(context.host).to include('us-west-2.amazonaws.com')
   end
   it('context param safety') do
     response = @client.list_buckets
-    context = Datadog::Contrib::Aws::ParsedContext.new(response.context)
-    context.stub(:resource, -> { raise }) do
-      actual = context.safely(:resource, 'fallback_name')
-      expect(actual).to(eq('fallback_name'))
-    end
+    context = described_class.new(response.context)
+    allow(context).to receive(:resource).and_raise
+
+    expect(context.safely(:resource, 'fallback_name')).to(eq('fallback_name'))
   end
 end
