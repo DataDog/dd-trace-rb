24673d921eafe0f429b9c230bf08b167b3ea110e
Pawel Chojnacki <pawel.chcki@gmail.com>
2018-08-09 20:24:54 +0200

    Use proper `delete_at` method

--- lib/ddtrace/buffer.rb
+++ lib/ddtrace/buffer.rb
@@ -18,7 +18,7 @@ module Datadog
     def push(trace)
       return if @closed
       len = @traces.length
-      @traces.delete(rand(len)) unless len < @max_size || @max_size <= 0
+      @traces.delete_at(rand(len)) if len >= @max_size && @max_size > 0
       @traces << trace
     end
 
