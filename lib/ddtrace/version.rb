2ee39beb7f2ab66f90928ca84e0b1ac859ed66dc
Pawel Chojnacki <pawel.chcki@gmail.com>
2018-09-14 16:29:58 +0200

    version - disable protocol 4

--- lib/ddtrace/version.rb
+++ lib/ddtrace/version.rb
@@ -3,7 +3,7 @@ module Datadog
     MAJOR = 0
     MINOR = 14
     PATCH = 2
-    PRE = 'withoutpriorityparsing1'
+    PRE = 'disableprotocolversion4'
 
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
