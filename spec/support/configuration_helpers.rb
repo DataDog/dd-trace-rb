1c1a1788028a6f5c4bb212b3bddc30581dd18b71
David Elner <david@davidelner.com>
2018-09-17 14:32:31 -0400

    Changed: ConfigurationHelpers#remove_patch! to work with Integration patchers.

--- spec/support/configuration_helpers.rb
+++ spec/support/configuration_helpers.rb
@@ -1,7 +1,15 @@
 module ConfigurationHelpers
   def remove_patch!(integration)
-    Datadog
-      .registry[integration]
-      .instance_variable_set('@patched', false)
+    if Datadog.registry[integration].respond_to?(:patcher)
+      Datadog.registry[integration].patcher.tap do |patcher|
+        if patcher.instance_variable_defined?(:@done_once)
+          patcher.instance_variable_get(:@done_once).delete(integration)
+        end
+      end
+    else
+      Datadog
+        .registry[integration]
+        .instance_variable_set('@patched', false)
+    end
   end
 end
