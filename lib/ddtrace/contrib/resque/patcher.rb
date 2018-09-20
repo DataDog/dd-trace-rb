982b739fb0d5efd62b36ba682529b83e37f1f87b
David Elner <david@davidelner.com>
2018-09-20 12:54:04 -0400

    Implement Resque integration configuration (#546)
    * Changed: ConfigurationHelpers#remove_patch! to work with Integration patchers.
    
    * Refactored: Resque to use Datadog::Contrib::Integration.

--- lib/ddtrace/contrib/resque/patcher.rb
+++ lib/ddtrace/contrib/resque/patcher.rb
@@ -1,49 +1,40 @@
+require 'ddtrace/contrib/patcher'
+require 'ddtrace/ext/app_types'
+require 'ddtrace/contrib/sidekiq/ext'
+
 module Datadog
   module Contrib
-    # Namespace for `resque` integration
     module Resque
-      SERVICE = 'resque'.freeze
-
-      class << self
-        # Globally-acccesible reference for pre-forking optimization
-        attr_accessor :sync_writer
-      end
-
-      # Patcher for Resque integration - sets up the pin for the integration
+      # Patcher enables patching of 'resque' module.
       module Patcher
-        include Base
-        register_as :resque, auto_patch: true
-        option :service_name, default: SERVICE
+        include Contrib::Patcher
 
-        @patched = false
-        option :workers, default: []
+        module_function
 
-        class << self
-          def patch
-            return @patched if patched? || !defined?(::Resque)
+        def patched?
+          done?(:resque)
+        end
 
-            require 'ddtrace/ext/app_types'
-            require_relative 'resque_job'
-
-            add_pin
-            get_option(:workers).each { |worker| worker.extend(ResqueJob) }
-            @patched = true
-          rescue => e
-            Tracer.log.error("Unable to apply Resque integration: #{e}")
-            @patched
+        def patch
+          do_once(:resque) do
+            begin
+              require_relative 'resque_job'
+              add_pin
+              get_option(:workers).each { |worker| worker.extend(ResqueJob) }
+            rescue StandardError => e
+              Datadog::Tracer.log.error("Unable to apply Resque integration: #{e}")
+            end
           end
+        end
 
-          def patched?
-            @patched
-          end
+        def add_pin
+          Pin
+            .new(get_option(:service_name), app: Ext::APP, app_type: Datadog::Ext::AppTypes::WORKER)
+            .onto(::Resque)
+        end
 
-          private
-
-          def add_pin
-            Pin
-              .new(get_option(:service_name), app: 'resque', app_type: Ext::AppTypes::WORKER)
-              .onto(::Resque)
-          end
+        def get_option(option)
+          Datadog.configuration[:resque].get_option(option)
         end
       end
     end
