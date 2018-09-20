8074a42dcceaf0c5eabac423c7765c3758152877
David Elner <david@davidelner.com>
2018-09-20 12:53:34 -0400

    Refactored: Sidekiq to use Datadog::Contrib::Integration. (#545)

---
+++ lib/ddtrace/contrib/sidekiq/ext.rb
@@ -0,0 +1,19 @@
+module Datadog
+  module Contrib
+    module Sidekiq
+      # Sidekiq integration constants
+      module Ext
+        APP = 'sidekiq'.freeze
+        SERVICE_NAME = 'sidekiq'.freeze
+
+        SPAN_JOB = 'sidekiq.job'.freeze
+
+        TAG_JOB_DELAY = 'sidekiq.job.delay'.freeze
+        TAG_JOB_ID = 'sidekiq.job.id'.freeze
+        TAG_JOB_QUEUE = 'sidekiq.job.queue'.freeze
+        TAG_JOB_RETRY = 'sidekiq.job.retry'.freeze
+        TAG_JOB_WRAPPER = 'sidekiq.job.wrapper'.freeze
+      end
+    end
+  end
+end
