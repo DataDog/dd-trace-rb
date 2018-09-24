416dc740968651df5e6c31f9b41066798f8d01ff
David Elner <david@davidelner.com>
2018-09-24 16:09:19 -0400

    Refactored: Mysql2 to use Datadog::Contrib::Integration.

---
+++ lib/ddtrace/contrib/mysql2/configuration/settings.rb
@@ -0,0 +1,16 @@
+require 'ddtrace/contrib/configuration/settings'
+require 'ddtrace/contrib/mysql2/ext'
+
+module Datadog
+  module Contrib
+    module Mysql2
+      module Configuration
+        # Custom settings for the Mysql2 integration
+        class Settings < Contrib::Configuration::Settings
+          option :service_name, default: Ext::SERVICE_NAME
+          option :tracer, default: Datadog.tracer
+        end
+      end
+    end
+  end
+end
