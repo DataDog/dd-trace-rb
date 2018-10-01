c3f31edb67eb64026fd790d562b77ae4b82f14ae
David Elner <david@davidelner.com>
2018-10-01 16:50:54 -0400

    Refactored: Grape to use Datadog::Contrib::Integration. (#571)

---
+++ lib/ddtrace/contrib/grape/configuration/settings.rb
@@ -0,0 +1,16 @@
+require 'ddtrace/contrib/configuration/settings'
+require 'ddtrace/ext/http'
+require 'ddtrace/contrib/grape/ext'
+
+module Datadog
+  module Contrib
+    module Grape
+      module Configuration
+        # Custom settings for the Grape integration
+        class Settings < Contrib::Configuration::Settings
+          option :service_name, default: Ext::SERVICE_NAME
+        end
+      end
+    end
+  end
+end
