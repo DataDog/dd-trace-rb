30bb41ea9e4e728ba99a507c951fcf6b5bcaafbb
Dirk Gadsden <dirk@esherido.com>
2018-11-01 11:28:28 -0700

    Missed a few spots with the rename
    Original rename commit: 4f57b005

--- test/contrib/sidekiq/server_tracer_test.rb
+++ test/contrib/sidekiq/server_tracer_test.rb
@@ -47,7 +47,7 @@ class ServerTracerTest < TracerTestBase
 
     span = spans[0]
     assert_equal('sidekiq', span.service)
-    assert_equal('TracerTest::EmptyWorker', span.resource)
+    assert_equal('ServerTracerTest::EmptyWorker', span.resource)
     assert_equal('default', span.get_tag('sidekiq.job.queue'))
     refute_nil(span.get_tag('sidekiq.job.delay'))
     assert_equal(0, span.status)
@@ -69,12 +69,12 @@ class ServerTracerTest < TracerTestBase
 
     span = spans[0]
     assert_equal('sidekiq', span.service)
-    assert_equal('TracerTest::ErrorWorker', span.resource)
+    assert_equal('ServerTracerTest::ErrorWorker', span.resource)
     assert_equal('default', span.get_tag('sidekiq.job.queue'))
     refute_nil(span.get_tag('sidekiq.job.delay'))
     assert_equal(1, span.status)
     assert_equal('job error', span.get_tag(Datadog::Ext::Errors::MSG))
-    assert_equal('TracerTest::TestError', span.get_tag(Datadog::Ext::Errors::TYPE))
+    assert_equal('ServerTracerTest::TestError', span.get_tag(Datadog::Ext::Errors::TYPE))
     assert_nil(span.parent)
   end
 
@@ -91,14 +91,14 @@ class ServerTracerTest < TracerTestBase
     custom, empty = spans
 
     assert_equal('sidekiq', empty.service)
-    assert_equal('TracerTest::EmptyWorker', empty.resource)
+    assert_equal('ServerTracerTest::EmptyWorker', empty.resource)
     assert_equal('default', empty.get_tag('sidekiq.job.queue'))
     refute_nil(empty.get_tag('sidekiq.job.delay'))
     assert_equal(0, empty.status)
     assert_nil(empty.parent)
 
     assert_equal('sidekiq-slow', custom.service)
-    assert_equal('TracerTest::CustomWorker', custom.resource)
+    assert_equal('ServerTracerTest::CustomWorker', custom.resource)
     assert_equal('default', custom.get_tag('sidekiq.job.queue'))
     assert_equal(0, custom.status)
     assert_nil(custom.parent)
