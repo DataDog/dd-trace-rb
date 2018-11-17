fc45686e2b49a08bd99a2ff98c51c66c4a9f8934
Dirk Gadsden <dirk@esherido.com>
2018-11-16 19:03:33 -0800

    Assign current runtime for easier access and common interfacing

--- lib/ddtrace/runtime.rb
+++ lib/ddtrace/runtime.rb
@@ -1,10 +1,15 @@
 module Datadog
   # Ruby runtime-specific functionality.
   module Runtime
+    class << self
+      attr_accessor :current
+    end
+
     ruby_engine = defined?(RUBY_ENGINE) && RUBY_ENGINE
 
     if ruby_engine == 'ruby'
       require 'ddtrace/runtime/mri'
+      self.current = MRI
     end
   end
 end
