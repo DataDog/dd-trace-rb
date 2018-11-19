cd5f88f2c6f52e35e4f7bd963e3b062bfac1f208
David Elner <david@davidelner.com>
2018-11-19 09:22:35 -0500

    Fixed: Compatibility with Statsd < 4.0

--- lib/ddtrace/metrics.rb
+++ lib/ddtrace/metrics.rb
@@ -20,23 +20,23 @@ module Datadog
     protected
 
     def distribution(stat, value, options = nil)
-      return if statsd.nil?
+      return if statsd.nil? || !statsd.respond_to?(:distribution)
       statsd.distribution(stat, value, metric_options(options))
     end
 
     def increment(stat, options = nil)
-      return if statsd.nil?
+      return if statsd.nil? || !statsd.respond_to?(:increment)
       statsd.increment(stat, metric_options(options))
     end
 
-    def time(stat, options = nil, &block)
-      return yield if statsd.nil?
+    def time(stat, options = nil)
+      return yield if statsd.nil? || !statsd.respond_to?(:distribution)
 
       # Calculate time, send it as a distribution.
       start = Utils::Time.get_time
       return yield
     ensure
-      unless statsd.nil?
+      unless statsd.nil? || !statsd.respond_to?(:distribution)
         finished = Utils::Time.get_time
         statsd.distribution(stat, ((finished - start) * 1000), metric_options(options))
       end
