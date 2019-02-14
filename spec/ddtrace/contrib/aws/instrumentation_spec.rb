181dde19b952854967cb61102763c4929f3b77c1
David Elner <david@davidelner.com>
2019-02-14 16:10:09 -0500

    Added: analytics_sample_rate tag to AWS integration.

--- spec/ddtrace/contrib/aws/instrumentation_spec.rb
+++ spec/ddtrace/contrib/aws/instrumentation_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'ddtrace/contrib/analytics_examples'
 
 require 'aws-sdk'
 require 'ddtrace'
@@ -7,6 +8,7 @@ require 'ddtrace/ext/http'
 
 RSpec.describe 'AWS instrumentation' do
   let(:tracer) { get_test_tracer }
+  let(:configuration_options) { { tracer: tracer } }
 
   let(:client) { ::Aws::S3::Client.new(stub_responses: responses) }
   let(:responses) { true }
@@ -16,8 +18,15 @@ RSpec.describe 'AWS instrumentation' do
 
   before(:each) do
     Datadog.configure do |c|
-      c.use :aws, tracer: tracer
+      c.use :aws, configuration_options
     end
+  end
+
+  around do |example|
+    # Reset before and after each example; don't allow global state to linger.
+    Datadog.registry[:aws].reset_configuration!
+    example.run
+    Datadog.registry[:aws].reset_configuration!
   end
 
   context 'when the client runs' do
@@ -26,6 +35,11 @@ RSpec.describe 'AWS instrumentation' do
 
       let(:responses) do
         { list_buckets: { buckets: [{ name: 'bucket1' }] } }
+      end
+
+      it_behaves_like 'analytics for integration' do
+        let(:analytics_enabled_var) { Datadog::Contrib::Aws::Ext::ENV_ANALYTICS_ENABLED }
+        let(:analytics_sample_rate_var) { Datadog::Contrib::Aws::Ext::ENV_ANALYTICS_SAMPLE_RATE }
       end
 
       it 'generates a span' do
