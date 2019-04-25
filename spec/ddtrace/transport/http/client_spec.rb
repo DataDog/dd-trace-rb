562663ecb9a8ca3cdbfe5da850e1c763ca8e2d4c
David Elner <david@davidelner.com>
2019-04-25 14:38:27 -0400

    Fixed: Transport::HTTP::Client spec to use correct trace agent host/port.

--- spec/ddtrace/transport/http/client_spec.rb
+++ spec/ddtrace/transport/http/client_spec.rb
@@ -5,12 +5,6 @@ require 'ddtrace/transport/http/service'
 
 RSpec.describe Datadog::Transport::HTTP::Client do
   subject(:client) { described_class.new(service, options) }
-  let(:service) do
-    Datadog::Transport::HTTP::Service.new(
-      ENV.fetch('TEST_DDAGENT_HOST', 'localhost'),
-      ENV.fetch('TEST_DDAGENT_PORT', 8126)
-    )
-  end
   let(:options) { {} }
 
   describe '#deliver' do
@@ -18,7 +12,17 @@ RSpec.describe Datadog::Transport::HTTP::Client do
 
     context 'given some traces' do
       let(:parcel) { Datadog::Transport::Traces::Parcel.new(get_test_traces(2)) }
-      it { expect(response.ok?).to be true }
+
+      context 'with an actual service' do
+        let(:service) do
+          Datadog::Transport::HTTP::Service.new(
+            ENV.fetch('DD_AGENT_HOST', 'localhost'),
+            ENV.fetch('DD_TRACE_AGENT_PORT', 8126)
+          )
+        end
+
+        it { expect(response.ok?).to be true }
+      end
 
       context 'with test service' do
         let(:service) { FauxHTTPService.new }
