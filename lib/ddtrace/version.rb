a995aaf6643d2fdde193537e1d1481f347a7f2ff
David Elner <david@davidelner.com>
2019-05-21 14:57:34 -0400

    bumping version 0.23.3 => 0.24.0

--- lib/ddtrace/version.rb
+++ lib/ddtrace/version.rb
@@ -1,8 +1,8 @@
 module Datadog
   module VERSION
     MAJOR = 0
-    MINOR = 23
-    PATCH = 3
+    MINOR = 24
+    PATCH = 0
     PRE = nil
 
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
