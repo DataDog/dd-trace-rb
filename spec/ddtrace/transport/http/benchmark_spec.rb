edd05d95771e2793a069481820ec69168b249f6f
David Elner <david@davidelner.com>
2019-06-25 08:35:11 -0400

    Changed: Benchmark to exclude trace generation.

--- spec/ddtrace/transport/http/benchmark_spec.rb
+++ spec/ddtrace/transport/http/benchmark_spec.rb
@@ -12,19 +12,18 @@ RSpec.describe 'Transport::HTTP benchmarks' do
   describe '#send' do
     let!(:http_transport) { Datadog::HTTPTransport.new }
     let!(:transport_http) { Datadog::Transport::HTTP.default }
+    let!(:traces) { get_test_traces(2) }
 
     it do
       Benchmark.bm do |x|
         x.report('Datadog::HTTPTransport') do
           iterations.times do
-            traces = get_test_traces(2)
             http_transport.send(:traces, traces)
           end
         end
 
         x.report('Datadog::Transport::HTTP') do
           iterations.times do
-            traces = get_test_traces(2)
             transport_http.send_traces(traces)
           end
         end
