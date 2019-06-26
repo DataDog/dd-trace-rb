1522b9d3f2bc9c1080355f3eb00ca011b4e6f99a
Brett Langdon <brett.langdon@datadoghq.com>
2019-06-26 12:41:21 -0400

    [core] Rename Ext::ForcedTracing to Ext::ManualTracing (#765)
    * [core] Rename Ext::ForcedTracing to Ext::ManualTracing
    
    * Do not use deprecate_constant for older version support

---
+++ lib/ddtrace/ext/manual_tracing.rb
@@ -0,0 +1,9 @@
+module Datadog
+  module Ext
+    # Defines constants for manual tracing
+    module ManualTracing
+      TAG_KEEP = 'manual.keep'.freeze
+      TAG_DROP = 'manual.drop'.freeze
+    end
+  end
+end
