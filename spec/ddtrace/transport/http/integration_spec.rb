43c434159d5053d198a6737bf15bdb18441e3652
David Elner <david@davidelner.com>
2019-07-17 17:29:06 -0400

    Refactored: Transport::HTTP integration tests slightly.

--- spec/ddtrace/transport/http/integration_spec.rb
+++ spec/ddtrace/transport/http/integration_spec.rb
@@ -14,7 +14,11 @@ RSpec.describe 'Datadog::Transport::HTTP integration tests' do
     describe '#send_traces' do
       subject(:response) { client.send_traces(traces) }
       let(:traces) { get_test_traces(2) }
-      it { expect(response.ok?).to be true }
+      it do
+        is_expected.to be_a_kind_of(Datadog::Transport::HTTP::Traces::Response)
+        expect(response.ok?).to be true
+        expect(response.service_rates).to_not be nil
+      end
     end
   end
 
