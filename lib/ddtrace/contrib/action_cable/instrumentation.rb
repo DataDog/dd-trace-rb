1a41921fe8fe357da4449a841475b9e777f36c2c
Marco Costa <marco.costa@datadoghq.com>
2019-10-08 18:31:31 +0200

    Address review comments

--- lib/ddtrace/contrib/action_cable/instrumentation.rb
+++ lib/ddtrace/contrib/action_cable/instrumentation.rb
@@ -2,20 +2,17 @@ module Datadog
   module Contrib
     module ActionCable
       module Instrumentation
-        # Instruments ActionCable::Connection to ensure the top-level
-        # rack request has the correct resource for this WebSockets connection.
-        #
-        # Some of our common assumptions don't hold under WebSockets, one example
-        # being the http status code, which is -1 for this request.
+        # When a new WebSocket is open, we receive a Rack request resource name "GET -1".
+        # This module overrides the current Rack resource name to provide a meaningful name.
         module ActionCableConnection
           def on_open
-            Datadog.tracer.trace('action_cable.on_open') do |span|
+            Datadog.tracer.trace(Ext::SPAN_ON_OPEN) do |span|
               begin
                 span.resource = "#{self.class}#on_open"
                 span.span_type = Datadog::Ext::AppTypes::WEB
 
-                span.set_tag('action_cable.action', 'on_open')
-                span.set_tag('action_cable.connection', self.class.to_s)
+                span.set_tag(Ext::TAG_ACTION, 'on_open')
+                span.set_tag(Ext::TAG_CONNECTION, self.class.to_s)
 
                 # Set the resource name of the Rack request span
                 rack_request_span = env[Datadog::Contrib::Rack::TraceMiddleware::RACK_REQUEST_SPAN]
