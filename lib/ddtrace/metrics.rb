ae766616f333e90be6459c70d513b1ae2b24f6e7
David Elner <david@davidelner.com>
2019-10-28 15:57:03 -0400

    Added: Datadog::Metrics logging adapter.

--- lib/ddtrace/metrics.rb
+++ lib/ddtrace/metrics.rb
@@ -1,6 +1,7 @@
 require 'ddtrace/ext/metrics'
 
 require 'set'
+require 'logger'
 require 'ddtrace/utils/time'
 require 'ddtrace/runtime/identity'
 
@@ -125,10 +126,10 @@ module Datadog
     module Options
       DEFAULT = {
         tags: DEFAULT_TAGS = [
-          "#{Ext::Metrics::TAG_LANG}:#{Runtime::Identity.lang}".freeze
-          # "#{Ext::Metrics::TAG_LANG_INTERPRETER}:#{Runtime::Identity.lang_interpreter}".freeze,
-          # "#{Ext::Metrics::TAG_LANG_VERSION}:#{Runtime::Identity.lang_version}".freeze,
-          # "#{Ext::Metrics::TAG_TRACER_VERSION}:#{Runtime::Identity.tracer_version}".freeze
+          "#{Ext::Metrics::TAG_LANG}:#{Runtime::Identity.lang}".freeze,
+          "#{Ext::Metrics::TAG_LANG_INTERPRETER}:#{Runtime::Identity.lang_interpreter}".freeze,
+          "#{Ext::Metrics::TAG_LANG_VERSION}:#{Runtime::Identity.lang_version}".freeze,
+          "#{Ext::Metrics::TAG_TRACER_VERSION}:#{Runtime::Identity.tracer_version}".freeze
         ].freeze
       }.freeze
 
@@ -168,6 +169,40 @@ module Datadog
           define_method(name) do |*args, &block|
             send(metric_type, stat, *args, &block)
           end
+        end
+      end
+    end
+
+    module Logging
+      # Surrogate for Datadog::Statsd to log elsewhere
+      class Adapter
+        attr_accessor :logger
+
+        def initialize(logger = nil)
+          @logger = logger || Logger.new(STDOUT).tap do |l|
+            l.level = Logger::INFO
+            l.progname = nil
+            l.formatter = proc do |_severity, datetime, _progname, msg|
+              stat = JSON.parse(msg[3..-1]) # Trim off leading progname...
+              "#{JSON.dump(timestamp: datetime.to_i, message: 'Metric sent.', metric: stat)}\n"
+            end
+          end
+        end
+
+        def count(stat, value, options = nil)
+          logger.info({ stat: stat, type: :count, value: value, options: options }.to_json)
+        end
+
+        def distribution(stat, value, options = nil)
+          logger.info({ stat: stat, type: :distribution, value: value, options: options }.to_json)
+        end
+
+        def increment(stat, options = nil)
+          logger.info({ stat: stat, type: :increment, options: options }.to_json)
+        end
+
+        def gauge(stat, value, options = nil)
+          logger.info({ stat: stat, type: :gauge, value: value, options: options }.to_json)
         end
       end
     end
