f5b6219c018092b7f98cdbcaf006f3af88ca8884
brettlangdon <brett.langdon@datadoghq.com>
2019-12-17 10:44:29 -0500

    Update RuleSampler rate_limit doc string

--- lib/ddtrace/sampling/rule_sampler.rb
+++ lib/ddtrace/sampling/rule_sampler.rb
@@ -19,7 +19,7 @@ module Datadog
       attr_reader :rules, :rate_limiter, :default_sampler
 
       # @param rules [Array<Rule>] ordered list of rules to be applied to a span
-      # @param rate_limit [Float] number of traces per second, defaults to 100
+      # @param rate_limit [Float] number of traces per second, defaults to no rate limit
       # @param rate_limiter [RateLimiter] limiter applied after rule matching
       # @param default_sample_rate [Float] fallback sample rate when no rules apply to a span,
       #   between +[0,1]+, defaults to +1+
