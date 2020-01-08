1873bcb61f3907fc9eed8d3fd9bf92d15102da8b
Marco Costa <marco.costa@datadoghq.com>
2020-01-08 17:45:56 -0500

    I like this iteration

--- lib/ddtrace/contrib/dalli/instrumentation.rb
+++ lib/ddtrace/contrib/dalli/instrumentation.rb
@@ -23,20 +23,35 @@ module Datadog
           def request(*args, &block)
             return super unless Datadog.tracer.enabled?
 
-            span = trace(name, service: service)
+            # Before method call
+            span = trace(Dalli::Ext::SPAN_COMMAND, service: service)
+            span.resource = op.to_s.upcase
+            span.span_type = Datadog::Contrib::Dalli::Ext::SPAN_TYPE_COMMAND
+
+            # Set analytics sample rate
+            if Contrib::Analytics.enabled?(configuration[:analytics_enabled])
+              Contrib::Analytics.set_sample_rate(span, configuration[:analytics_sample_rate])
+            end
+
             span.set_tag(Datadog::Ext::HTTP::BASE_URL, args.first)
+            span.set_tag(Datadog::Ext::NET::TARGET_HOST, hostname)
+            span.set_tag(Datadog::Ext::NET::TARGET_PORT, port)
+            cmd = Datadog::Contrib::Dalli::Quantize.format_command(op, args)
+            span.set_tag(Datadog::Contrib::Dalli::Ext::TAG_COMMAND, cmd)
 
             super.tap do |ret|
+              # After method call
               span.set_tag(Datadog::Ext::HTTP::STATUS_CODE, ret)
             end
           rescue Exception => e
+            # On method error
             span.set_error(e)
             raise e
           ensure
             span.finish
           end
 
-          dd_instrument.decorate(:request) do
+          dd_instrument.decorate(:request, Dalli::Ext::SPAN_COMMAND) do
             before do |span, args, block|
               span.set_tag(Datadog::Ext::HTTP::BASE_URL, args.first)
             end
@@ -50,9 +65,57 @@ module Datadog
             end
           end
 
+          dd_instrument.decorate(:request, Dalli::Ext::SPAN_COMMAND) do
+            around do |span, args, block|
+              span.set_tag(Datadog::Ext::HTTP::BASE_URL, args.first)
+
+              yield.tap do |ret|
+                span.set_tag(Datadog::Ext::HTTP::STATUS_CODE, ret.code)
+              end
+            rescue ::RestClient::ExceptionWithResponse => e
+              span.set_tag(Datadog::Ext::HTTP::STATUS_CODE, e.code)
+              raise e
+            end
+          end
+
+          dd_instrument.decorate(:request, Dalli::Ext::SPAN_COMMAND) do
+            before do |span, args, block|
+              span.resource = op.to_s.upcase
+              span.span_type = Datadog::Contrib::Dalli::Ext::SPAN_TYPE_COMMAND
+
+              span.set_tag(Datadog::Ext::NET::TARGET_HOST, hostname)
+              span.set_tag(Datadog::Ext::NET::TARGET_PORT, port)
+              cmd = Datadog::Contrib::Dalli::Quantize.format_command(op, args)
+              span.set_tag(Datadog::Contrib::Dalli::Ext::TAG_COMMAND, cmd)
+            end
+          end
+
+          # 👍
+          dd_instrument.around(:request, Dalli::Ext::SPAN_COMMAND) do |span, args, block|
+            span.set_tag(Datadog::Ext::HTTP::BASE_URL, args.first)
+
+            yield.tap do |ret|
+              span.set_tag(Datadog::Ext::HTTP::STATUS_CODE, ret.code)
+            end
+          rescue ::RestClient::ExceptionWithResponse => e
+            span.set_tag(Datadog::Ext::HTTP::STATUS_CODE, e.code)
+            raise e
+          end
+
+          # 👍
+          dd_instrument.before(:request, Dalli::Ext::SPAN_COMMAND) do |span, args, block|
+            span.resource = op.to_s.upcase
+            span.span_type = Datadog::Contrib::Dalli::Ext::SPAN_TYPE_COMMAND
+
+            span.set_tag(Datadog::Ext::NET::TARGET_HOST, hostname)
+            span.set_tag(Datadog::Ext::NET::TARGET_PORT, port)
+            cmd = Datadog::Contrib::Dalli::Quantize.format_command(op, args)
+            span.set_tag(Datadog::Contrib::Dalli::Ext::TAG_COMMAND, cmd)
+          end
+
           def request(op, *args)
             dd_instrumenter.with_configuration(my_configuration) do |instrument|
-              instrument.trace(Datadog::Contrib::Dalli::Ext::SPAN_COMMAND) do |span|
+              instrument.trace(Dalli::Ext::SPAN_COMMAND) do |span|
                 span.resource = op.to_s.upcase
                 span.span_type = Datadog::Contrib::Dalli::Ext::SPAN_TYPE_COMMAND
 
@@ -73,7 +136,7 @@ module Datadog
 
           # TODO: Possible future improvements
           # dd_instrumenter.trace(method(:request)) do |span|
-          #  span.name = Datadog::Contrib::Dalli::Ext::SPAN_COMMAND
+          #  span.name = Dalli::Ext::SPAN_COMMAND
           #  span.resource = op.to_s.upcase
           #  span.span_type = Datadog::Contrib::Dalli::Ext::SPAN_TYPE_COMMAND
           #
