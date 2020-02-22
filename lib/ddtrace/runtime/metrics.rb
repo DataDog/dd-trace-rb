ad6829be6cca5aec4368aea312cb0508c5ab6d72
David Elner <david@davidelner.com>
2020-02-22 11:29:44 -0500

    Fixed: RuntimeMetrics accumulating service names when disabled.

--- lib/ddtrace/runtime/metrics.rb
+++ lib/ddtrace/runtime/metrics.rb
@@ -19,7 +19,7 @@ module Datadog
       end
 
       def associate_with_span(span)
-        return if span.nil?
+        return if !enabled? || span.nil?
 
         # Register service as associated with metrics
         register_service(span.service) unless span.service.nil?
@@ -30,7 +30,7 @@ module Datadog
 
       # Associate service with runtime metrics
       def register_service(service)
-        return if service.nil?
+        return if !enabled? || service.nil?
 
         service = service.to_s
 
