418ce9cf5cf10fbea6927119140289f9856286be
David Elner <david@davidelner.com>
2020-03-23 13:39:10 -0400

    Changed: Span#set_tag with OpenTelemetry service.name sets service name.

--- spec/ddtrace/opentelemetry/span_spec.rb
+++ spec/ddtrace/opentelemetry/span_spec.rb
@@ -14,6 +14,16 @@ RSpec.describe Datadog::OpenTelemetry::Span do
     describe '#set_tag' do
       subject(:set_tag) { span.set_tag(tag_name, tag_value) }
 
+      context "when given '#{Datadog::OpenTelemetry::Span::TAG_SERVICE_NAME}'" do
+        let(:tag_name) { Datadog::OpenTelemetry::Span::TAG_SERVICE_NAME }
+        let(:tag_value) { 'opentelemetry-service' }
+
+        before { set_tag }
+
+        it { expect(span.get_tag(tag_name)).to eq tag_value }
+        it { expect(span.service).to eq tag_value }
+      end
+
       context "when given '#{Datadog::OpenTelemetry::Span::TAG_SERVICE_VERSION}'" do
         let(:tag_name) { Datadog::OpenTelemetry::Span::TAG_SERVICE_VERSION }
         let(:tag_value) { '1.2.3' }
@@ -22,6 +32,15 @@ RSpec.describe Datadog::OpenTelemetry::Span do
 
         it { expect(span.get_tag(tag_name)).to eq tag_value }
         it { expect(span.get_tag(Datadog::Ext::Environment::TAG_VERSION)).to eq tag_value }
+      end
+
+      context 'when given an arbitrary tag' do
+        let(:tag_name) { 'custom-tag' }
+        let(:tag_value) { 'custom-value' }
+
+        before { set_tag }
+
+        it { expect(span.get_tag(tag_name)).to eq tag_value }
       end
     end
   end
