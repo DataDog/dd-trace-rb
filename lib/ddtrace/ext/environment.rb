9afae657368f05f2e71eb75393e5c7cb653b9557
David Elner <david@davidelner.com>
2020-03-23 12:31:20 -0400

    Added: Datadog::Environment::service.

--- lib/ddtrace/ext/environment.rb
+++ lib/ddtrace/ext/environment.rb
@@ -2,6 +2,7 @@ module Datadog
   module Ext
     module Environment
       ENV_ENVIRONMENT = 'DD_ENV'.freeze
+      ENV_SERVICE = 'DD_SERVICE'.freeze
       ENV_TAGS = 'DD_TAGS'.freeze
       ENV_VERSION = 'DD_VERSION'.freeze
 
