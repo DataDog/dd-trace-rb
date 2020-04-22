4fac841fdfa5c076bf911b3f53f9b785df384c26
David Elner <david@davidelner.com>
2020-04-22 15:45:47 -0400

    Added: Profiling::Recorder class.

---
+++ lib/ddtrace/profiling/recorder.rb
@@ -0,0 +1,41 @@
+require 'ddtrace/profiling/buffer'
+
+module Datadog
+  module Profiling
+    # Profiling buffer that stores profiling events. The buffer has a maximum size and when
+    # the buffer is full, a random event is discarded. This class is thread-safe.
+    class Recorder
+      def initialize(event_classes, max_size)
+        @buffers = {}
+
+        # Add a buffer for each class
+        event_classes.each do |event_class|
+          @buffers[event_class] = Profiling::Buffer.new(max_size)
+        end
+      end
+
+      def push(event)
+        raise UnknownEventError, event.class unless @buffers.key?(event.class)
+        @buffers[event.class].push(event)
+      end
+
+      def pop(event_class)
+        raise UnknownEventError, event_class unless @buffers.key?(event_class)
+        @buffers[event_class].pop
+      end
+
+      # Error when event of an unknown type is used with the Recorder
+      class UnknownEventError < StandardError
+        attr_reader :event_class
+
+        def initialize(event_class)
+          @event_class = event_class
+        end
+
+        def message
+          @message ||= "Unknown event class '#{event_class}' for profiling recorder."
+        end
+      end
+    end
+  end
+end
