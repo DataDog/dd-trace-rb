d21db2783d5a06d8d2e69df181f45e73d6062174
David Elner <david@davidelner.com>
2020-05-05 17:31:58 -0400

    Added: Profiling constants.

---
+++ lib/ddtrace/ext/profiling.rb
@@ -0,0 +1,9 @@
+module Datadog
+  module Ext
+    module Profiling
+      ENV_MAX_FRAMES = 'DD_PROFILING_MAX_FRAMES'.freeze
+      ENV_MAX_TIME_USAGE_PCT = 'DD_PROFILING_MAX_TIME_USAGE_PCT'.freeze
+      ENV_IGNORE_PROFILER = 'DD_PROFILING_IGNORE_PROFILER'.freeze
+    end
+  end
+end
