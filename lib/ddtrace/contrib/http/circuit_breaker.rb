808ffb7253fc219cc2ad3ca060cee671cba9ec25
David Elner <david@davidelner.com>
2020-05-07 22:32:55 -0400

    Fixed: HTTP::CircuitBreaker not ignoring Datadog HTTP requests.

--- lib/ddtrace/contrib/http/circuit_breaker.rb
+++ lib/ddtrace/contrib/http/circuit_breaker.rb
@@ -4,8 +4,8 @@ module Datadog
       # HTTP integration circuit breaker behavior
       # For avoiding recursive traces.
       module CircuitBreaker
-        def should_skip_tracing?(req, address, port, tracer)
-          return true if datadog_http_request?(req, address, port, tracer)
+        def should_skip_tracing?(request, tracer)
+          return true if datadog_http_request?(request)
 
           # we don't want a "shotgun" effect with two nested traces for one
           # logical get, and request is likely to call itself recursively
@@ -16,38 +16,14 @@ module Datadog
         end
 
         # We don't want to trace our own call to the API (they use net/http)
-        # TODO: We don't want this kind of coupling with the transport.
+        # TODO: We don't want this kind of soft-check on HTTP requests.
         #       Remove this when transport implements its own "skip tracing" mechanism.
-        def datadog_http_request?(req, address, port, tracer)
-          transport = tracer.writer.transport
-
-          transport_hostname = nil
-          transport_port = nil
-
-          # Get settings from transport, if available.
-          case transport
-          when Datadog::Transport::HTTP::Client
-            adapter = transport.current_api.adapter
-            if adapter.is_a?(Datadog::Transport::HTTP::Adapters::Net)
-              transport_hostname = adapter.hostname.to_s
-              transport_port = adapter.port.to_i
-            end
+        def datadog_http_request?(request)
+          if request[Datadog::Ext::Transport::HTTP::HEADER_META_TRACER_VERSION]
+            true
+          else
+            false
           end
-
-          # When we know the host & port (from the URI) we use it, else (most-likely
-          # called with a block) rely on the URL at the end.
-          if req.respond_to?(:uri) && req.uri
-            if req.uri.host.to_s == transport_hostname &&
-               req.uri.port.to_i == transport_port
-              return true
-            end
-          elsif address && port &&
-                address.to_s == transport_hostname &&
-                port.to_i == transport_port
-            return true
-          end
-
-          false
         end
 
         def should_skip_distributed_tracing?(pin)
