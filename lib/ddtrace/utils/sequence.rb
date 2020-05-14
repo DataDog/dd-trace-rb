3455bea0a3a0a4800465a096c3fb8411dfc40531
David Elner <david@davidelner.com>
2020-05-14 11:10:38 -0400

    Added: Utils::Sequence class.

---
+++ lib/ddtrace/utils/sequence.rb
@@ -0,0 +1,17 @@
+module Datadog
+  module Utils
+    # Generates values from a consistent sequence
+    class Sequence
+      def initialize(seed = 0, &block)
+        @current = seed
+        @next_item = block
+      end
+
+      def next
+        next_item = @next_item ? @next_item.call(@current) : @current
+        @current += 1
+        next_item
+      end
+    end
+  end
+end
