874a0dc679cb00dfda2a12213b6ecfa51e6721b2
David Elner <david@davidelner.com>
2020-05-27 18:01:12 -0400

    Changed: Pprof Template + Builder + Converter pattern

--- lib/ddtrace/ext/profiling.rb
+++ lib/ddtrace/ext/profiling.rb
@@ -4,6 +4,13 @@ module Datadog
       ENV_MAX_FRAMES = 'DD_PROFILING_MAX_FRAMES'.freeze
       ENV_MAX_TIME_USAGE_PCT = 'DD_PROFILING_MAX_TIME_USAGE_PCT'.freeze
       ENV_IGNORE_PROFILER = 'DD_PROFILING_IGNORE_PROFILER'.freeze
+
+      module Pprof
+        LABEL_KEY_THREAD_ID = 'thread id'.freeze
+        SAMPLE_VALUE_NO_VALUE = 0
+        VALUE_TYPE_WALL = 'wall'.freeze
+        VALUE_UNIT_NANOSECONDS = 'nanoseconds'.freeze
+      end
     end
   end
 end
