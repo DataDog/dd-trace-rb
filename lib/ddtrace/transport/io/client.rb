215da9202880bd17af1517615b5a5be8be2a5402
David Elner <david@davidelner.com>
2020-06-12 16:28:56 -0400

    Changed: Transport::IO::Client to accept configured blocks.

--- lib/ddtrace/transport/io/client.rb
+++ lib/ddtrace/transport/io/client.rb
@@ -12,19 +12,24 @@ module Datadog
           :encoder,
           :out
 
-        def initialize(out, encoder)
+        def initialize(out, encoder, options = {})
           @out = out
           @encoder = encoder
+
+          @request_block = options.fetch(:request, method(:send_default_request))
+          @encode_block = options.fetch(:encode, method(:encode_data))
+          @write_block = options.fetch(:write, method(:write_data))
+          @response_block = options.fetch(:response, method(:build_response))
         end
 
         def send_request(request)
           # Write data to IO
           # If block is given, allow it to handle writing
-          # Otherwise use default encoding.
+          # Otherwise do a standard encode/write/response.
           response = if block_given?
                        yield(out, request)
                      else
-                       send_default_request(out, request)
+                       @request_block.call(out, request)
                      end
 
           # Update statistics
@@ -48,8 +53,6 @@ module Datadog
           InternalErrorResponse.new(e)
         end
 
-        protected
-
         def encode_data(encoder, request)
           request.parcel.encode_with(encoder)
         end
@@ -58,17 +61,21 @@ module Datadog
           out.puts(data)
         end
 
+        def build_response(_request, _data, result)
+          IO::Response.new(result)
+        end
+
         private
 
         def send_default_request(out, request)
           # Encode data
-          data = encode_data(encoder, request)
+          data = @encode_block.call(encoder, request)
 
           # Write to IO
-          result = write_data(out, data)
+          result = @write_block.call(out, data)
 
           # Generate a response
-          IO::Response.new(result)
+          @response_block.call(request, data, result)
         end
       end
     end
