6763bc234edbac2b43dd1727bf99aa5231892318
David Elner <david@davidelner.com>
2020-06-12 16:28:56 -0400

    Changed: Transport::IO::Client to accept configured blocks.

--- spec/ddtrace/profiling/transport/io/client_spec.rb
+++ spec/ddtrace/profiling/transport/io/client_spec.rb
@@ -4,12 +4,7 @@ require 'ddtrace/transport/io/client'
 require 'ddtrace/profiling/transport/io/client'
 
 RSpec.describe Datadog::Profiling::Transport::IO::Client do
-  subject(:client) do
-    Datadog::Transport::IO::Client.new(out, encoder).tap do |client|
-      client.extend(described_class)
-    end
-  end
-
+  subject(:client) { described_class.new(out, encoder) }
   let(:out) { instance_double(IO) }
   let(:encoder) { instance_double(Datadog::Encoding::Encoder) }
 
@@ -38,31 +33,22 @@ RSpec.describe Datadog::Profiling::Transport::IO::Client do
         expect(send_flushes.result).to eq(result)
       end
     end
+  end
 
-    context 'given events and a block' do
-      subject(:send_flushes) { client.send_flushes(events) { |out, data| target.write(out, data) } }
-      let(:events) { instance_double(Array) }
-      let(:encoded_events) { double('encoded events') }
-      let(:result) { double('IO result') }
-      let(:target) { double('target') }
+  describe '#build_response' do
+    subject(:build_response) { client.build_response(request, data, result) }
+    let(:request) { instance_double(Datadog::Profiling::Transport::Request) }
+    let(:data) { double('data') }
+    let(:result) { double('result') }
+    let(:response) { instance_double(Datadog::Profiling::Transport::IO::Response) }
 
-      before do
-        expect(client.encoder).to receive(:encode)
-          .with(events)
-          .and_return(encoded_events)
-
-        expect(target).to receive(:write)
-          .with(client.out, encoded_events)
-          .and_return(result)
-
-        expect(client).to receive(:update_stats_from_response!)
-          .with(kind_of(Datadog::Profiling::Transport::IO::Response))
-      end
-
-      it do
-        is_expected.to be_a_kind_of(Datadog::Profiling::Transport::IO::Response)
-        expect(send_flushes.result).to eq(result)
-      end
+    before do
+      expect(Datadog::Profiling::Transport::IO::Response)
+        .to receive(:new)
+        .with(result)
+        .and_return(response)
     end
+
+    it { is_expected.to be response }
   end
 end
