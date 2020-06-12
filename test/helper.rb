2d42d382b50d0d34a8adb91eb8716506eb1cd9bc
Marco Costa <marco.costa@datadoghq.com>
2020-06-12 17:47:39 -0400

    Restore delete helper snippet

--- test/helper.rb
+++ test/helper.rb
@@ -237,6 +237,14 @@ def remove_patch!(integration)
     .instance_variable_set('@patched', false)
 end
 
+require 'ddtrace/contrib/patcher'
+Datadog::Contrib::Patcher::CommonMethods.send(:prepend, Module.new do
+  # Raise error during tests that fail to patch integration, instead of simply printing a warning message.
+  def on_patch_error(e)
+    raise e
+  end
+end)
+
 require 'minitest/around/unit'
 require 'minitest/stub_any_instance'
 
