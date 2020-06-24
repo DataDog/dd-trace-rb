13a207321a369c31050d25bf1129d8c3b8ff79a9
David Elner <david@davidelner.com>
2020-06-24 16:23:09 -0400

    Fixed: Transport::IO load dependency on traces.

--- lib/ddtrace/transport/io/traces.rb
+++ lib/ddtrace/transport/io/traces.rb
@@ -10,6 +10,12 @@ module Datadog
       module Traces
         # Response from HTTP transport for traces
         class Response < IO::Response
+          include Transport::Traces::Response
+
+          def initialize(result, trace_count = 1)
+            super(result)
+            @trace_count = trace_count
+          end
         end
 
         # Extensions for HTTP client
