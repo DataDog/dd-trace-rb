f9c86f5b80f621a6e4654c7782cbf5b5a1e0c6e1
David Elner <david@davidelner.com>
2020-06-24 16:23:09 -0400

    Fixed: Transport::IO load dependency on traces.

--- spec/ddtrace/transport/io/response_spec.rb
+++ spec/ddtrace/transport/io/response_spec.rb
@@ -4,18 +4,12 @@ require 'ddtrace/transport/io/response'
 
 RSpec.describe Datadog::Transport::IO::Response do
   context 'when implemented by a class' do
-    subject(:response) { described_class.new(result, trace_count) }
+    subject(:response) { described_class.new(result) }
     let(:result) { double('result') }
-    let(:trace_count) { 1 }
 
     describe '#result' do
       subject(:get_result) { response.result }
       it { is_expected.to eq result }
-    end
-
-    describe '#trace_count' do
-      subject(:get_trace_count) { response.trace_count }
-      it { is_expected.to eq trace_count }
     end
 
     describe '#ok?' do
