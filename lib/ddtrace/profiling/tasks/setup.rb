685b9230fb0f64124174c157592c5257a204d9d4
David Elner <david@davidelner.com>
2020-06-24 15:46:57 -0400

    Added: CThread extension for profiling.

--- lib/ddtrace/profiling/tasks/setup.rb
+++ lib/ddtrace/profiling/tasks/setup.rb
@@ -1,9 +1,14 @@
 module Datadog
   module Profiling
     module Tasks
+      # Sets up profiling for the application
       class Setup
-        def execute
-          # TODO
+        def run
+          # Activate CPU timings
+          require 'ddtrace/profiling/ext/thread'
+          ::Thread.send(:prepend, Profiling::Ext::CThread)
+        rescue StandardError, ScriptError => e
+          puts "[DDTRACE] CPU profiling unavailable. Cause: #{e.message} Location: #{e.backtrace.first}"
         end
       end
     end
