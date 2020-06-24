79dd333f830313396da44e356dea4a5252e5dc0f
David Elner <david@davidelner.com>
2020-06-24 15:46:57 -0400

    Added: CThread extension for profiling.

--- lib/ddtrace/profiling.rb
+++ lib/ddtrace/profiling.rb
@@ -3,10 +3,18 @@ module Datadog
   module Profiling
     module_function
 
+    FFI_MINIMUM_VERSION = Gem::Version.new('1.0')
     GOOGLE_PROTOBUF_MINIMUM_VERSION = Gem::Version.new('3.0')
 
     def supported?
       google_protobuf_supported?
+    end
+
+    def native_cpu_time_supported?
+      RUBY_PLATFORM != 'java' \
+        && Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.1') \
+        && !Gem.loaded_specs['ffi'].nil? \
+        && Gem.loaded_specs['ffi'].version >= FFI_MINIMUM_VERSION
     end
 
     def google_protobuf_supported?
@@ -20,6 +28,7 @@ module Datadog
       require 'ddtrace/profiling/exporter'
       require 'ddtrace/profiling/recorder'
       require 'ddtrace/profiling/scheduler'
+      require 'ddtrace/profiling/tasks/setup'
       require 'ddtrace/profiling/transport/io'
 
       require 'ddtrace/profiling/pprof/pprof_pb' if google_protobuf_supported?
