fc44fa0595214603fcd2410de94599403e6af173
David Elner <david@davidelner.com>
2020-07-10 17:18:10 -0400

    Changed: Profiling::Recorder produces richer Flush object.

--- lib/ddtrace/profiling/flush.rb
+++ lib/ddtrace/profiling/flush.rb
@@ -1,6 +1,39 @@
+require 'ddtrace/runtime/identity'
+require 'ddtrace/runtime/socket'
+
 module Datadog
   module Profiling
+    # Represents a flush of all profiling events
+    Flush = Struct.new(
+      :start,
+      :finish,
+      :event_groups,
+      :event_count,
+      :runtime_id,
+      :service,
+      :env,
+      :version,
+      :host,
+      :language,
+      :runtime,
+      :runtime_version,
+      :profiler_version
+    ) do
+      def initialize(*args)
+        super
+        self.runtime_id = runtime_id || Datadog::Runtime::Identity.id
+        self.service = service || Datadog.configuration.service
+        self.env = env || Datadog.configuration.env
+        self.version = version || Datadog.configuration.version
+        self.host = host || Datadog::Runtime::Socket.hostname
+        self.language = language || Datadog::Runtime::Identity.lang
+        self.runtime = runtime || Datadog::Runtime::Identity.lang_interpreter
+        self.runtime_version = runtime_version || Datadog::Runtime::Identity.lang_version
+        self.profiler_version = profiler_version || Datadog::Runtime::Identity.tracer_version
+      end
+    end
+
     # Represents a collection of events of a specific type being flushed.
-    Flush = Struct.new(:event_class, :events).freeze
+    EventGroup = Struct.new(:event_class, :events).freeze
   end
 end
