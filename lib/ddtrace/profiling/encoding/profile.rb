df7d67acfcbf3bc1c26fd5acf0a668b82cd25112
David Elner <david@davidelner.com>
2020-07-10 17:18:10 -0400

    Changed: Profiling::Recorder produces richer Flush object.

--- lib/ddtrace/profiling/encoding/profile.rb
+++ lib/ddtrace/profiling/encoding/profile.rb
@@ -11,15 +11,15 @@ module Datadog
         module Protobuf
           module_function
 
-          def encode(flushes)
-            return if flushes.empty?
+          def encode(flush)
+            return unless flush
 
             # Create a pprof template from the list of event types
-            event_classes = flushes.collect(&:event_class).uniq
+            event_classes = flush.event_groups.collect(&:event_class).uniq
             template = Pprof::Template.for_event_classes(event_classes)
 
             # Add all events to the pprof
-            flushes.each { |flush| template.add_events!(flush.event_class, flush.events) }
+            flush.event_groups.each { |event_group| template.add_events!(event_group.event_class, event_group.events) }
 
             # Build the profile and encode it
             template.to_encoded_profile
