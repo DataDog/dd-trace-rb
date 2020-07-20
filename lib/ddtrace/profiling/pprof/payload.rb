6a507c7b7c8b1a50240341e065934dc8f9e6a61d
David Elner <david@davidelner.com>
2020-07-20 14:30:02 -0400

    Changed: PProf encoding yields Struct with data & types instead of String.

---
+++ lib/ddtrace/profiling/pprof/payload.rb
@@ -0,0 +1,18 @@
+module Datadog
+  module Profiling
+    module Pprof
+      # Pprof output data.
+      # Includes encoded data and list of types.
+      Payload = Struct.new(:data, :types) do
+        def initialize(*args)
+          super
+          self.types = types || []
+        end
+
+        def to_s
+          data
+        end
+      end
+    end
+  end
+end
