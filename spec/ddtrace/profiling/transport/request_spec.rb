80bf50f99b1ad56d06d0e4203189c0fa0860685c
David Elner <david@davidelner.com>
2020-07-21 17:22:04 -0400

    Added: HTTP transport for Profiling

--- spec/ddtrace/profiling/transport/request_spec.rb
+++ spec/ddtrace/profiling/transport/request_spec.rb
@@ -1,14 +1,16 @@
 require 'spec_helper'
 
+require 'ddtrace/profiling/flush'
 require 'ddtrace/profiling/transport/request'
 
 RSpec.describe Datadog::Profiling::Transport::Request do
-  subject(:request) { described_class.new(events) }
-  let(:events) { instance_double(Array) }
+  subject(:request) { described_class.new(flush) }
+  let(:flush) { instance_double(Datadog::Profiling::Flush) }
 
   it { is_expected.to be_a_kind_of(Datadog::Transport::Request) }
 
   describe '#initialize' do
     it { is_expected.to have_attributes(parcel: kind_of(Datadog::Profiling::Transport::Parcel)) }
+    it { expect(request.parcel.data).to be(flush) }
   end
 end
