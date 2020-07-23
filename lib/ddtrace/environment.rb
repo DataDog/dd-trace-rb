3a8bcb04e220328083ccd789db84189692798add
ericmustin <mustin.eric@gmail.com>
2020-07-23 23:48:13 +0200

    [fix]: env var key lookup check for nil

--- lib/ddtrace/environment.rb
+++ lib/ddtrace/environment.rb
@@ -7,7 +7,7 @@ module Datadog
     module Helpers
       def env_to_bool(var, default = nil)
         var = var.find { |env_var| ENV.key?(env_var) } if var.is_a?(Array)
-        ENV.key?(var) ? ENV[var].to_s.strip.downcase == 'true' : default
+        var && ENV.key?(var) ? ENV[var].to_s.strip.downcase == 'true' : default
       end
 
       def env_to_int(var, default = nil)
@@ -16,7 +16,7 @@ module Datadog
 
       def env_to_float(var, default = nil)
         var = var.find { |env_var| ENV.key?(env_var) } if var.is_a?(Array)
-        ENV.key?(var) ? ENV[var].to_f : default
+        var && ENV.key?(var) ? ENV[var].to_f : default
       end
 
       def env_to_list(var, default = [])
