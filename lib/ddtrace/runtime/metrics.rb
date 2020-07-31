13bb70eac69f5907759781f1786e4a2f19a57555
Marco Costa <marco.costa@datadoghq.com>
2020-07-31 14:50:19 -0400

    Fix Hash warning on nil array elements

--- lib/ddtrace/runtime/metrics.rb
+++ lib/ddtrace/runtime/metrics.rb
@@ -59,7 +59,7 @@ module Datadog
             next if v.is_a?(Hash) # TODO: JRuby supports additional nested metrics
 
             ["#{Ext::Runtime::Metrics::METRIC_GC_PREFIX}.#{k}", v]
-          end
+          end.compact
         ]
       end
 
