39c8d4e89b141145d14188f5c7b019c55a5ebbab
ericmustin <mustin.eric@gmail.com>
2020-08-10 11:50:32 +0200

    [poc]: add more robust rails tests for different controllers

--- spec/ddtrace/contrib/rails/rum_injection_middleware_spec.rb
+++ spec/ddtrace/contrib/rails/rum_injection_middleware_spec.rb
@@ -4,13 +4,22 @@ RSpec.describe 'Rails Rack Rum Injection middleware' do
   include Rack::Test::Methods
   include_context 'Rails test application'
 
-  let(:routes) { { '/' => 'rum_test#index' } }
-  let(:controllers) { [controller] }
+  let(:routes) { { '/' => 'rum_test#index', '/cached_page' => 'rum_test_cached_page#index' } }
+  let(:controllers) { [controller, cached_page_controller] }
 
   let(:controller) do
     stub_const('RumTestController', Class.new(ActionController::Base) do
       def index
         response.headers['Cache-Control'] = 'max-age=0'
+        render inline: '<html> <head> </head> <body> <div> Hello from index </div> </body> </html>'
+      end
+    end)
+  end
+
+  let(:cached_page_controller) do
+    stub_const('RumTestCachedPageController', Class.new(ActionController::Base) do
+      def index
+        response.headers['Cache-Control'] = 'max-age=60'
         render inline: '<html> <head> </head> <body> <div> Hello from index </div> </body> </html>'
       end
     end)
@@ -32,11 +41,50 @@ RSpec.describe 'Rails Rack Rum Injection middleware' do
   end
 
   context 'with RumInjection middleware' do
-    context 'injects rum related trace_id when enabled' do
+    context 'with no caching from controller' do
       subject(:response) { get '/' }
 
       it 'should inject trace_id into html response' do
         expect(response.body).to include(spans[0].trace_id.to_s)
+      end
+    end
+
+    context 'with caching from controller' do
+      subject(:response) { get '/cached_page' }
+
+      it 'should not inject trace_id into html response' do
+        expect(response.body).to_not include(spans[0].trace_id.to_s)
+      end
+    end
+
+    if Rails.version >= '3.2'
+      context 'with stream from controller' do
+        # i think this is when ActionControlller::Streaming was introduced
+        # ActionController::Live was in >=5
+
+        let(:streaming_page_controller) do
+          stub_const('RumTestStreamingPageController', Class.new(ActionController::Base) do
+            def index
+              response.headers['Cache-Control'] = 'max-age=0'
+              render inline: '<html> <head> </head> <body> <div> Hello from index </div> </body> </html>', stream: true
+            end
+          end)
+        end
+
+        let(:controllers) { [controller, cached_page_controller, streaming_page_controller] }
+        let(:routes) do
+          {
+            '/' => 'rum_test#index',
+            '/cached_page' => 'rum_test_cached_page#index',
+            '/streaming_page' => 'rum_test_streaming_page#index'
+          }
+        end
+
+        subject(:response) { get '/streaming_page' }
+
+        it 'should not inject trace_id into streamed html response regardless of caching behavior' do
+          expect(response.body).to_not include(spans[0].trace_id.to_s)
+        end
       end
     end
   end
