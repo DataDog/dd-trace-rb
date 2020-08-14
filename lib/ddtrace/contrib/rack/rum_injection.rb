372e7975e6a71e0228f761459f57e80a24337d03
ericmustin <mustin.eric@gmail.com>
2020-08-14 12:57:14 +0200

    [poc]: fix typos

--- lib/ddtrace/contrib/rack/rum_injection.rb
+++ lib/ddtrace/contrib/rack/rum_injection.rb
@@ -139,7 +139,7 @@ module Datadog
             !streaming?(headers, env) &&
             injectable_html?(headers) &&
             no_cache?(headers) &&
-            !user_defined_cached(env)
+            !user_defined_cached?(env)
         # catch everything and swallow it here for defensiveness
         rescue Exception => e # rubocop:disable Lint/RescueException
           Datadog.logger.warn("Error during rum injection  #{e.class}: #{e.message} #{e.backtrace.join("\n")}")
