bb9cbe565400bd82ba14f5334cfb26ee53c461d8
David Elner <david@davidelner.com>
2020-08-14 14:29:36 -0400

    Added: Forking extension with at_fork callbacks.

--- lib/ddtrace/profiling/tasks/setup.rb
+++ lib/ddtrace/profiling/tasks/setup.rb
@@ -4,6 +4,19 @@ module Datadog
       # Sets up profiling for the application
       class Setup
         def run
+          activate_main_extensions
+          activate_thread_extensions
+        end
+
+        def activate_main_extensions
+          # Add forking extensions
+          require 'ddtrace/profiling/ext/forking'
+          Ext::Forking.apply!
+        rescue StandardError, ScriptError => e
+          puts "[DDTRACE] Forking extensions unavailable. Cause: #{e.message} Location: #{e.backtrace.first}"
+        end
+
+        def activate_thread_extensions
           # Activate CPU timings
           require 'ddtrace/profiling/ext/thread'
           ::Thread.send(:prepend, Profiling::Ext::CThread)
