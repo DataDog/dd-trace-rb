b76b19f55d3948649b06f058be0768cbbde2ac2e
Bertan Guven <bertan@homestars.com>
2020-08-26 02:14:12 -0400

    readme update, tracer style update

--- lib/ddtrace/contrib/que/tracer.rb
+++ lib/ddtrace/contrib/que/tracer.rb
@@ -8,16 +8,28 @@ module Datadog
       # Tracer is a Que's server-side middleware which traces executed jobs
       class Tracer
         def call(job)
-          request_span = generate_spans(job)
-          set_sample_rate(request_span)
-          Contrib::Analytics.set_measured(request_span)
+          trace_options = {
+            service:   configuration[:service_name],
+            span_type: Datadog::Ext::AppTypes::WORKER
+          }
 
-          yield
-        rescue StandardError => e
-          request_span.set_error(e) unless request_span.nil?
-          raise e
-        ensure
-          request_span.finish if request_span
+          tracer.trace(Ext::SPAN_JOB, trace_options) do |request_span|
+            request_span.resource = job.class.name.to_s
+            request_span.set_tag(Ext::TAG_JOB_QUEUE, job.que_attrs[:queue])
+            request_span.set_tag(Ext::TAG_JOB_ID, job.que_attrs[:id])
+            request_span.set_tag(Ext::TAG_JOB_PRIORITY, job.que_attrs[:priority])
+            request_span.set_tag(Ext::TAG_JOB_ERROR_COUNT, job.que_attrs[:error_count])
+            request_span.set_tag(Ext::TAG_JOB_RUN_AT, job.que_attrs[:run_at])
+            request_span.set_tag(Ext::TAG_JOB_EXPIRED_AT, job.que_attrs[:expired_at])
+            request_span.set_tag(Ext::TAG_JOB_FINISHED_AT, job.que_attrs[:finished_at])
+            request_span.set_tag(Ext::TAG_JOB_ARGS, job.que_attrs[:args]) if configuration[:tag_args]
+            request_span.set_tag(Ext::TAG_JOB_DATA, job.que_attrs[:data]) if configuration[:tag_data]
+
+            set_sample_rate(request_span)
+            Contrib::Analytics.set_measured(request_span)
+
+            yield
+          end
         end
 
         private
@@ -29,27 +41,6 @@ module Datadog
               configuration[:analytics_sample_rate]
             )
           end
-        end
-
-        def generate_spans(job)
-          trace_options = {
-            service:   configuration[:service_name],
-            span_type: Datadog::Ext::AppTypes::WORKER
-          }
-          request_span = tracer.trace(Ext::SPAN_JOB, trace_options)
-
-          request_span.resource = job.class.name.to_s
-          request_span.set_tag(Ext::TAG_JOB_QUEUE, job.que_attrs[:queue])
-          request_span.set_tag(Ext::TAG_JOB_ID, job.que_attrs[:id])
-          request_span.set_tag(Ext::TAG_JOB_PRIORITY, job.que_attrs[:priority])
-          request_span.set_tag(Ext::TAG_JOB_ERROR_COUNT, job.que_attrs[:error_count])
-          request_span.set_tag(Ext::TAG_JOB_RUN_AT, job.que_attrs[:run_at])
-          request_span.set_tag(Ext::TAG_JOB_EXPIRED_AT, job.que_attrs[:expired_at])
-          request_span.set_tag(Ext::TAG_JOB_FINISHED_AT, job.que_attrs[:finished_at])
-          request_span.set_tag(Ext::TAG_JOB_ARGS, job.que_attrs[:args]) if configuration[:tag_args]
-          request_span.set_tag(Ext::TAG_JOB_DATA, job.que_attrs[:data]) if configuration[:tag_data]
-
-          request_span
         end
 
         def tracer
