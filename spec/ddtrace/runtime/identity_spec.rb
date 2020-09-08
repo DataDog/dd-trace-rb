45b0bf6804542b5538a7c3e543eb6f94bf0ad01c
David Elner <david@davidelner.com>
2020-09-08 14:41:37 -0400

    Added: #lang_engine and #lang_platform to Identity.

--- spec/ddtrace/runtime/identity_spec.rb
+++ spec/ddtrace/runtime/identity_spec.rb
@@ -35,4 +35,34 @@ RSpec.describe Datadog::Runtime::Identity do
       end
     end
   end
+
+  describe '::lang' do
+    subject(:lang) { described_class.lang }
+    it { is_expected.to eq(Datadog::Ext::Runtime::LANG) }
+  end
+
+  describe '::lang_engine' do
+    subject(:lang_engine) { described_class.lang_engine }
+    it { is_expected.to eq(Datadog::Ext::Runtime::LANG_ENGINE) }
+  end
+
+  describe '::lang_interpreter' do
+    subject(:lang_interpreter) { described_class.lang_interpreter }
+    it { is_expected.to eq(Datadog::Ext::Runtime::LANG_INTERPRETER) }
+  end
+
+  describe '::lang_platform' do
+    subject(:lang_platform) { described_class.lang_platform }
+    it { is_expected.to eq(Datadog::Ext::Runtime::LANG_PLATFORM) }
+  end
+
+  describe '::lang_version' do
+    subject(:lang_version) { described_class.lang_version }
+    it { is_expected.to eq(Datadog::Ext::Runtime::LANG_VERSION) }
+  end
+
+  describe '::tracer_version' do
+    subject(:tracer_version) { described_class.tracer_version }
+    it { is_expected.to eq(Datadog::Ext::Runtime::TRACER_VERSION) }
+  end
 end
