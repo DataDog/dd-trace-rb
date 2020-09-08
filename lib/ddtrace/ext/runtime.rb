0bbad650363d1076384515c265e02970040724c8
David Elner <david@davidelner.com>
2020-09-08 14:41:37 -0400

    Added: #lang_engine and #lang_platform to Identity.

--- lib/ddtrace/ext/runtime.rb
+++ lib/ddtrace/ext/runtime.rb
@@ -5,7 +5,9 @@ module Datadog
     module Runtime
       # Identity
       LANG = 'ruby'.freeze
+      LANG_ENGINE = RUBY_ENGINE
       LANG_INTERPRETER = (RUBY_ENGINE + '-' + RUBY_PLATFORM).freeze
+      LANG_PLATFORM = RUBY_PLATFORM
       LANG_VERSION = RUBY_VERSION
       RUBY_ENGINE =  ::RUBY_ENGINE # e.g. 'ruby', 'jruby', 'truffleruby'
       TRACER_VERSION = Datadog::VERSION::STRING
