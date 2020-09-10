c375212a6ca1c90f6b20046f021406cd04dd9e20
David Elner <david@davidelner.com>
2020-09-10 19:21:58 -0400

    Added: Profiling::Buffer#string_table and #cache

--- lib/ddtrace/profiling/buffer.rb
+++ lib/ddtrace/profiling/buffer.rb
@@ -1,9 +1,41 @@
 require 'ddtrace/buffer'
+require 'ddtrace/utils/string_table'
+require 'ddtrace/utils/object_set'
 
 module Datadog
   module Profiling
     # Profiling buffer that stores profiling events. The buffer has a maximum size and when
     # the buffer is full, a random event is discarded. This class is thread-safe.
-    class Buffer < Datadog::ThreadSafeBuffer; end
+    class Buffer < Datadog::ThreadSafeBuffer
+      def initialize(*args)
+        super
+        @caches = {}
+        @string_table = Utils::StringTable.new
+      end
+
+      def cache(cache_name)
+        synchronize do
+          @caches[cache_name] ||= Utils::ObjectSet.new
+        end
+      end
+
+      def string_table
+        synchronize do
+          @string_table
+        end
+      end
+
+      protected
+
+      def drain!
+        items = super
+
+        # Clear caches
+        @caches = {}
+        @string_table = Utils::StringTable.new
+
+        items
+      end
+    end
   end
 end
