b4c6f7ac1da731597590bbbacb1a6b731eaff1bd
David Elner <david@davidelner.com>
2020-11-09 16:16:33 -0500

    Added: #uuid_regex to spec helpers.

--- spec/support/container_helpers.rb
+++ spec/support/container_helpers.rb
@@ -2,6 +2,10 @@ require 'stringio'
 
 # rubocop:disable Metrics/ModuleLength
 module ContainerHelpers
+  def uuid_regex
+    /[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}/
+  end
+
   shared_context 'cgroup file' do
     let(:cgroup_file) { StringIO.new }
 
