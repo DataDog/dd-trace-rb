51e92bba006eb00298184852502443b90f0505ab
Kyle Neale <kyle.a.neale@gmail.com>
2020-11-10 11:28:30 -0500

    Update lib/ddtrace/contrib/grape/endpoint.rb
    Co-authored-by: Eric Mustin <mustin.eric@gmail.com>

--- lib/ddtrace/contrib/grape/endpoint.rb
+++ lib/ddtrace/contrib/grape/endpoint.rb
@@ -226,19 +226,23 @@ module Datadog
           end
 
           def set_range
-            set = Set.new
-            handle_statuses.each do |statuses|
-              status = statuses.to_s.split('-')
-              if status.length == 1
-                set.add(Integer(status[0]))
-              elsif status.length == 2
-                min, max = status.minmax
-                Array(min..max).each do |i|
-                  set.add(Integer(i))
+            @datadog_set ||= begin
+              set = Set.new
+              handle_statuses.each do |statuses|
+                status = statuses.to_s.split('-')
+                if status.length == 1
+                  set.add(Integer(status[0]))
+                elsif status.length == 2
+                  min, max = status.minmax
+                  Array(min..max).each do |i|
+                    set.add(Integer(i))
+                  end
                 end
               end
+              set
             end
-            set
+
+            @datadog_set
           end
 
           def exception_is_error?(exception)
