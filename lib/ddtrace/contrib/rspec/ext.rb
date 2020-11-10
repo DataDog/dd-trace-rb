c34e1e00ec74632e49a0a4d11894433f8501c89f
Jiri Kuncar <jiri.kuncar@datadoghq.com>
2020-11-10 15:17:01 +0100

    Update lib/ddtrace/contrib/rspec/ext.rb
    Co-authored-by: Marco Costa <marco.costa@datadoghq.com>

--- lib/ddtrace/contrib/rspec/ext.rb
+++ lib/ddtrace/contrib/rspec/ext.rb
@@ -9,8 +9,8 @@ module Datadog
         ENV_ENABLED = 'DD_TRACE_RSPEC_ENABLED'.freeze
         ENV_OPERATION_NAME = 'DD_TRACE_RSPEC_OPERATION_NAME'.freeze
         FRAMEWORK = 'rspec'.freeze
-        OPERATION_NAME = 'rspec.it'.freeze
-        EXAMPLE_GROUP_OPERATION_NAME = 'rspec.example'.freeze
+        OPERATION_NAME = 'rspec.example'.freeze
+        EXAMPLE_GROUP_OPERATION_NAME = 'rspec.example_group'.freeze
         SERVICE_NAME = 'rspec'.freeze
         TEST_TYPE = 'test'.freeze
       end
