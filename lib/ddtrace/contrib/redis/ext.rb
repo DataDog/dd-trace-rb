490d62b021f95cdaa867877b135fc9f6b26a134c
Callum Jones <callum.jones@airbnb.com>
2020-12-08 14:41:42 -0800

    fix

--- lib/ddtrace/contrib/redis/ext.rb
+++ lib/ddtrace/contrib/redis/ext.rb
@@ -11,7 +11,6 @@ module Datadog
         ENV_ANALYTICS_SAMPLE_RATE_OLD = 'DD_REDIS_ANALYTICS_SAMPLE_RATE'.freeze
         ENV_COMMAND_ARGS = 'DD_REDIS_COMMAND_ARGS'.freeze
         METRIC_PIPELINE_LEN = 'redis.pipeline_length'.freeze
-        PIPELINE_RESOURCE = 'PIPELINE'.freeze
         SERVICE_NAME = 'redis'.freeze
         SPAN_COMMAND = 'redis.command'.freeze
         TAG_DB = 'out.redis_db'.freeze
