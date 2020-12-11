0c55ddab845b0226e1858e981c0aa8820356cdd5
ericmustin <mustin.eric@gmail.com>
2020-12-11 16:00:10 +0100

    more spec fixes

--- spec/ddtrace/auto_instrument_spec.rb
+++ spec/ddtrace/auto_instrument_spec.rb
@@ -82,7 +82,7 @@ RSpec.describe 'Auto Instrumentation of non Rails' do
       expect(sqlite_span.service).to eq('sqlite')
       expect(sqlite_span.resource).to eq('SELECT 42')
       expect(sqlite_span.get_tag('active_record.db.vendor')).to eq('sqlite')
-      expect(sqlite_span.get_tag('active_record.db.name')).to eq(database_name)
+      expect(sqlite_span.get_tag('active_record.db.name')).to eq(':memory:')
       expect(sqlite_span.get_tag('out.host')).to eq(adapter_host.to_s) unless adapter_host.nil?
       expect(sqlite_span.get_tag('out.port')).to eq(adapter_port.to_s) unless adapter_port.nil?
       expect(sqlite_span.span_type).to eq(Datadog::Ext::SQL::TYPE)
