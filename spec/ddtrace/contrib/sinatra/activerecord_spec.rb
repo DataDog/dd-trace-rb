1f036eb8a6944eeb460080490b82b3205f852096
ericmustin <mustin.eric@gmail.com>
2020-12-11 15:57:05 +0100

    fix specs for 6.1 workaround

--- spec/ddtrace/contrib/sinatra/activerecord_spec.rb
+++ spec/ddtrace/contrib/sinatra/activerecord_spec.rb
@@ -91,7 +91,7 @@ RSpec.describe 'Sinatra instrumentation with ActiveRecord' do
       expect(sqlite_span.name).to eq('sqlite.query')
       expect(sqlite_span.service).to eq('sqlite')
       expect(sqlite_span.resource).to eq('SELECT 42')
-      expect(sqlite_span.get_tag('active_record.db.vendor')).to eq('sqlite3')
+      expect(sqlite_span.get_tag('active_record.db.vendor')).to eq('sqlite')
       expect(sqlite_span.get_tag('active_record.db.name')).to eq(database_name)
       expect(sqlite_span.get_tag('out.host')).to eq(adapter_host.to_s) unless adapter_host.nil?
       expect(sqlite_span.get_tag('out.port')).to eq(adapter_port.to_s) unless adapter_port.nil?
