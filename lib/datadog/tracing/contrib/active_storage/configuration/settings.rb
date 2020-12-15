ac138476c947bec4a57aebdee2d458a5cec7fbe0
ericmustin <mustin.eric@gmail.com>
2020-12-15 17:17:07 -0500

    add initial active storage integration skeleton
    moved and re-namespaced everything

---
+++ lib/datadog/tracing/contrib/active_storage/configuration/settings.rb
@@ -0,0 +1,32 @@
+require 'datadog/tracing/contrib/configuration/settings'
+require 'datadog/tracing/contrib/active_storage/ext'
+
+module Datadog
+  module Tracing
+    module Contrib
+      module ActiveStorge
+        module Configuration
+          # Custom settings for the ActiveStorge integration
+          class Settings < Contrib::Configuration::Settings
+            option :enabled do |o|
+              o.default { env_to_bool(Ext::ENV_ENABLED, true) }
+              o.lazy
+            end
+
+            option :analytics_enabled do |o|
+              o.default { env_to_bool([Ext::ENV_ANALYTICS_ENABLED, Ext::ENV_ANALYTICS_ENABLED_OLD], false) }
+              o.lazy
+            end
+
+            option :analytics_sample_rate do |o|
+              o.default { env_to_float([Ext::ENV_ANALYTICS_SAMPLE_RATE, Ext::ENV_ANALYTICS_SAMPLE_RATE_OLD], 1.0) }
+              o.lazy
+            end
+
+            option :service_name, default: Ext::SERVICE_NAME
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/datadog/tracing/contrib/active_storage/ext.rb
@@ -0,0 +1,38 @@
+module Datadog
+  module Tracing
+    module Contrib
+      module ActiveStorage
+        # ActiveStorage integration constants
+        module Ext
+          APP = 'active_storage'.freeze
+          ENV_ENABLED = 'DD_TRACE_ACTIVE_STORAGE_ENABLED'.freeze
+          ENV_ANALYTICS_ENABLED = 'DD_TRACE_ACTIVE_STORAGE_ANALYTICS_ENABLED'.freeze
+          ENV_ANALYTICS_ENABLED_OLD = 'DD_ACTIVE_STORAGE_ANALYTICS_ENABLED'.freeze
+          ENV_ANALYTICS_SAMPLE_RATE = 'DD_TRACE_ACTIVE_STORAGE_ANALYTICS_SAMPLE_RATE'.freeze
+          ENV_ANALYTICS_SAMPLE_RATE_OLD = 'DD_ACTIVE_STORAGE_ANALYTICS_SAMPLE_RATE'.freeze
+          SERVICE_NAME = 'active_storage'.freeze
+          SPAN_ACTION = 'active_storage.action'.freeze
+          TAG_SERVICE = 'active_storage.service'.freeze
+          TAG_KEY = 'active_storage.key'.freeze
+          TAG_PREFIX = 'active_storage.prefix'.freeze
+          TAG_RANGE = 'active_storage.range'.freeze
+          TAG_URL = 'active_storage.url'.freeze
+          TAG_EXIST = 'active_storage.exist'.freeze
+          TAG_CONTENT_TYPE = 'active_storage.content_type'.freeze
+          TAG_DISPOSITION = 'active_storage.disposition'.freeze
+          ACTION_DELETE = 'delete'.freeze
+          ACTION_DELETE_PREFIXED = 'delete_prefixed'.freeze
+          ACTION_DOWNLOAD = 'download'.freeze
+          ACTION_DOWNLOAD_CHUNK = 'download_chunk'.freeze
+          ACTION_EXIST = 'exist'.freeze
+          ACTION_PREVIEW = 'preview'.freeze
+          ACTION_STREAMING_DOWNLOAD = 'streaming_download'.freeze
+          ACTION_TRANSFORM = 'transform'.freeze
+          ACTION_UPDATE_METADATA = 'update_metadata'.freeze
+          ACTION_UPLOAD = 'upload'.freeze
+          ACTION_URL = 'url'.freeze
+        end
+      end
+    end
+  end
+end
