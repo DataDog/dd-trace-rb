61b451a893c8601b223a0723256e3a3103a66884
ericmustin <mustin.eric@gmail.com>
2020-12-15 17:17:07 -0500

    add initial active storage integration skeleton
    moved and re-namespaced everything

---
+++ lib/datadog/tracing/contrib/active_storage/integration.rb
@@ -0,0 +1,40 @@
+require 'datadog/tracing/contrib/integration'
+require 'datadog/tracing/contrib/active_storage/configuration/settings'
+require 'datadog/tracing/contrib/active_storage/patcher'
+
+module Datadog
+  module Tracing
+    module Contrib
+      module ActiveStorage
+        # Description of ActiveStorage integration
+        class Integration
+          include Contrib::Integration
+
+          MINIMUM_VERSION = Gem::Version.new('5.0.0')
+
+          register_as :active_storage, auto_patch: false
+
+          def self.version
+            Gem.loaded_specs['activestorage'] && Gem.loaded_specs['activestorage'].version
+          end
+
+          def self.loaded?
+            !defined?(::ActiveStorage).nil?
+          end
+
+          def self.compatible?
+            super && version >= MINIMUM_VERSION
+          end
+
+          def default_configuration
+            Configuration::Settings.new
+          end
+
+          def patcher
+            Patcher
+          end
+        end
+      end
+    end
+  end
+end
