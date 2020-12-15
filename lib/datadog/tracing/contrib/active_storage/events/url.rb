fd873e1278498cd48ef0bd72545a666ca35af234
ericmustin <mustin.eric@gmail.com>
2020-12-15 17:17:07 -0500

    add initial active storage integration skeleton
    moved and re-namespaced everything

---
+++ lib/datadog/tracing/contrib/active_storage/events/url.rb
@@ -0,0 +1,61 @@
+require 'datadog/tracing/contrib/analytics'
+require 'datadog/tracing/contrib/active_storage/event'
+require 'datadog/tracing/ext/http'
+require 'datadog/tracing/contrib/active_storage/ext'
+
+module Datadog
+  module Tracing
+    module Contrib
+      module ActiveStorage
+        module Events
+          # Defines instrumentation for 'service_url.active_storage' event.
+          #
+          # TODO: Define
+          module Url
+            include ActiveStorage::Event
+
+            EVENT_NAME = 'service_url.active_storage'.freeze
+
+            module_function
+
+            def event_name
+              self::EVENT_NAME
+            end
+
+            def span_name
+              Ext::SPAN_ACTION
+            end
+
+            def span_type
+              # Interacting with a cloud based image service
+              Datadog::Ext::HTTP::TYPE_OUTBOUND
+            end
+
+            def resource_prefix
+              Ext::ACTION_URL
+            end
+
+            def process(span, _event, _id, payload)
+              as_key = payload[:key]
+              as_service = payload[:service]
+              as_url = payload[:url]
+
+              span.service = configuration[:service_name]
+              span.resource = "#{resource_prefix} #{as_service}"
+              span.span_type = span_type
+
+              # Set analytics sample rate
+              if Contrib::Analytics.enabled?(configuration[:analytics_enabled])
+                Contrib::Analytics.set_sample_rate(span, configuration[:analytics_sample_rate])
+              end
+
+              span.set_tag(Ext::TAG_SERVICE, as_service)
+              span.set_tag(Ext::TAG_KEY, as_key)
+              span.set_tag(Ext::TAG_URL, as_url)
+            end
+          end
+        end
+      end
+    end
+  end
+end
