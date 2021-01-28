6392d76c29f0d50d69706ea73b8d4c76edfbcf01
Marco Costa <marco.costa@datadoghq.com>
2021-01-28 11:29:19 -0500

    Add check for initial condition

--- spec/ddtrace_integration_spec.rb
+++ spec/ddtrace_integration_spec.rb
@@ -22,7 +22,10 @@ RSpec.describe 'ddtrace integration' do
       end
 
       it 'closes tracer file descriptors' do
+        try_wait_until { thread_count > original_thread_count }
+
         shutdown
+
         expect(thread_count).to eq(original_thread_count)
       end
     end
@@ -40,7 +43,12 @@ RSpec.describe 'ddtrace integration' do
       end
 
       it 'closes tracer file descriptors' do
+        # There's currently a short period of time when we open
+        # a socket to send traces to the agent.
+        try_wait_until(attempts: 5000, backoff: 0.001) { fd_count > original_fd_count }
+
         shutdown
+
         expect(fd_count).to eq(original_fd_count)
       end
     end
