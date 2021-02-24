b233d53e8f8a84ecd2af18987587ce35f142dd82
Adrian Lopez <adrian.lopezcalvo@datadoghq.com>
2021-02-24 16:40:29 +0100

    Rework rspec instrumentation as separate traces

--- spec/ddtrace/contrib/rspec/instrumentation_spec.rb
+++ spec/ddtrace/contrib/rspec/instrumentation_spec.rb
@@ -10,8 +10,6 @@ RSpec.describe 'RSpec hooks' do
   let(:configuration_options) { {} }
 
   around do |example|
-    # Datadog.configuration.reset!
-
     old_configuration = ::RSpec.configuration
     ::RSpec.configuration = ::RSpec::Core::Configuration.new
 
@@ -24,37 +22,179 @@ RSpec.describe 'RSpec hooks' do
     Datadog.configuration.reset!
   end
 
-  context 'executing a test suite' do
-    it 'creates spans for each example' do
-      examples = {}
-      group = RSpec.describe 'group' do
-        it 'foo' do
-          examples['foo'] = Datadog.configuration[:rspec][:tracer].active_span
-          expect(1).to eq(1)
-        end
+  it 'creates span for example' do
+    spans = []
+    spec = RSpec.describe 'some test' do
+      let(:active_span) { Datadog.configuration[:rspec][:tracer].active_span }
 
-        it 'bar' do
-          examples['bar'] = Datadog.configuration[:rspec][:tracer].active_span
-          expect(1).to eq(2)
+      it 'foo' do
+        spans << active_span
+      end
+    end
+    spec.run
+
+    expect(spans.count).to eq(1)
+    span = spans.first
+    expect(span.span_type).to eq(Datadog::Ext::AppTypes::TEST)
+    expect(span.name).to eq(Datadog::Contrib::RSpec::Ext::OPERATION_NAME)
+    expect(span.resource).to eq('some test foo')
+    expect(span.get_tag(Datadog::Ext::Test::TAG_NAME)).to eq('some test foo')
+    expect(span.get_tag(Datadog::Ext::Test::TAG_SUITE)).to eq(spec.file_path)
+    expect(span.get_tag(Datadog::Ext::Test::TAG_SPAN_KIND)).to eq(Datadog::Ext::AppTypes::TEST)
+    expect(span.get_tag(Datadog::Ext::Test::TAG_TYPE)).to eq(Datadog::Contrib::RSpec::Ext::TEST_TYPE)
+    expect(span.get_tag(Datadog::Ext::Test::TAG_FRAMEWORK)).to eq(Datadog::Contrib::RSpec::Ext::FRAMEWORK)
+    expect(span.get_tag(Datadog::Ext::Test::TAG_STATUS)).to eq(Datadog::Ext::Test::Status::PASS)
+  end
+
+  it 'creates spans for several examples' do
+    spans = []
+    num_examples = 20
+    spec = RSpec.describe 'many tests' do
+      let(:active_span) { Datadog.configuration[:rspec][:tracer].active_span }
+
+      num_examples.times do |n|
+        it n do
+          spans << active_span
         end
       end
+    end
+    spec.run
 
-      group.run
+    expect(spans.count).to eq(num_examples)
+  end
 
-      expect(examples.count).to eq(2)
-      expect(spans.count).to eq(3)
+  it 'creates span for unnamed examples' do
+    spans = []
+    spec = RSpec.describe 'some unnamed test' do
+      let(:active_span) { Datadog.configuration[:rspec][:tracer].active_span }
 
-      group_span = spans.find { |s| s.resource == 'group' }
-      foo_span = examples['foo']
-      bar_span = examples['bar']
+      it { spans << active_span }
+    end
+    spec.run
 
-      expect(group_span.service).to eq(Datadog::Contrib::RSpec::Ext::SERVICE_NAME)
-      expect(group_span.span_type).to eq(Datadog::Ext::AppTypes::TEST)
-      expect(group_span.name).to eq(Datadog::Contrib::RSpec::Ext::EXAMPLE_GROUP_OPERATION_NAME)
-      expect(foo_span.span_type).to eq(Datadog::Ext::AppTypes::TEST)
-      expect(foo_span.name).to eq(Datadog::Contrib::RSpec::Ext::OPERATION_NAME)
-      expect(bar_span.span_type).to eq(Datadog::Ext::AppTypes::TEST)
-      expect(bar_span.name).to eq(Datadog::Contrib::RSpec::Ext::OPERATION_NAME)
+    expect(spans.count).to eq(1)
+    span = spans.first
+    expect(span.get_tag(Datadog::Ext::Test::TAG_NAME)).to match(/some unnamed test example at .+/)
+  end
+
+  it 'creates span for deeply nested examples' do
+    spans = []
+    spec = RSpec.describe 'some nested test' do
+      let(:active_span) { Datadog.configuration[:rspec][:tracer].active_span }
+
+      context '1' do
+        context '2' do
+          context '3' do
+            context '4' do
+              context '5' do
+                context '6' do
+                  context '7' do
+                    context '8' do
+                      context '9' do
+                        context '10' do
+                          it 'foo' do
+                            spans << active_span
+                          end
+                        end
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+    spec.run
+
+    expect(spans.count).to eq(1)
+    span = spans.first
+    expect(span.resource).to eq('some nested test 1 2 3 4 5 6 7 8 9 10 foo')
+    expect(span.get_tag(Datadog::Ext::Test::TAG_NAME)).to eq('some nested test 1 2 3 4 5 6 7 8 9 10 foo')
+    expect(span.get_tag(Datadog::Ext::Test::TAG_SUITE)).to eq(spec.file_path)
+  end
+
+  context 'catches failures' do
+    def expect_failure
+      expect(spans.count).to eq(1)
+      span = spans.first
+      expect(span.get_tag(Datadog::Ext::Test::TAG_STATUS)).to eq(Datadog::Ext::Test::Status::FAIL)
+      expect(span).to have_error
+      expect(span).to have_error_type
+      expect(span).to have_error_message
+      expect(span).to have_error_stack
+    end
+
+    it 'within let' do
+      spans = []
+      spec = RSpec.describe 'some failed test with let' do
+        let(:active_span) { Datadog.configuration[:rspec][:tracer].active_span }
+        let(:let_failure) { raise 'failure' }
+
+        it 'foo' do
+          spans << active_span
+          let_failure
+        end
+      end
+      spec.run
+
+      expect_failure
+    end
+
+    it 'within around' do
+      spans = []
+      spec = RSpec.describe 'some failed test with around' do
+        let(:active_span) { Datadog.configuration[:rspec][:tracer].active_span }
+
+        around do |example|
+          example.run
+          raise 'failure'
+        end
+
+        it 'foo' do
+          spans << active_span
+        end
+      end
+      spec.run
+
+      expect_failure
+    end
+
+    it 'within before' do
+      spans = []
+      spec = RSpec.describe 'some failed test with before' do
+        let(:active_span) { Datadog.configuration[:rspec][:tracer].active_span }
+
+        before do
+          spans << active_span
+          raise 'failure'
+        end
+
+        it 'foo' do
+        end
+      end
+      spec.run
+
+      expect_failure
+    end
+
+    it 'within after' do
+      spans = []
+      spec = RSpec.describe 'some failed test with before' do
+        let(:active_span) { Datadog.configuration[:rspec][:tracer].active_span }
+
+        after do
+          raise 'failure'
+        end
+
+        it 'foo' do
+          spans << active_span
+        end
+      end
+      spec.run
+
+      expect_failure
     end
   end
 end
