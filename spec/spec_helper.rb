e7f5b06026ad7d2303cf965fe0e5386f361ef43a
Ivo Anjo <ivo.anjo@datadoghq.com>
2021-02-26 12:54:27 +0000

    Add shortcut to disable simplecov
    Simplecov makes running the test suite slow, so it's nice to be able to
    disable it with a shortcut when running a lot of tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,7 @@ require 'rspec/collection_matchers'
 require 'webmock/rspec'
 require 'climate_control'
 
-# Skip for benchmarks, as coverage collection slows them down.
-unless RSpec.configuration.files_to_run.all? { |path| path.include?('/benchmark/') }
+if (ENV['SKIP_SIMPLECOV'] != '1') && !RSpec.configuration.files_to_run.all? { |path| path.include?('/benchmark/') }
   # +SimpleCov.start+ must be invoked before any application code is loaded
   require 'simplecov'
   SimpleCov.start do
