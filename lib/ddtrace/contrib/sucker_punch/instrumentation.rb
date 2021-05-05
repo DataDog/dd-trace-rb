f43932755960757a919da99fe1e6986b15c14fe7
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2021-05-05 17:40:06 +0200

    Fix kwargs passing on Ruby 3.0 for sucker_punch
    The kwargs separation change for Ruby 3.0 is tricky: there are different
    behaviours on 2.6, 2.7, and 3.0. While ruby2_keywords attempts to smooth
    that out, it is due to disappear on 3.2, therefore relying on it
    produces surefire broken code when 3.2 is released and 2.6 EOL'd.
    
    Thus the only future-proof and reliable syntax on Ruby >= 3 is to
    capture the arguments with *args, **kwargs and forward them the same
    way, without ruby2_keywords.
    
    Conversely, the only working syntax on Ruby < 3 is capturing both with
    *args and forwarding them the same way, using ruby2_keywords on 2.7 to
    restore the previous behaviour.
    
    Therefore either dynamic function definition per version is necessary
    because of the syntactic difference in argument passing and forwarding.
    
    An alternative is to cap the gem's maximum Ruby version.

--- lib/ddtrace/contrib/sucker_punch/instrumentation.rb
+++ lib/ddtrace/contrib/sucker_punch/instrumentation.rb
@@ -10,11 +10,11 @@ module Datadog
         module_function
 
         # rubocop:disable Metrics/MethodLength
+        # rubocop:disable Metrics/AbcSize
         def patch!
           # rubocop:disable Metrics/BlockLength
           ::SuckerPunch::Job::ClassMethods.class_eval do
-            alias_method :__run_perform_without_datadog, :__run_perform
-            def __run_perform(*args)
+            def __run_perform_datadog_around
               pin = Datadog::Pin.get_from(::SuckerPunch)
               pin.tracer.provider.context = Datadog::Context.new
 
@@ -29,26 +29,60 @@ module Datadog
                 # Measure service stats
                 Contrib::Analytics.set_measured(span)
 
-                __run_perform_without_datadog(*args)
+                yield
               end
-            rescue => e
-              ::SuckerPunch.__exception_handler.call(e, self, args)
+            end
+            private :__run_perform_datadog_around
+
+            alias_method :__run_perform_without_datadog, :__run_perform
+            if Gem::Version.new(RUBY_VERSION) < Gem::Version.new('3.0')
+              def __run_perform(*args)
+                __run_perform_datadog_around do
+                  __run_perform_without_datadog(*args)
+                end
+              rescue => e
+                ::SuckerPunch.__exception_handler.call(e, self, args)
+              end
+              ruby2_keywords :__run_perform if respond_to?(:ruby2_keywords, true)
+            else
+              def __run_perform(*args, **kwargs)
+                __run_perform_datadog_around do
+                  __run_perform_without_datadog(*args, **kwargs)
+                end
+              rescue => e
+                ::SuckerPunch.__exception_handler.call(e, self, args)
+              end
             end
 
-            alias_method :__perform_async, :perform_async
-            def perform_async(*args)
+            def perform_async_datadog_around
               __with_instrumentation(Ext::SPAN_PERFORM_ASYNC) do |span|
                 span.resource = "ENQUEUE #{self}"
 
                 # Measure service stats
                 Contrib::Analytics.set_measured(span)
 
-                __perform_async(*args)
+                yield
+              end
+            end
+            private :perform_async_datadog_around
+
+            alias_method :__perform_async, :perform_async
+            if Gem::Version.new(RUBY_VERSION) < Gem::Version.new('3.0')
+              def perform_async(*args)
+                perform_async_datadog_around do
+                  __perform_async(*args)
+                end
+              end
+              ruby2_keywords :perform_async if respond_to?(:ruby2_keywords, true)
+            else
+              def perform_async(*args, **kwargs)
+                perform_async_datadog_around do
+                  __perform_async(*args, **kwargs)
+                end
               end
             end
 
-            alias_method :__perform_in, :perform_in
-            def perform_in(interval, *args)
+            def perform_in_datadog_around(interval)
               __with_instrumentation(Ext::SPAN_PERFORM_IN) do |span|
                 span.resource = "ENQUEUE #{self}"
                 span.set_tag(Ext::TAG_PERFORM_IN, interval)
@@ -56,7 +90,24 @@ module Datadog
                 # Measure service stats
                 Contrib::Analytics.set_measured(span)
 
-                __perform_in(interval, *args)
+                yield
+              end
+            end
+            private :perform_in_datadog_around
+
+            alias_method :__perform_in, :perform_in
+            if Gem::Version.new(RUBY_VERSION) < Gem::Version.new('3.0')
+              def perform_in(interval, *args)
+                perform_in_datadog_around(interval) do
+                  __perform_in(interval, *args)
+                end
+              end
+              ruby2_keywords :perform_in if respond_to?(:ruby2_keywords, true)
+            else
+              def perform_in(interval, *args, **kwargs)
+                perform_in_datadog_around(interval) do
+                  __perform_in(interval, *args, **kwargs)
+                end
               end
             end
 
