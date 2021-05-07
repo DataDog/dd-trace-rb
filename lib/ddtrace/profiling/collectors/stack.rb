8b66a66ad699ad44acb5eb80d38b89d96c1b1993
Ivo Anjo <ivo.anjo@datadoghq.com>
2021-05-07 15:44:48 +0100

    Add hack to avoid race with opentelemetry load
    This is all awful

--- lib/ddtrace/profiling/collectors/stack.rb
+++ lib/ddtrace/profiling/collectors/stack.rb
@@ -165,13 +165,17 @@ module Datadog
         def get_trace_identifiers(thread)
           return unless thread.is_a?(::Thread)
 
-          if (defined?(::OpenTelemetry::Context::KEY) &&
-            (current_span = ::OpenTelemetry::Trace.current_span(thread[::OpenTelemetry::Context::KEY])) &&
-            current_span != ::OpenTelemetry::Trace::Span::INVALID)
+          if defined?(::OpenTelemetry::Context::KEY)
+            # Awful hack to make sure we don't race with another thread that's setting up opentelemetry as well
+            require 'opentelemetry/sdk'
 
-            result = [current_span.context.hex_trace_id[16..-1].to_i(16), current_span.context.hex_span_id.to_i(16)]
-            Datadog.logger.debug("Got OpenTelemetry: #{result}")
-            return result
+            current_span = ::OpenTelemetry::Trace.current_span(thread[::OpenTelemetry::Context::KEY])
+
+            if (current_span && current_span != ::OpenTelemetry::Trace::Span::INVALID)
+              result = [current_span.context.hex_trace_id[16..-1].to_i(16), current_span.context.hex_span_id.to_i(16)]
+              Datadog.logger.debug("Got OpenTelemetry: #{result}")
+              return result
+            end
           end
 
           return unless Datadog.respond_to?(:tracer) && Datadog.tracer.respond_to?(:active_correlation)
