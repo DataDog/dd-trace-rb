d5718db0e0babcc8d0d3b82c66b5be670cc379f4
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2021-05-26 13:25:34 +0200

    Propagate trace through Concurrent::Promises.future
    Argument passing is preserved through the composited executor.
    
    Concurrent::Future has been moved to a prepend style to resolve an
    infinite call loop situation.

--- lib/datadog/tracing/contrib/concurrent_ruby/context_composite_executor_service.rb
+++ lib/datadog/tracing/contrib/concurrent_ruby/context_composite_executor_service.rb
@@ -6,7 +6,7 @@ module Datadog
   module Tracing
     module Contrib
       module ConcurrentRuby
-        # wraps existing executor to carry over trace context
+        # Wraps existing executor to carry over trace context
         class ContextCompositeExecutorService
           include Concurrent::ExecutorService
 
@@ -16,16 +16,23 @@ module Datadog
             @composited_executor = composited_executor
           end
 
-          # post method runs the task within composited executor - in a different thread
+          # post method runs the task within composited executor - in a different thread. The original arguments are
+          # captured to be propagated to the composited executor post method
           def post(*args, &task)
             tracer = Tracing.send(:tracer)
             parent_context = tracer.provider.context
+            executor = @composited_executor.is_a?(Symbol) ? Concurrent.executor(@composited_executor) : @composited_executor
 
-            @composited_executor.post(*args) do
+            # Pass the original arguments to the composited executor, which
+            # pushes them (possibly transformed) as block args
+            executor.post(*args) do |*block_args|
               begin
                 original_context = tracer.provider.context
                 tracer.provider.context = parent_context
-                yield
+
+                # Pass the executor-provided block args as they should have been
+                # originally passed without composition, see ChainPromise#on_resolvable
+                yield(*block_args)
               ensure
                 # Restore context in case the current thread gets reused
                 tracer.provider.context = original_context
--- spec/datadog/tracing/contrib/concurrent_ruby/integration_test_spec.rb
+++ spec/datadog/tracing/contrib/concurrent_ruby/integration_test_spec.rb
@@ -1,43 +1,25 @@
-require 'concurrent/future'
+require 'concurrent-ruby' # concurrent-ruby is not modular
 
 require 'datadog/tracing/contrib/support/spec_helper'
 require 'ddtrace'
 require 'spec/support/thread_helpers'
 
 RSpec.describe 'ConcurrentRuby integration tests' do
-  # DEV We save an unmodified copy of Concurrent::Future.
-  let!(:unmodified_future) { ::Concurrent::Future.dup }
   let(:configuration_options) { {} }
   let(:outer_span) { spans.find { |s| s.name == 'outer_span' } }
   let(:inner_span) { spans.find { |s| s.name == 'inner_span' } }
 
-  before(:context) do
-    # Execute an async future to force the eager creation of internal
-    # global threads that are never closed.
-    #
-    # This allows us to separate internal concurrent-ruby threads
-    # from ddtrace threads for leak detection.
-    ThreadHelpers.with_leaky_thread_creation(:concurrent_ruby) do
-      Concurrent::Future.execute {}.value
-    end
-  end
+  # DEV We save an unmodified copy of Concurrent::Future.
+  let!(:unmodified_promises) { ::Concurrent::Promises.dup }
+  let!(:unmodified_future) { ::Concurrent::Future.dup }
 
   # DEV We then restore Concurrent::Future, a dangerous game.
   after do
+    ::Concurrent.send(:remove_const, :Promises)
+    ::Concurrent.const_set('Promises', unmodified_promises)
     ::Concurrent.send(:remove_const, :Future)
     ::Concurrent.const_set('Future', unmodified_future)
     remove_patch!(:concurrent_ruby)
-  end
-
-  subject(:deferred_execution) do
-    outer_span = tracer.trace('outer_span')
-    future = Concurrent::Future.new do
-      tracer.trace('inner_span') {}
-    end
-    future.execute
-
-    future.wait
-    outer_span.finish
   end
 
   shared_examples_for 'deferred execution' do
@@ -58,40 +40,124 @@ RSpec.describe 'ConcurrentRuby integration tests' do
     end
   end
 
-  describe 'patching' do
-    subject(:patch) do
-      Datadog.configure do |c|
-        c.tracing.instrument :concurrent_ruby
+  context 'Concurrent::Promises::Future' do
+    before(:context) do
+      # Execute an async future to force the eager creation of internal
+      # global threads that are never closed.
+      #
+      # This allows us to separate internal concurrent-ruby threads
+      # from ddtrace threads for leak detection.
+      ThreadHelpers.with_leaky_thread_creation(:concurrent_ruby) do
+        Concurrent::Promises.future {}.value
       end
     end
 
-    it 'adds FuturePatch to Future ancestors' do
-      expect { patch }.to change { ::Concurrent::Future.ancestors.map(&:to_s) }
-        .to include('Datadog::Tracing::Contrib::ConcurrentRuby::FuturePatch')
+    subject(:deferred_execution) do
+      outer_span = tracer.trace('outer_span')
+      future = Concurrent::Promises.future do
+        tracer.trace('inner_span') {}
+      end
+
+      future.wait
+      outer_span.finish
     end
-  end
 
-  context 'when context propagation is disabled' do
-    it_behaves_like 'deferred execution'
+    describe 'patching' do
+      subject(:patch) do
+        Datadog.configure do |c|
+          c.use :concurrent_ruby
+        end
+      end
 
-    it 'inner span should not have parent' do
-      deferred_execution
-      expect(inner_span).to be_root_span
-    end
-  end
-
-  context 'when context propagation is enabled' do
-    before do
-      Datadog.configure do |c|
-        c.tracing.instrument :concurrent_ruby
+      it 'adds PromisesFuturePatch to Promises ancestors' do
+        expect { patch }.to change { ::Concurrent::Promises.singleton_class.ancestors.map(&:to_s) }
+                              .to include('Datadog::Contrib::ConcurrentRuby::PromisesFuturePatch')
       end
     end
 
-    it_behaves_like 'deferred execution'
+    context 'when context propagation is disabled' do
+      it_behaves_like 'deferred execution'
 
-    it 'inner span parent should be included in outer span' do
-      deferred_execution
-      expect(inner_span.parent_id).to eq(outer_span.span_id)
+      it 'inner span should not have parent' do
+        deferred_execution
+        expect(inner_span.parent).to be_nil
+      end
+    end
+
+    context 'when context propagation is enabled' do
+      before do
+        Datadog.configure do |c|
+          c.use :concurrent_ruby
+        end
+      end
+
+      it_behaves_like 'deferred execution'
+
+      it 'inner span parent should be included in outer span' do
+        deferred_execution
+        expect(inner_span.parent).to eq(outer_span)
+      end
+    end
+  end
+
+  context 'Concurrent::Future (deprecated)' do
+    before(:context) do
+      # Execute an async future to force the eager creation of internal
+      # global threads that are never closed.
+      #
+      # This allows us to separate internal concurrent-ruby threads
+      # from ddtrace threads for leak detection.
+      ThreadHelpers.with_leaky_thread_creation(:concurrent_ruby) do
+        Concurrent::Future.execute {}.value
+      end
+    end
+
+    subject(:deferred_execution) do
+      outer_span = tracer.trace('outer_span')
+      future = Concurrent::Future.new do
+        tracer.trace('inner_span') {}
+      end
+      future.execute
+
+      future.wait
+      outer_span.finish
+    end
+
+    describe 'patching' do
+      subject(:patch) do
+        Datadog.configure do |c|
+          c.tracing.instrument :concurrent_ruby
+        end
+      end
+
+      it 'adds FuturePatch to Future ancestors' do
+        expect { patch }.to change { ::Concurrent::Future.ancestors.map(&:to_s) }
+                              .to include('Datadog::Tracing::Contrib::ConcurrentRuby::FuturePatch')
+      end
+    end
+
+    context 'when context propagation is disabled' do
+      it_behaves_like 'deferred execution'
+
+      it 'inner span should not have parent' do
+        deferred_execution
+        expect(inner_span).to be_root_span
+      end
+    end
+
+    context 'when context propagation is enabled' do
+      before do
+        Datadog.configure do |c|
+          c.tracing.instrument :concurrent_ruby
+        end
+      end
+
+      it_behaves_like 'deferred execution'
+
+      it 'inner span parent should be included in outer span' do
+        deferred_execution
+        expect(inner_span.parent_id).to eq(outer_span.span_id)
+      end
     end
   end
 end
