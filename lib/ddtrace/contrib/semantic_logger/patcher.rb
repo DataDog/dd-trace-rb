8db78ce400b9e3a5c4258bd26d36bd33af76b46e
ericmustin <mustin.eric@gmail.com>
2021-06-16 09:23:57 -0400

    feature: add semantic logger

---
+++ lib/ddtrace/contrib/semantic_logger/patcher.rb
@@ -0,0 +1,32 @@
+require 'ddtrace/contrib/patcher'
+require 'ddtrace/contrib/lograge/instrumentation'
+require 'ddtrace/utils/only_once'
+
+module Datadog
+  module Contrib
+    # Datadog Lograge integration.
+    module Lograge
+      # Patcher enables patching of 'lograge' module.
+      module Patcher
+        include Contrib::Patcher
+
+        PATCH_ONLY_ONCE = Datadog::Utils::OnlyOnce.new
+
+        module_function
+
+        def patched?
+          PATCH_ONLY_ONCE.ran?
+        end
+
+        def target_version
+          Integration.version
+        end
+
+        # patch applies our patch
+        def patch
+          ::Lograge::LogSubscribers::Base.include(Instrumentation)
+        end
+      end
+    end
+  end
+end
