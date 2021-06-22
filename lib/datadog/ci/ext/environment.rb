70032cf1fdb26de331ef088547eab9466e8da161
Marco Costa <marco.costa@datadoghq.com>
2021-06-22 17:02:28 -0400

    CI-App:Read metadata from local git repository (#1561)

--- lib/datadog/ci/ext/environment.rb
+++ lib/datadog/ci/ext/environment.rb
@@ -2,13 +2,14 @@
 
 require 'ddtrace/ext/git'
 
+require 'open3'
+
 module Datadog
   module CI
     module Ext
       # Defines constants for CI tags
       # rubocop:disable Metrics/ModuleLength:
       module Environment
-        TAG_STAGE_NAME = 'ci.stage.name'
         TAG_JOB_NAME = 'ci.job.name'
         TAG_JOB_URL = 'ci.job.url'
         TAG_PIPELINE_ID = 'ci.pipeline.id'
@@ -16,6 +17,7 @@ module Datadog
         TAG_PIPELINE_NUMBER = 'ci.pipeline.number'
         TAG_PIPELINE_URL = 'ci.pipeline.url'
         TAG_PROVIDER_NAME = 'ci.provider.name'
+        TAG_STAGE_NAME = 'ci.stage.name'
         TAG_WORKSPACE_PATH = 'ci.workspace_path'
 
         PROVIDERS = [
@@ -51,6 +53,11 @@ module Datadog
             end
           else
             tags = {}
+          end
+
+          # Fill out tags from local git as fallback
+          extract_local_git.each do |key, value|
+            tags[key] ||= value
           end
 
           tags.reject { |_, v| v.nil? }
@@ -310,6 +317,108 @@ module Datadog
             Datadog::Ext::Git::TAG_TAG => env['BITRISE_GIT_TAG'],
             Datadog::Ext::Git::TAG_COMMIT_MESSAGE => env['BITRISE_GIT_MESSAGE']
           }
+        end
+
+        def git_commit_users
+          # Get committer and author information in one command.
+          output = exec_git_command("git show -s --format='%an\t%ae\t%at\t%cn\t%ce\t%ct'")
+          return unless output
+
+          fields = output.split("\t").each(&:strip!)
+
+          {
+            author_name: fields[0],
+            author_email: fields[1],
+            # Because we can't get a reliable UTC time from all recent versions of git
+            # We have to rely on converting the date to UTC ourselves.
+            author_date: Time.at(fields[2].to_i).utc.to_datetime.iso8601,
+            committer_name: fields[3],
+            committer_email: fields[4],
+            # Because we can't get a reliable UTC time from all recent versions of git
+            # We have to rely on converting the date to UTC ourselves.
+            committer_date: Time.at(fields[5].to_i).utc.to_datetime.iso8601
+          }
+        rescue => e
+          Datadog.logger.debug("Unable to read git commit users: #{e.message} at #{e.backtrace.first}")
+          nil
+        end
+
+        def git_repository_url
+          exec_git_command('git ls-remote --get-url')
+        rescue => e
+          Datadog.logger.debug("Unable to read git repository url: #{e.message} at #{e.backtrace.first}")
+          nil
+        end
+
+        def git_commit_message
+          exec_git_command('git show -s --format=%s')
+        rescue => e
+          Datadog.logger.debug("Unable to read git commit message: #{e.message} at #{e.backtrace.first}")
+          nil
+        end
+
+        def git_branch
+          exec_git_command('git rev-parse --abbrev-ref HEAD')
+        rescue => e
+          Datadog.logger.debug("Unable to read git branch: #{e.message} at #{e.backtrace.first}")
+          nil
+        end
+
+        def git_commit_sha
+          exec_git_command('git rev-parse HEAD')
+        rescue => e
+          Datadog.logger.debug("Unable to read git commit SHA: #{e.message} at #{e.backtrace.first}")
+          nil
+        end
+
+        def git_tag
+          exec_git_command('git tag --points-at HEAD')
+        rescue => e
+          Datadog.logger.debug("Unable to read git tag: #{e.message} at #{e.backtrace.first}")
+          nil
+        end
+
+        def git_base_directory
+          exec_git_command('git rev-parse --show-toplevel')
+        rescue => e
+          Datadog.logger.debug("Unable to read git base directory: #{e.message} at #{e.backtrace.first}")
+          nil
+        end
+
+        def exec_git_command(cmd)
+          out, status = Open3.capture2e(cmd)
+
+          raise "Failed to run git command #{cmd}: #{out}" unless status.success?
+
+          out.strip! # There's always a "\n" at the end of the command output
+
+          return nil if out.empty?
+
+          out
+        end
+
+        def extract_local_git
+          env = {
+            TAG_WORKSPACE_PATH => git_base_directory,
+            Datadog::Ext::Git::TAG_REPOSITORY_URL => git_repository_url,
+            Datadog::Ext::Git::TAG_COMMIT_SHA => git_commit_sha,
+            Datadog::Ext::Git::TAG_BRANCH => git_branch,
+            Datadog::Ext::Git::TAG_TAG => git_tag,
+            Datadog::Ext::Git::TAG_COMMIT_MESSAGE => git_commit_message
+          }
+
+          if (commit_users = git_commit_users)
+            env.merge!(
+              Datadog::Ext::Git::TAG_COMMIT_AUTHOR_NAME => commit_users[:author_name],
+              Datadog::Ext::Git::TAG_COMMIT_AUTHOR_EMAIL => commit_users[:author_email],
+              Datadog::Ext::Git::TAG_COMMIT_AUTHOR_DATE => commit_users[:author_date],
+              Datadog::Ext::Git::TAG_COMMIT_COMMITTER_NAME => commit_users[:committer_name],
+              Datadog::Ext::Git::TAG_COMMIT_COMMITTER_EMAIL => commit_users[:committer_email],
+              Datadog::Ext::Git::TAG_COMMIT_COMMITTER_DATE => commit_users[:committer_date]
+            )
+          end
+
+          env
         end
 
         def branch_or_tag(branch_or_tag)
