d0d5d5b5a287ea5f1662bc5929936b6aca3bfc36
Ivo Anjo <ivo.anjo@datadoghq.com>
2021-07-12 12:13:42 +0100

    Fix integration test that had bitrotted
    Since the integration environment does not have a `DD_API_KEY` usually
    set, these tests were actually never running.
    
    I've fixed the tests, as well as refactored them to:
    * Make sure that transport construction is tested, even if then later
      the test doesn't run (to allow us to catch issues in transport
      creation earlier)
    * Run the reporting to agent test even when the agentless test can't
      run. Reporting to agent is our primary use case, so we definitely
      should test it! Agentless is mostly irrelevant, so it's ok if it's
      not running in the default setup for the integration environment.

--- spec/ddtrace/profiling/transport/http/integration_spec.rb
+++ spec/ddtrace/profiling/transport/http/integration_spec.rb
@@ -5,18 +5,20 @@ require 'ddtrace'
 require 'ddtrace/profiling/transport/http'
 
 RSpec.describe 'Datadog::Profiling::Transport::HTTP integration tests' do
-  before do
-    skip 'Only runs in fully integrated environment.' unless ENV['TEST_DATADOG_INTEGRATION']
-    skip 'Valid API key must be set.' unless ENV['DD_API_KEY'] && !ENV['DD_API_KEY'].empty?
-  end
-
   describe 'HTTP#default' do
-    subject(:transport) { Datadog::Profiling::Transport::HTTP.default(options, &option_block) }
+    subject(:transport) do
+      Datadog::Profiling::Transport::HTTP.default(
+        profiling_upload_timeout_seconds: settings.profiling.upload.timeout_seconds,
+        **options
+      )
+    end
 
-    let(:options) { {} }
-    let(:option_block) { proc { |_client| } }
+    before do
+      # Make sure the transport is being built correctly, even if we then skip the tests
+      transport
+    end
 
-    it { is_expected.to be_a(Datadog::Profiling::Transport::HTTP::Client) }
+    let(:settings) { Datadog::Configuration::Settings.new }
 
     describe '#send_profiling_flush' do
       subject(:response) { transport.send_profiling_flush(flush) }
@@ -25,17 +27,31 @@ RSpec.describe 'Datadog::Profiling::Transport::HTTP integration tests' do
 
       shared_examples_for 'a successful profile flush' do
         it do
+          skip 'Only runs in fully integrated environment.' unless ENV['TEST_DATADOG_INTEGRATION']
+
           is_expected.to be_a(Datadog::Profiling::Transport::HTTP::Response)
           expect(response.code).to eq(200).or eq(403)
         end
       end
 
       context 'agent' do
+        let(:options) { { agent_settings: Datadog::Configuration::AgentSettingsResolver::ENVIRONMENT_AGENT_SETTINGS } }
+
         it_behaves_like 'a successful profile flush'
       end
 
       context 'agentless' do
-        let(:options) { { site: 'datadoghq.com', api_key: ENV['DD_API_KEY'] } }
+        before do
+          skip 'Valid API key must be set.' unless ENV['DD_API_KEY'] && !ENV['DD_API_KEY'].empty?
+        end
+
+        let(:options) do
+          {
+            agent_settings: double('agent_settings which should not be used'),
+            site: 'datadoghq.com',
+            api_key: ENV['DD_API_KEY'] || 'Invalid API key',
+          }
+        end
 
         it_behaves_like 'a successful profile flush'
       end
