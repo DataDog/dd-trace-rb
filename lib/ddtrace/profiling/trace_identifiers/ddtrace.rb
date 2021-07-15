e568f52778af7de71d8c391b0ee93f7a0a05e825
Ivo Anjo <ivo.anjo@datadoghq.com>
2021-07-15 12:46:06 +0100

    Extract resource name from ddtrace HTTP::TYPE_INBOUND traces
    This extends the previous behavior of TraceIdentifiers::Ddtrace to also
    return the root span resource, which will be used to enable
    aggregation-by-endpoint functionality in the profiler.
    
    Due to PII concerns (as explained in the comments), we start with a
    rather conservative filter of only allowing HTTP::TYPE_INBOUND traces.
    
    I took some inspiration from the similar implementation for Python
    (<https://github.com/DataDog/dd-trace-py/pull/2573>) although I still
    need to validate with the wider team that the current filtering
    approach is reasonable for Ruby tracing as well.

--- lib/ddtrace/profiling/trace_identifiers/ddtrace.rb
+++ lib/ddtrace/profiling/trace_identifiers/ddtrace.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'ddtrace/ext/http'
+
 module Datadog
   module Profiling
     module TraceIdentifiers
@@ -7,17 +9,31 @@ module Datadog
       # given thread, if there is an active trace for that thread in Datadog.tracer.
       class Ddtrace
         def initialize(tracer: nil)
-          @tracer = (tracer if tracer.respond_to?(:active_correlation))
+          @tracer = (tracer if tracer.respond_to?(:call_context))
         end
 
         def trace_identifiers_for(thread)
           return unless @tracer
 
-          correlation = @tracer.active_correlation(thread)
-          trace_id = correlation.trace_id
-          span_id = correlation.span_id
+          context = @tracer.call_context(thread)
+          return unless context
 
-          [trace_id, span_id] if trace_id && trace_id != 0 && span_id && span_id != 0
+          trace_id = context.trace_id || 0
+          span_id = context.span_id || 0
+
+          [trace_id, span_id, maybe_extract_resource(context.current_root_span)] if trace_id != 0 && span_id != 0
+        end
+
+        private
+
+        # IMPORTANT NOTE: In many cases a span resource CAN include personal identifiable information (PII),
+        # which we MUST NOT gather in the profiler. To avoid gathering this information, we filter the spans to only
+        # be of types we deem to not have this kind of information. Over time, this list may be expanded, but if in
+        # doubt, don't allow it.
+        def maybe_extract_resource(root_span)
+          return unless root_span
+
+          root_span.resource if root_span.span_type == Datadog::Ext::HTTP::TYPE_INBOUND
         end
       end
     end
