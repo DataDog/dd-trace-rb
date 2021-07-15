d1b4a554df047acd0acfd208452d1a69b41d008b
Ivo Anjo <ivo.anjo@datadoghq.com>
2021-07-15 12:46:06 +0100

    Extract resource container from ddtrace HTTP::TYPE_INBOUND traces
    This extends the previous behavior of TraceIdentifiers::Ddtrace to also
    return the root span resource container, which will be used to enable
    aggregation-by-endpoint functionality in the profiler.

--- lib/ddtrace/profiling/trace_identifiers/ddtrace.rb
+++ lib/ddtrace/profiling/trace_identifiers/ddtrace.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'ddtrace/ext/http'
+
 module Datadog
   module Profiling
     module TraceIdentifiers
@@ -7,17 +9,30 @@ module Datadog
       # given thread, if there is an active trace for that thread in Datadog.tracer.
       class Ddtrace
         def initialize(tracer: nil)
-          @tracer = (tracer if tracer.respond_to?(:active_correlation))
+          @tracer = (tracer if tracer.respond_to?(:call_context))
         end
 
         def trace_identifiers_for(thread)
           return unless @tracer
 
-          correlation = @tracer.active_correlation(thread)
-          trace_id = correlation.trace_id
-          span_id = correlation.span_id
+          context = @tracer.call_context(thread)
+          return unless context
 
-          [trace_id, span_id] if trace_id && trace_id != 0 && span_id && span_id != 0
+          trace_id = context.trace_id || 0
+          span_id = context.span_id || 0
+
+          [trace_id, span_id, maybe_extract_resource(context.current_root_span)] if trace_id != 0 && span_id != 0
+        end
+
+        private
+
+        # NOTE: Currently we're only interested in HTTP service endpoints. Over time, this list may be expanded.
+        # Resources MUST NOT include personal identifiable information (PII); this should not be the case with
+        # ddtrace integrations, but worth mentioning just in case :)
+        def maybe_extract_resource(root_span)
+          return unless root_span
+
+          root_span.resource_container if root_span.span_type == Datadog::Ext::HTTP::TYPE_INBOUND
         end
       end
     end
