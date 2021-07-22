b264e9a95fe4a60aec4e7885e45ac0235f443a45
Marco Costa <marco.costa@datadoghq.com>
2021-07-22 15:29:42 -0700

    Refactor cgroup testing with shared example

--- spec/support/container_helpers.rb
+++ spec/support/container_helpers.rb
@@ -30,6 +30,7 @@ module ContainerHelpers
     include_context 'cgroup file'
 
     let(:platform) { nil }
+    let(:lines) { 13 }
 
     before do
       cgroup_file.puts '12:hugetlb:/'
@@ -54,6 +55,7 @@ module ContainerHelpers
 
     let(:platform) { 'user' }
     let(:terminal_id) { '6fec48c4-1f82-4313-a1c2-29e205a96958' }
+    let(:lines) { 13 }
 
     before do
       cgroup_file.puts '12:hugetlb:/'
@@ -78,6 +80,7 @@ module ContainerHelpers
 
     let(:platform) { 'docker' }
     let(:container_id) { '3726184226f5d3147c25fdeab5b60097e378e8a720503a5e19ecfdf29f869860' }
+    let(:lines) { 13 }
 
     before do
       cgroup_file.puts "13:name=systemd:/#{platform}/#{container_id}"
@@ -103,6 +106,7 @@ module ContainerHelpers
     let(:platform) { 'kubepods' }
     let(:container_id) { '3e74d3fd9db4c9dd921ae05c2502fb984d0cde1b36e581b13f79c639da4518a1' }
     let(:pod_id) { 'pod3d274242-8ee0-11e9-a8a6-1e68d864ef1a' }
+    let(:lines) { 11 }
 
     before do
       cgroup_file.puts "11:perf_event:/#{platform}/besteffort/#{pod_id}/#{container_id}"
@@ -126,6 +130,7 @@ module ContainerHelpers
     let(:platform) { 'kubepods' }
     let(:container_id) { '7b8952daecf4c0e44bbcefe1b5c5ebc7b4839d4eefeccefe694709d3809b6199' }
     let(:pod_id) { 'pod2d3da189_6407_48e3_9ab6_78188d75e609' }
+    let(:lines) { 11 }
 
     before do
       cgroup_file.puts "11:perf_event:/#{platform}.slice/kubepods-burstable.slice/kubepods-burstable-#{pod_id}.slice/docker-#{container_id}.scope"
@@ -149,6 +154,7 @@ module ContainerHelpers
     let(:platform) { 'ecs' }
     let(:container_id) { '38fac3e99302b3622be089dd41e7ccf38aff368a86cc339972075136ee2710ce' }
     let(:task_arn) { '5a0d5ceddf6c44c1928d367a815d890f' }
+    let(:lines) { 9 }
 
     before do
       cgroup_file.puts "9:perf_event:/#{platform}/haissam-ecs-classic/#{task_arn}/#{container_id}"
@@ -170,6 +176,7 @@ module ContainerHelpers
     let(:platform) { 'ecs' }
     let(:container_id) { '432624d2150b349fe35ba397284dea788c2bf66b885d14dfc1569b01890ca7da' }
     let(:task_arn) { '55091c13-b8cf-4801-b527-f4601742204d' }
+    let(:lines) { 11 }
 
     before do
       cgroup_file.puts "11:hugetlb:/#{platform}/#{task_arn}/#{container_id}"
@@ -193,6 +200,7 @@ module ContainerHelpers
     let(:platform) { 'ecs' }
     let(:container_id_with_random) { "#{container_id_without_random}-1234567890" }
     let(:container_id_without_random) { '34dc0b5e626f2c5c4c5170e34b10e765' }
+    let(:lines) { 11 }
 
     before do
       cgroup_file.puts "11:hugetlb:/#{platform}/#{container_id_with_random}"
@@ -216,6 +224,7 @@ module ContainerHelpers
     let(:platform) { 'ecs' }
     let(:container_id_with_random) { "#{container_id_without_random}-1234567890" }
     let(:container_id_without_random) { '34dc0b5e626f2c5c4c5170e34b10e765' }
+    let(:lines) { 11 }
 
     before do
       cgroup_file.puts "11:hugetlb:/#{platform}/#{container_id_without_random}/#{container_id_with_random}"
@@ -240,6 +249,7 @@ module ContainerHelpers
     # Container random ID is normally 10 characters, but we've seen cases with fewer characters in the wild.
     let(:container_id_with_random) { "#{container_id_without_random}-609015642" }
     let(:container_id_without_random) { 'cef584f232933b25e0c6933d7e86cb34' }
+    let(:lines) { 11 }
 
     before do
       cgroup_file.puts "11:hugetlb:/#{platform}/#{container_id_without_random}/#{container_id_with_random}"
@@ -264,6 +274,7 @@ module ContainerHelpers
     let(:task_arn) { 'c101a02b-a99d-4016-b52d-449e313a8087' }
     let(:host_container_id) { '746a2eb2aa309b64c604f5f798c2df761a92ba9e80ad1948edc5f1c63e2e125e' }
     let(:child_container_id) { '3fb61c869147b933a1c459a87eb11429f640e1600e4c9c7f38a9fe39ecbc5d2a' }
+    let(:lines) { 10 }
 
     before do
       cgroup_file.puts "10:pids:/#{platform}/#{task_arn}/#{host_container_id}/docker/#{child_container_id}"
