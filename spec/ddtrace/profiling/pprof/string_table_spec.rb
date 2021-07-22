c4baadf90498a8a9e989f69d01284cea554b7956
Ivo Anjo <ivo.anjo@datadoghq.com>
2021-07-22 17:53:46 +0100

    wip hang debugging 11

--- spec/ddtrace/profiling/pprof/string_table_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-require 'ddtrace/profiling/pprof/string_table'
-
-RSpec.describe Datadog::Profiling::Pprof::StringTable do
-  subject(:string_table) { described_class.new }
-
-  it { is_expected.to be_a_kind_of(Datadog::Utils::StringTable) }
-end
