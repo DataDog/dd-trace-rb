5c3b8ac930692d8f535f3e146e05aa5a5d9ba2b4
Ivo Anjo <ivo.anjo@datadoghq.com>
2021-07-22 15:15:44 +0100

    wip hang debugging

--- spec/ddtrace/configuration/option_set_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-require 'ddtrace'
-
-RSpec.describe Datadog::Configuration::OptionSet do
-  subject(:set) { described_class.new }
-
-  it { is_expected.to be_a_kind_of(Hash) }
-end
