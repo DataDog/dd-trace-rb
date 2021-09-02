e1cca326b7246ae7dd1457712c90094c957099e8
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2021-09-02 15:32:08 +0200

    Enable Rack integration for Sinatra
    This attemps to mimic what is currently done with Rails.
    
    Sinatra provides only limited public facilities to manipulate the stack
    (namely, appending with `use`), assumes his default middlewares are
    always first, and has a peculiar way to wrap the middlewares to build
    the app stack.
    
    Sinatra also freezes the stack when the builder is run, so we have to
    get access to the moment the builder is available but not yet run to
    alter the list by patching `Sinatra::Base#setup_default_middlewares`.
    
    The `Datadog::Contrib::Sinatra::Framework` class encapsulates these
    internal implementation details behind a few methods to achieve the
    desired manipulations.

--- lib/ddtrace/contrib/sinatra/patcher.rb
+++ lib/ddtrace/contrib/sinatra/patcher.rb
@@ -1,9 +1,32 @@
 # typed: true
 require 'ddtrace/contrib/patcher'
+require 'ddtrace/contrib/sinatra/framework'
+require 'ddtrace/contrib/rack/middlewares'
 
 module Datadog
   module Contrib
     module Sinatra
+      # Set tracer configuration at a late enough time
+      module TracerSetupPatch
+        def setup_middleware(*args, &block)
+          super.tap do
+            Datadog::Contrib::Sinatra::Framework.setup
+          end
+        end
+      end
+
+      # Hook into builder before the middleware list gets frozen
+      module DefaultMiddlewarePatch
+        def setup_middleware(*args, &block)
+          builder = args.first
+
+          super.tap do
+            Datadog::Contrib::Sinatra::Framework.add_middleware(builder)
+            Datadog::Contrib::Sinatra::Framework.inspect_middlewares(builder)
+          end
+        end
+      end
+
       # Patcher enables patching of 'sinatra' module.
       module Patcher
         include Kernel # Ensure that kernel methods are always available (https://sorbet.org/docs/error-reference#7003)
@@ -18,11 +41,22 @@ module Datadog
         def patch
           require 'ddtrace/contrib/sinatra/tracer'
           register_tracer
+
+          patch_default_middlewares
+          setup_tracer
         end
 
         def register_tracer
           ::Sinatra.send(:register, Datadog::Contrib::Sinatra::Tracer)
           ::Sinatra::Base.prepend(Sinatra::Tracer::Base)
+        end
+
+        def setup_tracer
+          ::Sinatra::Base.singleton_class.prepend(TracerSetupPatch)
+        end
+
+        def patch_default_middlewares
+          ::Sinatra::Base.singleton_class.prepend(DefaultMiddlewarePatch)
         end
       end
     end
