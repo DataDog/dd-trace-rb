7c54ce09d01a80de4283d95c02d88f13838f9a63
David Elner <david@davidelner.com>
2021-09-20 16:13:40 -0400

    Changed: Updated profile submission benchmark data.

--- spec/ddtrace/profiling/validate_benchmarks_spec.rb
+++ spec/ddtrace/profiling/validate_benchmarks_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe 'Profiling benchmarks', if: (RUBY_VERSION >= '2.4.0' && PlatformH
   end
 
   describe 'profiler_submission' do
-    before { skip('TODO: Generate new dump file.') }
     it('runs without raising errors') { expect_in_fork { load './benchmarks/profiler_submission.rb' } }
   end
 
