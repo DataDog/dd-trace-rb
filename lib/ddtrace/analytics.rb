79b1c940c4d41e3b4e8ef0077f0f882cf02f0783
David Elner <david@davidelner.com>
2021-10-08 18:24:13 -0400

    Refactored: Moved Analytics span extension into SpanOperation.

--- lib/ddtrace/analytics.rb
+++ lib/ddtrace/analytics.rb
@@ -5,34 +5,18 @@ module Datadog
   # Defines analytics behavior
   module Analytics
     class << self
-      def set_sample_rate(span, sample_rate)
-        return if span.nil? || !sample_rate.is_a?(Numeric)
+      def set_sample_rate(span_op, sample_rate)
+        return if span_op.nil? || !sample_rate.is_a?(Numeric)
 
-        span.set_metric(Datadog::Ext::Analytics::TAG_SAMPLE_RATE, sample_rate)
+        span_op.set_metric(Datadog::Ext::Analytics::TAG_SAMPLE_RATE, sample_rate)
       end
 
-      def set_measured(span, value = true)
-        return if span.nil?
+      def set_measured(span_op, value = true)
+        return if span_op.nil?
 
         # rubocop:disable Style/MultipleComparison
         value = value == true || value == 1 ? 1 : 0
-        span.set_metric(Datadog::Ext::Analytics::TAG_MEASURED, value)
-      end
-    end
-
-    # Extension for Datadog::Span
-    module Span
-      def set_tag(key, value)
-        case key
-        when Ext::Analytics::TAG_ENABLED
-          # If true, set rate to 1.0, otherwise set 0.0.
-          value = value == true ? Ext::Analytics::DEFAULT_SAMPLE_RATE : 0.0
-          Analytics.set_sample_rate(self, value)
-        when Ext::Analytics::TAG_SAMPLE_RATE
-          Analytics.set_sample_rate(self, value)
-        else
-          super if defined?(super)
-        end
+        span_op.set_metric(Datadog::Ext::Analytics::TAG_MEASURED, value)
       end
     end
   end
