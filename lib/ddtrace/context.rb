b6c4de27136c473e1907e8315e15de045925c0fd
David Elner <david@davidelner.com>
2021-10-14 17:44:13 -0400

    Refactored: Renamed span to span_op

--- lib/ddtrace/context.rb
+++ lib/ddtrace/context.rb
@@ -76,36 +76,39 @@ module Datadog
     # item in the trace list. This cannot be considered as the current active
     # span in asynchronous environments, because some spans can be closed
     # earlier while child spans still need to finish their traced execution.
-    def current_span
+    def current_span_op
       @mutex.synchronize do
-        @current_span
+        @current_span_op
       end
     end
+    alias current_span current_span_op
 
-    def current_root_span
+    def current_root_span_op
       @mutex.synchronize do
-        @current_root_span
+        @current_root_span_op
       end
     end
+    alias current_root_span current_root_span_op
 
     # Same as calling #current_span and #current_root_span, but works atomically thus preventing races when we need to
     # retrieve both
-    def current_span_and_root_span
+    def current_span_and_root_span_ops
       @mutex.synchronize do
-        [@current_span, @current_root_span]
+        [@current_span_op, @current_root_span_op]
       end
     end
+    alias current_span_and_root_span current_span_and_root_span_ops
 
-    def add_span(operation)
+    def add_span(span_op)
       @mutex.synchronize do
         # Make the span follow the current span
-        if @current_span
-          operation.parent = @current_span
+        if @current_span_op
+          span_op.parent = @current_span_op
         # If there isn't a current span, inherit the trace & parent ID if available.
         # This happens when the trace originates from a distributed trace.
         else
-          operation.trace_id = @parent_trace_id unless @parent_trace_id.nil?
-          operation.parent_id = @parent_span_id unless @parent_span_id.nil?
+          span_op.trace_id = @parent_trace_id unless @parent_trace_id.nil?
+          span_op.parent_id = @parent_span_id unless @parent_span_id.nil?
         end
 
         # If hitting the hard limit, just drop spans. This is really a rare case
@@ -114,8 +117,8 @@ module Datadog
         # larger trace. This is a catch-all to reduce global memory usage.
         if full?
           # Detach the span from the context; it's being dropped and ignored.
-          operation.detach_from_context!
-          Datadog.logger.debug("context full, ignoring span #{operation.name}")
+          span_op.detach_from_context!
+          Datadog.logger.debug("context full, ignoring span #{span_op.name}")
 
           # If overflow has already occurred, don't send this metric.
           # Prevents metrics spam if buffer repeatedly overflows for the same trace.
@@ -128,29 +131,29 @@ module Datadog
         end
 
         # Add the span to the context
-        operation.context = self
-        self.current_span = operation
-        @current_root_span = operation if @trace.empty?
-        @trace << operation
+        span_op.context = self
+        self.current_span_op = span_op
+        @current_root_span_op = span_op if @trace.empty?
+        @trace << span_op
       end
     end
 
     # Mark a span as a finished, increasing the internal counter to prevent
     # cycles inside _trace list.
-    def close_span(operation)
+    def close_span(span_op)
       @mutex.synchronize do
         @finished_spans += 1
 
         # Find the new current span: it should be an ancestor that is not finished.
         # This is because it's possible that a parent span will finish before a child.
         # If this happens, we don't want to set the current span to a completed span.
-        parent = operation.parent
+        parent = span_op.parent
         parent = parent.parent while !parent.nil? && parent.finished?
 
         # Current span is only meaningful for linear tree-like traces.
         # In other cases, this is just broken and one should rely
         # on per-instrumentation code to handle parent/child relations.
-        self.current_span = parent
+        self.current_span_op = parent
 
         # If root span has been closed and spans are still unfinished...
         # ...emit some warnings/metrics to bring attention to this.
@@ -159,11 +162,11 @@ module Datadog
         if parent.nil? && !all_spans_finished?
           if Datadog.configuration.diagnostics.debug
             opened_spans = @trace.length - @finished_spans
-            Datadog.logger.debug("root span #{operation.name} closed but has #{opened_spans} unfinished spans:")
+            Datadog.logger.debug("Root span #{span_op.name} closed but has #{opened_spans} unfinished spans:")
           end
 
           @trace.reject(&:finished?).group_by(&:name).each do |unfinished_span_name, unfinished_spans|
-            Datadog.logger.debug("unfinished span: #{unfinished_spans.first}") if Datadog.configuration.diagnostics.debug
+            Datadog.logger.debug("Unfinished span: #{unfinished_spans.first}") if Datadog.configuration.diagnostics.debug
             Datadog.health_metrics.error_unfinished_spans(
               unfinished_spans.length,
               tags: ["name:#{unfinished_span_name}"]
@@ -217,7 +220,7 @@ module Datadog
         return nil, sampled unless all_spans_finished?
 
         # Root span is finished at this point, we can configure it
-        annotate_for_flush!(@current_root_span)
+        annotate_for_flush!(@current_root_span_op)
 
         # Allow caller to modify trace before context is reset
         yield(trace) if block_given?
@@ -238,13 +241,13 @@ module Datadog
         deleted_spans = []
         return deleted_spans unless block_given?
 
-        @trace.delete_if do |operation|
-          finished = operation.finished?
+        @trace.delete_if do |span_op|
+          finished = span_op.finished?
 
           # Check condition
-          next unless yield(operation)
+          next unless yield(span_op)
 
-          deleted_spans << operation.span
+          deleted_spans << span_op.span
 
           # Acknowledge there's one span less to finish, if needed.
           # It's very important to keep this balanced.
@@ -258,20 +261,20 @@ module Datadog
     end
 
     # Set tags to root span required for flush
-    def annotate_for_flush!(span)
-      attach_sampling_priority(span) if @sampled && @sampling_priority
-      attach_origin(span) if @origin
+    def annotate_for_flush!(span_op)
+      attach_sampling_priority(span_op) if @sampled && @sampling_priority
+      attach_origin(span_op) if @origin
     end
 
-    def attach_sampling_priority(span)
-      span.set_metric(
+    def attach_sampling_priority(span_op)
+      span_op.set_metric(
         Ext::DistributedTracing::SAMPLING_PRIORITY_KEY,
         @sampling_priority
       )
     end
 
-    def attach_origin(span)
-      span.set_tag(
+    def attach_origin(span_op)
+      span_op.set_tag(
         Ext::DistributedTracing::ORIGIN_KEY,
         @origin
       )
@@ -281,7 +284,7 @@ module Datadog
     def to_s
       @mutex.synchronize do
         # rubocop:disable Layout/LineLength
-        "Context(trace.length:#{@trace.length},sampled:#{@sampled},finished_spans:#{@finished_spans},current_span:#{@current_span})"
+        "Context(trace.length:#{@trace.length},sampled:#{@sampled},finished_spans:#{@finished_spans},current_span:#{@current_span_op})"
       end
     end
 
@@ -310,17 +313,17 @@ module Datadog
       @sampling_priority = options.fetch(:sampling_priority, nil)
       @origin = options.fetch(:origin, nil)
       @finished_spans = 0
-      @current_span = nil
-      @current_root_span = nil
+      @current_span_op = nil
+      @current_root_span_op = nil
       @overflow = false
     end
 
-    def current_span=(span)
-      @current_span = span
-      if span
-        @parent_trace_id = span.trace_id
-        @parent_span_id = span.span_id
-        @sampled = span.sampled
+    def current_span_op=(span_op)
+      @current_span_op = span_op
+      if span_op
+        @parent_trace_id = span_op.trace_id
+        @parent_span_id = span_op.span_id
+        @sampled = span_op.sampled
       else
         @parent_span_id = nil
       end
--- spec/ddtrace/sampling/rule_spec.rb
+++ spec/ddtrace/sampling/rule_spec.rb
@@ -5,7 +5,7 @@ require 'ddtrace/sampling/matcher'
 require 'ddtrace/sampling/rule'
 
 RSpec.describe Datadog::Sampling::Rule do
-  let(:span) { Datadog::SpanOperation.new(span_name, service: span_service) }
+  let(:span_op) { Datadog::SpanOperation.new(span_name, service: span_service) }
   let(:span_name) { 'operation.name' }
   let(:span_service) { nil }
 
@@ -14,21 +14,21 @@ RSpec.describe Datadog::Sampling::Rule do
   let(:sampler) { instance_double(Datadog::Sampler) }
 
   describe '#match?' do
-    subject(:match) { rule.match?(span) }
+    subject(:match) { rule.match?(span_op) }
 
     let(:matched) { true }
 
     before do
-      allow(matcher).to receive(:match?).with(span).and_return(matched)
+      allow(matcher).to receive(:match?).with(span_op).and_return(matched)
     end
 
-    context 'with matching span' do
+    context 'with matching span operation' do
       let(:matched) { true }
 
       it { is_expected.to eq(true) }
     end
 
-    context 'with span not matching' do
+    context 'with span operation not matching' do
       let(:matched) { false }
 
       it { is_expected.to eq(false) }
@@ -49,24 +49,24 @@ RSpec.describe Datadog::Sampling::Rule do
   end
 
   describe '#sample?' do
-    subject(:sample) { rule.sample?(span) }
+    subject(:sample) { rule.sample?(span_op) }
 
     let(:sample) { double }
 
     before do
-      allow(sampler).to receive(:sample?).with(span).and_return(sample)
+      allow(sampler).to receive(:sample?).with(span_op).and_return(sample)
     end
 
     it { is_expected.to be(sample) }
   end
 
   describe '#sample_rate' do
-    subject(:sample_rate) { rule.sample_rate(span) }
+    subject(:sample_rate) { rule.sample_rate(span_op) }
 
     let(:sample_rate) { double }
 
     before do
-      allow(sampler).to receive(:sample_rate).with(span).and_return(sample_rate)
+      allow(sampler).to receive(:sample_rate).with(span_op).and_return(sample_rate)
     end
 
     it { is_expected.to be(sample_rate) }
