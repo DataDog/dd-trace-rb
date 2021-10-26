bb8c455eea782f0e81674550d605786b70a06ee7
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2021-10-26 17:26:41 +0200

    Bulletproof AppSec autoload

--- lib/datadog/security/autoload.rb
+++ lib/datadog/security/autoload.rb
@@ -5,6 +5,10 @@ if ['1', 'true'].include?(ENV['DD_APPSEC_ENABLED'].downcase)
     puts "AppSec failed to load. No security check will be performed. error: #{e.message}"
   end
 
-  require 'datadog/security/contrib/auto_instrument'
-  Datadog::Security::Contrib::AutoInstrument.patch_all
+  begin
+    require 'datadog/security/contrib/auto_instrument'
+    Datadog::Security::Contrib::AutoInstrument.patch_all
+  rescue Exception => e
+    puts "AppSec failed to instrument. No security check will be performed. error: #{e.message}"
+  end
 end
