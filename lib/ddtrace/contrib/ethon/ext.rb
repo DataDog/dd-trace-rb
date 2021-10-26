49cd50c7263b96880c607033d2f2be4c20726752
Marco Costa <marco.costa@datadoghq.com>
2021-10-26 13:34:10 -0700

    Deprecation:DD_#{integration}_ANALYTICS_ENABLED and DD_#{integration}_ANALYTICS_SAMPLE_RATE (#1734)

--- lib/ddtrace/contrib/ethon/ext.rb
+++ lib/ddtrace/contrib/ethon/ext.rb
@@ -7,9 +7,7 @@ module Datadog
         APP = 'ethon'.freeze
         ENV_ENABLED = 'DD_TRACE_ETHON_ENABLED'.freeze
         ENV_ANALYTICS_ENABLED = 'DD_TRACE_ETHON_ANALYTICS_ENABLED'.freeze
-        ENV_ANALYTICS_ENABLED_OLD = 'DD_ETHON_ANALYTICS_ENABLED'.freeze
         ENV_ANALYTICS_SAMPLE_RATE = 'DD_TRACE_ETHON_ANALYTICS_SAMPLE_RATE'.freeze
-        ENV_ANALYTICS_SAMPLE_RATE_OLD = 'DD_ETHON_ANALYTICS_SAMPLE_RATE'.freeze
         SERVICE_NAME = 'ethon'.freeze
         SPAN_REQUEST = 'ethon.request'.freeze
         SPAN_MULTI_REQUEST = 'ethon.multi.request'.freeze
--- lib/ddtrace/contrib/rest_client/ext.rb
+++ lib/ddtrace/contrib/rest_client/ext.rb
@@ -7,9 +7,7 @@ module Datadog
         APP = 'rest_client'.freeze
         ENV_ENABLED = 'DD_TRACE_REST_CLIENT_ENABLED'.freeze
         ENV_ANALYTICS_ENABLED = 'DD_TRACE_REST_CLIENT_ANALYTICS_ENABLED'.freeze
-        ENV_ANALYTICS_ENABLED_OLD = 'DD_REST_CLIENT_ANALYTICS_ENABLED'.freeze
         ENV_ANALYTICS_SAMPLE_RATE = 'DD_TRACE_REST_CLIENT_ANALYTICS_SAMPLE_RATE'.freeze
-        ENV_ANALYTICS_SAMPLE_RATE_OLD = 'DD_REST_CLIENT_ANALYTICS_SAMPLE_RATE'.freeze
         SERVICE_NAME = 'rest_client'.freeze
         SPAN_REQUEST = 'rest_client.request'.freeze
       end
