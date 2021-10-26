bd34bc07523804664a96aeef7bc31f35289ecc40
Ivo Anjo <ivo.anjo@datadoghq.com>
2021-10-26 10:17:29 +0100

    Move static `thread_data_type` inside the only function it uses it

--- ext/ddtrace_profiling_native_extension/private_vm_api_access.c
+++ ext/ddtrace_profiling_native_extension/private_vm_api_access.c
@@ -6,8 +6,6 @@
 #ifdef USE_MJIT_HEADER
 #include RUBY_MJIT_HEADER
 
-static const rb_data_type_t *thread_data_type = NULL;
-
 // MRI has a similar rb_thread_ptr() function which we can't call it directly
 // because Ruby does not expose the thread_data_type publicly.
 // Instead, we have our own version of that function, and we lazily initialize the thread_data_type pointer
@@ -16,6 +14,7 @@ static const rb_data_type_t *thread_data_type = NULL;
 // Note that beyond returning the rb_thread_struct*, rb_check_typeddata() raises an exception
 // if the argument passed in is not actually a `Thread` instance.
 static inline struct rb_thread_struct *thread_struct_from_object(VALUE thread) {
+  static const rb_data_type_t *thread_data_type = NULL;
   if (thread_data_type == NULL) thread_data_type = RTYPEDDATA_TYPE(rb_thread_current());
 
   return (struct rb_thread_struct *) rb_check_typeddata(thread, thread_data_type);
