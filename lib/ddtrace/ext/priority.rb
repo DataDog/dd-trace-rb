1569260257886ceeebdfafd8a4a1efb147340e04
Marco Costa <marco.costa@datadoghq.com>
2021-11-15 16:22:34 -0800

    Use RFC requirement language

--- lib/ddtrace/ext/priority.rb
+++ lib/ddtrace/ext/priority.rb
@@ -4,14 +4,14 @@ module Datadog
     # Priority is a hint given to the backend so that it knows which traces to reject or kept.
     # In a distributed context, it should be set before any context propagation (fork, RPC calls) to be effective.
     module Priority
-      # Use this to explicitly inform the backend that a trace should be rejected and not stored.
+      # Use this to explicitly inform the backend that a trace MUST be rejected and not stored.
       # This includes rules and rate limits configured by the user through the {RuleSampler}.
       USER_REJECT = -1
       # Used by the {PrioritySampler} to inform the backend that a trace should be rejected and not stored.
       AUTO_REJECT = 0
       # Used by the {PrioritySampler} to inform the backend that a trace should be kept and stored.
       AUTO_KEEP = 1
-      # Use this to explicitly inform the backend that a trace should be kept and stored.
+      # Use this to explicitly inform the backend that a trace MUST be kept and stored.
       # This includes rules and rate limits configured by the user through the {RuleSampler}.
       USER_KEEP = 2
     end
