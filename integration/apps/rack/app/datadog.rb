a5ddc769a76f377602b27ca7c075b9e053921588
Marco Costa <marco.costa@datadoghq.com>
2021-11-30 17:09:30 -0800

    Sacrifice for the linting gods ☠️

--- integration/apps/rack/app/datadog.rb
+++ integration/apps/rack/app/datadog.rb
@@ -6,9 +6,7 @@ Datadog.configure do |c|
   c.analytics.enabled = true if Datadog::DemoEnv.feature?('analytics')
   c.runtime_metrics.enabled = true if Datadog::DemoEnv.feature?('runtime_metrics')
 
-  if Datadog::DemoEnv.feature?('tracing')
-    c.use :rack, service_name: 'acme-rack'
-  end
+  c.use :rack, service_name: 'acme-rack' if Datadog::DemoEnv.feature?('tracing')
 
   if Datadog::DemoEnv.feature?('pprof_to_file')
     # Reconfigure transport to write pprof to file
