f5fcbbbf8a27a1482500ec4b4b8a9a5516721a2c
David Elner <david@davidelner.com>
2021-12-01 22:15:45 -0500

    Added: component, kind, operation and peer.hostname to Ext::Metadata

--- lib/ddtrace/ext/metadata.rb
+++ lib/ddtrace/ext/metadata.rb
@@ -2,6 +2,14 @@
 module Datadog
   module Ext
     module Metadata
+      # Name of package that was instrumented
+      TAG_COMPONENT = 'component'.freeze
+      # Role of the service in executing an operation (e.g. client vs server)
+      TAG_KIND = 'kind'.freeze
+      # Type of operation being performed (e.g. )
+      TAG_OPERATION = 'operation'.freeze
+      # Hostname of external service interacted with
+      TAG_PEER_HOSTNAME = 'peer.hostname'.freeze
       # Name of external service that performed the work
       TAG_PEER_SERVICE = 'peer.service'.freeze
     end
