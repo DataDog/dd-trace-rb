8365989f5414fe3ee554afed5341a2f6af363f0e
Marco Costa <marco.costa@datadoghq.com>
2021-12-10 14:17:47 -0800

    httpclient: default peer service name

--- lib/ddtrace/contrib/httpclient/ext.rb
+++ lib/ddtrace/contrib/httpclient/ext.rb
@@ -8,7 +8,7 @@ module Datadog
         ENV_ENABLED = 'DD_TRACE_HTTPCLIENT_ENABLED'.freeze
         ENV_ANALYTICS_ENABLED = 'DD_TRACE_HTTPCLIENT_ANALYTICS_ENABLED'.freeze
         ENV_ANALYTICS_SAMPLE_RATE = 'DD_TRACE_HTTPCLIENT_ANALYTICS_SAMPLE_RATE'.freeze
-        SERVICE_NAME = 'httpclient'.freeze
+        DEFAULT_PEER_SERVICE_NAME = 'httpclient'.freeze
         SPAN_REQUEST = 'httpclient.request'.freeze
       end
     end
