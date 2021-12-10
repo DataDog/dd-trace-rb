2d28f6c8a6bff7295fb36bfbf968daa8255851a0
Marco Costa <marco.costa@datadoghq.com>
2021-12-10 14:44:34 -0800

    httprb: peer service, component, and operation tags

--- lib/ddtrace/contrib/httprb/ext.rb
+++ lib/ddtrace/contrib/httprb/ext.rb
@@ -4,12 +4,13 @@ module Datadog
     module Httprb
       # Httprb integration constants
       module Ext
-        APP = 'httprb'.freeze
         ENV_ENABLED = 'DD_TRACE_HTTPRB_ENABLED'.freeze
         ENV_ANALYTICS_ENABLED = 'DD_TRACE_HTTPRB_ANALYTICS_ENABLED'.freeze
         ENV_ANALYTICS_SAMPLE_RATE = 'DD_TRACE_EHTTPRB_ANALYTICS_SAMPLE_RATE'.freeze
         DEFAULT_PEER_SERVICE_NAME = 'httprb'.freeze
         SPAN_REQUEST = 'httprb.request'.freeze
+        TAG_COMPONENT = 'httprb'.freeze
+        TAG_OPERATION_REQUEST = 'request'.freeze
       end
     end
   end
