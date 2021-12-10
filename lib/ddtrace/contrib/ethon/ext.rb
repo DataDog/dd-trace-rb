17425da15c83917efd06c4d495292ff2cb383221
Marco Costa <marco.costa@datadoghq.com>
2021-12-09 16:42:17 -0800

    Ethon: peer service name

--- lib/ddtrace/contrib/ethon/ext.rb
+++ lib/ddtrace/contrib/ethon/ext.rb
@@ -8,7 +8,7 @@ module Datadog
         ENV_ENABLED = 'DD_TRACE_ETHON_ENABLED'.freeze
         ENV_ANALYTICS_ENABLED = 'DD_TRACE_ETHON_ANALYTICS_ENABLED'.freeze
         ENV_ANALYTICS_SAMPLE_RATE = 'DD_TRACE_ETHON_ANALYTICS_SAMPLE_RATE'.freeze
-        SERVICE_NAME = 'ethon'.freeze
+        DEFAULT_PEER_SERVICE_NAME = 'ethon'.freeze
         SPAN_REQUEST = 'ethon.request'.freeze
         SPAN_MULTI_REQUEST = 'ethon.multi.request'.freeze
         NOT_APPLICABLE_METHOD = 'N/A'.freeze
