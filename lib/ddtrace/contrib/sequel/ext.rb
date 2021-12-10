3166f067539bca1bbc972bd97dd51268a03c8895
Marco Costa <marco.costa@datadoghq.com>
2021-12-10 15:02:03 -0800

    sequel: remove unused service constant

--- lib/ddtrace/contrib/sequel/ext.rb
+++ lib/ddtrace/contrib/sequel/ext.rb
@@ -8,7 +8,6 @@ module Datadog
         ENV_ENABLED = 'DD_TRACE_SEQUEL_ENABLED'.freeze
         ENV_ANALYTICS_ENABLED = 'DD_TRACE_SEQUEL_ANALYTICS_ENABLED'.freeze
         ENV_ANALYTICS_SAMPLE_RATE = 'DD_TRACE_SEQUEL_ANALYTICS_SAMPLE_RATE'.freeze
-        SERVICE_NAME = 'sequel'.freeze
         SPAN_QUERY = 'sequel.query'.freeze
         TAG_DB_VENDOR = 'sequel.db.vendor'.freeze
         TAG_PREPARED_NAME = 'sequel.prepared.name'.freeze
