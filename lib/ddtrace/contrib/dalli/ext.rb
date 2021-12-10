58edd8bc2539359300baec55406b695e48600433
Marco Costa <marco.costa@datadoghq.com>
2021-12-09 16:27:21 -0800

    Dalli: Rename default service name to peer name

--- lib/ddtrace/contrib/dalli/ext.rb
+++ lib/ddtrace/contrib/dalli/ext.rb
@@ -8,7 +8,7 @@ module Datadog
         ENV_ANALYTICS_ENABLED = 'DD_TRACE_DALLI_ANALYTICS_ENABLED'.freeze
         ENV_ANALYTICS_SAMPLE_RATE = 'DD_TRACE_DALLI_ANALYTICS_SAMPLE_RATE'.freeze
         QUANTIZE_MAX_CMD_LENGTH = 100
-        SERVICE_NAME = 'memcached'.freeze
+        DEFAULT_PEER_SERVICE_NAME = 'memcached'.freeze
         SPAN_COMMAND = 'memcached.command'.freeze
         SPAN_TYPE_COMMAND = 'memcached'.freeze
         TAG_COMMAND = 'memcached.command'.freeze
