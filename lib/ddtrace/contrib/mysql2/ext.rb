4b44b45387fc9e7458d336a35abfe10e6c8b3da4
Marco Costa <marco.costa@datadoghq.com>
2021-12-13 16:52:35 -0800

    mysql2: default service name

--- lib/ddtrace/contrib/mysql2/ext.rb
+++ lib/ddtrace/contrib/mysql2/ext.rb
@@ -8,7 +8,7 @@ module Datadog
         ENV_ENABLED = 'DD_TRACE_MYSQL2_ENABLED'.freeze
         ENV_ANALYTICS_ENABLED = 'DD_TRACE_MYSQL2_ANALYTICS_ENABLED'.freeze
         ENV_ANALYTICS_SAMPLE_RATE = 'DD_TRACE_MYSQL2_ANALYTICS_SAMPLE_RATE'.freeze
-        SERVICE_NAME = 'mysql2'.freeze
+        DEFAULT_PEER_SERVICE_NAME = 'mysql2'.freeze
         SPAN_QUERY = 'mysql2.query'.freeze
         TAG_DB_NAME = 'mysql2.db.name'.freeze
       end
