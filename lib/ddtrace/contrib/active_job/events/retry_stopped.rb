56102e98f04c9950deb33eec6ac44fd22e37eb06
Marco Costa <marco.costa@datadoghq.com>
2021-12-14 15:43:52 -0800

    Fix retry_stopped.active_job comment

--- lib/ddtrace/contrib/active_job/events/retry_stopped.rb
+++ lib/ddtrace/contrib/active_job/events/retry_stopped.rb
@@ -8,7 +8,7 @@ module Datadog
   module Contrib
     module ActiveJob
       module Events
-        # Defines instrumentation for enqueue.active_job event
+        # Defines instrumentation for retry_stopped.active_job event
         module RetryStopped
           include ActiveJob::Event
 
