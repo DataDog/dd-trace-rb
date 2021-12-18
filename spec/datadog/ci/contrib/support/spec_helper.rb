01fddfabb2d7e8f74e37858741081b5d62573399
Marco Costa <marco.costa@datadoghq.com>
2021-12-17 16:33:56 -0800

    Fix warning gem check

--- spec/datadog/ci/contrib/support/spec_helper.rb
+++ spec/datadog/ci/contrib/support/spec_helper.rb
@@ -3,7 +3,7 @@ require 'datadog/ci/spec_helper'
 require 'datadog/ci/contrib/support/mode_helpers'
 require 'ddtrace/contrib/support/spec_helper'
 
-if defined?(Warning)
+if defined?(Warning.ignore)
   # Caused by https://github.com/cucumber/cucumber-ruby/blob/47c8e2d7c97beae8541c895a43f9ccb96324f0f1/lib/cucumber/encoding.rb#L5-L6
   Warning.ignore(/setting Encoding.default_external/, GEMS_PATH)
   Warning.ignore(/setting Encoding.default_internal/, GEMS_PATH)
