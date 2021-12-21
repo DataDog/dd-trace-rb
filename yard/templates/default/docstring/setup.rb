879bc288884f2439929f6f629aab3bec1077b2b6
Marco Costa <marco.costa@datadoghq.com>
2021-12-21 14:13:42 -0800

    Add deprecation tag documentation

---
+++ yard/templates/default/docstring/setup.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+# def init
+#   return if object.docstring.blank? && !object.has_tag?(:api)
+#   sections :index, [:private, :deprecated, :abstract, :todo, :note, :returns_void, :text], T('tags')
+# end
+
+def deprecated
+  return unless object.has_tag?(:deprecated)
+  erb(:deprecated)
+end
