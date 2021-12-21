6dd1a26449ef7addf67e4c80019ac1d097418b7d
Marco Costa <marco.costa@datadoghq.com>
2021-12-21 14:14:55 -0800

    Sacrifice for the linting gods ☠️

--- yard/templates/default/docstring/setup.rb
+++ yard/templates/default/docstring/setup.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # def init
 #   return if object.docstring.blank? && !object.has_tag?(:api)
 #   sections :index, [:private, :deprecated, :abstract, :todo, :note, :returns_void, :text], T('tags')
@@ -6,5 +7,6 @@
 
 def deprecated
   return unless object.has_tag?(:deprecated)
+
   erb(:deprecated)
 end
