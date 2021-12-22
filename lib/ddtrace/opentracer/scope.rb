79adc74908569b6389a5b0ce14e8de54bdcdc329
Marco Costa <marco.costa@datadoghq.com>
2021-12-21 16:41:20 -0800

    Move public_api tag to top-level object: Otel::Scope

--- lib/ddtrace/opentracer/scope.rb
+++ lib/ddtrace/opentracer/scope.rb
@@ -4,12 +4,10 @@ module Datadog
     # OpenTracing adapter for scope
     # @public_api
     class Scope < ::OpenTracing::Scope
-      # @public_api
       attr_reader \
         :manager,
         :span
 
-      # @public_api
       def initialize(manager:, span:)
         @manager = manager
         @span = span
