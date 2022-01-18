233d458f35d9a03c4554cd93b62cfd58f12788b7
David Elner <david@davidelner.com>
2022-01-18 17:40:03 -0500

    Refactored: Datadog::Buffer --> Datadog::Core::Buffer

--- spec/datadog/core/buffer/shared_examples.rb
+++ spec/datadog/core/buffer/shared_examples.rb
@@ -1,33 +1,14 @@
 # typed: false
 require 'spec_helper'
 
-require 'ddtrace'
-require 'ddtrace/buffer'
-
 require 'benchmark'
 require 'concurrent'
-
-RSpec.describe Datadog::TraceBuffer do
-  subject(:buffer_class) { described_class }
-
-  context 'with CRuby' do
-    before { skip unless PlatformHelpers.mri? }
-
-    it { is_expected.to eq Datadog::CRubyTraceBuffer }
-  end
-
-  context 'with JRuby' do
-    before { skip unless PlatformHelpers.jruby? }
-
-    it { is_expected.to eq Datadog::ThreadSafeTraceBuffer }
-  end
-end
 
 RSpec.shared_examples 'thread-safe buffer' do
   subject(:buffer) { described_class.new(max_size) }
 
   let(:max_size) { 0 }
-  let(:items) { defined?(super) ? super() : Array.new(items_count) { double('item') } }
+  let(:items) { defined?(super) ? super() : Array.new(items_count) { Object.new } }
   let(:items_count) { 10 }
 
   describe '#push' do
@@ -46,9 +27,6 @@ RSpec.shared_examples 'thread-safe buffer' do
         end
       end
     end
-    let(:output) { buffer.pop }
-
-    let(:push) { wait_for_threads }
 
     subject(:push) { threads.each(&:join) }
 
@@ -123,7 +101,6 @@ RSpec.shared_examples 'thread-safe buffer' do
         end
       end
     end
-    let(:output) { buffer.pop }
 
     subject(:concat) { wait_for_threads }
 
@@ -185,135 +162,13 @@ RSpec.shared_examples 'thread-safe buffer' do
   describe '#pop' do
     subject(:pop) { buffer.pop }
 
-    let(:traces) { get_test_traces(2) }
+    let(:items) { Array.new(2) { Object.new } }
 
     before do
-      traces.each { |t| buffer.push(t) }
-    end
-  end
-end
-
-RSpec.shared_examples 'trace buffer' do
-  include_context 'health metrics'
-
-  subject(:buffer) { described_class.new(max_size) }
-
-  let(:max_size) { 0 }
-
-  def measure_traces_size(traces)
-    traces.inject(ObjectSpaceHelper.estimate_bytesize(traces)) do |sum, trace|
-      sum + measure_trace_size(trace)
-    end
-  end
-
-  def measure_trace_size(trace)
-    trace.inject(ObjectSpaceHelper.estimate_bytesize(trace)) do |sum, span|
-      sum + ObjectSpaceHelper.estimate_bytesize(span)
-    end
-  end
-
-  describe '#initialize' do
-    it { is_expected.to be_a_kind_of(described_class) }
-  end
-
-  describe '#push' do
-    subject(:push) { items.each { |t| buffer.push(t) } }
-
-    let(:items_count) { max_size + 1 }
-    let(:pop) { buffer.pop }
-
-    context 'given a max size' do
-      let(:max_size) { 3 }
-
-      it 'records health metrics' do
-        push
-
-        accepted_spans = items.inject(0) { |sum, t| sum + t.length }
-
-        # A trace will be dropped at random, except the trace
-        # that triggered the overflow.
-        dropped_traces = items.reject { |t| pop.include?(t) }
-
-        expected_traces = items - dropped_traces
-        net_spans = expected_traces.inject(0) { |sum, t| sum + t.length }
-
-        # Calling #pop produces metrics:
-        # Accept events for every #push, and one drop event for overflow
-        expect(health_metrics).to have_received(:queue_accepted)
-          .with(items.length)
-        expect(health_metrics).to have_received(:queue_accepted_lengths)
-          .with(accepted_spans)
-
-        expect(health_metrics).to have_received(:queue_dropped)
-          .with(dropped_traces.length)
-
-        # Metrics for queue gauges.
-        expect(health_metrics).to have_received(:queue_max_length)
-          .with(max_size)
-        expect(health_metrics).to have_received(:queue_spans)
-          .with(net_spans)
-        expect(health_metrics).to have_received(:queue_length)
-          .with(max_size)
-      end
-    end
-  end
-
-  describe '#concat' do
-    let(:output) { buffer.pop }
-
-    context 'given no limit' do
-      let(:items) { get_test_traces(4) }
-      let(:max_size) { 0 }
-
-      it 'retains all items' do
-        buffer.concat(items)
-        expect(output.length).to eq(4)
-      end
+      items.each { |i| buffer.push(i) }
     end
 
-    context 'given a max size' do
-      let(:items) { get_test_traces(max_size + 1) }
-      let(:max_size) { 3 }
-
-      it 'does not exceed it' do
-        buffer.concat(items)
-
-        expect(output.length).to eq(max_size)
-        expect(output).to include(items.last)
-      end
-    end
-  end
-
-  describe '#pop' do
-    subject(:pop) { buffer.pop }
-
-    let(:traces) { get_test_traces(2) }
-
-    before { traces.each { |t| buffer.push(t) } }
-
-    it 'records health metrics' do
-      pop
-
-      expected_spans = traces.inject(0) { |sum, t| sum + t.length }
-
-      # Calling #pop produces metrics:
-      # Metrics for accept events and one drop event
-      expect(health_metrics).to have_received(:queue_accepted)
-        .with(traces.length)
-      expect(health_metrics).to have_received(:queue_accepted_lengths)
-        .with(expected_spans)
-
-      expect(health_metrics).to have_received(:queue_dropped)
-        .with(0)
-
-      # Metrics for queue gauges.
-      expect(health_metrics).to have_received(:queue_max_length)
-        .with(max_size)
-      expect(health_metrics).to have_received(:queue_spans)
-        .with(expected_spans)
-      expect(health_metrics).to have_received(:queue_length)
-        .with(traces.length)
-    end
+    it { is_expected.to eq(items) }
   end
 end
 
@@ -328,7 +183,7 @@ RSpec.shared_examples 'performance' do
   let(:test_item_count) { 20 }
 
   def get_test_items(n = 1)
-    Array.new(n) { double('item') }
+    Array.new(n) { Object.new }
   end
 
   before { skip('Performance test does not run in CI.') }
@@ -440,21 +295,3 @@ RSpec.shared_examples 'performance' do
   end
 end
 # :nocov:
-
-RSpec.describe Datadog::ThreadSafeTraceBuffer do
-  let(:items) { get_test_traces(items_count) }
-
-  it_behaves_like 'trace buffer'
-  it_behaves_like 'thread-safe buffer'
-  it_behaves_like 'performance'
-end
-
-RSpec.describe Datadog::CRubyTraceBuffer do
-  before { skip unless PlatformHelpers.mri? }
-
-  let(:items) { get_test_traces(items_count) }
-
-  it_behaves_like 'trace buffer'
-  it_behaves_like 'thread-safe buffer'
-  it_behaves_like 'performance'
-end
