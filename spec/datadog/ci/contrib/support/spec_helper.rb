d6295ac066cf4867e2e7e7be6415cc603e60bb55
Marco Costa <marco.costa@datadoghq.com>
2022-01-31 17:07:34 -0800

    Simplify warning declaration

--- spec/datadog/ci/contrib/support/spec_helper.rb
+++ spec/datadog/ci/contrib/support/spec_helper.rb
@@ -5,6 +5,8 @@ require 'ddtrace/contrib/support/spec_helper'
 
 if defined?(Warning.ignore)
   # Caused by https://github.com/cucumber/cucumber-ruby/blob/47c8e2d7c97beae8541c895a43f9ccb96324f0f1/lib/cucumber/encoding.rb#L5-L6
-  Warning.ignore(/setting Encoding.default_external/, GEMS_PATH)
-  Warning.ignore(/setting Encoding.default_internal/, GEMS_PATH)
+  Gem.path.each do |path|
+    Warning.ignore(/setting Encoding.default_external/, path)
+    Warning.ignore(/setting Encoding.default_internal/, path)
+  end
 end
