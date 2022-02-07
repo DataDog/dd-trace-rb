9a7d777dffa5ee99bddb797f783819a6aa204f5a
Ivo Anjo <ivo.anjo@datadoghq.com>
2022-02-07 15:46:06 +0000

    Introduce new Datadog::Profiling::Flush class

--- lib/datadog/profiling/flush.rb
+++ lib/datadog/profiling/flush.rb
@@ -64,5 +64,31 @@ module Datadog
 
     # Represents a collection of events of a specific type being flushed.
     EventGroup = Struct.new(:event_class, :events).freeze
+
+    class Flush
+      attr_reader \
+        :start,
+        :finish,
+        :pprof_file_name,
+        :pprof_data, # gzipped pprof bytes
+        :code_provenance_file_name,
+        :code_provenance_data # gzipped json bytes
+
+      def initialize(
+        start:,
+        finish:,
+        pprof_file_name:,
+        pprof_data:,
+        code_provenance_file_name:,
+        code_provenance_data:
+      )
+        @start = start
+        @finish = finish
+        @pprof_file_name = pprof_file_name
+        @pprof_data = pprof_data
+        @code_provenance_file_name = code_provenance_file_name
+        @code_provenance_data = code_provenance_data
+      end
+    end
   end
 end
--- spec/datadog/profiling/flush_spec.rb
+++ spec/datadog/profiling/flush_spec.rb
@@ -98,3 +98,36 @@ RSpec.describe Datadog::Profiling::OldFlush do
     end
   end
 end
+
+RSpec.describe Datadog::Profiling::Flush do
+  describe '.new' do
+    let(:start) { instance_double(Time, 'start time') }
+    let(:finish) { instance_double(Time, 'finish time') }
+    let(:pprof_file_name) { 'the_pprof_file_name.pprof' }
+    let(:pprof_data) { 'the_pprof_data' }
+    let(:code_provenance_file_name) { 'the_code_provenance_file_name.json' }
+    let(:code_provenance_data) { 'the_code_provenance_data' }
+
+    subject(:flush) do
+      described_class.new(
+        start: start,
+        finish: finish,
+        pprof_file_name: pprof_file_name,
+        pprof_data: pprof_data,
+        code_provenance_file_name: code_provenance_file_name,
+        code_provenance_data: code_provenance_data,
+      )
+    end
+
+    it do
+      expect(flush).to have_attributes(
+        start: start,
+        finish: finish,
+        pprof_file_name: pprof_file_name,
+        pprof_data: pprof_data,
+        code_provenance_file_name: code_provenance_file_name,
+        code_provenance_data: code_provenance_data,
+      )
+    end
+  end
+end
