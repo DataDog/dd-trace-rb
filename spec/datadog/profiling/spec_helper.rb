08c475952ac4ca869f197fe6ed9896e257ad1e12
Ivo Anjo <ivo.anjo@datadoghq.com>
2022-03-01 11:05:32 +0000

    Enable running profiler specs on macOS
    Profiler is currently not supported on macOS due to missing libddprof
    binaries for it, although if you locally compile libddprof it actually
    works.
    
    Thus, I've added a `DD_PROFILING_MACOS_TESTING` environment variable
    that can be used to allow testing on macOS, but is disabled by default.
    
    I also added a check that the profiler is properly available, when
    we expect it to be available. Without this check, specs that run with
    a profiler that is not available would fail later with confusing error
    messages.

--- spec/datadog/profiling/spec_helper.rb
+++ spec/datadog/profiling/spec_helper.rb
@@ -69,7 +69,21 @@ module ProfileHelpers
   def skip_if_profiling_not_supported(testcase)
     testcase.skip('Profiling is not supported on JRuby') if PlatformHelpers.jruby?
     testcase.skip('Profiling is not supported on TruffleRuby') if PlatformHelpers.truffleruby?
-    testcase.skip('Profiling is not supported on macOS') if PlatformHelpers.mac?
+
+    # Profiling is not officially supported on macOS due to missing libddprof binaries,
+    # but it's still useful to allow it to be enabled for development.
+    if PlatformHelpers.mac? && ENV['DD_PROFILING_MACOS_TESTING'] != 'true'
+      testcase.skip(
+        'Profiling is not supported on macOS. If you still want to run these specs, you can use ' \
+        'DD_PROFILING_MACOS_TESTING=true to override this check.'
+      )
+    end
+
+    return if Datadog::Profiling.supported?
+
+    # Ensure profiling was loaded correctly
+    raise "Profiling does not seem to be available: #{Datadog::Profiling.unsupported_reason}. " \
+      'Try running `bundle exec rake compile` before running this test.'
   end
 end
 
