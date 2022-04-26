609df166302ed8699e5790b56e686885ac187d73
Ivo Anjo <ivo.anjo@datadoghq.com>
2022-04-26 13:52:49 +0100

    Minor: Improve class doc

--- lib/datadog/profiling/collectors/stack.rb
+++ lib/datadog/profiling/collectors/stack.rb
@@ -3,7 +3,8 @@
 module Datadog
   module Profiling
     module Collectors
-      # Used to gather a stack trace from a given Ruby thread
+      # Used to gather a stack trace from a given Ruby thread. Almost all of this class is implemented as native code.
+      #
       # Methods prefixed with _native_ are implemented in `collectors_stack.c`
       class Stack
         def sample(thread, recorder_instance, metric_values_hash, labels_array, max_frames: 400)
