a360a17c3d828f761f19957c82922ccc8a6b8c1c
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2022-04-28 01:04:15 +0200

    Remove spurious word

--- lib/datadog/kit.rb
+++ lib/datadog/kit.rb
@@ -1,8 +1,8 @@
 # typed: false
 
 module Datadog
-  # Datadog::Kit holds public-facing APIs for higher level user-facing features
-  # live, these features not belonging to a specific product. Contrary to
+  # Datadog::Kit holds public-facing APIs for higher level user-facing
+  # features, these features not belonging to a specific product. Contrary to
   # Datadog::Core, Kit depends on products.
   module Kit
   end
