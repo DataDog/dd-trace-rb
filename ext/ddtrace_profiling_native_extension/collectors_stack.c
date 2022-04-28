e4be125f52496eb7e9cb42b4277332b33c7ab564
Ivo Anjo <ivo.anjo@datadoghq.com>
2022-04-28 14:22:09 +0100

    Fix potential memory leaks in error code paths for Stack collector
    Whenever something is dynamically allocated, we must ensure that either
    no exceptions can be thrown or that we clean it up before throwing
    an exception.

--- ext/ddtrace_profiling_native_extension/collectors_stack.c
+++ ext/ddtrace_profiling_native_extension/collectors_stack.c
@@ -244,10 +244,11 @@ void record_placeholder_stack_in_native_code(VALUE recorder_instance, ddprof_ffi
 }
 
 sampling_buffer *sampling_buffer_new(unsigned int max_frames) {
-  sampling_buffer* buffer = xcalloc(1, sizeof(sampling_buffer));
-  if (buffer == NULL) rb_raise(rb_eNoMemError, "Failed to allocate memory for sampling buffer");
   if (max_frames < 5) rb_raise(rb_eArgError, "Invalid max_frames: value must be >= 5");
   if (max_frames > MAX_FRAMES_LIMIT) rb_raise(rb_eArgError, "Invalid max_frames: value must be <= " MAX_FRAMES_LIMIT_AS_STRING);
+
+  sampling_buffer* buffer = xcalloc(1, sizeof(sampling_buffer));
+  if (buffer == NULL) rb_raise(rb_eNoMemError, "Failed to allocate memory for sampling buffer");
 
   buffer->max_frames = max_frames;
 
