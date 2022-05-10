51d8baa660a87a6debcbef03717d724f1a4b9d4d
Ivo Anjo <ivo.anjo@datadoghq.com>
2022-05-10 12:11:49 +0100

    Document in the Ruby side that Stack#sample should not be used
    There was already a comment in `collectors_stack.c` but having it
    on the Ruby side makes it a lot more visible.

--- lib/datadog/profiling/collectors/stack.rb
+++ lib/datadog/profiling/collectors/stack.rb
@@ -7,6 +7,9 @@ module Datadog
       #
       # Methods prefixed with _native_ are implemented in `collectors_stack.c`
       class Stack
+
+        # This method exists only to enable testing Datadog::Profiling::Collectors::Stack behavior using RSpec.
+        # It SHOULD NOT be used for other purposes.
         def sample(thread, recorder_instance, metric_values_hash, labels_array, max_frames: 400)
           self.class._native_sample(thread, recorder_instance, metric_values_hash, labels_array, max_frames)
         end
