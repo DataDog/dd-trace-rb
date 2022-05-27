3258ec650a4cc54a6b9e0bddb98aecb55f34ed12
Marco Costa <marco.costa@datadoghq.com>
2022-05-27 14:52:02 -0700

    [Single Span Sampling] Add single span matcher

--- lib/datadog/tracing/sampling/rate_sampler.rb
+++ lib/datadog/tracing/sampling/rate_sampler.rb
@@ -37,11 +37,11 @@ module Datadog
 
         def sample_rate=(sample_rate)
           @sample_rate = sample_rate
-          @sampling_id_threshold = sample_rate * Span::EXTERNAL_MAX_ID
+          @sampling_id_threshold = sample_rate * Tracing::Span::EXTERNAL_MAX_ID
         end
 
         def sample?(trace)
-          ((trace.id * KNUTH_FACTOR) % Span::EXTERNAL_MAX_ID) <= @sampling_id_threshold
+          ((trace.id * KNUTH_FACTOR) % Tracing::Span::EXTERNAL_MAX_ID) <= @sampling_id_threshold
         end
 
         def sample!(trace)
