455a808eadf819f2d88e5eb656c0cde9e01546db
Ivo Anjo <ivo.anjo@datadoghq.com>
2022-06-08 11:27:11 +0100

    Use inspect when logging PKG_CONFIG_PATH

--- ext/ddtrace_profiling_native_extension/extconf.rb
+++ ext/ddtrace_profiling_native_extension/extconf.rb
@@ -140,7 +140,7 @@ $defs << '-DUSE_LEGACY_LIVING_THREADS_ST' if RUBY_VERSION < '2.2'
 
 # If we got here, libddprof is available and loaded
 ENV['PKG_CONFIG_PATH'] = "#{ENV['PKG_CONFIG_PATH']}:#{Libddprof.pkgconfig_folder}"
-Logging.message(" [ddtrace] PKG_CONFIG_PATH set to '#{ENV['PKG_CONFIG_PATH']}'\n")
+Logging.message(" [ddtrace] PKG_CONFIG_PATH set to #{ENV['PKG_CONFIG_PATH'].inspect}\n")
 unless pkg_config('ddprof_ffi_with_rpath')
   skip_building_extension!(Datadog::Profiling::NativeExtensionHelpers::Supported::FAILED_TO_CONFIGURE_LIBDDPROF)
 end
