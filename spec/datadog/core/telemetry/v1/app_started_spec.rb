d833ef53ebb6eaa6221896d6e45f4130873299b9
Jennifer Chen <32009013+jennchenn@users.noreply.github.com>
2022-06-23 15:50:21 -0400

    Define app-started payload object (#2071)
    * Define classes relevant to app_started event
    
    * Add more tests and base classes
    
    * Use kwargs and invert naming
    
    * Format multi-line blocks and sort parameters
    
    * Add validation and unit tests
    
    * Add documentaiton for param types and description
    
    * fixup! Add validation and unit tests
    
    * Condense namespace to Telemetry::V1::
    
    * fixup! Condense namespace to Telemetry::V1::
    
    * Remove type checking

---
+++ spec/datadog/core/telemetry/v1/app_started_spec.rb
@@ -0,0 +1,125 @@
+require 'spec_helper'
+
+require 'datadog/core/telemetry/v1/app_started'
+require 'datadog/core/telemetry/v1/configuration'
+require 'datadog/core/telemetry/v1/dependency'
+require 'datadog/core/telemetry/v1/integration'
+
+RSpec.describe Datadog::Core::Telemetry::V1::AppStarted do
+  subject(:app_started) do
+    described_class.new(
+      additional_payload: additional_payload,
+      configuration: configuration,
+      dependencies: dependencies,
+      integrations: integrations,
+    )
+  end
+
+  let(:additional_payload) { [Datadog::Core::Telemetry::V1::Configuration.new(name: 'ENV_VARIABLE')] }
+  let(:configuration) { [Datadog::Core::Telemetry::V1::Configuration.new(name: 'DD_TRACE_DEBUG')] }
+  let(:dependencies) { [Datadog::Core::Telemetry::V1::Dependency.new(name: 'pg')] }
+  let(:integrations) { [Datadog::Core::Telemetry::V1::Integration.new(name: 'pg', enabled: true)] }
+
+  it do
+    is_expected.to have_attributes(
+      additional_payload: additional_payload,
+      configuration: configuration,
+      dependencies: dependencies,
+      integrations: integrations,
+    )
+  end
+
+  describe '#initialize' do
+    context 'when :additional_payload' do
+      context 'is nil' do
+        let(:additional_payload) { nil }
+        it { is_expected.to be_a_kind_of(described_class) }
+      end
+
+      context 'is array of Configuration' do
+        let(:additional_payload) { [Datadog::Core::Telemetry::V1::Configuration.new(name: 'ENV_VARIABLE')] }
+        it { is_expected.to be_a_kind_of(described_class) }
+      end
+
+      context 'is array of Configurations with multiple elements' do
+        let(:additional_payload) do
+          [
+            Datadog::Core::Telemetry::V1::Configuration.new(name: 'ENV_VARIABLE'),
+            Datadog::Core::Telemetry::V1::Configuration.new(name: 'ANOTHER_VAR'),
+            Datadog::Core::Telemetry::V1::Configuration.new(name: 'SOME_OTHER_VARIABLE')
+          ]
+        end
+        it { is_expected.to be_a_kind_of(described_class) }
+      end
+    end
+
+    context 'when :configuration' do
+      context 'is nil' do
+        let(:configuration) { nil }
+        it { is_expected.to be_a_kind_of(described_class) }
+      end
+
+      context 'is array of Configuration' do
+        let(:configuration) { [Datadog::Core::Telemetry::V1::Configuration.new(name: 'DD_TRACE_DEBUG')] }
+        it { is_expected.to be_a_kind_of(described_class) }
+      end
+
+      context 'is array of Configurations with multiple elements' do
+        let(:configuration) do
+          [
+            Datadog::Core::Telemetry::V1::Configuration.new(name: 'DD_TRACE_DEBUG'),
+            Datadog::Core::Telemetry::V1::Configuration.new(name: 'DD_SERVICE_NAME'),
+            Datadog::Core::Telemetry::V1::Configuration.new(name: 'DD_ENV')
+          ]
+        end
+        it { is_expected.to be_a_kind_of(described_class) }
+      end
+    end
+
+    context 'when :dependencies' do
+      context 'is nil' do
+        let(:dependencies) { nil }
+        it { is_expected.to be_a_kind_of(described_class) }
+      end
+
+      context 'is array of Dependency' do
+        let(:dependencies) { [Datadog::Core::Telemetry::V1::Dependency.new(name: 'pg')] }
+        it { is_expected.to be_a_kind_of(described_class) }
+      end
+
+      context 'is array of Dependencies with multiple elements' do
+        let(:dependencies) do
+          [
+            Datadog::Core::Telemetry::V1::Dependency.new(name: 'pg'),
+            Datadog::Core::Telemetry::V1::Dependency.new(name: 'kafka'),
+            Datadog::Core::Telemetry::V1::Dependency.new(name: 'mongodb')
+          ]
+        end
+        it { is_expected.to be_a_kind_of(described_class) }
+      end
+    end
+
+    context 'when :integrations' do
+      context 'is nil' do
+        let(:integrations) { nil }
+        it { is_expected.to be_a_kind_of(described_class) }
+      end
+
+      context 'is array of Integration' do
+        let(:integrations) { [Datadog::Core::Telemetry::V1::Integration.new(name: 'pg', enabled: true)] }
+        it { is_expected.to be_a_kind_of(described_class) }
+      end
+
+      context 'is array of Integrations with multiple elements' do
+        let(:integrations) do
+          [
+            Datadog::Core::Telemetry::V1::Integration.new(name: 'pg', enabled: true),
+            Datadog::Core::Telemetry::V1::Integration.new(name: 'kafka', enabled: true),
+            Datadog::Core::Telemetry::V1::Integration.new(name: 'mongodb', enabled: false)
+          ]
+        end
+        it { is_expected.to be_a_kind_of(described_class) }
+      end
+    end
+  end
+end
