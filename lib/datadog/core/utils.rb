11df34545895de3fef423fca83d10f65b06e10c4
Marco Costa <marco.costa@datadoghq.com>
2022-06-28 14:13:02 -0700

    Fix circular dependency

--- lib/datadog/core/utils.rb
+++ lib/datadog/core/utils.rb
@@ -9,9 +9,6 @@ module Datadog
     # @public_api
     module Utils
       extend Forking
-      
-      # Excludes zero from possible values
-      ID_RANGE = (1..Tracing::Span::RUBY_MAX_ID).freeze
 
       EMPTY_STRING = ''.encode(::Encoding::UTF_8).freeze
       # We use a custom random number generator because we want no interference
@@ -22,7 +19,7 @@ module Datadog
       # This method is thread-safe and fork-safe.
       def self.next_id
         after_fork! { reset! }
-        id_rng.rand(ID_RANGE)
+        id_rng.rand(Tracing::Span::RUBY_ID_RANGE)
       end
 
       def self.id_rng
