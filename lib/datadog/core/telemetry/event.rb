93b125e3a654cd500f99ea8a31a5dadbb12cdaca
Jennifer Chen <32009013+jennchenn@users.noreply.github.com>
2022-06-29 19:08:26 -0400

    Create Telemetry Event class (#2117)
    * Create Event class to instantiate TelemetryRequest
    
    * Remove request_type, seq_id from Event init
    
    * Run linter
    
    * Remove validation tests
    
    * Update request name
    
    * Pass in seq_id as argument
    
    * Remove api_version param from init

---
+++ lib/datadog/core/telemetry/event.rb
@@ -0,0 +1,63 @@
+# typed: true
+
+require 'datadog/core/telemetry/collector'
+require 'datadog/core/telemetry/v1/app_started'
+require 'datadog/core/telemetry/v1/telemetry_request'
+
+module Datadog
+  module Core
+    module Telemetry
+      # Class defining methods to construct a Telemetry event
+      class Event
+        include Kernel
+
+        include Telemetry::Collector
+
+        API_VERSION = 'v1'.freeze
+
+        attr_reader \
+          :api_version
+
+        def initialize
+          @api_version = API_VERSION
+        end
+
+        # Forms a TelemetryRequest object based on the event request_type
+        # @param request_type [String] the type of telemetry request to collect data for
+        # @param seq_id [Integer] the ID of the request; incremented each time a telemetry request is sent to the API
+        def telemetry_request(request_type:, seq_id:)
+          Telemetry::V1::TelemetryRequest.new(
+            api_version: @api_version,
+            application: application,
+            host: host,
+            payload: payload(request_type),
+            request_type: request_type,
+            runtime_id: runtime_id,
+            seq_id: seq_id,
+            tracer_time: tracer_time,
+          )
+        end
+
+        private
+
+        def payload(request_type)
+          case request_type
+          when 'app-started'
+            app_started
+          else
+            raise ArgumentError, "Request type invalid, received request_type: #{@request_type}"
+          end
+        end
+
+        def app_started
+          Telemetry::V1::AppStarted.new(
+            dependencies: dependencies,
+            integrations: integrations,
+            configuration: configurations,
+            additional_payload: additional_payload
+          )
+        end
+      end
+    end
+  end
+end
