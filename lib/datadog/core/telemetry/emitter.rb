d984d9b25d39fe2caafa5d4fa99ad3127c3805a2
Jennifer Chen <jenn.chenn93@gmail.com>
2022-06-30 10:54:34 -0400

    Fix emitter tests

--- lib/datadog/core/telemetry/emitter.rb
+++ lib/datadog/core/telemetry/emitter.rb
@@ -1,6 +1,7 @@
 # typed: true
 
 require 'datadog/core/telemetry/event'
+require 'datadog/core/telemetry/http/transport'
 
 module Datadog
   module Core
@@ -10,17 +11,18 @@ module Datadog
         module_function
 
         def request(request_type:)
-          request = Telemetry::Event.new.telemetry_request(request_type: request_type, seq_id: seq_id).to_h
-          # send to telemetry API
-          res = http_transport.request(request_type: request_type, payload: request.to_json)
-          if res.ok?
-            increment_seq_id
+          begin
+            request = Datadog::Core::Telemetry::Event.new.telemetry_request(request_type: request_type, seq_id: seq_id).to_h
+            res = http_transport.request(request_type: request_type, payload: request.to_json)
+            increment_seq_id if res.ok?
+            res
+          rescue StandardError => e
+            Datadog.logger.info("Unable to send telemetry request for #{request_type}: #{e}")
           end
-          res
         end
 
         def http_transport
-          @transporter ||= Telemetry::Http::Transport.new
+          @transporter ||= Datadog::Core::Telemetry::Http::Transport.new
         end
 
         def increment_seq_id
