ba80cc2cce7265035e844177d7f148168be9577d
Jennifer Chen <32009013+jennchenn@users.noreply.github.com>
2022-07-04 16:03:00 -0400

    Add telemetry environment flag (#2122)
    * Add telemetry enabled env flag
    
    * Set DD_INSTRUMENTATION_TELEMETRY_ENABLED false in ci
    
    * fixup! Set DD_INSTRUMENTATION_TELEMETRY_ENABLED false in ci
    
    * Update GettingStarted doc
    
    * Trigger CI
    
    * Disable telemetry in test-head workflow

---
+++ lib/datadog/core/telemetry/ext.rb
@@ -0,0 +1,11 @@
+# typed: true
+
+module Datadog
+  module Core
+    module Telemetry
+      module Ext
+        ENV_ENABLED = 'DD_INSTRUMENTATION_TELEMETRY_ENABLED'.freeze
+      end
+    end
+  end
+end
