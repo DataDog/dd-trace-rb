9d7af3dbead216d1d297e7ee72b0135e01208814
Jennifer Chen <32009013+jennchenn@users.noreply.github.com>
2022-07-04 16:07:00 -0400

    Set up transport for telemetry (#2121)
    * Setup basic http post request methods
    
    * Create env class
    
    * Lint files
    
    * Add basic transport tests
    
    * Remove references to agentless
    
    * Update transport tests
    
    * Format files
    
    * Use agent settings resolver to get agent url
    
    * Rename file to http/transport
    
    * Remove self.build method from net.rb
    
    * Include Kernel to fix typechecking issue

---
+++ lib/datadog/core/telemetry/http/env.rb
@@ -0,0 +1,20 @@
+# typed: true
+
+module Datadog
+  module Core
+    module Telemetry
+      module Http
+        # Data structure for an HTTP request
+        class Env
+          attr_accessor :path, :body
+
+          attr_writer :headers
+
+          def headers
+            @headers ||= {}
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/datadog/core/telemetry/http/transport.rb
@@ -0,0 +1,53 @@
+# typed: true
+
+require 'datadog/core/configuration/settings'
+require 'datadog/core/telemetry/http/env'
+require 'datadog/core/telemetry/http/ext'
+require 'datadog/core/telemetry/http/adapters/net'
+
+module Datadog
+  module Core
+    module Telemetry
+      module Http
+        # Class to send telemetry data to Telemetry API
+        class Transport
+          attr_reader \
+            :host,
+            :port,
+            :ssl,
+            :path
+
+          def initialize
+            agent_settings = Configuration::AgentSettingsResolver.call(Datadog.configuration)
+            @host = agent_settings.hostname
+            @port = agent_settings.port
+            @ssl = false
+            @path = Http::Ext::AGENT_ENDPOINT
+          end
+
+          def request(request_type:, payload:)
+            env = Http::Env.new
+            env.path = @path
+            env.body = payload
+            env.headers = headers(request_type: request_type)
+            adapter.post(env)
+          end
+
+          private
+
+          def headers(request_type:, api_version: Http::Ext::API_VERSION)
+            {
+              Http::Ext::HEADER_CONTENT_TYPE => Http::Ext::CONTENT_TYPE_APPLICATION_JSON,
+              Http::Ext::HEADER_DD_TELEMETRY_API_VERSION => api_version,
+              Http::Ext::HEADER_DD_TELEMETRY_REQUEST_TYPE => request_type,
+            }
+          end
+
+          def adapter
+            @adapter ||= Http::Adapters::Net.new(hostname: @host, port: @port, ssl: @ssl)
+          end
+        end
+      end
+    end
+  end
+end
