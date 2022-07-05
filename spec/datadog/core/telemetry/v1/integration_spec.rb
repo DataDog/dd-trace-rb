f3e0605c5b0371ccbaa201be581d19db3ee4b5b3
Jennifer Chen <32009013+jennchenn@users.noreply.github.com>
2022-07-05 13:29:58 -0400

    Add to_h Method to Telemetry V1 Classes (#2124)
    * Define to_h functions for telemetry objects
    
    * Add tests for to_h function
    
    * fixup! Add tests for to_h function
    
    * Remove appsec and profiler classes
    
    * Change test dummy runtime session ID values

--- spec/datadog/core/telemetry/v1/integration_spec.rb
+++ spec/datadog/core/telemetry/v1/integration_spec.rb
@@ -58,4 +58,26 @@ RSpec.describe Datadog::Core::Telemetry::V1::Integration do
       it_behaves_like 'an optional string parameter', 'version'
     end
   end
+
+  describe '#to_h' do
+    subject(:to_h) { integration.to_h }
+
+    let(:auto_enabled) { false }
+    let(:compatible) { true }
+    let(:enabled) { false }
+    let(:error) { 'Failed to enable' }
+    let(:name) { 'pg' }
+    let(:version) { '1.7.0' }
+
+    it do
+      is_expected.to eq(
+        auto_enabled: auto_enabled,
+        compatible: compatible,
+        enabled: enabled,
+        error: error,
+        name: name,
+        version: version
+      )
+    end
+  end
 end
