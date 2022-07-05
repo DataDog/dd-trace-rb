bc3c6c15a2ed82f4cfeeb7aa3ae7d2eb4adc4455
Jennifer Chen <jenn.chenn93@gmail.com>
2022-07-05 18:50:49 -0400

    Add tests for emitter

--- lib/datadog/core/telemetry/emitter.rb
+++ lib/datadog/core/telemetry/emitter.rb
@@ -9,19 +9,28 @@ module Datadog
     module Telemetry
       # Class that emits telemetry events
       class Emitter
-        def initializer(sequence: Datadog::Core::Utils::Sequence.new(1),
-                        http_transport: Datadog::Core::Telemetry::Http::Transport.new)
+        attr_reader \
+          :sequence,
+          :http_transport
+
+        # @param sequence [Datadog::Core::Utils::Sequence] Sequence object that stores and increments a counter
+        # @param http_transport [Datadog::Core::Telemetry::Http::Transport] Transport object that can be used to send
+        #   telemetry requests via the agent
+        def initialize(sequence: Datadog::Core::Utils::Sequence.new(1),
+                       http_transport: Datadog::Core::Telemetry::Http::Transport.new)
           @sequence = sequence
           @http_transport = http_transport
         end
 
+        # Retrieves and emits a TelemetryRequest object based on the request type specified
+        # @param request_type [String] the type of telemetry request to collect data for
         def request(request_type)
           begin
             request = Datadog::Core::Telemetry::Event.new.telemetry_request(request_type: request_type,
-                                                                            seq_id: sequence.next).to_h
+                                                                            seq_id: @sequence.next).to_h
             @http_transport.request(request_type: request_type, payload: request.to_json)
           rescue StandardError => e
-            Datadog.logger.info("Unable to send telemetry request for #{request_type}: #{e}")
+            Datadog.logger.info("Unable to send telemetry request for event `#{request_type}`: #{e}")
           end
         end
       end
--- spec/datadog/core/telemetry/emitter_spec.rb
+++ spec/datadog/core/telemetry/emitter_spec.rb
@@ -3,8 +3,44 @@ require 'spec_helper'
 require 'datadog/core/telemetry/emitter'
 
 RSpec.describe Datadog::Core::Telemetry::Emitter do
-  describe '.request' do
-    subject(:request) { described_class.request(request_type: request_type) }
+  subject(:emitter) { described_class.new(sequence: sequence, http_transport: http_transport) }
+  let(:sequence) { Datadog::Core::Utils::Sequence.new(1) }
+  let(:http_transport) { double(Datadog::Core::Telemetry::Http::Transport) }
+  let(:response) { double(Datadog::Core::Telemetry::Http::Adapters::Net::Response) }
+  let(:response_ok) { true }
+
+  before do
+    allow(http_transport).to receive(:request).and_return(response)
+    allow(response).to receive(:ok?).and_return(response_ok)
+  end
+
+  describe '#initialize' do
+    context 'when no params provided' do
+      let(:sequence) { nil }
+      let(:http_transport) { nil }
+      it { is_expected.to be_a_kind_of(described_class) }
+    end
+
+    context 'when :sequence is provided' do
+      let(:sequence) { Datadog::Core::Utils::Sequence.new(1) }
+      it { is_expected.to be_a_kind_of(described_class) }
+      it { expect(emitter.sequence).to be(sequence) }
+    end
+
+    context 'when :http_transport is provided' do
+      let(:http_transport) { double(Datadog::Core::Telemetry::Http::Transport) }
+      it { is_expected.to be_a_kind_of(described_class) }
+      it { expect(emitter.http_transport).to be(http_transport) }
+    end
+
+    it 'seq_id begins with 1' do
+      original_seq_id = emitter.sequence.instance_variable_get(:@current)
+      expect(original_seq_id).to be(1)
+    end
+  end
+
+  describe '#request' do
+    subject(:request) { emitter.request(request_type) }
     let(:request_type) { 'app-started' }
 
     before do
@@ -14,22 +50,18 @@ RSpec.describe Datadog::Core::Telemetry::Emitter do
     end
 
     context 'when :request_type' do
+      context 'is invalid' do
+        let(:request_type) { 'app' }
+        it do
+          request
+          expect(Datadog.logger).to have_received(:info) do |message|
+            expect(message).to include('Unable to send telemetry request')
+          end
+        end
+      end
+
       context 'is app-started' do
         let(:request_type) { 'app-started' }
-        let(:response_ok) { true }
-        let(:response) { double(Datadog::Core::Telemetry::Http::Adapters::Net::Response) }
-
-        before do
-          http_transport = double(Datadog::Core::Telemetry::Http::Transport)
-          allow(Datadog::Core::Telemetry::Http::Transport).to receive(:new).and_return(http_transport)
-          allow(response).to receive(:ok?).and_return(response_ok)
-          allow(http_transport).to receive(:request).and_return(response)
-        end
-
-        after do
-          # Clear instance variable to prevent leaking double in the examples
-          described_class.remove_instance_variable(:@transporter)
-        end
 
         context 'when call is successful' do
           let(:response_ok) { true }
@@ -40,30 +72,9 @@ RSpec.describe Datadog::Core::Telemetry::Emitter do
           end
 
           it 'seq_id is incremented' do
-            original_seq_id = described_class.seq_id
-            described_class.request(request_type: request_type)
-            expect(described_class.seq_id).to be(original_seq_id + 1)
-          end
-        end
-
-        context 'when call is unsuccessful' do
-          let(:response_ok) { false }
-
-          it 'seq_id is not incremented' do
-            original_seq_id = described_class.seq_id
-            described_class.request(request_type: request_type)
-            expect(described_class.seq_id).to be(original_seq_id)
-          end
-        end
-      end
-
-      context 'is invalid' do
-        let(:request_type) { 'app' }
-
-        it 'fails with log message' do
-          request
-          expect(Datadog.logger).to have_received(:info) do |message|
-            expect(message).to include('Unable to send telemetry request')
+            original_seq_id = emitter.sequence.instance_variable_get(:@current)
+            request
+            expect(emitter.sequence.instance_variable_get(:@current)).to be(original_seq_id + 1)
           end
         end
       end
