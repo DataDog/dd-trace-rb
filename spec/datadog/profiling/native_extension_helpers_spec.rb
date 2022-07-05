fd4621e30d06ade4a4bb4e744f5ed243d2fc2313
Ivo Anjo <ivo.anjo@datadoghq.com>
2022-07-05 10:04:11 +0100

    Rubocop fixes

--- spec/datadog/profiling/native_extension_helpers_spec.rb
+++ spec/datadog/profiling/native_extension_helpers_spec.rb
@@ -20,13 +20,16 @@ RSpec.describe Datadog::Profiling::NativeExtensionHelpers do
         # RbConfig::CONFIG['SOEXT'] was only introduced in Ruby 2.5, so we have a fallback for older Rubies...
         libddprof_extension =
           RbConfig::CONFIG['SOEXT'] ||
-          PlatformHelpers.linux? ? 'so' : (PlatformHelpers.mac? ? 'dylib' : raise('Missing SOEXT for current platform'))
+          ('so' if PlatformHelpers.linux?) ||
+          ('dylib' if PlatformHelpers.mac?) ||
+          raise('Missing SOEXT for current platform')
 
         gem_lib_folder = "#{Gem.loaded_specs['ddtrace'].gem_dir}/lib"
         full_libddprof_path = "#{gem_lib_folder}/#{relative_path}/libddprof_ffi.#{libddprof_extension}"
 
         expect(relative_path).to start_with('../')
-        expect(File.exist?(full_libddprof_path)).to be(true), "Libddprof not available in expected path: #{full_libddprof_path.inspect}"
+        expect(File.exist?(full_libddprof_path))
+          .to be(true), "Libddprof not available in expected path: #{full_libddprof_path.inspect}"
       end
     end
 
