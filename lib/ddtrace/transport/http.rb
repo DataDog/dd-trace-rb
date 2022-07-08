d5c9a44c3fe08f2616e4e1ba62da4b8aa18ed304
Marco Costa <marco.costa@datadoghq.com>
2022-07-07 18:13:17 -0700

    Tell agent to trust trace top-level span tagging

--- lib/ddtrace/transport/http.rb
+++ lib/ddtrace/transport/http.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # typed: true
 
 require 'uri'
@@ -67,6 +69,7 @@ module Datadog
 
       def default_headers
         {
+          Datadog::Transport::Ext::HTTP::HEADER_CLIENT_COMPUTED_TOP_LEVEL => '1',
           Datadog::Transport::Ext::HTTP::HEADER_META_LANG => Datadog::Core::Environment::Ext::LANG,
           Datadog::Transport::Ext::HTTP::HEADER_META_LANG_VERSION => Datadog::Core::Environment::Ext::LANG_VERSION,
           Datadog::Transport::Ext::HTTP::HEADER_META_LANG_INTERPRETER => Datadog::Core::Environment::Ext::LANG_INTERPRETER,
