d0f91f7d26bf2f4ced19c6946eef593ae4beb58b
Ivo Anjo <ivo.anjo@datadoghq.com>
2022-07-08 14:29:17 +0100

    Update rack integration app tests to not test for profiler on Ruby 2.1

--- integration/images/include/build_ddtrace_profiling_native_extension.rb
+++ integration/images/include/build_ddtrace_profiling_native_extension.rb
@@ -1,10 +1,14 @@
 #!/usr/bin/env ruby
 
 if local_gem_path = ENV['DD_DEMO_ENV_GEM_LOCAL_DDTRACE']
-  puts "\n== Building profiler native extension =="
-  success =
-    system("export BUNDLE_GEMFILE=#{local_gem_path}/Gemfile && cd #{local_gem_path} && bundle install && bundle exec rake compile")
-  raise 'Failure to compile profiler native extension' unless success
+  if RUBY_VERSION.start_with?('2.1.')
+    puts "\n== Skipping build of profiler native extension on Ruby 2.1 =="
+  else
+    puts "\n== Building profiler native extension =="
+    success =
+      system("export BUNDLE_GEMFILE=#{local_gem_path}/Gemfile && cd #{local_gem_path} && bundle install && bundle exec rake compile")
+    raise 'Failure to compile profiler native extension' unless success
+  end
 else
   puts "\n== Skipping build of profiler native extension, no DD_DEMO_ENV_GEM_LOCAL_DDTRACE set =="
 end
