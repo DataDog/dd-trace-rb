4cae25c2750c3cb5146a06c57c736594720b643c
Jennifer Chen <jenn.chenn93@gmail.com>
2022-07-18 15:03:06 -0400

    Rename to rails-seven-delayed-job

--- integration/apps/rails-seven-sidekiq/config/initializers/sidekiq.rb
+++
@@ -1,6 +0,0 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
-end
-Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
-end
