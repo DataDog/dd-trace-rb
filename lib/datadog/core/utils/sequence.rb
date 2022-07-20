1e42068a661b296d3a972b120ecc53f03e856d72
Jennifer Chen <jenn.chenn93@gmail.com>
2022-07-20 15:58:27 -0400

    Add reset! method to sequence util

--- lib/datadog/core/utils/sequence.rb
+++ lib/datadog/core/utils/sequence.rb
@@ -6,6 +6,7 @@ module Datadog
       # Generates values from a consistent sequence
       class Sequence
         def initialize(seed = 0, &block)
+          @seed = seed
           @current = seed
           @next_item = block
         end
@@ -14,6 +15,10 @@ module Datadog
           next_item = @next_item ? @next_item.call(@current) : @current
           @current += 1
           next_item
+        end
+
+        def reset!
+          @current = @seed
         end
       end
     end
--- spec/datadog/core/utils/sequence_spec.rb
+++ spec/datadog/core/utils/sequence_spec.rb
@@ -40,4 +40,40 @@ RSpec.describe Datadog::Core::Utils::Sequence do
       end
     end
   end
+
+  describe '#reset!' do
+    context 'for a sequence' do
+      context 'with default settings' do
+        let(:sequence) { described_class.new }
+
+        it 'produces an integer sequence' do
+          expect(sequence.next).to eq 0
+          sequence.reset!
+          expect(sequence.next).to eq 0
+        end
+      end
+
+      context 'with a seed' do
+        let(:sequence) { described_class.new(seed) }
+        let(:seed) { 10 }
+
+        it 'produces an integer sequence starting at the seed' do
+          expect(sequence.next).to eq 10
+          sequence.reset!
+          expect(sequence.next).to eq 10
+        end
+      end
+
+      context 'with a block' do
+        let(:sequence) { described_class.new(&block) }
+        let(:block) { ->(i) { i.to_s } }
+
+        it 'returns the block value for each iteration' do
+          expect(sequence.next).to eq '0'
+          sequence.reset!
+          expect(sequence.next).to eq '0'
+        end
+      end
+    end
+  end
 end
