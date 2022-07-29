e8e2860812e54064ec17848cdbb2dd5a69ba6cfe
Marco Costa <marco.costa@datadoghq.com>
2022-07-29 15:01:08 -0700

    Fix lint issues

--- spec/datadog/tracing/contrib/rails/cache_spec.rb
+++ spec/datadog/tracing/contrib/rails/cache_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe 'Rails cache' do
       after { Datadog.configuration.tracing[:active_support].reset! }
 
       it 'does not instrument' do
-        expect { subject }.to_not change { fetch_spans }
+        expect { subject }.to_not(change { fetch_spans })
       end
     end
 
@@ -46,7 +46,7 @@ RSpec.describe 'Rails cache' do
       after { Datadog.configuration.tracing.reset! }
 
       it 'does not instrument' do
-        expect { subject }.to_not change { fetch_spans }
+        expect { subject }.to_not(change { fetch_spans })
       end
     end
   end
