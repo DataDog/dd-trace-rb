a51fcc9d90f7496b5521cbf6b29fa230d13d7e42
Jennifer Chen <jenn.chenn93@gmail.com>
2022-08-03 17:03:48 -0400

    Update mysql2 patcher to use trace_method

--- lib/datadog/tracing/contrib/mysql2/patcher.rb
+++ lib/datadog/tracing/contrib/mysql2/patcher.rb
@@ -22,7 +22,46 @@ module Datadog
           end
 
           def patch_mysql2_client
-            ::Mysql2::Client.include(Instrumentation)
+            # ::Mysql2::Client.include(Instrumentation)
+
+            Datadog::Tracing.trace_method(Ext::SPAN_QUERY, 'Mysql2::Client#query').around do |env, span, _trace, &block|
+              sql = env.args[0]
+              query_options = env.self.query_options
+
+              service = Datadog.configuration_for(env.self, :service_name) || datadog_configuration[:service_name]
+              span.service = service
+
+              span.resource = sql
+              span.span_type = Tracing::Metadata::Ext::SQL::TYPE
+
+              span.set_tag(Tracing::Metadata::Ext::TAG_COMPONENT, Ext::TAG_COMPONENT)
+              span.set_tag(Tracing::Metadata::Ext::TAG_OPERATION, Ext::TAG_OPERATION_QUERY)
+
+              # Tag as an external peer service
+              span.set_tag(Tracing::Metadata::Ext::TAG_PEER_SERVICE, service)
+              span.set_tag(Tracing::Metadata::Ext::TAG_PEER_HOSTNAME, query_options[:host])
+
+              # Set analytics sample rate
+              Contrib::Analytics.set_sample_rate(span, analytics_sample_rate) if analytics_enabled?
+
+              span.set_tag(Ext::TAG_DB_NAME, query_options[:database])
+              span.set_tag(Tracing::Metadata::Ext::NET::TAG_TARGET_HOST, query_options[:host])
+              span.set_tag(Tracing::Metadata::Ext::NET::TAG_TARGET_PORT, query_options[:port])
+
+              block.call
+            end
+          end
+
+          def datadog_configuration
+            Datadog.configuration.tracing[:mysql2]
+          end
+
+          def analytics_enabled?
+            datadog_configuration[:analytics_enabled]
+          end
+
+          def analytics_sample_rate
+            datadog_configuration[:analytics_sample_rate]
           end
         end
       end
