ad70552d28a6a5d2f8a2b746647de56a7abfae86
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2022-09-29 22:14:00 +0200

    Bumping version 1.4.2 to 1.5.0

--- lib/ddtrace/version.rb
+++ lib/ddtrace/version.rb
@@ -3,8 +3,8 @@
 module DDTrace
   module VERSION
     MAJOR = 1
-    MINOR = 4
-    PATCH = 2
+    MINOR = 5
+    PATCH = 0
     PRE = nil
 
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
