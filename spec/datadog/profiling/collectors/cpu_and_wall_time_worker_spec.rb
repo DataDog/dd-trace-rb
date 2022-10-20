7f838778796ec744461a64281eed7fe6e7b3612c
Ivo Anjo <ivo.anjo@datadoghq.com>
2022-10-20 17:34:39 -0400

    [PROF-6260] Enable tracking of time spent in GC
    **What does this PR do?**:
    
    (Important note: This feature is only available on the new CPU
    Profiling 2.0 profiler which is still in **alpha**, see #2209)
    
    This PR is the last piece of the puzzle started in #2304 and #2323.
    With this change, time (both cpu-time and wall-time) spent by threads
    doing garbage collection is now accounted for, and shows up in the
    flamegraph.
    
    This works by creating a new Ruby VM TracePoint to handle the
    `RUBY_INTERNAL_EVENT_GC_ENTER` and `RUBY_INTERNAL_EVENT_GC_EXIT`
    events. (These events are only available at the C-level;
    Ruby-level Tracepoints cannot use them).
    
    Then, whenever Ruby calls the TracePoint, we call the previously-added
    `cpu_and_wall_time_collector_on_gc_start` and
    `cpu_and_wall_time_collector_on_gc_finish` to track the time spent
    in GC, and then insert it as a sample in the profiling output
    (using `cpu_and_wall_time_collector_sample_after_gc`).
    
    **Motivation**:
    
    Without this work, time spent doing garbage collection is invisible
    and blamed on methods directly. By making it visible, we enable
    customers to make better informed decisions on what needs to be
    optimized (or fixed!).
    
    **Additional Notes**:
    
    As I mentioned below, this only affects the new CPU Profiling 2.0
    profiler codepath.
    
    During development, I initially attempted to compare the time spent
    in GC gathered via the TracePoint to the one that Ruby exposes via
    [`GC::Profiler#total_time`](https://rubyapi.org/3.1/o/gc/profiler).
    The results were off and when I looked into why, I discovered by
    looking at Ruby's `gc_start` function in `gc.c` that the time
    tracking for `GC::Profiler` only covers a subset of the GC process,
    not the entire time.
    
    Furthermore, from what I was able to observe, it accounts for
    cpu-time process-wide on Linux, and in some cases (macos),
    only cpu-time in usermode, so that's another reason for
    discrepancies.
    
    Thus, when comparing values, expect the `GC::Profiler` to be way
    less than the values exposed by the profiler.
    
    I belive the tracepoint-based approach and values recorded by the
    profiler to be more accurate since Ruby calls the `ENTER` event
    quite early in the GC process, and `EXIT` quite late. See also
    <https://github.com/ivoanjo/gvl-tracing/pull/6> for a similar
    approach proposed by a Ruby core developer.
    
    **How to test the change?**:
    
    Beyond the included code coverage, you should see
    `Garbage Collection` frames representing time spent in GC
    in flamegraphs (when using the new CPU Profiling 2.0
    codepath, see above).
    
    Here's a tiny Ruby script that triggers a lot of allocation:
    
    ```ruby
    def do_alloc(n)
      alloc(n)
    end
    
    def alloc(n)
      n.times do
        Object.new
      end
    end
    
    while true
      do_alloc(100_000)
    end
    ```

--- spec/datadog/profiling/collectors/cpu_and_wall_time_worker_spec.rb
+++ spec/datadog/profiling/collectors/cpu_and_wall_time_worker_spec.rb
@@ -10,6 +10,12 @@ RSpec.describe Datadog::Profiling::Collectors::CpuAndWallTimeWorker do
 
   subject(:cpu_and_wall_time_worker) { described_class.new(recorder: recorder, max_frames: 400) }
 
+  describe '.new' do
+    it 'creates the garbage collection tracepoint in the disabled state' do
+      expect(described_class::Testing._native_gc_tracepoint(cpu_and_wall_time_worker)).to_not be_enabled
+    end
+  end
+
   describe '#start' do
     subject(:start) do
       cpu_and_wall_time_worker.start
@@ -53,6 +59,12 @@ RSpec.describe Datadog::Profiling::Collectors::CpuAndWallTimeWorker do
       start
 
       expect(described_class::Testing._native_current_sigprof_signal_handler).to be :profiling
+    end
+
+    it 'enables the garbage collection tracepoint' do
+      start
+
+      expect(described_class::Testing._native_gc_tracepoint(cpu_and_wall_time_worker)).to be_enabled
     end
 
     context 'when a previous signal handler existed' do
@@ -103,6 +115,39 @@ RSpec.describe Datadog::Profiling::Collectors::CpuAndWallTimeWorker do
 
       expect(current_thread_sample).to_not be nil
     end
+
+    it 'records garbage collection cycles' do
+      pending 'Currently broken on Ruby 2.2 due to missing ruby_thread_has_gvl_p API' if RUBY_VERSION.start_with?('2.2.')
+
+      start
+
+      described_class::Testing._native_trigger_sample
+
+      invoke_gc_times = 5
+
+      invoke_gc_times.times do
+        Thread.pass
+        GC.start
+      end
+
+      cpu_and_wall_time_worker.stop
+
+      serialization_result = recorder.serialize
+      raise 'Unexpected: Serialization failed' unless serialization_result
+
+      all_samples = samples_from_pprof(serialization_result.last)
+
+      current_thread_gc_samples =
+        all_samples
+          .select { |it| it.fetch(:labels).fetch(:'thread id') == Thread.current.object_id.to_s }
+          .select { |it| it.fetch(:locations).first.fetch(:path) == 'Garbage Collection' }
+
+      # NOTE: In some cases, Ruby may actually call two GC's back-to-back without us having the possibility to take
+      # a sample. I don't expect this to happen for this test (that's what the `Thread.pass` above is trying to avoid)
+      # but if this spec turns out to be flaky, that is probably the issue, and that would mean we'd need to relax the
+      # check.
+      expect(current_thread_gc_samples.first.fetch(:values).fetch(:'cpu-samples')).to be >= invoke_gc_times
+    end
   end
 
   describe '#stop' do
@@ -125,6 +170,12 @@ RSpec.describe Datadog::Profiling::Collectors::CpuAndWallTimeWorker do
       stop
 
       expect(described_class::Testing._native_current_sigprof_signal_handler).to be nil
+    end
+
+    it 'disables the garbage collection tracepoint' do
+      stop
+
+      expect(described_class::Testing._native_gc_tracepoint(cpu_and_wall_time_worker)).to_not be_enabled
     end
   end
 
