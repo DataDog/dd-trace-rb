d79b2bf93ce45dae19fa5683becbbcfbca364deb
Marco Costa <marco.costa@datadoghq.com>
2022-11-07 12:03:54 -0800

    Fix lint

--- lib/datadog/appsec/event.rb
+++ lib/datadog/appsec/event.rb
@@ -51,6 +51,7 @@ module Datadog
         end
       end
 
+      # rubocop:disable Metrics/MethodLength
       def self.record_via_span(*events) # rubocop:disable Metrics/AbcSize
         events.group_by { |e| e[:trace] }.each do |trace, event_group|
           unless trace
@@ -110,6 +111,7 @@ module Datadog
           end
         end
       end
+      # rubocop:enable Metrics/MethodLength
     end
   end
 end
