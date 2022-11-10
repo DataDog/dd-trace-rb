f8dd8491857066b854e8f148d53c87d1d2a9b4b7
Tony Hsu <tonyc.t.hsu@gmail.com>
2022-11-10 11:52:10 +0100

    Fix `Datadog.configure_onto(redis)`

--- lib/datadog/tracing/contrib/redis/patcher.rb
+++ lib/datadog/tracing/contrib/redis/patcher.rb
@@ -12,6 +12,38 @@ module Datadog
         module Patcher
           include Contrib::Patcher
 
+          module InstancePatch
+            def self.included(base)
+              base.prepend(InstanceMethods)
+            end
+
+            module InstanceMethods
+              def initialize(options = {})
+                options[:redis_instance] = self
+
+                super(options)
+              end
+            end
+          end
+
+          module ClientPatch
+            def self.included(base)
+              base.prepend(InstanceMethods)
+            end
+
+            module InstanceMethods
+              def initialize(options = {})
+                @redis_instance = options[:redis_instance]
+
+                super(options)
+              end
+
+              private
+
+              attr_reader :redis_instance
+            end
+          end
+
           module_function
 
           def target_version
@@ -26,6 +58,11 @@ module Datadog
             require_relative 'quantize'
             require_relative 'instrumentation'
 
+            # InstancePatch and ClientPatch allows the client object to access pin on redis instance
+            ::Redis.include(InstancePatch)
+            ::Redis::Client.include(ClientPatch)
+
+            # TODO: To support redis-rb 5.x, Redis::Client -> RedisClient
             ::Redis::Client.include(Instrumentation)
           end
         end
