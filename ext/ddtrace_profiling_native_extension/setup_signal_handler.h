65080ec0e921404d040b56d51639130754ce62cb
Ivo Anjo <ivo.anjo@datadoghq.com>
2022-11-23 17:33:22 +0000

    Minor: Add missing include

--- ext/ddtrace_profiling_native_extension/setup_signal_handler.h
+++ ext/ddtrace_profiling_native_extension/setup_signal_handler.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <signal.h>
+
 void empty_signal_handler(DDTRACE_UNUSED int _signal, DDTRACE_UNUSED siginfo_t *_info, DDTRACE_UNUSED void *_ucontext);
 void install_sigprof_signal_handler(void (*signal_handler_function)(int, siginfo_t *, void *), const char *handler_pretty_name);
 void replace_sigprof_signal_handler_with_empty_handler(void (*expected_existing_handler)(int, siginfo_t *, void *));
