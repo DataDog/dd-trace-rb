6e70ec1df94adbb4ca13c4bdcb9b572af7f62062
Tony Hsu <tonyc.t.hsu@gmail.com>
2022-12-06 14:24:14 +0100

    Environment variable service_name for `mongodb`

--- lib/datadog/tracing/contrib/mongodb/configuration/settings.rb
+++ lib/datadog/tracing/contrib/mongodb/configuration/settings.rb
@@ -29,7 +29,15 @@ module Datadog
             end
 
             option :quantize, default: DEFAULT_QUANTIZE
-            option :service_name, default: Ext::DEFAULT_PEER_SERVICE_NAME
+
+            option :service_name do |o|
+              o.default do
+                ENV.fetch(Ext::ENV_SERVICE_NAME) do
+                  Ext::DEFAULT_PEER_SERVICE_NAME
+                end
+              end
+              o.lazy
+            end
           end
         end
       end
