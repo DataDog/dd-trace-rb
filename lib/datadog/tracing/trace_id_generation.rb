ff02318781fa366da739d82f5e1ef300e11d0cca
Tony Hsu <tonyc.t.hsu@gmail.com>
2022-12-21 11:10:35 +0100

    Refactor module and test

--- lib/datadog/tracing/trace_id_generation.rb
+++ lib/datadog/tracing/trace_id_generation.rb
@@ -14,33 +14,21 @@ module Datadog
       end
 
       def generate_trace_id
-        if generation_enabled?
-          high_order = Tracing::Utils.next_id
-          low_order  = Tracing::Utils.next_id
-
-          # tag with hex encoded high order
-          if propagation_enabled?
-            set_tags(Tracing::Metadata::Ext::Distributed::TAG_TID => high_order.to_s(16))
-          end
-
-          # high_order + low_order
-          high_order << 64 | low_order
-        else
-          Tracing::Utils.next_id
+        unless Datadog.configuration.tracing.trace_id_128_bit_generation_enabled
+          return Tracing::Utils.next_id
         end
-      end
 
-      def generation_enabled?
-        Datadog.configuration.tracing.trace_id_128_bit_generation_enabled
-      end
+        high_order = Tracing::Utils.next_id
+        low_order  = Tracing::Utils.next_id
 
-      def propagation_enabled?
-        Datadog.configuration.tracing.trace_id_128_bit_propagation_enabled
-      end
+        # tag with hex encoded high order
+        if Datadog.configuration.tracing.trace_id_128_bit_propagation_enabled
+          set_tags(Tracing::Metadata::Ext::Distributed::TAG_TID => high_order.to_s(16))
+        end
 
-      # def logging_enabled?
-      #   Datadog.configuration.tracing.trace_id_128_bit_logging_enabled
-      # end
+        # high_order + low_order
+        high_order << 64 | low_order
+      end
     end
   end
 end
