1a6263bfdcc844ad3ceeaf1207832872037ce8fb
Alex Robbin <agrobbin@gmail.com>
2023-01-14 14:19:09 -0500

    add Stripe instrumentation
    https://github.com/stripe/stripe-ruby

---
+++ lib/datadog/tracing/contrib/stripe/configuration/settings.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require_relative '../../configuration/settings'
+require_relative '../ext'
+
+module Datadog
+  module Tracing
+    module Contrib
+      module Stripe
+        module Configuration
+          # Custom settings for the Stripe integration
+          # @public_api
+          class Settings < Contrib::Configuration::Settings
+            option :enabled do |o|
+              o.default { env_to_bool(Ext::ENV_ENABLED, true) }
+              o.lazy
+            end
+
+            option :analytics_enabled do |o|
+              o.default { env_to_bool(Ext::ENV_ANALYTICS_ENABLED, false) }
+              o.lazy
+            end
+
+            option :analytics_sample_rate do |o|
+              o.default { env_to_float(Ext::ENV_ANALYTICS_SAMPLE_RATE, 1.0) }
+              o.lazy
+            end
+          end
+        end
+      end
+    end
+  end
+end
