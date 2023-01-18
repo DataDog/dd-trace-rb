c61d89561a66cc0f3a31205fc8b7fa9fc1ad1653
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2023-01-18 21:13:17 +0100

    Update release spec

--- spec/ddtrace/release_gem_spec.rb
+++ spec/ddtrace/release_gem_spec.rb
@@ -6,28 +6,33 @@ RSpec.describe 'gem release process' do
       # It's easy to forget to ship new files, especially when a new paradigm is
       # introduced (e.g. introducing native files requires the inclusion `ext/`)
       it 'includes all important files' do
-        single_files_excluded = %w[
-          .dockerignore
-          .editorconfig
-          .env
-          .git-blame-ignore-revs
-          .gitattributes
-          .gitignore
-          .gitlab-ci.yml
-          .pryrc
-          .rspec
-          .rubocop.yml
-          .rubocop_todo.yml
-          .simplecov
-          .yardopts
-          Appraisals
-          CONTRIBUTING.md
-          Gemfile
-          Rakefile
-          Steepfile
-          ddtrace.gemspec
-          docker-compose.yml
-        ]
+        single_files_excluded = %r{
+          ^
+          (
+            \.dockerignore
+           |\.editorconfig
+           |\.env
+           |\.git-blame-ignore-revs
+           |\.gitattributes
+           |\.gitignore
+           |\.gitlab-ci.yml
+           |\.pryrc
+           |\.rspec
+           |\.rubocop.yml
+           |\.rubocop_todo.yml
+           |\.simplecov
+           |\.yardopts
+           |Appraisals
+           |CONTRIBUTING.md
+           |Gemfile
+           |Gemfile-.*
+           |Rakefile
+           |Steepfile
+           |ddtrace\.gemspec
+           |docker-compose\.yml
+          )
+          $
+        }x
 
         directories_excluded = %r{
           ^(sig|spec|docs|\.circleci|\.github|benchmarks|gemfiles|integration|tasks|sorbet|yard|vendor/rbs)/
@@ -38,7 +43,7 @@ RSpec.describe 'gem release process' do
             `git ls-files -z`
               .split("\x0")
               .reject { |f| f.match(directories_excluded) }
-              .reject { |f| single_files_excluded.include?(f) }
+              .reject { |f| f.match(single_files_excluded) }
           )
       end
     end
