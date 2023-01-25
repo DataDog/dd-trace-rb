1598709668c3d238ce5d2e2fd9ca23b6442e6fe9
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2023-01-25 14:46:26 +0100

    Factor trace and meta as let

--- spec/datadog/kit/events_spec.rb
+++ spec/datadog/kit/events_spec.rb
@@ -10,29 +10,29 @@ require 'datadog/kit/events'
 RSpec.describe Datadog::Kit::Events do
   subject(:trace_op) { Datadog::Tracing::TraceOperation.new }
 
+  let(:trace) { trace_op.flush! }
+  let(:meta) { trace.send(:meta) }
+
   describe '#track_login_success' do
     it 'sets event tracking key on trace' do
       trace_op.measure('root') do
         described_class.track_login_success(trace_op, user: { id: '42' })
       end
-      trace = trace_op.flush!
-      expect(trace.send(:meta)).to include('appsec.events.users.login.success.track' => 'true')
+      expect(meta).to include('appsec.events.users.login.success.track' => 'true')
     end
 
     it 'sets successful user id on trace' do
       trace_op.measure('root') do
         described_class.track_login_success(trace_op, user: { id: '42' })
       end
-      trace = trace_op.flush!
-      expect(trace.send(:meta)).to include('usr.id' => '42')
+      expect(meta).to include('usr.id' => '42')
     end
 
     it 'sets other keys on trace' do
       trace_op.measure('root') do
         described_class.track_login_success(trace_op, user: { id: '42' }, foo: 'bar')
       end
-      trace = trace_op.flush!
-      expect(trace.send(:meta)).to include('usr.id' => '42', 'appsec.events.users.login.success.foo' => 'bar')
+      expect(meta).to include('usr.id' => '42', 'appsec.events.users.login.success.foo' => 'bar')
     end
   end
 
@@ -41,40 +41,35 @@ RSpec.describe Datadog::Kit::Events do
       trace_op.measure('root') do
         described_class.track_login_failure(trace_op, user_id: '42', user_exists: true)
       end
-      trace = trace_op.flush!
-      expect(trace.send(:meta)).to include('appsec.events.users.login.failure.track' => 'true')
+      expect(meta).to include('appsec.events.users.login.failure.track' => 'true')
     end
 
     it 'sets failing user id on trace' do
       trace_op.measure('root') do
         described_class.track_login_failure(trace_op, user_id: '42', user_exists: true)
       end
-      trace = trace_op.flush!
-      expect(trace.send(:meta)).to include('appsec.events.users.login.failure.usr.id' => '42')
+      expect(meta).to include('appsec.events.users.login.failure.usr.id' => '42')
     end
 
     it 'sets user existence on trace' do
       trace_op.measure('root') do
         described_class.track_login_failure(trace_op, user_id: '42', user_exists: true)
       end
-      trace = trace_op.flush!
-      expect(trace.send(:meta)).to include('appsec.events.users.login.failure.usr.exists' => 'true')
+      expect(meta).to include('appsec.events.users.login.failure.usr.exists' => 'true')
     end
 
     it 'sets user non-existence  on trace' do
       trace_op.measure('root') do
         described_class.track_login_failure(trace_op, user_id: '42', user_exists: false)
       end
-      trace = trace_op.flush!
-      expect(trace.send(:meta)).to include('appsec.events.users.login.failure.usr.exists' => 'false')
+      expect(meta).to include('appsec.events.users.login.failure.usr.exists' => 'false')
     end
 
     it 'sets other keys on trace' do
       trace_op.measure('root') do
         described_class.track_login_failure(trace_op, user_id: '42', user_exists: true, foo: 'bar')
       end
-      trace = trace_op.flush!
-      expect(trace.send(:meta)).to include('appsec.events.users.login.failure.foo' => 'bar')
+      expect(meta).to include('appsec.events.users.login.failure.foo' => 'bar')
     end
   end
 
@@ -83,25 +78,21 @@ RSpec.describe Datadog::Kit::Events do
       trace_op.measure('root') do
         expect { described_class.track(:foo, 'bar', trace_op) }.to raise_error ArgumentError, /namespace cannot be/
       end
-
-      trace = trace_op.flush!
-      expect(trace.send(:meta)).to_not include('foo.events.bar.track' => 'true')
+      expect(meta).to_not include('foo.events.bar.track' => 'true')
     end
 
     it 'sets event tracking key on trace' do
       trace_op.measure('root') do
         described_class.track(:appsec, 'foo', trace_op)
       end
-      trace = trace_op.flush!
-      expect(trace.send(:meta)).to include('appsec.events.foo.track' => 'true')
+      expect(meta).to include('appsec.events.foo.track' => 'true')
     end
 
     it 'sets other keys on trace' do
       trace_op.measure('root') do
         described_class.track(:appsec, 'foo', trace_op, bar: 'baz')
       end
-      trace = trace_op.flush!
-      expect(trace.send(:meta)).to include('appsec.events.foo.bar' => 'baz')
+      expect(meta).to include('appsec.events.foo.bar' => 'baz')
     end
   end
 end
