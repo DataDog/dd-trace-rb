fd5b865cb206d9fb7ac94ddff8dd55c35d99320c
Rodrigo Arguello <rodrigo.arguello@datadoghq.com>
2023-02-10 16:16:20 +0100

    Add mandatory tags for rabbitmq integrations

--- spec/datadog/tracing/contrib/sneakers/tracer_spec.rb
+++ spec/datadog/tracing/contrib/sneakers/tracer_spec.rb
@@ -89,6 +89,7 @@ RSpec.describe Datadog::Tracing::Contrib::Sneakers::Tracer do
       expect(span.get_tag(Datadog::Tracing::Metadata::Ext::TAG_OPERATION)).to eq('job')
       expect(span.get_tag('span.kind')).to eq('consumer')
       expect(span.get_tag('messaging.system')).to eq('rabbitmq')
+      expect(span.get_tag('messaging.rabbitmq.routing_key')).to eq('something')
     end
 
     context 'when the tag_body is true' do
