76458d7a0123b71f405d78a528f46cc08c01bdf3
Tony Hsu <tonyc.t.hsu@gmail.com>
2023-02-15 13:26:00 +0100

    Add version and remove DDTRACE_AUTOINJECT

--- lib-injection/auto_inject.rb
+++ lib-injection/auto_inject.rb
@@ -1,10 +1,12 @@
-if ENV['DDTRACE_AUTOINJECT'] && !ENV['skip_autoinject']
-  if system 'skip_autoinject=true bundle show ddtrace'
-    puts 'ddtrace already installed...'
-  else
-    puts 'ddtrace is not installed... Perform auto-injection...'
 
-    if system 'skip_autoinject=true bundle add ddtrace --require ddtrace/auto_instrument'
+if !ENV['skip_autoinject']
+  if system 'skip_autoinject=true bundle show ddtrace'
+    puts 'ddtrace already installed... skipping auto-injection'
+  else
+    version = "<DD_TRACE_VERSION_TO_BE_REPLACED>"
+    puts "ddtrace is not installed... Perform auto-injection... for dd-trace-rb:#{version}"
+
+    if system "skip_autoinject=true bundle add ddtrace --version #{version} --require ddtrace/auto_instrument"
       puts 'ddtrace added to bundle...'
     else
       puts 'Something went wrong when adding ddtrace to bundle...'
