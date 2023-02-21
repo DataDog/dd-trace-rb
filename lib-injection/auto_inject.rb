4b648ee8433873eae3c2a9550f754d16b8b87cfa
Tony Hsu <tonyc.t.hsu@gmail.com>
2023-02-21 11:55:38 +0100

    Release workflow

--- lib-injection/auto_inject.rb
+++ lib-injection/auto_inject.rb
@@ -5,28 +5,34 @@ if !ENV['skip_autoinject']
     version = "<DD_TRACE_VERSION_TO_BE_REPLACED>"
     sha = "<DD_TRACE_SHA_TO_BE_REPLACED>"
 
-    condition = if !version.empty?
-      "--version '#{version}'"
-    elsif !sha.empty?
-      "--github 'datadog/dd-trace-rb' --ref '#{sha}'"
-    else
-      puts "NO VERSION"
-    end
+    condition =
+      if !version.empty?
+        # For public release
+        "--version '#{version.gsub(/^v/, '')}'"
+      elsif !sha.empty?
+        # For internal testing
+        "--github 'datadog/dd-trace-rb' --ref '#{sha}'"
+      else
+        puts "NO VERSION"
+      end
 
     puts "ddtrace is not installed... Perform auto-injection... for dd-trace-rb"
 
+    # Copies for trial
     system "cp Gemfile Gemfile-datadog"
     system "cp Gemfile.lock Gemfile-datadog.lock"
 
     if system "skip_autoinject=true BUNDLE_GEMFILE=Gemfile-datadog bundle add ddtrace #{condition} --require ddtrace/auto_instrument"
       puts 'ddtrace added to bundle...'
 
+      # Trial success, replace the original
       system "cp Gemfile-datadog Gemfile"
       system "cp Gemfile-datadog.lock Gemfile.lock"
     else
       puts 'Something went wrong when adding ddtrace to bundle...'
     end
 
+    # Remove the copies
     system "rm Gemfile-datadog Gemfile-datadog.lock"
   end
 
