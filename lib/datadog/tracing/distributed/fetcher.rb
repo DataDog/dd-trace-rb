ffb81de5c02c22eaa2d8c8e1a4158078170b8aae
Tony Hsu <tonyc.t.hsu@gmail.com>
2023-03-02 16:43:38 +0100

    Cleanup

--- lib/datadog/tracing/distributed/fetcher.rb
+++ lib/datadog/tracing/distributed/fetcher.rb
@@ -15,22 +15,6 @@ module Datadog
         def [](key)
           @data[key]
         end
-
-        # def id(key, base: 10)
-        #   Helpers.value_to_id(self[key], base: base)
-        # end
-
-        # def number(key, base: 10)
-        #   Helpers.value_to_number(self[key], base: base)
-        # end
-
-        # def hex_trace_id(key)
-        #   Helpers.parse_hex_id(self[key], length: 32)
-        # end
-
-        # def hex_span_id(key)
-        #   Helpers.parse_hex_id(self[key], length: 16)
-        # end
       end
     end
   end
--- lib/datadog/tracing/distributed/helpers.rb
+++ lib/datadog/tracing/distributed/helpers.rb
@@ -20,53 +20,6 @@ module Datadog
           sampling_priority
         end
 
-        # def self.format_base16_number(value)
-        #   # Lowercase if we want to parse base16 e.g. 3E8 => 3e8
-        #   # DEV: Ruby will parse `3E8` just fine, but to test
-        #   #      `num.to_s(base) == value` we need to lowercase
-        #   value = value.downcase
-
-        #   # Remove any leading zeros
-        #   # DEV: When we call `num.to_s(16)` later Ruby will not add leading zeros
-        #   #      for us so we want to make sure the comparision will work as expected
-        #   # DEV: regex, remove all leading zeros up until we find the last 0 in the string
-        #   #      or we find the first non-zero, this allows `'0000' -> '0'` and `'00001' -> '1'`
-        #   value.sub(/^0*(?=(0$)|[^0])/, '')
-        # end
-
-        # def self.value_to_id(value, base: 10)
-        #   id = value_to_number(value, base: base)
-
-        #   # Return early if we could not parse a number
-        #   return if id.nil?
-
-        #   # Zero or greater than max allowed value of 2**64
-        #   return if id.zero? || id > Tracing::Utils::TraceId::MAX
-
-        #   id < 0 ? id + (2**64) : id
-        # end
-
-        # def self.value_to_number(value, base: 10)
-        #   # It's important to make a difference between no data and zero.
-        #   return if value.nil?
-
-        #   # Be sure we have a string
-        #   value = value.to_s
-
-        #   # If we are parsing base16 number then truncate to 64-bit
-        #   value = Helpers.format_base16_number(value) if base == 16
-
-        #   # Convert value to an integer
-        #   # DEV: Ruby `.to_i` will return `0` if a number could not be parsed
-        #   num = value.to_i(base)
-
-        #   # Ensure the parsed number is the same as the original string value
-        #   # e.g. We want to make sure to throw away `'nan'.to_i == 0`
-        #   return unless num.to_s(base) == value
-
-        #   num
-        # end
-
         def self.parse_decimal_id(value)
           return unless value
 
