bf4d47048a5dbfc9dc238c247bd2f2cfb0c1e52f
Ivo Anjo <ivo.anjo@datadoghq.com>
2023-03-16 16:22:55 +0000

    Scope constant stubbing to Rails 3+ only, since 2 didn't have it

--- spec/datadog/tracing/contrib/rails/utils_spec.rb
+++ spec/datadog/tracing/contrib/rails/utils_spec.rb
@@ -5,10 +5,6 @@ RSpec.describe Datadog::Tracing::Contrib::Rails::Utils do
   describe 'railtie_supported?' do
     subject(:railtie_supported?) { described_class.railtie_supported? }
 
-    before do
-      stub_const('::Rails::Railtie', instance_double('::Rails::Railtie'))
-    end
-
     context 'on rails 2 and below' do
       before { stub_const('::Rails::VERSION::MAJOR', 2) }
 
@@ -18,6 +14,7 @@ RSpec.describe Datadog::Tracing::Contrib::Rails::Utils do
     context 'on rails 3 and above' do
       before do
         stub_const('::Rails::VERSION::MAJOR', 3)
+        stub_const('::Rails::Railtie', instance_double('::Rails::Railtie'))
       end
 
       it { is_expected.to be true }
