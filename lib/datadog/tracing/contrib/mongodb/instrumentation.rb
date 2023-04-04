e08ec4cf7fbd5b0afc903752472364a45399972c
Marco Costa <marco.costa@datadoghq.com>
2023-04-04 14:32:27 -0700

    Lint:frozen_string_literal when only string usage is for require

--- lib/datadog/tracing/contrib/mongodb/instrumentation.rb
+++ lib/datadog/tracing/contrib/mongodb/instrumentation.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'ext'
 require_relative 'parsers'
 require_relative 'subscribers'
--- lib/datadog/tracing/contrib/shoryuken/patcher.rb
+++ lib/datadog/tracing/contrib/shoryuken/patcher.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'tracer'
 
 module Datadog
