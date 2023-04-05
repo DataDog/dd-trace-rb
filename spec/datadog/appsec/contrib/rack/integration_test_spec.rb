867914654c223d62b08260668942a4a4c95e385e
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2023-04-05 23:04:49 +0200

    Set libddwaf timeout to a high value for specs
    This removes a frequent cause of flakiness

--- spec/datadog/appsec/contrib/rack/integration_test_spec.rb
+++ spec/datadog/appsec/contrib/rack/integration_test_spec.rb
@@ -130,6 +130,7 @@ RSpec.describe 'Rack integration tests' do
       c.tracing.instrument :rack
 
       c.appsec.enabled = appsec_enabled
+      c.appsec.waf_timeout = 10_000_000 # in us
       c.appsec.instrument :rack
       c.appsec.ip_denylist = appsec_ip_denylist
       c.appsec.user_id_denylist = appsec_user_id_denylist
