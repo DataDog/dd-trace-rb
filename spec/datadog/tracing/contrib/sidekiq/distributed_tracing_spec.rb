5c213302c919f04bcb31bb7f07f97a4e8b8ff391
Tony Hsu <tonyc.t.hsu@gmail.com>
2023-04-05 13:16:13 +0200

    Add negative distributed tracing tests

--- spec/datadog/tracing/contrib/sidekiq/distributed_tracing_spec.rb
+++ spec/datadog/tracing/contrib/sidekiq/distributed_tracing_spec.rb
@@ -19,12 +19,6 @@ RSpec.describe 'Sidekiq distributed tracing' do
     end
   end
 
-  before do
-    Datadog.configure do |c|
-      c.tracing.instrument :sidekiq, distributed_tracing: true
-    end
-  end
-
   after do
     Datadog.configuration.tracing[:sidekiq].reset!
     Sidekiq::Queues.clear_all
@@ -40,78 +34,174 @@ RSpec.describe 'Sidekiq distributed tracing' do
     )
   end
 
-  context 'when dispatching' do
-    it do
-      EmptyWorker.perform_async
-
-      job = EmptyWorker.jobs.first
-
-      expect(span).to be_root_span
-      expect(span.service).to eq(tracer.default_service)
-      expect(span.resource).to eq('EmptyWorker')
-      expect(span.get_tag('sidekiq.job.queue')).to eq('default')
-      expect(span.status).to eq(0)
-      expect(span.get_tag('component')).to eq('sidekiq')
-      expect(span.get_tag('operation')).to eq('push')
-      expect(span.get_tag('span.kind')).to eq('producer')
-
-      expect(job['x-datadog-trace-id']).to eq(span.trace_id.to_s)
-      expect(job['x-datadog-parent-id']).to eq(span.id.to_s)
-      expect(job['x-datadog-sampling-priority']).to eq('1')
-      # expect(job["x-datadog-tags"]).to eq("_dd.p.dm=-0")
+  context 'when distributed tracing enabled' do
+    before do
+      Datadog.configure do |c|
+        c.tracing.instrument :sidekiq, distributed_tracing: true
+      end
     end
-  end
 
-  context 'when receiving' do
-    let(:trace_id) { Datadog::Tracing::Utils.next_id }
-    let(:span_id) { Datadog::Tracing::Utils.next_id }
-    let(:jid) { '123abc' }
+    context 'when dispatching' do
+      it 'propagates through serialized job' do
+        EmptyWorker.perform_async
 
-    it do
-      Sidekiq::Queues.push(
-        EmptyWorker.queue,
-        EmptyWorker.to_s,
-        EmptyWorker.sidekiq_options.merge(
-          'args' => [],
-          'class' => EmptyWorker.to_s,
-          'jid' => jid,
-          'x-datadog-trace-id' => trace_id.to_s,
-          'x-datadog-parent-id' => span_id.to_s,
-          'x-datadog-sampling-priority' => '1',
-          'x-datadog-tags' => '_dd.p.dm=-0',
+        job = EmptyWorker.jobs.first
+
+        expect(span).to be_root_span
+        expect(span.service).to eq(tracer.default_service)
+        expect(span.resource).to eq('EmptyWorker')
+        expect(span.get_tag('sidekiq.job.queue')).to eq('default')
+        expect(span.status).to eq(0)
+        expect(span.get_tag('component')).to eq('sidekiq')
+        expect(span.get_tag('operation')).to eq('push')
+        expect(span.get_tag('span.kind')).to eq('producer')
+
+        expect(job['x-datadog-trace-id']).to eq(span.trace_id.to_s)
+        expect(job['x-datadog-parent-id']).to eq(span.id.to_s)
+        expect(job['x-datadog-sampling-priority']).to eq('1')
+        # expect(job["x-datadog-tags"]).to eq("_dd.p.dm=-0")
+      end
+    end
+
+    context 'when receiving' do
+      let(:trace_id) { Datadog::Tracing::Utils.next_id }
+      let(:span_id) { Datadog::Tracing::Utils.next_id }
+      let(:jid) { '123abc' }
+
+      it 'continues trace from serialized job' do
+        Sidekiq::Queues.push(
+          EmptyWorker.queue,
+          EmptyWorker.to_s,
+          EmptyWorker.sidekiq_options.merge(
+            'args' => [],
+            'class' => EmptyWorker.to_s,
+            'jid' => jid,
+            'x-datadog-trace-id' => trace_id.to_s,
+            'x-datadog-parent-id' => span_id.to_s,
+            'x-datadog-sampling-priority' => '1',
+            'x-datadog-tags' => '_dd.p.dm=-0',
+          )
         )
-      )
 
-      EmptyWorker.perform_one
+        EmptyWorker.perform_one
 
-      expect(span.trace_id).to eq(trace_id)
-      expect(span.parent_id).to eq(span_id)
-      expect(span.service).to eq(tracer.default_service)
-      expect(span.resource).to eq('EmptyWorker')
-      expect(span.get_tag('sidekiq.job.queue')).to eq('default')
-      expect(span.status).to eq(0)
-      expect(span.get_tag('component')).to eq('sidekiq')
-      expect(span.get_tag('operation')).to eq('job')
-      expect(span.get_tag('span.kind')).to eq('consumer')
+        expect(span.trace_id).to eq(trace_id)
+        expect(span.parent_id).to eq(span_id)
+        expect(span.service).to eq(tracer.default_service)
+        expect(span.resource).to eq('EmptyWorker')
+        expect(span.get_tag('sidekiq.job.queue')).to eq('default')
+        expect(span.status).to eq(0)
+        expect(span.get_tag('component')).to eq('sidekiq')
+        expect(span.get_tag('operation')).to eq('job')
+        expect(span.get_tag('span.kind')).to eq('consumer')
 
-      # "x-datadog-sampling-priority" => "1",
-      # "x-datadog-tags" => "_dd.p.dm=-0",
+        # "x-datadog-sampling-priority" => "1",
+        # "x-datadog-tags" => "_dd.p.dm=-0",
+      end
+    end
+
+    context 'round trip' do
+      it 'creates 2 spans for a distributed trace' do
+        EmptyWorker.perform_async
+        EmptyWorker.perform_one
+
+        expect(spans).to have(2).items
+
+        job_span, push_span = spans
+
+        expect(push_span).to be_root_span
+
+        expect(job_span.trace_id).to eq(push_span.trace_id)
+        expect(job_span.parent_id).to eq(push_span.id)
+      end
     end
   end
 
-  context 'round trip' do
-    it do
-      EmptyWorker.perform_async
-      EmptyWorker.perform_one
+  context 'when distributed tracing disabled' do
+    before do
+      Datadog.configure do |c|
+        c.tracing.instrument :sidekiq, distributed_tracing: false
+      end
+    end
 
-      expect(spans).to have(2).items
+    context 'when dispatching' do
+      it 'does not propagate through serialized job' do
+        EmptyWorker.perform_async
 
-      job_span, push_span = spans
+        job = EmptyWorker.jobs.first
 
-      expect(push_span).to be_root_span
+        expect(span).to be_root_span
+        expect(span.service).to eq(tracer.default_service)
+        expect(span.resource).to eq('EmptyWorker')
+        expect(span.get_tag('sidekiq.job.queue')).to eq('default')
+        expect(span.status).to eq(0)
+        expect(span.get_tag('component')).to eq('sidekiq')
+        expect(span.get_tag('operation')).to eq('push')
+        expect(span.get_tag('span.kind')).to eq('producer')
 
-      expect(job_span.trace_id).to eq(push_span.trace_id)
-      expect(job_span.parent_id).to eq(push_span.id)
+        expect(job).to_not include('x-datadog-trace-id')
+        expect(job).to_not include('x-datadog-parent-id')
+        expect(job).to_not include('x-datadog-sampling-priority')
+        expect(job).to_not include('x-datadog-tags')
+      end
+    end
+
+    context 'when receiving' do
+      let(:trace_id) { Datadog::Tracing::Utils.next_id }
+      let(:span_id) { Datadog::Tracing::Utils.next_id }
+      let(:jid) { '123abc' }
+
+      it 'does not continue trace from serialized job' do
+        Sidekiq::Queues.push(
+          EmptyWorker.queue,
+          EmptyWorker.to_s,
+          EmptyWorker.sidekiq_options.merge(
+            'args' => [],
+            'class' => EmptyWorker.to_s,
+            'jid' => jid,
+            'x-datadog-trace-id' => trace_id.to_s,
+            'x-datadog-parent-id' => span_id.to_s,
+            'x-datadog-sampling-priority' => '1',
+            'x-datadog-tags' => '_dd.p.dm=-0',
+          )
+        )
+
+        EmptyWorker.perform_one
+
+        expect(span).to be_root_span
+        expect(span.trace_id).not_to eq(trace_id)
+        expect(span.parent_id).to eq(0)
+        expect(span.service).to eq(tracer.default_service)
+        expect(span.resource).to eq('EmptyWorker')
+        expect(span.get_tag('sidekiq.job.queue')).to eq('default')
+        expect(span.status).to eq(0)
+        expect(span.get_tag('component')).to eq('sidekiq')
+        expect(span.get_tag('operation')).to eq('job')
+        expect(span.get_tag('span.kind')).to eq('consumer')
+
+        # "x-datadog-sampling-priority" => "1",
+        # "x-datadog-tags" => "_dd.p.dm=-0",
+      end
+    end
+
+    context 'round trip' do
+      it 'creates 2 spans with separate traces' do
+        EmptyWorker.perform_async
+        EmptyWorker.perform_one
+
+        expect(spans).to have(2).items
+
+        job_span, push_span = spans
+
+        expect(push_span.trace_id).to_not eq(job_span.trace_id)
+        expect(push_span.get_tag('sidekiq.job.id')).to eq(job_span.get_tag('sidekiq.job.id'))
+
+        expect(push_span).to be_root_span
+        expect(job_span.resource).to eq('EmptyWorker')
+
+        expect(job_span).to be_root_span
+        expect(job_span.resource).to eq('EmptyWorker')
+      end
     end
   end
 end
