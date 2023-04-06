426f47b563edbdcc6ee8afd4018d8b042ba7bb78
Marco Costa <marco.costa@datadoghq.com>
2023-04-06 14:54:54 -0700

    Enable Telemetry by default

--- spec/datadog/core/configuration/settings_spec.rb
+++ spec/datadog/core/configuration/settings_spec.rb
@@ -1086,7 +1086,7 @@ RSpec.describe Datadog::Core::Configuration::Settings do
         context 'is not defined' do
           let(:environment) { nil }
 
-          it { is_expected.to be false }
+          it { is_expected.to be true }
         end
 
         { 'true' => true, 'false' => false }.each do |string, value|
