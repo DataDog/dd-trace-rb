ce599f6ceb1d0a6c8e3c0e4b7dad96610d098699
Dmytro Yurchenko <dmytro.yurchenko@datadoghq.com>
2023-04-06 13:05:51 +0200

    Fix tests

--- spec/ddtrace/release_gem_spec.rb
+++ spec/ddtrace/release_gem_spec.rb
@@ -35,7 +35,7 @@ RSpec.describe 'gem release process' do
         /x
 
         directories_excluded = %r{
-          ^(sig|spec|docs|\.circleci|\.github|lib-injection|benchmarks|gemfiles|integration|tasks|yard|vendor/rbs)/
+          ^(sig|spec|docs|\.circleci|\.github|\.gitlab|lib-injection|benchmarks|gemfiles|integration|tasks|yard|vendor/rbs)/
         }x
 
         expect(files)
