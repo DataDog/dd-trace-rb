26a03231865c12b56d124ee5b4d90c14e1ddb2f2
Zarir Hamza <zarir.hamza@datadoghq.com>
2023-04-07 14:16:07 -0400

    Adds default service naming and tests for httpclient

--- lib/datadog/tracing/contrib/httpclient/configuration/settings.rb
+++ lib/datadog/tracing/contrib/httpclient/configuration/settings.rb
@@ -27,7 +27,12 @@ module Datadog
             option :distributed_tracing, default: true
 
             option :service_name do |o|
-              o.default { ENV.fetch(Ext::ENV_SERVICE_NAME, Ext::DEFAULT_PEER_SERVICE_NAME) }
+              o.default do
+                Contrib::SpanAttributeSchema.fetch_service_name(
+                  Ext::ENV_SERVICE_NAME,
+                  Ext::DEFAULT_PEER_SERVICE_NAME
+                )
+              end
               o.lazy
             end
 
