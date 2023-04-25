56b5df7ab7bb3e12cbd05c7970df9e55f28e07e3
Zarir Hamza <zarir.hamza@datadoghq.com>
2023-04-25 19:09:28 -0400

    Adds shared example testing for service name setting for part two

--- spec/datadog/tracing/contrib/racecar/configuration/settings_spec.rb
+++ spec/datadog/tracing/contrib/racecar/configuration/settings_spec.rb
@@ -1,31 +1,6 @@
 require 'datadog/tracing/contrib/racecar/configuration/settings'
+require 'datadog/tracing/contrib/service_name_settings_examples'
 
 RSpec.describe Datadog::Tracing::Contrib::Racecar::Configuration::Settings do
-  describe 'Option `service_name`' do
-    context 'when with service_name' do # default to include base
-      it do
-        expect(described_class.new(service_name: 'test-service').service_name).to eq('test-service')
-      end
-    end
-
-    context 'when without service_name v0' do # default to include base
-      it do
-        with_modified_env DD_TRACE_SPAN_ATTRIBUTE_SCHEMA: 'v0' do
-          expect(described_class.new.service_name).to eq('racecar')
-        end
-      end
-    end
-
-    context 'when without service_name v1' do # default to include base
-      it do
-        with_modified_env DD_TRACE_SPAN_ATTRIBUTE_SCHEMA: 'v1' do
-          expect(described_class.new.service_name).to eq('rspec')
-        end
-      end
-    end
-  end
-
-  def with_modified_env(options = {}, &block)
-    ClimateControl.modify(options, &block)
-  end
+  it_behaves_like 'service name setting', 'racecar'
 end
