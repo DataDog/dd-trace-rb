f66d83ed4b2fd01696f0b6bd416d63057e52a2e5
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2023-04-26 15:58:44 +0200

    Handle dev build versions

--- spec/datadog/core/environment/identity_spec.rb
+++ spec/datadog/core/environment/identity_spec.rb
@@ -88,5 +88,21 @@ RSpec.describe Datadog::Core::Environment::Identity do
 
       it { is_expected.to eq('10.20.30-beta40') }
     end
+
+    context 'when development' do
+      before do
+        expect(described_class).to receive(:tracer_version).and_return('10.20.30.gha12345.ga1b2c3d4.a.branch.name')
+      end
+
+      it { is_expected.to eq('10.20.30+gha12345.ga1b2c3d4.a-branch-name') }
+    end
+
+    context 'when prerelease and development' do
+      before do
+        expect(described_class).to receive(:tracer_version).and_return('10.20.30.beta40.gha12345.ga1b2c3d4.a.branch.name')
+      end
+
+      it { is_expected.to eq('10.20.30-beta40+gha12345.ga1b2c3d4.a-branch-name') }
+    end
   end
 end
