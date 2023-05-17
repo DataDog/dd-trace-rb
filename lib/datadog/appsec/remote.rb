02d7f8d80230c2e955215834d0ce62bd9364e598
Gustavo Caso <gustavo.caso@datadoghq.com>
2023-05-17 11:29:39 +0200

    parse and combine custom rules when receiving information from remote configuration

--- lib/datadog/appsec/remote.rb
+++ lib/datadog/appsec/remote.rb
@@ -48,6 +48,7 @@ module Datadog
           remote_features_enabled? ? ASM_PRODUCTS : []
         end
 
+        # rubocop:disable Metrics/MethodLength
         def receivers
           return [] unless remote_features_enabled?
 
@@ -58,6 +59,7 @@ module Datadog
             end
 
             rules = []
+            custom_rules = []
             data = []
             overrides = []
             exclusions = []
@@ -73,6 +75,7 @@ module Datadog
               when 'ASM'
                 overrides << parsed_content['rules_override'] if parsed_content['rules_override']
                 exclusions << parsed_content['exclusions'] if parsed_content['exclusions']
+                custom_rules << parsed_content['custom_rules'] if parsed_content['custom_rules']
               end
             end
 
@@ -89,6 +92,7 @@ module Datadog
               data: data,
               overrides: overrides,
               exclusions: exclusions,
+              custom_rules: custom_rules,
             )
 
             Datadog::AppSec.reconfigure(ruleset: ruleset)
@@ -96,6 +100,7 @@ module Datadog
 
           [receiver]
         end
+        # rubocop:enable Metrics/MethodLength
 
         private
 
