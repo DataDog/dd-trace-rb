bc3e34e9de7ec5d2bb7f4822edc21ebee426710c
Gustavo Caso <gustavo.caso@datadoghq.com>
2023-06-05 16:22:51 +0200

    rename variable name to service_entry_tags

--- lib/datadog/appsec/event.rb
+++ lib/datadog/appsec/event.rb
@@ -61,13 +61,13 @@ module Datadog
           )
 
           # prepare and gather tags to apply
-          trace_root_tags = build_service_entry_tags(event_group)
+          service_entry_tags = build_service_entry_tags(event_group)
           # complex types are unsupported, we need to serialize to a string
-          triggers = trace_root_tags.delete('_dd.appsec.triggers')
+          triggers = service_entry_tags.delete('_dd.appsec.triggers')
           span.set_tag('_dd.appsec.json', JSON.dump({ triggers: triggers }))
 
           # apply tags to service entry span
-          trace_root_tags.each do |key, value|
+          service_entry_tags.each do |key, value|
             span.set_tag(key, value)
           end
         end
