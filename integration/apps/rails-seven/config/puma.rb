463d76ca3d8735066b92121713cc59c5e2c9817e
Tony Hsu <tonyc.t.hsu@gmail.com>
2023-06-07 16:11:25 +0200

    Refactor with rails 7 minimal setup

--- integration/apps/rails-seven/config/puma.rb
+++ integration/apps/rails-seven/config/puma.rb
@@ -45,16 +45,3 @@ preload_app!
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
-
-on_worker_boot do
-  ActiveSupport.on_load(:active_record) do
-    ActiveRecord::Base.establish_connection
-  end
-
-  Datadog::DemoEnv.print_env('Puma worker environment')
-end
-
-before_fork do
-  ActiveRecord::Base.connection_pool.disconnect!
-  #$redis.pool_shutdown { |conn| conn.quit }
-end
