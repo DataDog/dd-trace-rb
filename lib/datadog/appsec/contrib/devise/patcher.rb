11e72fbf765d43af07a5057f3c3284d8fc0f8209
Gustavo Caso <gustavo.caso@datadoghq.com>
2023-06-08 17:07:54 +0200

    Extract modules to separate files to be able to unit test them

--- lib/datadog/appsec/contrib/devise/patcher.rb
+++ lib/datadog/appsec/contrib/devise/patcher.rb
@@ -1,122 +1,13 @@
 # frozen_string_literal: true
 
 require_relative '../patcher'
-require_relative 'resource'
-require_relative 'ext'
-require_relative 'tracking'
+require_relative 'patcher/authenticatable_patch'
+require_relative 'patcher/regsitration_controller_patch'
 
 module Datadog
   module AppSec
     module Contrib
       module Devise
-        # Hook in devise validate method
-        module AuthenticatablePatch
-          # rubocop:disable Metrics/MethodLength, Metrics/PerceivedComplexity
-          def validate(resource, &block)
-            result = super
-            return result unless AppSec.enabled?
-            return result if AppSec.settings.automated_track_user_events == Ext::DISABLED_MODE
-
-            appsec_scope = Datadog::AppSec.active_scope
-
-            return result unless appsec_scope
-
-            devise_resource = resource ? Resource.new(resource) : nil
-
-            event_information = {}
-            user_id = nil
-
-            if AppSec.settings.automated_track_user_events == Ext::EXTENDED_MODE && devise_resource
-              resource_email = devise_resource.email
-              resource_username = devise_resource.username
-
-              event_information[:email] = resource_email if resource_email
-              event_information[:username] = resource_username if resource_username
-            end
-
-            user_id = devise_resource.id if devise_resource && devise_resource.id
-
-            if result
-              if user_id
-                Tracking.track_login_success(
-                  appsec_scope.trace,
-                  appsec_scope.service_entry_span,
-                  user: { id: user_id.to_s },
-                  **event_information
-                )
-                Datadog.logger.debug { 'User Login Event success' }
-              else
-                Datadog.logger.warn { 'User Login Event success, but can\'t extract user ID. No event emitted' }
-              end
-
-              return result
-            end
-
-            if devise_resource
-              Tracking.track_login_failure(
-                appsec_scope.trace,
-                appsec_scope.service_entry_span,
-                user_id: user_id,
-                user_exists: true,
-                **event_information
-              )
-              Datadog.logger.debug { 'User Login Event failure users exists' }
-            else
-              Tracking.track_login_failure(
-                appsec_scope.trace,
-                appsec_scope.service_entry_span,
-                user_id: nil,
-                user_exists: false,
-                **event_information
-              )
-              Datadog.logger.debug { 'User Login Event failure users do not exists' }
-            end
-
-            result
-          end
-          # rubocop:enable Metrics/MethodLength, Metrics/PerceivedComplexity
-        end
-
-        # Hook in devise registration controller
-        module ResgistrationControllerPatch
-          def create
-            return super unless AppSec.enabled?
-            return super if AppSec.settings.automated_track_user_events == Ext::DISABLED_MODE
-
-            appsec_scope = Datadog::AppSec.active_scope
-            return super unless appsec_scope
-
-            super do |resource|
-              if resource.persisted?
-                devise_resource = Resource.new(resource)
-
-                event_information = {}
-                user_id = devise_resource.id if devise_resource && devise_resource.id
-
-                if AppSec.settings.automated_track_user_events == Ext::EXTENDED_MODE
-                  resource_email = devise_resource.email
-                  resource_username = devise_resource.username
-
-                  event_information[:email] = resource_email if resource_email
-                  event_information[:username] = resource_username if resource_username
-                end
-
-                if user_id
-                  Tracking.track_signup(
-                    appsec_scope.trace,
-                    appsec_scope.service_entry_span,
-                    user: { id: user_id.to_s },
-                    **event_information
-                  )
-                  Datadog.logger.debug { 'User Signup Event' }
-                else
-                  Datadog.logger.warn { 'User Signup Event, but can\'t extract user ID. No event emitted' }
-                end
-              end
-            end
-          end
-        end
-
         # Patcher for AppSec on Devise
         module Patcher
           include Datadog::AppSec::Contrib::Patcher
@@ -144,7 +35,7 @@ module Datadog
 
           def patch_registration_controller
             ::ActiveSupport.on_load(:after_initialize) do
-              ::Devise::RegistrationsController.prepend(ResgistrationControllerPatch)
+              ::Devise::RegistrationsController.prepend(RegistrationControllerPatch)
             end
           end
         end
