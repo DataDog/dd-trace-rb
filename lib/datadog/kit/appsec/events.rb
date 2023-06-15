6e78fb6eef6e6228046afdfe1197db5d69c5808f
Gustavo Caso <gustavo.caso@datadoghq.com>
2023-06-15 17:00:39 +0200

    move module methods from using self. to open the class singleton

--- lib/datadog/kit/appsec/events.rb
+++ lib/datadog/kit/appsec/events.rb
@@ -11,143 +11,145 @@ module Datadog
         LOGIN_FAILURE_EVENT = 'users.login.failure'
         SIGNUP_EVENT = 'users.signup'
 
-        # Attach login success event information to the trace
-        #
-        # This method is experimental and may change in the future.
-        #
-        # @param trace [TraceOperation] Trace to attach data to. Defaults to
-        #   active trace.
-        # @param span [SpanOperation] Span to attach data to. Defaults to
-        #   active span on trace. Note that this should be a service entry span.
-        #   When AppSec is enabled, the expected span and trace are automatically
-        #   used as defaults.
-        # @param user [Hash<Symbol, String>] User information to pass to
-        #   Datadog::Kit::Identity.set_user. Must contain at least :id as key.
-        # @param others [Hash<String || Symbol, String>] Additional free-form
-        #   event information to attach to the trace.
-        def self.track_login_success(trace = nil, span = nil, user:, **others)
-          if (appsec_scope = Datadog::AppSec.active_scope)
-            trace = appsec_scope.trace
-            span = appsec_scope.service_entry_span
+        class << self
+          # Attach login success event information to the trace
+          #
+          # This method is experimental and may change in the future.
+          #
+          # @param trace [TraceOperation] Trace to attach data to. Defaults to
+          #   active trace.
+          # @param span [SpanOperation] Span to attach data to. Defaults to
+          #   active span on trace. Note that this should be a service entry span.
+          #   When AppSec is enabled, the expected span and trace are automatically
+          #   used as defaults.
+          # @param user [Hash<Symbol, String>] User information to pass to
+          #   Datadog::Kit::Identity.set_user. Must contain at least :id as key.
+          # @param others [Hash<String || Symbol, String>] Additional free-form
+          #   event information to attach to the trace.
+          def track_login_success(trace = nil, span = nil, user:, **others)
+            if (appsec_scope = Datadog::AppSec.active_scope)
+              trace = appsec_scope.trace
+              span = appsec_scope.service_entry_span
+            end
+
+            trace ||= Datadog::Tracing.active_trace
+            span ||= trace.active_span || Datadog::Tracing.active_span
+
+            raise ArgumentError, "span #{span.span_id} does not belong to trace #{trace.id}" if trace.id != span.trace_id
+
+            track(LOGIN_SUCCESS_EVENT, trace, span, **others)
+
+            user_options = user.dup
+            user_id = user_options.delete(:id)
+
+            raise ArgumentError, 'missing required key: :user => { :id }' if user_id.nil?
+
+            Kit::Identity.set_user(trace, span, id: user_id, **user_options)
           end
 
-          trace ||= Datadog::Tracing.active_trace
-          span ||= trace.active_span || Datadog::Tracing.active_span
+          # Attach login failure event information to the trace
+          #
+          # This method is experimental and may change in the future.
+          #
+          # @param trace [TraceOperation] Trace to attach data to. Defaults to
+          #   active trace.
+          # @param span [SpanOperation] Span to attach data to. Defaults to
+          #   active span on trace. Note that this should be a service entry span.
+          #   When AppSec is enabled, the expected span and trace are automatically
+          #   used as defaults.
+          # @param user_id [String] User id that attempted login
+          # @param user_exists [bool] Whether the user id that did a login attempt exists.
+          # @param others [Hash<String || Symbol, String>] Additional free-form
+          #   event information to attach to the trace.
+          def track_login_failure(trace = nil, span = nil, user_id:, user_exists:, **others)
+            if (appsec_scope = Datadog::AppSec.active_scope)
+              trace = appsec_scope.trace
+              span = appsec_scope.service_entry_span
+            end
 
-          raise ArgumentError, "span #{span.span_id} does not belong to trace #{trace.id}" if trace.id != span.trace_id
+            trace ||= Datadog::Tracing.active_trace
+            span ||= trace.active_span || Datadog::Tracing.active_span
 
-          track(LOGIN_SUCCESS_EVENT, trace, span, **others)
+            raise ArgumentError, "span #{span.span_id} does not belong to trace #{trace.id}" if trace.id != span.trace_id
 
-          user_options = user.dup
-          user_id = user_options.delete(:id)
+            track(LOGIN_FAILURE_EVENT, trace, span, **others)
 
-          raise ArgumentError, 'missing required key: :user => { :id }' if user_id.nil?
+            raise ArgumentError, 'user_id cannot be nil' if user_id.nil?
 
-          Kit::Identity.set_user(trace, span, id: user_id, **user_options)
-        end
-
-        # Attach login failure event information to the trace
-        #
-        # This method is experimental and may change in the future.
-        #
-        # @param trace [TraceOperation] Trace to attach data to. Defaults to
-        #   active trace.
-        # @param span [SpanOperation] Span to attach data to. Defaults to
-        #   active span on trace. Note that this should be a service entry span.
-        #   When AppSec is enabled, the expected span and trace are automatically
-        #   used as defaults.
-        # @param user_id [String] User id that attempted login
-        # @param user_exists [bool] Whether the user id that did a login attempt exists.
-        # @param others [Hash<String || Symbol, String>] Additional free-form
-        #   event information to attach to the trace.
-        def self.track_login_failure(trace = nil, span = nil, user_id:, user_exists:, **others)
-          if (appsec_scope = Datadog::AppSec.active_scope)
-            trace = appsec_scope.trace
-            span = appsec_scope.service_entry_span
+            span.set_tag('appsec.events.users.login.failure.usr.id', user_id)
+            span.set_tag('appsec.events.users.login.failure.usr.exists', user_exists)
           end
 
-          trace ||= Datadog::Tracing.active_trace
-          span ||= trace.active_span || Datadog::Tracing.active_span
+          # Attach signup event information to the trace
+          #
+          # This method is experimental and may change in the future.
+          #
+          # @param trace [TraceOperation] Trace to attach data to. Defaults to
+          #   active trace.
+          # @param span [SpanOperation] Span to attach data to. Defaults to
+          #   active span on trace. Note that this should be a service entry span.
+          #   When AppSec is enabled, the expected span and trace are automatically
+          #   used as defaults.
+          # @param user [Hash<Symbol, String>] User information to pass to
+          #   Datadog::Kit::Identity.set_user. Must contain at least :id as key.
+          # @param others [Hash<String || Symbol, String>] Additional free-form
+          #   event information to attach to the trace.
+          def track_signup(trace = nil, span = nil, user:, **others)
+            user_options = user.dup
+            user_id = user_options.delete(:id)
 
-          raise ArgumentError, "span #{span.span_id} does not belong to trace #{trace.id}" if trace.id != span.trace_id
+            raise ArgumentError, 'missing required key: :user => { :id }' if user_id.nil?
 
-          track(LOGIN_FAILURE_EVENT, trace, span, **others)
+            if (appsec_scope = Datadog::AppSec.active_scope)
+              trace = appsec_scope.trace
+              span = appsec_scope.service_entry_span
+            end
 
-          raise ArgumentError, 'user_id cannot be nil' if user_id.nil?
+            trace ||= Datadog::Tracing.active_trace
+            span ||= trace.active_span || Datadog::Tracing.active_span
 
-          span.set_tag('appsec.events.users.login.failure.usr.id', user_id)
-          span.set_tag('appsec.events.users.login.failure.usr.exists', user_exists)
-        end
+            raise ArgumentError, "span #{span.span_id} does not belong to trace #{trace.id}" if trace.id != span.trace_id
 
-        # Attach signup event information to the trace
-        #
-        # This method is experimental and may change in the future.
-        #
-        # @param trace [TraceOperation] Trace to attach data to. Defaults to
-        #   active trace.
-        # @param span [SpanOperation] Span to attach data to. Defaults to
-        #   active span on trace. Note that this should be a service entry span.
-        #   When AppSec is enabled, the expected span and trace are automatically
-        #   used as defaults.
-        # @param user [Hash<Symbol, String>] User information to pass to
-        #   Datadog::Kit::Identity.set_user. Must contain at least :id as key.
-        # @param others [Hash<String || Symbol, String>] Additional free-form
-        #   event information to attach to the trace.
-        def self.track_signup(trace = nil, span = nil, user:, **others)
-          user_options = user.dup
-          user_id = user_options.delete(:id)
-
-          raise ArgumentError, 'missing required key: :user => { :id }' if user_id.nil?
-
-          if (appsec_scope = Datadog::AppSec.active_scope)
-            trace = appsec_scope.trace
-            span = appsec_scope.service_entry_span
+            track(SIGNUP_EVENT, trace, **others)
+            Kit::Identity.set_user(trace, id: user_id, **user_options)
           end
 
-          trace ||= Datadog::Tracing.active_trace
-          span ||= trace.active_span || Datadog::Tracing.active_span
+          # Attach custom event information to the trace
+          #
+          # This method is experimental and may change in the future.
+          #
+          # @param event [String] Mandatory. Event code.
+          # @param trace [TraceOperation] Trace to attach data to. Defaults to
+          #   active trace.
+          # @param span [SpanOperation] Span to attach data to. Defaults to
+          #   active span on trace. Note that this should be a service entry span.
+          #   When AppSec is enabled, the expected span and trace are automatically
+          #   used as defaults.
+          # @param others [Hash<Symbol, String>] Additional free-form
+          #   event information to attach to the trace. Key must not
+          #   be :track.
+          def track(event, trace = nil, span = nil, **others)
+            if (appsec_scope = Datadog::AppSec.active_scope)
+              trace = appsec_scope.trace
+              span = appsec_scope.service_entry_span
+            end
 
-          raise ArgumentError, "span #{span.span_id} does not belong to trace #{trace.id}" if trace.id != span.trace_id
+            trace ||= Datadog::Tracing.active_trace
+            span ||= trace.active_span || Datadog::Tracing.active_span
 
-          track(SIGNUP_EVENT, trace, **others)
-          Kit::Identity.set_user(trace, id: user_id, **user_options)
-        end
+            raise ArgumentError, "span #{span.span_id} does not belong to trace #{trace.id}" if trace.id != span.trace_id
 
-        # Attach custom event information to the trace
-        #
-        # This method is experimental and may change in the future.
-        #
-        # @param event [String] Mandatory. Event code.
-        # @param trace [TraceOperation] Trace to attach data to. Defaults to
-        #   active trace.
-        # @param span [SpanOperation] Span to attach data to. Defaults to
-        #   active span on trace. Note that this should be a service entry span.
-        #   When AppSec is enabled, the expected span and trace are automatically
-        #   used as defaults.
-        # @param others [Hash<Symbol, String>] Additional free-form
-        #   event information to attach to the trace. Key must not
-        #   be :track.
-        def self.track(event, trace = nil, span = nil, **others)
-          if (appsec_scope = Datadog::AppSec.active_scope)
-            trace = appsec_scope.trace
-            span = appsec_scope.service_entry_span
+            span.set_tag("appsec.events.#{event}.track", 'true')
+            span.set_tag("_dd.appsec.appsec.events.#{event}.sdk", 'true')
+
+            others.each do |k, v|
+              raise ArgumentError, 'key cannot be :track' if k.to_sym == :track
+
+              span.set_tag("appsec.events.#{event}.#{k}", v) unless v.nil?
+            end
+
+            trace.keep!
           end
-
-          trace ||= Datadog::Tracing.active_trace
-          span ||= trace.active_span || Datadog::Tracing.active_span
-
-          raise ArgumentError, "span #{span.span_id} does not belong to trace #{trace.id}" if trace.id != span.trace_id
-
-          span.set_tag("appsec.events.#{event}.track", 'true')
-          span.set_tag("_dd.appsec.appsec.events.#{event}.sdk", 'true')
-
-          others.each do |k, v|
-            raise ArgumentError, 'key cannot be :track' if k.to_sym == :track
-
-            span.set_tag("appsec.events.#{event}.#{k}", v) unless v.nil?
-          end
-
-          trace.keep!
         end
       end
     end
--- lib/datadog/kit/identity.rb
+++ lib/datadog/kit/identity.rb
@@ -6,84 +6,86 @@ module Datadog
   module Kit
     # Tracking identity via traces
     module Identity
-      # Attach user information to the trace
-      #
-      # @param trace [TraceOperation] Trace to attach data to. Defaults to
-      #   active trace.
-      # @param span [SpanOperation] Span to attach data to. Defaults to
-      #   active span on trace. Note that this should be a service entry span.
-      #   When AppSec is enabled, the expected span and trace are automatically
-      #   used as defaults.
-      # @param id [String] Mandatory. Username or client id extracted
-      #   from the access token or Authorization header in the inbound request
-      #   from outside the system.
-      # @param email [String] Email of the authenticated user associated
-      #   to the trace.
-      # @param name [String] User-friendly name. To be displayed in the
-      #   UI if set.
-      # @param session_id [String] Session ID of the authenticated user.
-      # @param role [String] Actual/assumed role the client is making
-      #   the request under extracted from token or application security
-      #   context.
-      # @param scope [String] Scopes or granted authorities the client
-      #   currently possesses extracted from token or application security
-      #   context. The value would come from the scope associated with an OAuth
-      #   2.0 Access Token or an attribute value in a SAML 2.0 Assertion.
-      # @param others [Hash<Symbol, String>] Additional free-form
-      #   user information to attach to the trace.
-      #
-      # rubocop:disable Metrics/CyclomaticComplexity
-      # rubocop:disable Metrics/PerceivedComplexity
-      # rubocop:disable Metrics/AbcSize
-      def self.set_user(
-        trace = nil, span = nil, id:, email: nil, name: nil, session_id: nil, role: nil, scope: nil, **others
-      )
-        raise ArgumentError, 'missing required key: :id' if id.nil?
+      class << self
+        # Attach user information to the trace
+        #
+        # @param trace [TraceOperation] Trace to attach data to. Defaults to
+        #   active trace.
+        # @param span [SpanOperation] Span to attach data to. Defaults to
+        #   active span on trace. Note that this should be a service entry span.
+        #   When AppSec is enabled, the expected span and trace are automatically
+        #   used as defaults.
+        # @param id [String] Mandatory. Username or client id extracted
+        #   from the access token or Authorization header in the inbound request
+        #   from outside the system.
+        # @param email [String] Email of the authenticated user associated
+        #   to the trace.
+        # @param name [String] User-friendly name. To be displayed in the
+        #   UI if set.
+        # @param session_id [String] Session ID of the authenticated user.
+        # @param role [String] Actual/assumed role the client is making
+        #   the request under extracted from token or application security
+        #   context.
+        # @param scope [String] Scopes or granted authorities the client
+        #   currently possesses extracted from token or application security
+        #   context. The value would come from the scope associated with an OAuth
+        #   2.0 Access Token or an attribute value in a SAML 2.0 Assertion.
+        # @param others [Hash<Symbol, String>] Additional free-form
+        #   user information to attach to the trace.
+        #
+        # rubocop:disable Metrics/CyclomaticComplexity
+        # rubocop:disable Metrics/PerceivedComplexity
+        # rubocop:disable Metrics/AbcSize
+        def set_user(
+          trace = nil, span = nil, id:, email: nil, name: nil, session_id: nil, role: nil, scope: nil, **others
+        )
+          raise ArgumentError, 'missing required key: :id' if id.nil?
 
-        # enforce types
+          # enforce types
 
-        raise TypeError, ':id must be a String'         unless id.is_a?(String)
-        raise TypeError, ':email must be a String'      unless email.nil? || email.is_a?(String)
-        raise TypeError, ':name must be a String'       unless name.nil? || name.is_a?(String)
-        raise TypeError, ':session_id must be a String' unless session_id.nil? || session_id.is_a?(String)
-        raise TypeError, ':role must be a String'       unless role.nil? || role.is_a?(String)
-        raise TypeError, ':scope must be a String'      unless scope.nil? || scope.is_a?(String)
+          raise TypeError, ':id must be a String'         unless id.is_a?(String)
+          raise TypeError, ':email must be a String'      unless email.nil? || email.is_a?(String)
+          raise TypeError, ':name must be a String'       unless name.nil? || name.is_a?(String)
+          raise TypeError, ':session_id must be a String' unless session_id.nil? || session_id.is_a?(String)
+          raise TypeError, ':role must be a String'       unless role.nil? || role.is_a?(String)
+          raise TypeError, ':scope must be a String'      unless scope.nil? || scope.is_a?(String)
 
-        others.each do |k, v|
-          raise TypeError, "#{k.inspect} must be a String" unless v.nil? || v.is_a?(String)
+          others.each do |k, v|
+            raise TypeError, "#{k.inspect} must be a String" unless v.nil? || v.is_a?(String)
+          end
+
+          if (appsec_scope = Datadog::AppSec.active_scope)
+            trace = appsec_scope.trace
+            span = appsec_scope.service_entry_span
+          end
+
+          trace ||= Datadog::Tracing.active_trace
+          span ||= trace.active_span || Datadog::Tracing.active_span
+
+          raise ArgumentError, "span #{span.span_id} does not belong to trace #{trace.id}" if trace.id != span.trace_id
+
+          # set tags once data is known consistent
+
+          span.set_tag('usr.id', id)
+          span.set_tag('usr.email', email)           unless email.nil?
+          span.set_tag('usr.name', name)             unless name.nil?
+          span.set_tag('usr.session_id', session_id) unless session_id.nil?
+          span.set_tag('usr.role', role)             unless role.nil?
+          span.set_tag('usr.scope', scope)           unless scope.nil?
+
+          others.each do |k, v|
+            span.set_tag("usr.#{k}", v) unless v.nil?
+          end
+
+          if appsec_scope
+            user = ::Datadog::AppSec::Instrumentation::Gateway::User.new(id)
+            ::Datadog::AppSec::Instrumentation.gateway.push('identity.set_user', user)
+          end
         end
-
-        if (appsec_scope = Datadog::AppSec.active_scope)
-          trace = appsec_scope.trace
-          span = appsec_scope.service_entry_span
-        end
-
-        trace ||= Datadog::Tracing.active_trace
-        span ||= trace.active_span || Datadog::Tracing.active_span
-
-        raise ArgumentError, "span #{span.span_id} does not belong to trace #{trace.id}" if trace.id != span.trace_id
-
-        # set tags once data is known consistent
-
-        span.set_tag('usr.id', id)
-        span.set_tag('usr.email', email)           unless email.nil?
-        span.set_tag('usr.name', name)             unless name.nil?
-        span.set_tag('usr.session_id', session_id) unless session_id.nil?
-        span.set_tag('usr.role', role)             unless role.nil?
-        span.set_tag('usr.scope', scope)           unless scope.nil?
-
-        others.each do |k, v|
-          span.set_tag("usr.#{k}", v) unless v.nil?
-        end
-
-        if appsec_scope
-          user = ::Datadog::AppSec::Instrumentation::Gateway::User.new(id)
-          ::Datadog::AppSec::Instrumentation.gateway.push('identity.set_user', user)
-        end
+        # rubocop:enable Metrics/AbcSize
+        # rubocop:enable Metrics/PerceivedComplexity
+        # rubocop:enable Metrics/CyclomaticComplexity
       end
-      # rubocop:enable Metrics/AbcSize
-      # rubocop:enable Metrics/PerceivedComplexity
-      # rubocop:enable Metrics/CyclomaticComplexity
     end
   end
 end
