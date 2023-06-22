e80a9e64aa0d863a2c74e6b9b6a1c8c36df3c1a0
Zarir Hamza <zarir.hamza@datadoghq.com>
2023-06-22 15:09:17 -0400

    updates comment for more explicit context for set_peer_service?

--- lib/datadog/tracing/contrib/span_attribute_schema.rb
+++ lib/datadog/tracing/contrib/span_attribute_schema.rb
@@ -29,25 +29,36 @@ module Datadog
           # TODO: add logic for remap as long as the above expression is true
         end
 
-        # set_peer_service?: checks to see if any edited peer.service tags exist so that they are not overwritten
+        # set_peer_service?: returns boolean to reflect if peer.service should be set as long
+        # This is to prevent overwriting of pre-existing peer.service tags
         def set_peer_service?(span)
           ps = span.get_tag(Tracing::Metadata::Ext::TAG_PEER_SERVICE)
           if ps && (ps != '')
 
-            # if peer.service is not equal to span.service we know it is edited
+            # In v0, peer.service is set directly equal to span.service by default.
+            # That value is not the expected functionality here so we can overwrite that peer.service tag
+            # assuming it is in the default state of being equal to the global service.
+            #
+            # If peer.service is not equal to span.service we know this value was explicitly set so we do not overwrite it
             if ps != span.service
               span.set_tag(Tracing::Metadata::Ext::TAG_PEER_SERVICE_SOURCE, Tracing::Metadata::Ext::TAG_PEER_SERVICE)
               return false
             end
 
-            # if span.service is not the global service, we know it was changed to change the peer.service value
+            # There is a unique scenario where the user enables peer.service while in v0.
+            # This means that the peer.service tag can be changed with span.service.
+            # In order to respect that change, we check if span.service was actually modified or in its default state.
+            # Thus if span.service is not equal to the global service,
+            # we can assume the user changed it meaning that this peer.service value must stay.
+            #
+            # If span.service is not the global service, we know it was changed and we keep the peer.service value
             if (ps == span.service) && (span.service != Datadog.configuration.service)
               span.set_tag(Tracing::Metadata::Ext::TAG_PEER_SERVICE_SOURCE, Tracing::Metadata::Ext::TAG_PEER_SERVICE)
               return false
             end
           end
 
-          # only allow peer.service to be changed if it is an outbound span with the correct schema version
+          # Check that peer.service is only set on spankind client/producer spans while v1 is enabled
           if ((span.get_tag(Tracing::Metadata::Ext::TAG_KIND) == Tracing::Metadata::Ext::SpanKind::TAG_CLIENT) ||
             (span.get_tag(Tracing::Metadata::Ext::TAG_KIND) == Tracing::Metadata::Ext::SpanKind::TAG_PRODUCER)) &&
               (Datadog.configuration.tracing.span_attribute_schema ==
@@ -65,6 +76,8 @@ module Datadog
         # Also sets the source of where the information for peer.service was extracted from
         # Returns a boolean if peer.service was successfully set or not
         def set_peer_service_from_source(span)
+
+          # outsource to each integration to make them provide it as a mandatory integration
           case
           when span.get_tag(Aws::Ext::TAG_AWS_SERVICE)
             sources = Tracing::Contrib::Ext::SpanAttributeSchema::PEER_SERVICE_SOURCE_AWS
@@ -77,6 +90,8 @@ module Datadog
           else
             return false
           end
+
+          # make separate array and freeze it as constant and search separately
           sources <<
             Tracing::Metadata::Ext::NET::TAG_DESTINATION_NAME <<
             Tracing::Metadata::Ext::TAG_PEER_HOSTNAME <<
