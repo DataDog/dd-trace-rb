c1430d0bbf3fb52d46d7ffff4ae650d2aee5e3a4
Tony Hsu <tonyc.t.hsu@gmail.com>
2023-06-30 11:16:17 +0200

    Implement mutatble default

--- lib/datadog/tracing/contrib/rails/log_injection.rb
+++ lib/datadog/tracing/contrib/rails/log_injection.rb
@@ -7,15 +7,12 @@ module Datadog
         module LogInjection
           module_function
 
-          def add_as_tagged_logging_logger(app)
-            # we want to check if the current logger is a tagger logger instance
-            # log_tags defaults to nil so we have to set as an array if nothing exists yet
-            if (log_tags = app.config.log_tags).nil?
-              app.config.log_tags = [proc { Tracing.log_correlation }]
-            # if existing log_tags configuration exists, append to the end of the array
-            elsif log_tags.is_a?(Array)
-              app.config.log_tags << proc { Tracing.log_correlation }
-            end
+          def set_mutatable_default(app)
+            app.config.log_tags = Array(app.config.log_tags)
+          end
+
+          def append_datadog_correlation_tags(app)
+            app.config.log_tags << proc { Tracing.log_correlation }
           rescue StandardError => e
             # TODO: can we use Datadog.logger at this point?
             Datadog.logger.warn(
