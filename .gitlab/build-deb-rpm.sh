37503a9607a5176aaec6b2a3eb7c0c801735dcb3
Tony Hsu <tonyc.t.hsu@gmail.com>
2023-07-11 12:14:20 +0200

    Rename directories by removing -static suffix

--- .gitlab/build-deb-rpm.sh
+++ .gitlab/build-deb-rpm.sh
@@ -6,6 +6,25 @@ fi
 
 source common_build_functions.sh
 
+# Get a list of directories with the "-static" suffix
+directories=($(find ../vendor -maxdepth 4 -type d -name "*-static"))
+
+# Check if any directories were found
+if [ ${#directories[@]} -eq 0 ]; then
+  echo "No directories with the '-static' suffix found."
+  exit 1
+fi
+
+# Loop through the directories
+for dir in "${directories[@]}"; do
+  # Remove the "./" prefix and the "-static" suffix from the directory name
+  new_dir="${dir#./}"
+  new_dir="${new_dir%-static}"
+  # Rename the directory
+  mv "$dir" "$new_dir"
+  echo "Directory '$dir' renamed to '$new_dir'."
+done
+
 # Not sure how it is called
 fpm_wrapper "datadog-apm-library-ruby" "$RUBY_PACKAGE_VERSION" \
  --input-type dir \
