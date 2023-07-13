be7e099469d2345f0c668d06f9b2b83a24c06f43
Gustavo Caso <gustavo.caso@datadoghq.com>
2023-07-13 12:49:01 +0200

    Add backport support for dup for Bignum and Fixnum

--- lib/datadog/core/backport.rb
+++ lib/datadog/core/backport.rb
@@ -23,14 +23,16 @@ module Datadog
     # This module is used to provide features from Ruby 2.4+ to older Rubies
     module BackportFrom24
       if RUBY_VERSION < '2.4'
+        # rubocop:disable Lint/UnifiedInteger
         def self.dup(value)
           case value
-          when NilClass, TrueClass, FalseClass, Integer, Float
+          when NilClass, TrueClass, FalseClass, Integer, Float, Bignum, Fixnum
             value
           else
             value.dup
           end
         end
+        # rubocop:enable Lint/UnifiedInteger
       else
         def self.dup(value)
           value.dup
