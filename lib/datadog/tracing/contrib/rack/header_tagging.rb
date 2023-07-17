132c4f98f6d308e95192da797c36b452d0efeb75
Marco Costa <marco.costa@datadoghq.com>
2023-07-17 13:46:09 -0700

    Simplify rack/header_tagging.rb

--- lib/datadog/tracing/contrib/rack/header_tagging.rb
+++ lib/datadog/tracing/contrib/rack/header_tagging.rb
@@ -28,23 +28,18 @@ module Datadog
           end
 
           def self.tag_response_headers(span, headers, configuration)
+            headers = Core::Utils::Hash::CaseInsensitiveWrapper.new(headers) # Make header access case-insensitive
+
             # Use global DD_TRACE_HEADER_TAGS if integration-level configuration is not provided
             tags = if configuration.using_default?(:headers) && Datadog.configuration.tracing.header_tags.configured?
-                     Datadog.configuration.tracing.header_tags.response_tags(
-                       Core::Utils::Hash::CaseInsensitiveWrapper.new(headers)
-                     )
+                     Datadog.configuration.tracing.header_tags.response_tags(headers)
                    else
                      whitelist = configuration[:headers][:response] || []
                      whitelist.each_with_object({}) do |header, result|
-                       if headers.key?(header)
-                         result[Tracing::Metadata::Ext::HTTP::ResponseHeaders.to_tag(header)] = headers[header]
-                       else
-                         # Try a case-insensitive lookup
-                         uppercased_header = header.to_s.upcase
-                         matching_header = headers.keys.find { |h| h.upcase == uppercased_header }
-                         if matching_header
-                           result[Tracing::Metadata::Ext::HTTP::ResponseHeaders.to_tag(header)] = headers[matching_header]
-                         end
+                       header_value = headers[header]
+                       unless header_value.nil?
+                         header_tag = Tracing::Metadata::Ext::HTTP::ResponseHeaders.to_tag(header)
+                         result[header_tag] = header_value
                        end
                      end
                    end
