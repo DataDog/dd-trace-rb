047e85da71c829c1d9dcd956f3cf6038c6ad3d46
Tony Hsu <tony.hsu@datadoghq.com>
2023-08-02 15:16:13 +0200

    Fix hash log_tags warning

--- lib/datadog/tracing/contrib/rails/log_injection.rb
+++ lib/datadog/tracing/contrib/rails/log_injection.rb
@@ -8,9 +8,12 @@ module Datadog
           module_function
 
           # Use `app.config.log_tags` to inject propagation tags into the default Rails logger.
-          def configure_log_tags(app)
-            app.config.log_tags ||= [] # Can be nil, we initialized it if so
-            app.config.log_tags << proc { Tracing.log_correlation if Datadog.configuration.tracing.log_injection }
+          def configure_log_tags(app_cofig)
+            # Semantic Logger's named tags override Rails built-in config.log_tags with a hash value
+            return if Hash === app_cofig.log_tags
+
+            app_cofig.log_tags ||= [] # Can be nil, we initialized it if so
+            app_cofig.log_tags << proc { Tracing.log_correlation if Datadog.configuration.tracing.log_injection }
           rescue StandardError => e
             Datadog.logger.warn(
               "Unable to add Datadog Trace context to ActiveSupport::TaggedLogging: #{e.class.name} #{e.message}"
