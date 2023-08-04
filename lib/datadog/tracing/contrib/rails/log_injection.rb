23753a82ada0e36b830aa2657e712f87d9f17760
rqz <pyrchanton@gmail.com>
2023-08-05 01:05:03 +0200

    Avoid override Rails application config 'log_tags' option when SemanticLogger used as application logger

--- lib/datadog/tracing/contrib/rails/log_injection.rb
+++ lib/datadog/tracing/contrib/rails/log_injection.rb
@@ -9,6 +9,8 @@ module Datadog
 
           # Use `app.config.log_tags` to inject propagation tags into the default Rails logger.
           def configure_log_tags(app)
+            return if defined?(::SemanticLogger) && app.cofig.logger.is_a?(::SemanticLogger::Logger)
+
             app.config.log_tags ||= [] # Can be nil, we initialized it if so
             app.config.log_tags << proc { Tracing.log_correlation if Datadog.configuration.tracing.log_injection }
           rescue StandardError => e
