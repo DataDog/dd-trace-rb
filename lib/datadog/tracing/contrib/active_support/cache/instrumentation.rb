37a2e2227fe9e89b03d5d84e5ac1e6970ed4161b
Marco Costa <marco.costa@datadoghq.com>
2023-08-21 15:16:55 -0700

    Refresh comment

--- lib/datadog/tracing/contrib/active_support/cache/instrumentation.rb
+++ lib/datadog/tracing/contrib/active_support/cache/instrumentation.rb
@@ -111,14 +111,20 @@ module Datadog
                   tracing_context: {},
                   # The name of the store is never saved anywhere.
                   # ActiveSupport looks up stores by converting a symbol into a 'require' path:
-                  # https://github.com/rails/rails/blob/261975dbef77731d2c76f907f1076c5132ebc0e4/activesupport/lib/active_support/cache.rb#L139-L149
+                  # ```
+                  # require "active_support/cache/#{store}"
+                  # ```
+                  # @see https://github.com/rails/rails/blob/261975dbef77731d2c76f907f1076c5132ebc0e4/activesupport/lib/active_support/cache.rb#L139-L149
                   #
-                  # Given we are inside the store object itself, we can reverse engineer
-                  # the `require` path by converting the class name to snake case:
+                  # As `self` is the store object, we can reverse engineer
+                  # the original symbol by converting the class name to snake case:
                   # e.g. ActiveSupport::Cache::RedisStore -> active_support/cache/redis_store
+                  # In this case `redis_store` is the store name.
                   #
-                  # We only care about the last part of the path, but it's easier to convert
-                  # the whole class name then extract the last part.
+                  # Because there's no API retrieve only the class name
+                  # (only `RedisStore`, and not `ActiveSupport::Cache::RedisStore`)
+                  # the easiest way to retrieve the store symbol is to convert the fully qualified
+                  # name using Rails-provided methods, then extracting the last part.
                   store: self.class.name.underscore.match(/active_support\/cache\/(.*)/)[1]
                 }
 
