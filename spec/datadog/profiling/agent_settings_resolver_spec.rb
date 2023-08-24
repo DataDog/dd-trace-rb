f0d52c810ba82312c0f6e738debdc34a1642664c
Edmund Kump <edmund.kump@datadoghq.com>
2023-08-24 13:48:50 -0400

    remove empty spec files for profiling and tracing AgentSettingsResolvers

--- spec/datadog/profiling/agent_settings_resolver_spec.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-require 'spec_helper'
-require 'datadog/profiling/agent_settings_resolver'
-require 'datadog/core/configuration/settings'
--- spec/datadog/tracing/configuration/agent_settings_resolver_spec.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-require 'spec_helper'
-require 'datadog/tracing/configuration/agent_settings_resolver'
-require 'datadog/core/configuration/settings'
