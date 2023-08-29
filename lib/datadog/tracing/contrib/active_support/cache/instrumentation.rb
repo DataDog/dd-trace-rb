6f2528e7ed51927de09424a7093090cd53418f66
Zarir Hamza <zarir.hamza@datadoghq.com>
2023-08-29 13:15:23 -0400

    fix active_support service reference

--- lib/datadog/tracing/contrib/active_support/cache/instrumentation.rb
+++ lib/datadog/tracing/contrib/active_support/cache/instrumentation.rb
@@ -29,7 +29,7 @@ module Datadog
                 span.set_tag(Tracing::Metadata::Ext::TAG_COMPONENT, Ext::TAG_COMPONENT)
                 span.set_tag(Tracing::Metadata::Ext::TAG_OPERATION, Ext::TAG_OPERATION_CACHE)
 
-                if service != Datadog.configuration.service
+                if span.service != Datadog.configuration.service
                   span.set_tag(Tracing::Contrib::Ext::Metadata::TAG_BASE_SERVICE, Datadog.configuration.service)
                 end
 
