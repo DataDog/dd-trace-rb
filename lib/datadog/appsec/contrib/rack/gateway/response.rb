58c349a982778397a9ae2a74d3cccd75caa5e1e9
Gustavo Caso <gustavo.caso@datadoghq.com>
2023-09-18 17:38:26 +0200

    Parse response body information and propagate it to the WAF.

--- lib/datadog/appsec/contrib/rack/gateway/response.rb
+++ lib/datadog/appsec/contrib/rack/gateway/response.rb
@@ -19,8 +19,48 @@ module Datadog
               @scope = scope
             end
 
+            def parsed_body
+              return unless body.instance_of?(Array)
+              return unless supported_response_type
+
+              body_dup = body.dup # avoid interating over the body. This is just in case code.
+              result = ''.dup
+              all_body_parts_are_string = true
+
+              body_dup.each do |body_part|
+                if body_part.is_a?(String)
+                  result.concat(body_part)
+                else
+                  all_body_parts_are_string = false
+                  break
+                end
+              end
+
+              return unless all_body_parts_are_string
+
+              if json?
+                JSON.parse(result)
+              else
+                result
+              end
+            end
+
             def response
               @response ||= ::Rack::Response.new(body, status, headers)
+            end
+
+            private
+
+            def supported_response_type
+              json? || text?
+            end
+
+            def json?
+              headers['content-type'].include?('json')
+            end
+
+            def text?
+              headers['content-type'].include?('text')
             end
           end
         end
--- spec/datadog/appsec/contrib/rack/reactive/response_spec.rb
+++ spec/datadog/appsec/contrib/rack/reactive/response_spec.rb
@@ -10,10 +10,11 @@ RSpec.describe Datadog::AppSec::Contrib::Rack::Reactive::Response do
   let(:operation) { Datadog::AppSec::Reactive::Operation.new('test') }
   let(:processor_context) { instance_double(Datadog::AppSec::Processor::Context) }
   let(:scope) { instance_double(Datadog::AppSec::Scope, processor_context: processor_context) }
+  let(:body) { ['Ok'] }
 
   let(:response) do
     Datadog::AppSec::Contrib::Rack::Gateway::Response.new(
-      'Ok',
+      body,
       200,
       { 'content-type' => 'text/html', 'set-cookie' => 'foo' },
       scope: scope,
@@ -27,6 +28,10 @@ RSpec.describe Datadog::AppSec::Contrib::Rack::Reactive::Response do
         'response.headers',
         { 'content-type' => 'text/html', 'set-cookie' => 'foo' },
       )
+      expect(operation).to receive(:publish).with(
+        'response.body',
+        'Ok',
+      )
 
       described_class.publish(operation, response)
     end
@@ -35,35 +40,78 @@ RSpec.describe Datadog::AppSec::Contrib::Rack::Reactive::Response do
   describe '.subscribe' do
     context 'not all addresses have been published' do
       it 'does not call the waf context' do
-        expect(operation).to receive(:subscribe).with('response.status', 'response.headers').and_call_original
+        expect(operation).to receive(:subscribe).with(
+          'response.status',
+          'response.headers',
+          'response.body'
+        ).and_call_original
         expect(processor_context).to_not receive(:run)
         described_class.subscribe(operation, processor_context)
       end
     end
 
-    context 'all addresses have been published' do
-      it 'does call the waf context with the right arguments' do
+    context 'waf arguments' do
+      before do
         expect(operation).to receive(:subscribe).and_call_original
+      end
 
-        expected_waf_arguments = {
-          'server.response.status' => '200',
-          'server.response.headers' => {
-            'content-type' => 'text/html',
-            'set-cookie' => 'foo',
-          },
-          'server.response.headers.no_cookies' => {
-            'content-type' => 'text/html',
+      let(:waf_result) { double(:waf_result, status: :ok, timeout: false) }
+
+      context 'all addresses have been published' do
+        let(:expected_waf_arguments) do
+          {
+            'server.response.status' => '200',
+            'server.response.headers' => {
+              'content-type' => 'text/html',
+              'set-cookie' => 'foo',
+            },
+            'server.response.headers.no_cookies' => {
+              'content-type' => 'text/html',
+            },
+            'server.response.body' => 'Ok',
           }
-        }
+        end
 
-        waf_result = double(:waf_result, status: :ok, timeout: false)
-        expect(processor_context).to receive(:run).with(
-          expected_waf_arguments,
-          Datadog.configuration.appsec.waf_timeout
-        ).and_return(waf_result)
-        described_class.subscribe(operation, processor_context)
-        result = described_class.publish(operation, response)
-        expect(result).to be_nil
+        it 'does call the waf context with the right arguments' do
+          expect(processor_context).to receive(:run).with(
+            expected_waf_arguments,
+            Datadog.configuration.appsec.waf_timeout
+          ).and_return(waf_result)
+          described_class.subscribe(operation, processor_context)
+          result = described_class.publish(operation, response)
+          expect(result).to be_nil
+        end
+
+        context 'when parsed response body is nil' do
+          # since the body is not all string values we bail out in Datadog::AppSec::Contrib::Rack::Gateway::Response
+          let(:body) do
+            ['Ok', proc {
+            }]
+          end
+
+          let(:expected_waf_arguments) do
+            {
+              'server.response.status' => '200',
+              'server.response.headers' => {
+                'content-type' => 'text/html',
+                'set-cookie' => 'foo',
+              },
+              'server.response.headers.no_cookies' => {
+                'content-type' => 'text/html',
+              }
+            }
+          end
+
+          it 'does call the waf context with the right arguments' do
+            expect(processor_context).to receive(:run).with(
+              expected_waf_arguments,
+              Datadog.configuration.appsec.waf_timeout
+            ).and_return(waf_result)
+            described_class.subscribe(operation, processor_context)
+            result = described_class.publish(operation, response)
+            expect(result).to be_nil
+          end
+        end
       end
     end
 
