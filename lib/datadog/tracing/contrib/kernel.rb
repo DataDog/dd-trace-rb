1e3189178f839dc2112c36d260e3fa4609a39c49
Marco Costa <marco.costa@datadoghq.com>
2023-10-06 12:24:46 -0700

    Add Kernel patcher

---
+++ lib/datadog/tracing/contrib/kernel.rb
@@ -0,0 +1,54 @@
+module Datadog
+  module Tracing
+    module Contrib
+      module Kernel
+        def require(name)
+          # Returns `true` if file was loaded by this `super` operation.
+          # If it was already loaded, returns `false`.
+          # If `super` errs, it will raise an exception.
+          just_loaded = super
+
+          @@dd_instance.require(name) if just_loaded
+
+          just_loaded
+        end
+
+        class Instance
+          def initialize
+            @on_require = {}
+          end
+
+          def require(name)
+            if @on_require.include?(name)
+              Datadog.logger.debug { "Gem '#{name}' loaded. Invoking callback." }
+
+              @on_require[name].call
+            end
+          rescue => e
+            Datadog.logger.debug { "Failed to execute callback for gem '#{name}': #{e.class.name} #{e.message} at #{Array(e.backtrace).join("\n")}" }
+          end
+
+          def on_require(gem, &block)
+            @on_require[gem] = block
+          end
+        end
+
+        def self.on_require(gem, &block)
+          @@dd_instance.on_require(gem, &block)
+        end
+
+        def self.patch!
+          DD_PATCH_ONLY_ONCE.run do
+            @@dd_instance = Instance.new
+            ::Kernel.prepend(self)
+          end
+        end
+
+        DD_PATCH_ONLY_ONCE = Datadog::Core::Utils::OnlyOnce.new
+        private_constant :DD_PATCH_ONLY_ONCE
+      end
+    end
+  end
+end
+
+# Datadog::Tracing::Contrib::Kernel.patch! # TODO: I think this stays here actually!
