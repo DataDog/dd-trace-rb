92e379a81c3b7aba9d4bcff4c4af1e0a172b1bbc
Gustavo Caso <gustavocaso@gmail.com>
2023-11-10 18:35:52 +0100

    revert https://github.com/DataDog/dd-trace-rb/pull/3153 (#3252)

--- spec/datadog/appsec/contrib/rack/reactive/response_spec.rb
+++ spec/datadog/appsec/contrib/rack/reactive/response_spec.rb
@@ -29,55 +29,7 @@ RSpec.describe Datadog::AppSec::Contrib::Rack::Reactive::Response do
         'response.headers',
         headers,
       )
-      expect(operation).to receive(:publish).with(
-        'response.body',
-        nil,
-      )
-
       described_class.publish(operation, response)
-    end
-
-    context 'JSON response' do
-      let(:headers) { { 'content-type' => 'application/json', 'set-cookie' => 'foo' } }
-      let(:body) { ['{"a":"b"}'] }
-
-      context 'when parsed_response_body is enabled' do
-        it 'propagates response attributes to the operation' do
-          expect(operation).to receive(:publish).with('response.status', 200)
-          expect(operation).to receive(:publish).with(
-            'response.headers',
-            headers,
-          )
-          expect(operation).to receive(:publish).with(
-            'response.body',
-            { 'a' => 'b' },
-          )
-
-          described_class.publish(operation, response)
-        end
-      end
-
-      context 'when parsed_response_body is disabled' do
-        around do |example|
-          ClimateControl.modify('DD_API_SECURITY_PARSE_RESPONSE_BODY' => 'false') do
-            example.run
-          end
-        end
-
-        it 'propagates response attributes to the operation' do
-          expect(operation).to receive(:publish).with('response.status', 200)
-          expect(operation).to receive(:publish).with(
-            'response.headers',
-            headers,
-          )
-          expect(operation).to receive(:publish).with(
-            'response.body',
-            nil,
-          )
-
-          described_class.publish(operation, response)
-        end
-      end
     end
   end
 
@@ -87,7 +39,6 @@ RSpec.describe Datadog::AppSec::Contrib::Rack::Reactive::Response do
         expect(operation).to receive(:subscribe).with(
           'response.status',
           'response.headers',
-          'response.body'
         ).and_call_original
         expect(processor_context).to_not receive(:run)
         described_class.subscribe(operation, processor_context)
@@ -123,100 +74,6 @@ RSpec.describe Datadog::AppSec::Contrib::Rack::Reactive::Response do
           described_class.subscribe(operation, processor_context)
           result = described_class.publish(operation, response)
           expect(result).to be_nil
-        end
-      end
-
-      context 'JSON response' do
-        let(:headers) { { 'content-type' => 'application/json', 'set-cookie' => 'foo' } }
-        let(:body) { ['{"a":"b"}'] }
-
-        context 'when parsed_response_body is enabled' do
-          context 'all addresses have been published' do
-            let(:expected_waf_arguments) do
-              {
-                'server.response.status' => '200',
-                'server.response.headers' => {
-                  'content-type' => 'application/json',
-                  'set-cookie' => 'foo',
-                },
-                'server.response.headers.no_cookies' => {
-                  'content-type' => 'application/json',
-                },
-                'server.response.body' => { 'a' => 'b' },
-              }
-            end
-
-            it 'does call the waf context with the right arguments' do
-              expect(processor_context).to receive(:run).with(
-                expected_waf_arguments,
-                Datadog.configuration.appsec.waf_timeout
-              ).and_return(waf_result)
-              described_class.subscribe(operation, processor_context)
-              result = described_class.publish(operation, response)
-              expect(result).to be_nil
-            end
-          end
-
-          context 'when unparsable response body' do
-            # since the body is not all string values we bail out in Datadog::AppSec::Contrib::Rack::Gateway::Response
-            let(:body) do
-              [proc {}]
-            end
-
-            let(:expected_waf_arguments) do
-              {
-                'server.response.status' => '200',
-                'server.response.headers' => {
-                  'content-type' => 'application/json',
-                  'set-cookie' => 'foo',
-                },
-                'server.response.headers.no_cookies' => {
-                  'content-type' => 'application/json',
-                }
-              }
-            end
-
-            it 'does call the waf context with the right arguments' do
-              expect(processor_context).to receive(:run).with(
-                expected_waf_arguments,
-                Datadog.configuration.appsec.waf_timeout
-              ).and_return(waf_result)
-              described_class.subscribe(operation, processor_context)
-              result = described_class.publish(operation, response)
-              expect(result).to be_nil
-            end
-          end
-        end
-
-        context 'when parsed_response_body is disabled' do
-          around do |example|
-            ClimateControl.modify('DD_API_SECURITY_PARSE_RESPONSE_BODY' => 'false') do
-              example.run
-            end
-          end
-
-          let(:expected_waf_arguments) do
-            {
-              'server.response.status' => '200',
-              'server.response.headers' => {
-                'content-type' => 'application/json',
-                'set-cookie' => 'foo',
-              },
-              'server.response.headers.no_cookies' => {
-                'content-type' => 'application/json',
-              },
-            }
-          end
-
-          it 'does call the waf context with the right arguments' do
-            expect(processor_context).to receive(:run).with(
-              expected_waf_arguments,
-              Datadog.configuration.appsec.waf_timeout
-            ).and_return(waf_result)
-            described_class.subscribe(operation, processor_context)
-            result = described_class.publish(operation, response)
-            expect(result).to be_nil
-          end
         end
       end
     end
