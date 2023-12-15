e953f636810c82cd9c481cd153b3d1ce14d057bc
Alex Evanczuk <alexevanczuk@gmail.com>
2023-12-15 15:16:17 -0500

    Add `on_error` option

--- lib/datadog/tracing/contrib/faraday/middleware.rb
+++ lib/datadog/tracing/contrib/faraday/middleware.rb
@@ -24,7 +24,7 @@ module Datadog
             # Do this once to reduce expensive regex calls.
             request_options = build_request_options!(env)
 
-            Tracing.trace(Ext::SPAN_REQUEST) do |span, trace|
+            Tracing.trace(Ext::SPAN_REQUEST, on_error: request_options[:on_error]) do |span, trace|
               annotate!(span, env, request_options)
               propagate!(trace, span, env) if request_options[:distributed_tracing] && Tracing.enabled?
               app.call(env).on_complete { |resp| handle_response(span, resp, request_options) }
