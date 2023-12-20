3540276c767b80dc44edebc7d113bfae6fbd1d5c
Tony Hsu <tony.hsu@datadoghq.com>
2023-12-20 12:06:09 +0100

    Spec for `error_status_codes` settings

--- spec/datadog/tracing/contrib/shared_settings_examples.rb
+++ spec/datadog/tracing/contrib/shared_settings_examples.rb
@@ -27,3 +27,45 @@ RSpec.shared_examples_for 'with on_error setting' do
     end
   end
 end
+
+RSpec.shared_examples_for 'with error_status_codes setting' do |env:|
+  context 'default without settings' do
+    subject { described_class.new }
+
+    it { expect(subject.error_status_codes).to eq(400...600) }
+  end
+
+  context 'when configured with environment variable' do
+    subject { described_class.new }
+
+    context 'when given a single value' do
+      around do |example|
+        ClimateControl.modify(env => '500') do
+          example.run
+        end
+      end
+
+      it { expect(subject.error_status_codes).to eq(['500']) }
+    end
+
+    context 'when given a comma separated list' do
+      around do |example|
+        ClimateControl.modify(env => '400,500') do
+          example.run
+        end
+      end
+
+      it { expect(subject.error_status_codes).to eq(['400', '500']) }
+    end
+
+    context 'when given a comma separated list with space' do
+      around do |example|
+        ClimateControl.modify(env => '400,,500') do
+          example.run
+        end
+      end
+
+      it { expect(subject.error_status_codes).to eq(['400', '500']) }
+    end
+  end
+end
