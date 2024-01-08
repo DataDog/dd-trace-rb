e0fd1926a34e1d2b5ba9860691a0ff46cfcf2e24
Tony Hsu <tony.hsu@datadoghq.com>
2024-01-08 20:42:44 +0100

    Enable settings spec

--- spec/datadog/tracing/contrib/excon/configuration/settings_spec.rb
+++ spec/datadog/tracing/contrib/excon/configuration/settings_spec.rb
@@ -5,5 +5,5 @@ require 'datadog/tracing/contrib/shared_settings_examples'
 RSpec.describe Datadog::Tracing::Contrib::Excon::Configuration::Settings do
   it_behaves_like 'service name setting', 'excon'
   it_behaves_like 'with on_error setting'
-  it_behaves_like 'with error_status_codes setting', env: 'DD_TRACE_EXCON_ERROR_STATUS_CODES'
+  it_behaves_like 'with error_status_codes setting', env: 'DD_TRACE_EXCON_ERROR_STATUS_CODES', default: 400...600
 end
--- spec/datadog/tracing/contrib/grape/configuration/settings_spec.rb
+++ spec/datadog/tracing/contrib/grape/configuration/settings_spec.rb
@@ -2,6 +2,6 @@ require 'datadog/tracing/contrib/grape/configuration/settings'
 require 'datadog/tracing/contrib/shared_settings_examples'
 
 RSpec.describe Datadog::Tracing::Contrib::Grape::Configuration::Settings do
-  # it_behaves_like 'with on_error setting'
-  # it_behaves_like 'with error_status_codes setting', env: 'DD_TRACE_GRAPE_ERROR_STATUS_CODES'
+  it_behaves_like 'with on_error setting'
+  it_behaves_like 'with error_status_codes setting', env: 'DD_TRACE_GRAPE_ERROR_STATUS_CODES', default: 500...600
 end
