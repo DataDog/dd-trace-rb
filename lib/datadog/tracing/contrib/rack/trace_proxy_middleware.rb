913c5aeb8520941164d97e6f90eb79f9a73051dc
Tony Hsu <tony.hsu@datadoghq.com>
2024-01-08 13:16:08 +0100

    Cleanup module

--- lib/datadog/tracing/contrib/rack/trace_proxy_middleware.rb
+++ lib/datadog/tracing/contrib/rack/trace_proxy_middleware.rb
@@ -1,21 +1,13 @@
-require 'date'
-
-require_relative '../../../core/environment/variable_helpers'
-require_relative '../../../core/backport'
-require_relative '../../client_ip'
 require_relative '../../metadata/ext'
-require_relative '../../propagation/http'
 require_relative '../analytics'
-require_relative '../utils/quantization/http'
 require_relative 'ext'
-require_relative 'header_collection'
-require_relative 'header_tagging'
 require_relative 'request_queue'
 
 module Datadog
   module Tracing
     module Contrib
       module Rack
+        # Module to create virtual proxy span
         module TraceProxyMiddleware
           module_function
 
@@ -23,31 +15,17 @@ module Datadog
             return yield unless configuration[:request_queuing]
 
             # parse the request queue time
-            request_start = Contrib::Rack::QueueTime.get_request_start(env)
-            return yield unless request_start
+            start_time = Contrib::Rack::QueueTime.get_request_start(env)
+            return yield unless start_time
+
+            options = {
+              service: configuration[:web_service_name],
+              start_time: start_time,
+              span_type: Tracing::Metadata::Ext::HTTP::TYPE_PROXY
+            }
 
             case configuration[:request_queuing]
-            when :include_request
-
-              options = {
-                service: configuration[:web_service_name],
-                start_time: request_start,
-                span_type: Tracing::Metadata::Ext::HTTP::TYPE_PROXY
-              }
-
-              queue_span = Tracing.trace(Ext::SPAN_HTTP_SERVER_QUEUE, **options)
-              queue_span.set_tag(Tracing::Metadata::Ext::TAG_COMPONENT, Ext::TAG_COMPONENT)
-              queue_span.set_tag(Tracing::Metadata::Ext::TAG_OPERATION, Ext::TAG_OPERATION_HTTP_SERVER_QUEUE)
-              queue_span.set_tag(Tracing::Metadata::Ext::TAG_KIND, Tracing::Metadata::Ext::SpanKind::TAG_SERVER)
-
-              yield.tap { queue_span.finish }
-            when :exclude_request
-              options = {
-                service: configuration[:web_service_name],
-                start_time: request_start,
-                span_type: Tracing::Metadata::Ext::HTTP::TYPE_PROXY
-              }
-
+            when :exclude_request, true
               request_span = Tracing.trace(Ext::SPAN_HTTP_PROXY_REQUEST, **options)
 
               request_span.set_tag(Tracing::Metadata::Ext::TAG_COMPONENT, Ext::TAG_COMPONENT_HTTP_PROXY)
@@ -66,6 +44,13 @@ module Datadog
               queue_span.finish
 
               yield.tap { request_span.finish }
+            when :include_request
+              queue_span = Tracing.trace(Ext::SPAN_HTTP_SERVER_QUEUE, **options)
+              queue_span.set_tag(Tracing::Metadata::Ext::TAG_COMPONENT, Ext::TAG_COMPONENT)
+              queue_span.set_tag(Tracing::Metadata::Ext::TAG_OPERATION, Ext::TAG_OPERATION_HTTP_SERVER_QUEUE)
+              queue_span.set_tag(Tracing::Metadata::Ext::TAG_KIND, Tracing::Metadata::Ext::SpanKind::TAG_SERVER)
+
+              yield.tap { queue_span.finish }
             end
           end
         end
