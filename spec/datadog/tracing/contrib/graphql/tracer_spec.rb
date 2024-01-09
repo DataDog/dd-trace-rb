786d705c34ff2f813aeda97123a3f9ec4182c549
Tony Hsu <tony.hsu@datadoghq.com>
2024-01-09 18:10:10 +0100

    Fix without lex event

--- spec/datadog/tracing/contrib/graphql/tracer_spec.rb
+++ spec/datadog/tracing/contrib/graphql/tracer_spec.rb
@@ -77,7 +77,12 @@ RSpec.describe 'GraphQL patcher' do
 
           valid_operations << 'authorized'
 
-          expect(spans).to have(11).items
+          # Since 2.2,  new Ruby-based parser doesn't emit a "lex" event. (graphql/c_parser still does.)
+          if Gem::Version.new(GraphQL::VERSION) > Gem::Version.new('2.2')
+            expect(spans).to have(10).items
+          else
+            expect(spans).to have(11).items
+          end
         end
 
         # Expect root span to be 'execute.graphql'
