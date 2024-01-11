f76c87ffc02384f75c059b5b8a94038d9fa84ddf
Marco Costa <marco.costa@datadoghq.com>
2024-01-11 11:29:28 -0800

    Restore Rails 4 testing

--- appraisal/jruby-9.2.rb
+++ appraisal/jruby-9.2.rb
@@ -4,6 +4,50 @@ appraise 'hanami-1' do
   gem 'hanami', '~> 1'
 end
 
+appraise 'rails4-mysql2' do
+  # Rails 4.2.11.3 with bundler unlocked to > 2.0
+  gem 'rails', git: 'https://github.com/marcotc/rails', ref: '592dfae8747db3bb28c3292a9730817f0fa76885'
+  gem 'activerecord-jdbcmysql-adapter', platform: :jruby
+  gem 'sprockets', '< 4'
+  gem 'lograge', '~> 0.11'
+end
+
+appraise 'rails4-postgres' do
+  # Rails 4.2.11.3 with bundler unlocked to > 2.0
+  gem 'rails', git: 'https://github.com/marcotc/rails', ref: '592dfae8747db3bb28c3292a9730817f0fa76885'
+  gem 'pg', '< 1.0'
+  gem 'sprockets', '< 4'
+  gem 'lograge', '~> 0.11'
+end
+
+appraise 'rails4-semantic-logger' do
+  # Rails 4.2.11.3 with bundler unlocked to > 2.0
+  gem 'rails', git: 'https://github.com/marcotc/rails', ref: '592dfae8747db3bb28c3292a9730817f0fa76885'
+  gem 'pg', '< 1.0'
+  gem 'sprockets', '< 4'
+  gem 'rails_semantic_logger', '~> 4.0'
+end
+
+appraise 'rails4-postgres-redis' do
+  # Rails 4.2.11.3 with bundler unlocked to > 2.0
+  gem 'rails', git: 'https://github.com/marcotc/rails', ref: '592dfae8747db3bb28c3292a9730817f0fa76885'
+  gem 'pg', '< 1.0'
+  gem 'redis-rails'
+  gem 'redis', '< 4.0'
+  gem 'sprockets', '< 4'
+  gem 'lograge', '~> 0.11'
+end
+
+appraise 'rails4-postgres-sidekiq' do
+  # Rails 4.2.11.3 with bundler unlocked to > 2.0
+  gem 'rails', git: 'https://github.com/marcotc/rails', ref: '592dfae8747db3bb28c3292a9730817f0fa76885'
+  gem 'pg', '< 1.0'
+  gem 'sidekiq'
+  gem 'activejob'
+  gem 'sprockets', '< 4'
+  gem 'lograge', '~> 0.11'
+end
+
 appraise 'rails5-mysql2' do
   gem 'rails', '~> 5.2.1'
   gem 'activerecord-jdbcmysql-adapter', platform: :jruby
--- tasks/appraisal.rake
+++ tasks/appraisal.rake
@@ -155,17 +155,17 @@ namespace :appraisal do # rubocop:disable Metrics/BlockLength
 end
 
 TRACER_VERSIONS = [
-  '2.5',
-  '2.6',
-  '2.7',
-  '3.0',
-  '3.1',
-  '3.2',
-  '3.3',
+  # '2.5',
+  # '2.6',
+  # '2.7',
+  # '3.0',
+  # '3.1',
+  # '3.2',
+  # '3.3',
   # ADD NEW RUBIES HERE
   'jruby-9.2',
-  'jruby-9.3',
-  'jruby-9.4',
+  # 'jruby-9.3',
+  # 'jruby-9.4',
 ].freeze
 
 FORCE_BUNDLER_VERSION = {
