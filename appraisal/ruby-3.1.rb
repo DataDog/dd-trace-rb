ae1ec814d7d743e383857996bb9172828c368b3d
Tony Hsu <tony.hsu@datadoghq.com>
2024-01-25 21:50:03 +0100

    Update test matrix with official release

--- appraisal/ruby-3.1.rb
+++ appraisal/ruby-3.1.rb
@@ -105,7 +105,6 @@ end
 appraise 'contrib' do
   gem 'concurrent-ruby'
   gem 'dalli', '>= 3.0.0'
-  gem 'graphql', '>= 2.0'
   gem 'grpc', '>= 1.38.0', platform: :ruby # Minimum version with Ruby 3.0 support
   gem 'mongo', '>= 2.8.0', '< 2.15.0' # TODO: FIX TEST BREAKAGES ON >= 2.15 https://github.com/DataDog/dd-trace-rb/issues/1596
   gem 'rack-test' # Dev dependencies for testing rack-based code
@@ -119,14 +118,14 @@ appraise 'contrib' do
   gem 'que', '>= 1.0.0'
 end
 
-{
-  '2.2' => 'tonycthsu/datadog-tracing-span-type-change',
-  '2.1' => 'tonycthsu/backport-span-type-2.1.x',
-  '2.0' => 'tonycthsu/backport-datadog-span-type-to-2.0.x',
-  '1.13' => 'tonycthsu/backport-datadog-span-type-to-1.13.x',
-}.each do |v, ref|
+[
+  '2.2',
+  '2.1',
+  '2.0',
+  '1.13',
+].each do |v|
   appraise "graphql-#{v}" do
-    gem 'graphql', github: 'TonyCTHsu/graphql-ruby', ref: ref
+    gem 'graphql', "~> #{v}.0"
   end
 end
 
@@ -160,7 +159,6 @@ end
 
 appraise 'contrib-old' do
   gem 'dalli', '< 3.0.0'
-  gem 'graphql', '~> 1.12.0', '< 2.0' # TODO: Support graphql 1.13.x
   gem 'presto-client', '>= 0.5.14' # Renamed to trino-client in >= 1.0
   gem 'qless', '0.12.0'
 end
