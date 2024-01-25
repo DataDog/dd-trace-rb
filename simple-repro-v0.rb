5ab1061c3708a4e6429ba30d92bb78f4b3ececc1
Ivo Anjo <ivo.anjo@datadoghq.com>
2024-01-25 17:59:53 +0000

    Print malloc stats at end

--- simple-repro-v0.rb
+++ simple-repro-v0.rb
@@ -1,4 +1,5 @@
 require 'libdatadog'
+require 'datadog/profiling/load_native_extension'
 
 puts "Libdatadog from: #{Libdatadog.pkgconfig_folder}"
 
@@ -11,6 +12,11 @@ def workload
   end
 end
 
-100.times { Thread.new { workload } }
+@start = Time.now
 
-sleep # 60
+Integer(ENV['THREADS'] || 100).times { Thread.new { workload } }
+
+sleep (60*10)
+
+Datadog::Profiling::NativeExtension::Testing._native_malloc_stats
+puts "Finished after #{(Time.now - @start).to_f} seconds"
