f626f912f80a4f262ee0f7da7903dee8fca11d48
Marco Costa <marco.costa@datadoghq.com>
2024-02-22 11:41:09 -0800

    Skip validation when forking is not available

--- spec/datadog/tracing/validate_benchmarks_spec.rb
+++ spec/datadog/tracing/validate_benchmarks_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 RSpec.describe 'Tracing benchmarks' do
+  before { skip('Spec requires Ruby VM supporting fork') unless PlatformHelpers.supports_fork? }
+
   around do |example|
     ClimateControl.modify('VALIDATE_BENCHMARK' => 'true') do
       example.run
