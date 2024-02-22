110997c770aeeabff02dd4973b23fc4fb15abf9a
Marco Costa <marco.costa@datadoghq.com>
2024-02-22 14:30:48 -0800

    Add benchmark that only excludes network

--- benchmarks/tracing_trace.rb
+++ benchmarks/tracing_trace.rb
@@ -7,22 +7,53 @@ require 'benchmark/ips'
 require 'ddtrace'
 
 class TracingTraceBenchmark
-  module FauxWriter
+  module NoopWriter
     def write(trace)
       # no-op
     end
-
-    ::Datadog::Tracing::Writer.prepend(self)
   end
 
-  def run_benchmark
+  module NoopAdapter
+    Response = Struct.new(:code, :body)
+
+    def open
+      Response.new(200)
+    end
+  end
+
+  def benchmark_no_writer
+    ::Datadog::Tracing::Writer.prepend(NoopWriter)
+
     Benchmark.ips do |x|
       benchmark_time = VALIDATE_BENCHMARK_MODE ? { time: 0.001, warmup: 0 } : { time: 10.5, warmup: 2 }
       x.config(**benchmark_time)
 
       def trace(x, depth)
         x.report(
-          "#{depth} span trace",
+          "#{depth} span trace - no writer",
+          (depth.times.map { "Datadog::Tracing.trace('op.name') {" } + depth.times.map { "}" }).join
+        )
+      end
+
+      trace(x, 1)
+      trace(x, 10)
+      trace(x, 100)
+
+      x.save! "#{__FILE__}-results.json" unless VALIDATE_BENCHMARK_MODE
+      x.compare!
+    end
+  end
+
+  def benchmark_no_network
+    ::Datadog::Core::Transport::HTTP::Adapters::Net.prepend(NoopAdapter)
+
+    Benchmark.ips do |x|
+      benchmark_time = VALIDATE_BENCHMARK_MODE ? { time: 0.001, warmup: 0 } : { time: 10.5, warmup: 2 }
+      x.config(**benchmark_time)
+
+      def trace(x, depth)
+        x.report(
+          "#{depth} span trace - no network",
           (depth.times.map { "Datadog::Tracing.trace('op.name') {" } + depth.times.map { "}" }).join
         )
       end
@@ -39,6 +70,15 @@ end
 
 puts "Current pid is #{Process.pid}"
 
+def run_benchmark(&block)
+  # Forking to avoid monkey-patching leaking between benchmarks
+  pid = fork { block.call }
+  _, status = Process.wait2(pid)
+
+  raise "Benchmark failed with status #{status}" unless status.success?
+end
+
 TracingTraceBenchmark.new.instance_exec do
-  run_benchmark
+  run_benchmark { benchmark_no_writer }
+  run_benchmark { benchmark_no_network }
 end
