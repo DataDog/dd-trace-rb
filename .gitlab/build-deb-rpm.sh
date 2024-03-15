b6fd30075f8e40f5667bbb40b2b5ed0fb5733e80
Tony Hsu <tony.hsu@datadoghq.com>
2024-03-15 21:45:42 +0100

    Add comment

--- .gitlab/build-deb-rpm.sh
+++ .gitlab/build-deb-rpm.sh
@@ -5,6 +5,10 @@ echo -n "$RUBY_PACKAGE_VERSION" > auto_inject-ruby.version
 
 source common_build_functions.sh
 
+# The normal settings for /tmp are 1777, which ls shows as drwxrwxrwt. That is wide open.
+#
+# This gives all users read access, and removes write access for group and others,
+# to all files and directories in the tmp directory.
 chmod -R a+r,go-w ../tmp/*
 
 fpm_wrapper "datadog-apm-library-ruby" "$RUBY_PACKAGE_VERSION" \
