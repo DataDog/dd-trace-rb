deff9cfe3c790e360956170c35fed6838d8e2bdb
Ivo Anjo <ivo.anjo@datadoghq.com>
2024-03-18 14:29:45 +0000

    [NO-TICKET] Add validation for gem file permissions
    **What does this PR do?**
    
    This PR adds validation so we catch the issue from #3529 before the
    incorrect packages are published on rubygems.org.
    
    Specifically, it checks that every file in packaged `.gem` file has
    the expected permissions.
    
    **Motivation:**
    
    Avoid #3529 happening again.
    
    **Additional Notes:**
    
    N/A
    
    **How to test the change?**
    
    You can run `bundle exec rake build` to trigger this validation. Try
    setting incorrect permissions on one of the files and running, and
    you should see the validation failing.

---
+++ spec/ddtrace/gem_packaging_spec.rb
@@ -0,0 +1,30 @@
+require 'rubygems'
+require 'rubygems/package'
+require 'rubygems/package/tar_reader'
+
+RSpec.describe 'gem release process (after packaging)' do
+  # TODO: This will need to be updated for the 2.0 branch
+  let(:gem_name) { 'ddtrace' }
+  let(:gem_version) { DDTrace::VERSION::STRING }
+  let(:packaged_gem_file) { "pkg/#{gem_name}-#{gem_version}.gem" }
+  let(:executable_permissions) { ['bin/ddprofrb', 'bin/ddtracerb'] }
+
+  it 'sets the right permissions on the gem files' do
+    Gem::Package::TarReader.new(File.open(packaged_gem_file)) do |tar|
+      data = tar.find { |entry| entry.header.name == 'data.tar.gz' }
+
+      Gem::Package::TarReader.new(Zlib::GzipReader.new(StringIO.new(data.read))) do |data_tar|
+        data_tar.each do |entry|
+          filename = entry.header.name
+          octal_permissions = entry.header.mode.to_s(8)[-3..-1]
+
+          expected_permissions = executable_permissions.include?(filename) ? '755' : '644'
+
+          expect(octal_permissions).to eq(expected_permissions),
+            "Unexpected permissions for #{filename} inside #{packaged_gem_file} (got #{octal_permissions}, " \
+            "expected #{expected_permissions})"
+        end
+      end
+    end
+  end
+end
