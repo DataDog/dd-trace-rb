4c585a451ae5b74b86285b85119ab325dfc18650
Marco Costa <marco.costa@datadoghq.com>
2024-04-02 12:16:33 -0700

    Rails: Remove circular require

--- lib/datadog/tracing/contrib/active_support/integration.rb
+++ lib/datadog/tracing/contrib/active_support/integration.rb
@@ -4,7 +4,7 @@ require_relative '../integration'
 require_relative 'configuration/settings'
 require_relative 'patcher'
 require_relative 'cache/redis'
-require_relative '../rails/integration'
+require_relative '../rails/ext'
 require_relative '../rails/utils'
 
 module Datadog
