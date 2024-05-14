d70f9564a66d2a5f95c63ebe9b67e990540d4d44
Oleg Pudeyev <code@olegp.name>
2024-05-14 12:41:07 -0400

    add missing requires to appsec

--- lib/datadog/appsec.rb
+++ lib/datadog/appsec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative 'core/configuration'
 require_relative 'appsec/configuration'
 require_relative 'appsec/extensions'
 require_relative 'appsec/scope'
