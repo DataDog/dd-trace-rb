9152470ad34b9abaf05ed4a0de4c0f6369fd9da3
Ivo Anjo <ivo.anjo@datadoghq.com>
2024-06-14 14:46:11 +0100

    Add nice description to dir interruption workaround setting
    ...that mentions https://github.com/DataDog/dd-trace-rb/issues/3450 .

--- lib/datadog/core/configuration/settings.rb
+++ lib/datadog/core/configuration/settings.rb
@@ -397,7 +397,13 @@ module Datadog
               end
             end
 
-            # TODO: description
+            # The profiler gathers data by sending `SIGPROF` unix signals to Ruby application threads.
+            #
+            # We've discovered that this can trigger a bug in a number of Ruby APIs in the `Dir` class, as
+            # described in https://github.com/DataDog/dd-trace-rb/issues/3450 . This workaround prevents the issue
+            # from happening by monkey patching the affected APIs.
+            #
+            # (In the future, once a fix lands upstream, we'll disable this workaround for Rubies that don't need it)
             #
             # @default `DD_PROFILING_DIR_INTERRUPTION_WORKAROUND_ENABLED` environment variable as a boolean,
             # otherwise `true`
