b6ebe19bb3bfb8c2f6e72dba9a30cc4a20bc77e4
Victor Pellan <victor.pellan@datadoghq.com>
2024-06-17 14:33:22 +0200

    Added userByName in test schema

--- spec/datadog/appsec/contrib/graphql/graphql_helper.rb
+++ spec/datadog/appsec/contrib/graphql/graphql_helper.rb
@@ -13,22 +13,39 @@ class TestGraphQLQuery < ::GraphQL::Schema::Object
   end
 
   def user(id:)
-    return OpenStruct.new(id: id, name: 'Caniche') if id == 10
+    return OpenStruct.new(id: id, name: 'Caniche') if Integer(id) == 10
 
     OpenStruct.new(id: id, name: 'Bits')
   end
+
+  field :userByName, TestUserType, null: false, description: 'Find an user by name' do
+    argument :name, ::GraphQL::Types::String, required: true
+  end
+
+  # rubocop:disable Naming/MethodName
+  def userByName(name:)
+    return OpenStruct.new(id: 10, name: name) if name == 'Caniche'
+
+    OpenStruct.new(id: 1, name: name)
+  end
+  # rubocop:enable Naming/MethodName
 end
 
 class TestGraphQLSchema < ::GraphQL::Schema
   query(TestGraphQLQuery)
 end
 
-RSpec.shared_context 'with GraphQL multiplex' do
+RSpec.shared_context 'with GraphQL schema' do
   let(:operation) { Datadog::AppSec::Reactive::Operation.new('test') }
   let(:schema) { TestGraphQLSchema }
+end
+
+RSpec.shared_context 'with GraphQL multiplex' do
+  include_context 'with GraphQL schema'
+
   let(:first_query) { ::GraphQL::Query.new(schema, 'query test{ user(id: 1) { name } }') }
   let(:second_query) { ::GraphQL::Query.new(schema, 'query test{ user(id: 10) { name } }') }
-  let(:third_query) { ::GraphQL::Query.new(schema, 'query { user(id: 5) { name } }') }
+  let(:third_query) { ::GraphQL::Query.new(schema, 'query { userByName(name: "Caniche") { id } }') }
   let(:queries) { [first_query, second_query, third_query] }
   let(:context) { { :dataloader => GraphQL::Dataloader.new(nonblocking: nil) } }
   let(:multiplex) do
