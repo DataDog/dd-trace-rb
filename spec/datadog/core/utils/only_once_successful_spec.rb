b9ed222d32ff28248a072935fa06ec4b8f2ec9fa
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2024-06-21 09:48:03 +0200

    add limit option to OnlyOnceSuccessful util

--- spec/datadog/core/utils/only_once_successful_spec.rb
+++ spec/datadog/core/utils/only_once_successful_spec.rb
@@ -1,28 +1,62 @@
 require 'datadog/core/utils/only_once_successful'
 
 RSpec.describe Datadog::Core::Utils::OnlyOnceSuccessful do
-  subject(:only_once_successful) { described_class.new }
+  subject(:only_once_successful) { described_class.new(limit) }
+
+  let(:limit) { 0 }
 
   describe '#run' do
-    context 'before running once' do
-      it do
-        expect { |block| only_once_successful.run(&block) }.to yield_control
+    context 'when limitless' do
+      context 'before running once' do
+        it do
+          expect { |block| only_once_successful.run(&block) }.to yield_control
+        end
+
+        it 'returns the result of the block ran' do
+          expect(only_once_successful.run { :result }).to be :result
+        end
       end
 
-      it 'returns the result of the block ran' do
-        expect(only_once_successful.run { :result }).to be :result
+      context 'after running once' do
+        let(:result) { nil }
+
+        before do
+          only_once_successful.run { result }
+        end
+
+        context 'when block returns truthy value' do
+          let(:result) { true }
+
+          it do
+            expect { |block| only_once_successful.run(&block) }.to_not yield_control
+          end
+
+          it do
+            expect(only_once_successful.run { :result }).to be nil
+          end
+        end
+
+        context 'when block returns falsey value' do
+          let(:result) { false }
+
+          it do
+            expect { |block| only_once_successful.run(&block) }.to yield_control
+          end
+
+          it 'runs again until block returns truthy value' do
+            expect(only_once_successful.run { :result }).to be :result
+
+            expect(only_once_successful.run { :result }).to be nil
+          end
+        end
       end
     end
 
-    context 'after running once' do
-      let(:result) { nil }
-
-      before do
-        only_once_successful.run { result }
-      end
+    context 'when limited' do
+      let(:limit) { 2 }
 
       context 'when block returns truthy value' do
-        let(:result) { true }
+        before { only_once_successful.run { true } }
 
         it do
           expect { |block| only_once_successful.run(&block) }.to_not yield_control
@@ -33,16 +67,18 @@ RSpec.describe Datadog::Core::Utils::OnlyOnceSuccessful do
         end
       end
 
-      context 'when block returns falsey value' do
-        let(:result) { false }
-
-        it do
-          expect { |block| only_once_successful.run(&block) }.to yield_control
+      context 'when block returns falsey value "limit" times' do
+        before do
+          limit.times do
+            only_once_successful.run { false }
+          end
         end
 
-        it 'runs again until block returns truthy value' do
-          expect(only_once_successful.run { :result }).to be :result
+        it do
+          expect { |block| only_once_successful.run(&block) }.to_not yield_control
+        end
 
+        it do
           expect(only_once_successful.run { :result }).to be nil
         end
       end
@@ -89,6 +125,108 @@ RSpec.describe Datadog::Core::Utils::OnlyOnceSuccessful do
         it do
           expect(only_once_successful.ran?).to be false
         end
+      end
+    end
+
+    context 'when limited and ran "limit" times' do
+      let(:limit) { 2 }
+
+      before do
+        limit.times do
+          only_once_successful.run { false }
+        end
+      end
+
+      it do
+        expect(only_once_successful.ran?).to be true
+      end
+    end
+  end
+
+  describe '#success?' do
+    context 'before running once' do
+      it do
+        expect(only_once_successful.success?).to be false
+      end
+    end
+
+    context 'after running once' do
+      let(:result) { nil }
+
+      before do
+        only_once_successful.run { result }
+      end
+
+      context 'when block returns truthy value' do
+        let(:result) { true }
+
+        it do
+          expect(only_once_successful.success?).to be true
+        end
+      end
+
+      context 'when block returns falsey value' do
+        it do
+          expect(only_once_successful.success?).to be false
+        end
+      end
+    end
+
+    context 'when limited and ran "limit" times' do
+      let(:limit) { 2 }
+
+      before do
+        limit.times do
+          only_once_successful.run { false }
+        end
+      end
+
+      it do
+        expect(only_once_successful.success?).to be false
+      end
+    end
+  end
+
+  describe '#failed?' do
+    context 'before running once' do
+      it do
+        expect(only_once_successful.failed?).to be false
+      end
+    end
+
+    context 'after running once' do
+      let(:result) { nil }
+
+      before do
+        only_once_successful.run { result }
+      end
+
+      context 'when block returns truthy value' do
+        let(:result) { true }
+
+        it do
+          expect(only_once_successful.failed?).to be false
+        end
+      end
+
+      context 'when block returns falsey value' do
+        it do
+          expect(only_once_successful.failed?).to be false
+        end
+      end
+    end
+
+    context 'when limited and ran "limit" times' do
+      let(:limit) { 2 }
+
+      before do
+        limit.times do
+          only_once_successful.run { false }
+        end
+      end
+
+      it do
+        expect(only_once_successful.failed?).to be true
       end
     end
   end
