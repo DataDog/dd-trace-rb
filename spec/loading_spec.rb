cf2c5d30b48d19e50f9b1a56a6ec085678116f2a
Oleg Pudeyev <code@olegp.name>
2024-06-25 13:37:26 -0400

    redo test to use system

---
+++ spec/loading_spec.rb
@@ -0,0 +1,39 @@
+require 'English'
+require 'shellwords'
+
+REQUIRES = {
+  'datadog/appsec' => 'Datadog::AppSec',
+  'datadog/core' => 'Datadog::Core',
+  'datadog/kit' => 'Datadog::Kit',
+  'datadog/profiling' => 'Datadog::Profiling',
+  'datadog/tracing' => 'Datadog::Tracing',
+}.freeze
+
+RSpec.describe 'loading of products' do
+  REQUIRES.each do |req, const|
+    context req do
+      let(:code) do
+        <<-EOT
+          if defined?(Datadog)
+            unless Datadog.constants == [:VERSION]
+              exit 1
+            end
+          end
+
+          require "#{req}"
+
+          unless defined?(#{const})
+            exit 1
+          end
+
+          exit 0
+EOT
+      end
+
+      it 'loads successfully by itself' do
+        rv = system("ruby -e #{Shellwords.shellescape(code)}")
+        expect(rv).to be true
+      end
+    end
+  end
+end
