455744ed3136b795a7aa1970b7ebb51dcbd79ae9
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2024-06-26 17:10:57 +0200

    Extract matrix from Rakefile

---
+++ matrix.rb
@@ -0,0 +1,30 @@
+require 'yaml'
+require 'json'
+
+map = eval(File.read('Matrixfile'))
+ruby_to_jruby = {
+  '2.5' => ['9.2'],
+  '2.6' => ['9.3'],
+  '3.1' => ['9.4'],
+}
+
+matrix = map.each_with_object({}) do |(spec, appraisals), h|
+  h[spec] = appraisals.each_with_object({}) do |(group, rubies), h2|
+    ruby = rubies.scan(/✅ (\S+)/).flatten
+
+    has_jruby = ruby.delete('jruby')
+
+    engines = { 'ruby' => ruby }
+
+    if has_jruby && (versions = ruby_to_jruby.select { |r, _| ruby.include?(r) }.values.flatten).any?
+      engines['jruby'] = versions
+    end
+
+    h2[group] = engines
+  end
+end
+
+# Rails 4.x is not supported on JRuby 9.2 (which is RUBY_VERSION 2.5)
+matrix.each { |_, appraisals| appraisals.each { |appraisal, engines| appraisal =~ /^rails4/ && engines.key?('jruby') && engines['jruby'].delete('9.2') } }
+
+puts(JSON.dump(matrix))
