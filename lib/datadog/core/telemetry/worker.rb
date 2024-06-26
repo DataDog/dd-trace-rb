3c42a260cfba19e8be56bcd7306cc7e27cf00beb
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2024-06-26 13:02:33 +0200

    minor fix for older rubies

--- lib/datadog/core/telemetry/worker.rb
+++ lib/datadog/core/telemetry/worker.rb
@@ -87,7 +87,7 @@ module Datadog
           return if events.nil?
           return if !enabled? || !sent_started_event?
 
-          Datadog.logger.debug { "Sending #{events&.count} telemetry events" }
+          Datadog.logger.debug { "Sending #{events.count} telemetry events" }
           events.each do |event|
             send_event(event)
           end
