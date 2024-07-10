fcad30269bb0e4d5dce2a434792550635c375f70
Munir Abdinur <munir_abdinur@hotmail.com>
2024-07-10 16:31:56 -0400

    fmt

--- lib/datadog/tracing/span_event.rb
+++ lib/datadog/tracing/span_event.rb
@@ -1,34 +1,33 @@
-require 'time'
-
 # frozen_string_literal: true
 
+require 'time'
+
 module Datadog
-    module Tracing
-      # SpanEvent represents an annotation on a span.
-      # @public_api
-      class SpanEvent
-        # @!attribute [r] name
-        #   @return [Integer]
-        attr_reader :name
-  
-        # @!attribute [r] attributes
-        #   @return [Hash<String,String>]
-        attr_reader :attributes
-  
-        # @!attribute [r] time_unix_nano
-        #   @return [Integer]
-        attr_reader :time_unix_nano
-  
-        def initialize(
-            name,
-            attributes: nil,
-            time_unix_nano: nil
-        )
-          @name = name
-          @attributes = attributes&.map{|key, val| [key, val.to_s]}&.to_h || {}
-          @time_unix_nano = time_unix_nano || Core::Utils::Time.now.to_f * 1e9
-        end
+  module Tracing
+    # SpanEvent represents an annotation on a span.
+    # @public_api
+    class SpanEvent
+      # @!attribute [r] name
+      #   @return [Integer]
+      attr_reader :name
+
+      # @!attribute [r] attributes
+      #   @return [Hash<String,String>]
+      attr_reader :attributes
+
+      # @!attribute [r] time_unix_nano
+      #   @return [Integer]
+      attr_reader :time_unix_nano
+
+      def initialize(
+        name,
+        attributes: nil,
+        time_unix_nano: nil
+      )
+        @name = name
+        @attributes = attributes&.map { |key, val| [key, val.to_s] }&.to_h || {}
+        @time_unix_nano = time_unix_nano || Core::Utils::Time.now.to_f * 1e9
       end
     end
   end
-  
+end
