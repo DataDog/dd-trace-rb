21d615b00f8969b2824834149ef2c8ba492e30b5
Ivo Anjo <ivo.anjo@datadoghq.com>
2024-07-11 10:24:50 +0100

    checkpoint 1

--- missing-testcase.rb
+++ missing-testcase.rb
@@ -1,13 +1,2 @@
-Datadog::Profiling.wait_until_running # Originally was enabled
-allocator = Thread.new { loop { BasicObject.new } }
-# exceptioner = Thread.new { loop {
-#   begin
-#     print '.'
-#     raise 'Testing...'
-#   rescue
-#   end
-# }}
-sleep 1 # originally was 10
-allocator.kill; allocator.join
-# exceptioner.kill; exceptioner.join
-puts "Allocated #{GC.stat(:total_allocated_objects)} objects total"
+Datadog::Profiling.wait_until_running
+puts "Finished!"
