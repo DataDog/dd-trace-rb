c0e04d923a0efa73e11203eb70168dfdeb119e8d
Ivo Anjo <ivo.anjo@datadoghq.com>
2024-07-16 13:36:34 +0100

    Document need to be careful with object references in allocate
    After actually solving one of these (potential?) issues in the
    `ThreadContext` collector, I've gone around and added a nice comment to
    other parts of the codebase that use `TypedData_Wrap_Struct` so
    hopefully we don't introduce bugs related to this issue.

--- ext/datadog_profiling_native_extension/collectors_discrete_dynamic_sampler.c
+++ ext/datadog_profiling_native_extension/collectors_discrete_dynamic_sampler.c
@@ -359,6 +359,10 @@ static VALUE _native_new(VALUE klass) {
   }
   discrete_dynamic_sampler_init(&state->sampler, "test sampler", now_ns);
 
+  // Note: As of this writing, no new Ruby objects get created and stored in the state. If that ever changes, remember
+  // to keep them on the stack and mark them with RB_GC_GUARD -- otherwise it's possible for a GC to run and
+  // since the instance representing the state does not yet exist, such objects will not get marked.
+
   return TypedData_Wrap_Struct(klass, &sampler_typed_data, state);
 }
 
