faab41ec5b2f82efa6a8411c424ec7219bd4f11d
Laplie Anderson <laplie.anderson@datadoghq.com>
2024-07-17 13:09:56 -0400

    add some debug ls

--- .gitlab/prepare-oci-package.sh
+++ .gitlab/prepare-oci-package.sh
@@ -11,6 +11,12 @@ cp sources/host_inject.rb sources/auto_inject.rb
 sed -i "s#PATH_PLACEHOLDER#/opt/datadog-packages/datadog-apm-library-ruby/${RUBY_PACKAGE_VERSION}#g" sources/host_inject.rb
 sed -i "s#PATH_PLACEHOLDER#/datadog-lib#g" sources/auto_inject.rb
 
+echo "PRINTING ALL FILES IN TMP"
+ls  -lR ../tmp/${ARCH}/
+
 cp -r ../tmp/${ARCH}/* sources
+
+echo "PRINTING ALL FILES IN SOURCES"
+ls -lR sources
 
 cp ../tmp/version.txt sources/version
