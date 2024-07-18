247c01fdd9956efa6c7a9c85d0bec65da6c09822
Laplie Anderson <laplie.anderson@datadoghq.com>
2024-07-18 14:52:40 -0400

    extract version from version file for use in host_inject

--- .gitlab/prepare-oci-package.sh
+++ .gitlab/prepare-oci-package.sh
@@ -8,9 +8,11 @@ cp ../lib-injection/host_inject.rb sources
 # Kubernetes injection expects a different path
 cp sources/host_inject.rb sources/auto_inject.rb
 
-sed -i "s#PATH_PLACEHOLDER#/opt/datadog-packages/datadog-apm-library-ruby/${RUBY_PACKAGE_VERSION}#g" sources/host_inject.rb
-sed -i "s#PATH_PLACEHOLDER#/datadog-lib#g" sources/auto_inject.rb
-
 cp -r ../tmp/${ARCH}/* sources
 
 cp ../tmp/version.txt sources/version
+
+RUBY_PACKAGE_VERSION=$(<sources/version)
+
+sed -i "s#PATH_PLACEHOLDER#/opt/datadog-packages/datadog-apm-library-ruby/${RUBY_PACKAGE_VERSION}#g" sources/host_inject.rb
+sed -i "s#PATH_PLACEHOLDER#/datadog-lib#g" sources/auto_inject.rb
