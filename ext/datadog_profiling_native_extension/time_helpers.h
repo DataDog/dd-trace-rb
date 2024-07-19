fd11902296604ced0ab90e57d6ce35e778e3e0f4
Ivo Anjo <ivo.anjo@datadoghq.com>
2024-07-19 11:37:47 +0100

    Add helper for accessing CLOCK_MONOTONIC_COARSE
    This will be used by the discrete dynamic sampler to make cheaper checks
    of when to re-evaluate if it should work again.

--- ext/datadog_profiling_native_extension/time_helpers.h
+++ ext/datadog_profiling_native_extension/time_helpers.h
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <time.h>
 
+#include "extconf.h" // Needed for HAVE_CLOCK_MONOTONIC_COARSE
 #include "ruby_helpers.h"
 
 #define SECONDS_AS_NS(value) (value * 1000 * 1000 * 1000L)
@@ -33,5 +34,21 @@ inline long retrieve_clock_as_ns(clockid_t clock_id, raise_on_failure_setting ra
 
 inline long monotonic_wall_time_now_ns(raise_on_failure_setting raise_on_failure) { return retrieve_clock_as_ns(CLOCK_MONOTONIC, raise_on_failure); }
 inline long system_epoch_time_now_ns(raise_on_failure_setting raise_on_failure)   { return retrieve_clock_as_ns(CLOCK_REALTIME,  raise_on_failure); }
+
+// Coarse instants use CLOCK_MONOTONIC_COARSE on Linux which is expected to provide resolution in the millisecond range:
+// https://docs.redhat.com/en/documentation/red_hat_enterprise_linux_for_real_time/7/html/reference_guide/sect-posix_clocks#Using_clock_getres_to_compare_clock_resolution
+// We introduce here a separate type for it, so as to make it harder/more explicit when these timestamps are used
+
+typedef struct coarse_instant {
+  long timestamp_ns;
+} coarse_instant;
+
+inline coarse_instant monotonic_coarse_wall_time_now_ns(void) {
+  #ifdef HAVE_CLOCK_MONOTONIC_COARSE // Linux
+    return (coarse_instant) {.timestamp_ns = retrieve_clock_as_ns(CLOCK_MONOTONIC_COARSE, DO_NOT_RAISE_ON_FAILURE)};
+  #else // macOS
+    return (coarse_instant) {.timestamp_ns = retrieve_clock_as_ns(CLOCK_MONOTONIC, DO_NOT_RAISE_ON_FAILURE)};
+  #endif
+}
 
 long monotonic_to_system_epoch_ns(monotonic_to_system_epoch_state *state, long monotonic_wall_time_ns);
