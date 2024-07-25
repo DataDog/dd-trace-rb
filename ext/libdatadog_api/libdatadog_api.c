ed05c7b1301a1ce9b5d2d784bb59004edfa34410
Ivo Anjo <ivo.anjo@datadoghq.com>
2024-07-25 17:17:22 +0100

    Successful experimental extraction of crashtracker to new module
    The specs still pass! :)

--- ext/libdatadog_api/libdatadog_api.c
+++ ext/libdatadog_api/libdatadog_api.c
@@ -1,3 +1,5 @@
+#include <ruby.h>
+
 // Used to mark function arguments that are deliberately left unused
 #ifdef __GNUC__
   #define DDTRACE_UNUSED  __attribute__((unused))
@@ -7,6 +9,11 @@
 
 #define DDTRACE_EXPORT __attribute__ ((visibility ("default")))
 
-void DDTRACE_EXPORT Init_libdatadog_api(void) {
+void crashtracker_init(VALUE profiling_module);
 
+void DDTRACE_EXPORT Init_libdatadog_api(void) {
+  VALUE datadog_module = rb_define_module("Datadog");
+  VALUE profiling_module = rb_define_module_under(datadog_module, "Profiling");
+
+  crashtracker_init(profiling_module);
 }
