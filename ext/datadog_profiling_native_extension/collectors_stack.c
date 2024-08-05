c29899eea620c299ca3fff70430a3c24ea5d8db4
Ivo Anjo <ivo.anjo@datadoghq.com>
2024-08-05 14:43:45 +0100

    Disable debug print

--- ext/datadog_profiling_native_extension/collectors_stack.c
+++ ext/datadog_profiling_native_extension/collectors_stack.c
@@ -167,13 +167,13 @@ void sample_thread(
     return;
   }
 
-  if (captured_frames > 0) {
-    int cache_hits = 0;
-    for (int i = 0; i < captured_frames; i++) {
-      if (buffer->same_frame[i]) cache_hits++;
-    }
-    fprintf(stderr, "Sampling cache hits: %f\n", ((double) cache_hits / captured_frames) * 100);
-  }
+  // if (captured_frames > 0) {
+  //   int cache_hits = 0;
+  //   for (int i = 0; i < captured_frames; i++) {
+  //     if (buffer->same_frame[i]) cache_hits++;
+  //   }
+  //   fprintf(stderr, "Sampling cache hits: %f\n", ((double) cache_hits / captured_frames) * 100);
+  // }
 
   // Ruby does not give us path and line number for methods implemented using native code.
   // The convention in Kernel#caller_locations is to instead use the path and line number of the first Ruby frame
