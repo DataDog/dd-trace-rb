297f95283b4298911a170b6680261ca0762f155e
Tony Hsu <tony.hsu@datadoghq.com>
2024-08-07 12:18:45 +0200

    Add test for url resolver

--- lib/datadog/core/crashtracking/agent_base_url.rb
+++ lib/datadog/core/crashtracking/agent_base_url.rb
@@ -5,6 +5,7 @@ require_relative '../configuration/ext'
 module Datadog
   module Core
     module Crashtracking
+      # This module provides a method to resolve the base URL of the agent
       module AgentBaseUrl
         module_function
 
@@ -14,8 +15,6 @@ module Datadog
             "#{agent_settings.ssl ? 'https' : 'http'}://#{agent_settings.hostname}:#{agent_settings.port}/"
           when Datadog::Core::Configuration::Ext::Agent::UnixSocket::ADAPTER
             "unix://#{agent_settings.uds_path}"
-          else
-            nil
           end
         end
       end
