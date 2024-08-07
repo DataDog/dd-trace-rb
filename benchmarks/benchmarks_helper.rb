fb70648abe01f4afccc94775fc58316368290997
Oleg Pudeyev <code@olegp.name>
2024-08-07 09:15:29 -0400

    fix old rubies

--- benchmarks/benchmarks_helper.rb
+++ benchmarks/benchmarks_helper.rb
@@ -6,7 +6,20 @@ module JobReporter
   def report(name, *args, **opts, &block)
     caller_path = caller_locations.first.path
     prefix = File.basename(caller_path).sub(/_.*\z/, '')
-    super("#{prefix} - #{name}", *args, **opts, &block)
+    name = "#{prefix} - #{name}"
+    # Older Rubies (e.g. 2.5) do not permit passing *args and &block
+    # in the same invocation.
+    if args.any? && block_given?
+      raise ArgumentError, 'Unsupported usage'
+    elsif block_given?
+      if opts.any?
+        super(name, **opts, &block)
+      else
+        super(name, &block)
+      end
+    else
+      super(name, *args, **opts)
+    end
   end
 end
 
