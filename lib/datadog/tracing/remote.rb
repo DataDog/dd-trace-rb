00dc6aa579d0c4b9bde95e023a45f62e7379dbf0
Zarir Hamza <zarir.hamza@datadoghq.com>
2024-09-04 12:25:00 -0400

    adds more RC configs

--- lib/datadog/tracing/remote.rb
+++ lib/datadog/tracing/remote.rb
@@ -13,7 +13,10 @@ module Datadog
         PRODUCT = 'APM_TRACING'
 
         CAPABILITIES = [
-          1 << 29 # APM_TRACING_SAMPLE_RULES: Dynamic trace sampling rules configuration
+          1 << 12, # APM_TRACING_SAMPLE_RATE: Dynamic trace sampling rate configuration
+          1 << 13, # APM_TRACING_LOGS_INJECTION: Dynamic trace logs injection configuration
+          1 << 14, # APM_TRACING_HTTP_HEADER_TAGS: Dynamic trace HTTP header tags configuration
+          1 << 29, # APM_TRACING_SAMPLE_RULES: Dynamic trace sampling rules configuration
         ].freeze
 
         def products
