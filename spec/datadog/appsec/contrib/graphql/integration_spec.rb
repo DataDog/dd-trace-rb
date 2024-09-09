20a00b85f76dfabd33951767353f89014c030df7
Yury Lebedev <yury.lebedev@datadoghq.com>
2024-09-09 13:29:24 +0200

    Add specs for graphql integration .ast_node_classes_defined?

---
+++ spec/datadog/appsec/contrib/graphql/integration_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'datadog/appsec/spec_helper'
+require 'datadog/appsec/contrib/graphql/integration'
+
+RSpec.describe Datadog::AppSec::Contrib::GraphQL::Integration do
+  describe '.ast_node_classes_defined?' do
+    it 'returns true when all AST node classes are defined' do
+      expect(described_class.ast_node_classes_defined?).to eq(true)
+    end
+
+    it 'returns false when at least one of AST node classes is not defined' do
+      hide_const('GraphQL::Language::Nodes::Field')
+      expect(described_class.ast_node_classes_defined?).to eq(false)
+    end
+  end
+end
