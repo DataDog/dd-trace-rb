f2bc8e91e7c451d1786af28cfff8c75c44381830
Ivo Anjo <ivo.anjo@datadoghq.com>
2024-09-23 11:20:52 +0100

    Fix broken profiler build on Ruby < 3.2

--- ext/datadog_profiling_native_extension/collectors_thread_context.h
+++ ext/datadog_profiling_native_extension/collectors_thread_context.h
@@ -16,6 +16,9 @@ VALUE thread_context_collector_sample_after_gc(VALUE self_instance);
 void thread_context_collector_on_gc_start(VALUE self_instance);
 __attribute__((warn_unused_result)) bool thread_context_collector_on_gc_finish(VALUE self_instance);
 VALUE enforce_thread_context_collector_instance(VALUE object);
-void thread_context_collector_on_gvl_waiting(gvl_profiling_thread thread);
-__attribute__((warn_unused_result)) bool thread_context_collector_on_gvl_running(gvl_profiling_thread thread);
-VALUE thread_context_collector_sample_after_gvl_running(VALUE self_instance);
+
+#ifndef NO_GVL_INSTRUMENTATION
+  void thread_context_collector_on_gvl_waiting(gvl_profiling_thread thread);
+  __attribute__((warn_unused_result)) bool thread_context_collector_on_gvl_running(gvl_profiling_thread thread);
+  VALUE thread_context_collector_sample_after_gvl_running(VALUE self_instance);
+#endif
