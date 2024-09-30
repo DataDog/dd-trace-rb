81d20f240fbfd31632d64aa7ba49c7028f14e339
Victor Pellan <victor.pellan@datadoghq.com>
2024-09-30 18:16:03 +0200

    Add conditional access to scope.trace

--- lib/datadog/appsec/monitor/gateway/watcher.rb
+++ lib/datadog/appsec/monitor/gateway/watcher.rb
@@ -42,12 +42,14 @@ module Datadog
 
                       # Propagate to downstream services the information that the current distributed trace is
                       # containing at least one ASM security event
-                      scope.trace.keep!
-                      scope.trace.set_tag(
-                        Datadog::Tracing::Metadata::Ext::Distributed::TAG_DECISION_MAKER,
-                        Datadog::Tracing::Sampling::Ext::Decision::ASM
-                      )
-                      scope.trace.set_tag(Datadog::AppSec::Ext::TAG_APPSEC_EVENT, '1')
+                      if scope.trace
+                        scope.trace.keep!
+                        scope.trace.set_tag(
+                          Datadog::Tracing::Metadata::Ext::Distributed::TAG_DECISION_MAKER,
+                          Datadog::Tracing::Sampling::Ext::Decision::ASM
+                        )
+                        scope.trace.set_tag(Datadog::AppSec::Ext::TAG_APPSEC_EVENT, '1')
+                      end
 
                       scope.processor_context.events << event
                     end
