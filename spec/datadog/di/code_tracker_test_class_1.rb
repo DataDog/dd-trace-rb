1f1afc142c143624fd676784889fda7ff384605e
Oleg Pudeyev <156273877+p-datadog@users.noreply.github.com>
2024-10-02 08:54:00 -0400

    DEBUG-2334 Dynamic Instrumentation code tracker component (#3942)

---
+++ spec/datadog/di/code_tracker_test_class_1.rb
@@ -0,0 +1,2 @@
+class CodeTrackerTestClass1
+end
---
+++ spec/datadog/di/code_tracker_test_classes/code_tracker_test_class_1.rb
@@ -0,0 +1,4 @@
+# Different name to not conflict with the upper-level class definition.
+# Note that file basenames need to be identical for some of the test cases.
+class SubdirCodeTrackerTestClass1
+end
