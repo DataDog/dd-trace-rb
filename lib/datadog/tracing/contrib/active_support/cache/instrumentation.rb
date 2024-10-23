45ef5e6d21390dafde6cdedc43eb894367950971
Steven Bouwkamp <steven.bouwkamp@datadoghq.com>
2024-10-23 16:59:15 -0400

    Reapply "Use shorthand `configuration` access"
    This reverts commit 866c8888c9f07a0f13fcebd5297c5085d8291055.
    
    It's missing a require_relative

--- lib/datadog/tracing/contrib/active_support/cache/instrumentation.rb
+++ lib/datadog/tracing/contrib/active_support/cache/instrumentation.rb
@@ -3,6 +3,7 @@
 require_relative '../../../../core/utils'
 require_relative '../../../metadata/ext'
 require_relative '../ext'
+require_relative 'event'
 
 module Datadog
   module Tracing
@@ -59,7 +60,7 @@ module Datadog
 
                 span.set_tag(Ext::TAG_CACHE_BACKEND, store) if store
 
-                set_cache_key(span, key, multi_key) if Datadog.configuration.tracing[:active_support][:cache_key_enabled]
+                set_cache_key(span, key, multi_key) if configuration[:cache_key_enabled]
 
                 yield
               end
