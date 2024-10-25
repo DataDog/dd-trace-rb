0b83cab12435a106c9d0f22db03dd56a65ed95fb
Munir Abdinur <munir.abdinur@datadoghq.com>
2024-10-25 14:09:12 -0400

    fix unix path regex

--- spec/datadog/core/configuration/agent_settings_resolver_spec.rb
+++ spec/datadog/core/configuration/agent_settings_resolver_spec.rb
@@ -192,7 +192,7 @@ RSpec.describe Datadog::Core::Configuration::AgentSettingsResolver do
 
             it 'logs a warning including the uds path' do
               expect(logger).to receive(:warn)
-                .with(%r{Configuration mismatch.*Using "unix:[/|]some/path"})
+                .with(%r{Configuration mismatch.*Using "unix:[\/]{1,3}some/path"})
 
               resolver
             end
@@ -207,7 +207,7 @@ RSpec.describe Datadog::Core::Configuration::AgentSettingsResolver do
 
             it 'logs a warning including the uds configuration' do
               expect(logger).to receive(:warn)
-                .with(%r{Configuration mismatch.*Using "unix:[/|]some/path"})
+                .with(%r{Configuration mismatch.*Using "unix:[\/]{1,3}some/path"})
 
               resolver
             end
@@ -239,7 +239,7 @@ RSpec.describe Datadog::Core::Configuration::AgentSettingsResolver do
 
             it 'logs a warning including the uds configuration' do
               expect(logger).to receive(:warn)
-                .with(%r{Configuration mismatch.*Using "unix:[/|]some/path"})
+                .with(%r{Configuration mismatch.*Using "unix:[\/]{1,3}some/path"})
 
               resolver
             end
@@ -254,7 +254,7 @@ RSpec.describe Datadog::Core::Configuration::AgentSettingsResolver do
 
             it 'logs a warning including the uds configuration' do
               expect(logger).to receive(:warn)
-                .with(%r{Configuration mismatch.*Using "unix:[/|]some/path"})
+                .with(%r{Configuration mismatch.*Using "unix:[\/]{1,3}some/path"})
 
               resolver
             end
