0615e9f65d930f618f9098e1fb154482dbbd8dcf
Oleg Pudeyev <code@olegp.name>
2024-11-07 13:04:44 -0500

    DEBUG-2334 custom ActiveRecord model serialization

---
+++ lib/datadog/di/contrib/active_record.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+Datadog::DI::Serializer.register(condition: lambda { |value| ActiveRecord::Base === value }) \
+do |serializer, value, name:, depth:|
+  value_to_serialize = {
+    attributes: value.attributes,
+  }
+  serializer.serialize_value(value_to_serialize, depth: depth ? depth - 1 : nil, type: value.class)
+end
