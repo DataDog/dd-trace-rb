a4a90c069664de6fd17cf1ad30a54ef5c8a0f55f
Oleg Pudeyev <code@olegp.name>
2024-11-07 13:45:02 -0500

    types

--- lib/datadog/di/contrib/active_record.rb
+++ lib/datadog/di/contrib/active_record.rb
@@ -2,8 +2,11 @@
 
 Datadog::DI::Serializer.register(condition: lambda { |value| ActiveRecord::Base === value } # steep:ignore
 ) do |serializer, value, name:, depth:|
+  # steep thinks all of the arguments are nil here
+  # steep:ignore:start
   value_to_serialize = {
     attributes: value.attributes,
   }
   serializer.serialize_value(value_to_serialize, depth: depth ? depth - 1 : nil, type: value.class)
+  # steep:ignore:end
 end
