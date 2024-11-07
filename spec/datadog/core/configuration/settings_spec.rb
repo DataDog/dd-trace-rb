6b6e884a85216a418613e702de7174d1f9f0a60a
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2024-11-07 12:29:36 +0100

    Revert "Disable crashtracking by default"
    This reverts commit c8a2165607cba186b3a3b23742448dc94e53f224.

--- spec/datadog/core/configuration/settings_spec.rb
+++ spec/datadog/core/configuration/settings_spec.rb
@@ -2075,7 +2075,7 @@ RSpec.describe Datadog::Core::Configuration::Settings do
         context 'is not defined' do
           let(:environment) { nil }
 
-          it { is_expected.to be false }
+          it { is_expected.to be true }
         end
 
         [true, false].each do |value|
@@ -2090,9 +2090,9 @@ RSpec.describe Datadog::Core::Configuration::Settings do
 
     describe '#enabled=' do
       it 'updates the #enabled setting' do
-        expect { settings.crashtracking.enabled = true }
+        expect { settings.crashtracking.enabled = false }
           .to change { settings.crashtracking.enabled }
-          .from(false).to(true)
+          .from(true).to(false)
       end
     end
   end
