f7569237529acd266143d5694d889b3c7115dc3c
Oleg Pudeyev <code@olegp.name>
2024-11-08 11:34:05 -0500

    di component

--- lib/datadog/di.rb
+++ lib/datadog/di.rb
@@ -1,21 +1,44 @@
 # frozen_string_literal: true
 
 require_relative 'di/error'
-require_relative 'di/configuration'
 require_relative 'di/code_tracker'
+require_relative 'di/component'
+require_relative 'di/configuration'
 require_relative 'di/extensions'
 require_relative 'di/instrumenter'
 require_relative 'di/probe'
+require_relative 'di/probe_builder'
+require_relative 'di/probe_manager'
+require_relative 'di/probe_notification_builder'
+require_relative 'di/probe_notifier_worker'
 require_relative 'di/redactor'
 require_relative 'di/serializer'
 require_relative 'di/transport'
 require_relative 'di/utils'
+
+if defined?(ActiveRecord::Base)
+  # The third-party library integrations need to be loaded after the
+  # third-party libraries are loaded. Tracing and appsec use Railtie
+  # to delay integrations until all of the application's dependencies
+  # are loaded, when running under Rails. We should do the same here in
+  # principle, however DI currently only has an ActiveRecord integration
+  # and AR should be loaded before any application code is loaded, being
+  # part of Rails, therefore for now we should be OK to just require the
+  # AR integration from here.
+  require_relative 'di/contrib/active_record'
+end
 
 module Datadog
   # Namespace for Datadog dynamic instrumentation.
   #
   # @api private
   module DI
+    class << self
+      def enabled?
+        Datadog.configuration.dynamic_instrumentation.enabled
+      end
+    end
+
     # Expose DI to global shared objects
     Extensions.activate!
 
@@ -52,6 +75,28 @@ module Datadog
       def code_tracking_active?
         code_tracker&.active? || false
       end
+
+      def component
+        Datadog.send(:components).dynamic_instrumentation
+      end
+    end
+  end
+end
+
+# :script_compiled trace point was added in Ruby 2.6.
+if RUBY_VERSION >= '2.6'
+  begin
+    # Activate code tracking by default because line trace points will not work
+    # without it.
+    Datadog::DI.activate_tracking!
+  rescue => exc
+    if defined?(Datadog.logger)
+      Datadog.logger.warn("Failed to activate code tracking for DI: #{exc.class}: #{exc}")
+    else
+      # We do not have Datadog logger potentially because DI code tracker is
+      # being loaded early in application boot process and the rest of datadog
+      # wasn't loaded yet. Output to standard error.
+      warn("Failed to activate code tracking for DI: #{exc.class}: #{exc}")
     end
   end
 end
