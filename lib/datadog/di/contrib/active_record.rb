3a5f21e700817e8908ce1e45c7187cfb324f77ba
Oleg Pudeyev <156273877+p-datadog@users.noreply.github.com>
2024-11-11 14:09:15 -0500

    DEBUG-2334 custom ActiveRecord model serialization (#4088)
    Adds a custom serializer for ActiveRecord models
    Adds serializers for Time, Date and DateTime (needed for ActiveRecord timestamps)
    
    Motivation:
    
    AR models have many fields that are not interesting for DI, and serializing them would 1) consume a lot of resources 2) cause the serialized payload to exceed the limits of the backend 3) not serialize actual attributes due to depth limits (actual attributes are already 3 levels down from the AR model). The custom serializer currently only serializes the attributes, though additional fields can be added in the future such as the new_record? flag.

---
+++ lib/datadog/di/contrib/active_record.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+Datadog::DI::Serializer.register(condition: lambda { |value| ActiveRecord::Base === value }) do |serializer, value, name:, depth:| # steep:ignore
+  # steep thinks all of the arguments are nil here
+  # steep:ignore:start
+  value_to_serialize = {
+    attributes: value.attributes,
+  }
+  serializer.serialize_value(value_to_serialize, depth: depth ? depth - 1 : nil, type: value.class)
+  # steep:ignore:end
+end
