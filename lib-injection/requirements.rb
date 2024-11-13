20f2fa55d96933740d0020e132836d2962ee80dd
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2024-11-13 12:27:39 +0100

    Deny `rubygems-update` injection
    `rubygems-update` `setup.rb` re-execs itself when `RUBYOPT` is detected,
    which causes endless looping because injection adds it back immediately.
    
    See: https://github.com/rubygems/rubygems/blob/90c90addee4bda3130cf44f1321eebf162367d1b/setup.rb#L13-L20
    
    Coverage is twofold:
    
    - Deny on `--disable-gems`: gems are required for ruby injector
    - Deny on `setup.rb`: This may be wide-catching but we don't want to
      instrument any of them anyway.

--- lib-injection/requirements.rb
+++ lib-injection/requirements.rb
@@ -50,6 +50,31 @@ def requirements
     'envars' => nil,
   }
 
+  reqs['deny'] << {
+    'id' => 'ruby_disable_gems',
+    'description' => 'Rubygems is required for injection to function',
+    'os' => nil,
+    'cmds' => [
+      '**/ruby'
+    ],
+    'args' => [{ 'args' => ['--disable-gems'], 'position' => nil }],
+    'envars' => nil,
+  }
+
+  # Prevent endless reexecution when RUBYOPTS is forcefully set
+  # Command: {"Path":"/usr/local/bin/ruby","Args":["/usr/local/bin/ruby","--disable-gems","setup.rb","--no-document","--previous-version","3.3.26"]}
+  # See: https://github.com/rubygems/rubygems/blob/90c90addee4bda3130cf44f1321eebf162367d1b/setup.rb#L13-L20
+  reqs['deny'] << {
+    'id' => 'gem_update_system',
+    'description' => 'Ignore the rubygems update setup.rb',
+    'os' => nil,
+    'cmds' => [
+      '**/ruby'
+    ],
+    'args' => [{ 'args' => ['setup.rb'], 'position' => nil }],
+    'envars' => nil,
+  }
+
   # `bundle exec` is the only command we want to inject into.
   # there is no `allow` overriding `deny` so we're left to exclude all of the
   # possible others.
