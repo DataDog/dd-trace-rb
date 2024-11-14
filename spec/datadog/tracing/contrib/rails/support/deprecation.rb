61e70f42e9a146ff5f9c0157b6b7bf51c8565de4
Sarah Chen <sarah.chen@datadoghq.com>
2024-11-14 11:32:39 -0500

    Address Rails.deprecator

--- spec/datadog/tracing/contrib/rails/support/deprecation.rb
+++ spec/datadog/tracing/contrib/rails/support/deprecation.rb
@@ -8,6 +8,8 @@ def raise_on_rails_deprecation!
   # In Rails 7.1 calling ActiveSupport::Deprecation.behavior= is deprecated
   if defined?(Rails) && Rails.gem_version >= Gem::Version.new(7.1)
     Rails.deprecator.behavior = :raise
+  elsif RUBY_VERSION.start_with?('3.4.')
+    puts 'TO DO: Delete this case once Ruby 3.4 is supported.'
   else
     ActiveSupport::Deprecation.behavior = :raise
   end
