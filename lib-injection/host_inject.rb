5591d97f9c24023456439ef8e537eed56ac0abbd
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2024-11-21 16:14:24 +0100

    Add frontend `host_inject.rb`, guarding against older versions
    These versions may be unable to execute - or even parse - the main host
    injection script.

---
+++ lib-injection/host_inject.rb
@@ -0,0 +1,7 @@
+# This file's intent is to be parseable and executable by all ruby versions
+# to call into the main one only for versions for which it is known-compatible
+# with at the language level.
+
+if RUBY_VERSION >= '2.5.'
+  require File.expand_path(File.join(File.dirname(__FILE__), 'host_inject_main.rb'))
+end
