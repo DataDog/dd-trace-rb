85e116f097295706f50e99a1ec171b1deff74bb9
Victor Pellan <victor.pellan@datadoghq.com>
2024-11-26 18:02:52 +0100

    Use TraceOperation method in should_skip_distributed_tracing

--- lib/datadog/appsec/utils/trace_operation.rb
+++ lib/datadog/appsec/utils/trace_operation.rb
@@ -5,6 +5,10 @@ module Datadog
     module Utils
       # Utility class to to AppSec-specific trace operations
       class TraceOperation
+        # When APM is running in 'non-billing' mode, we should skip distributed tracing
+        # unless specific distributed tags are present. These tags can come from upstream
+        # services, or from the service itself.
+        # For now, only '_dd.p.appsec=1' is supported.
         def self.appsec_standalone_reject?(trace)
           !Datadog.configuration.tracing.apm.enabled &&
             (trace.nil? || trace.get_tag(Datadog::AppSec::Ext::TAG_DISTRIBUTED_APPSEC_EVENT) != '1')
