f9aec1a9725b24981fa9fab494eab8b1d4171b78
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2024-11-27 13:48:37 +0100

    Lower to ruby 2.3

--- lib-injection/host_inject.rb
+++ lib-injection/host_inject.rb
@@ -2,6 +2,6 @@
 # to call into the main one only for versions for which it is known-compatible
 # with at the language level.
 
-if RUBY_VERSION >= '2.5.'
+if RUBY_VERSION >= '2.3.'
   require File.expand_path(File.join(File.dirname(__FILE__), 'host_inject_main.rb'))
 end
