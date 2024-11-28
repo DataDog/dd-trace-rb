c3c558b39c53b90560db5a9ce25edb2f292fc53f
Tony Hsu <tony.hsu@datadoghq.com>
2024-11-28 13:01:22 +0100

    Bump version 2.7.0 to 2.7.1

--- lib/datadog/version.rb
+++ lib/datadog/version.rb
@@ -4,7 +4,7 @@ module Datadog
   module VERSION
     MAJOR = 2
     MINOR = 7
-    PATCH = 0
+    PATCH = 1
     PRE = nil
     BUILD = nil
     # PRE and BUILD above are modified for dev gems during gem build GHA workflow
