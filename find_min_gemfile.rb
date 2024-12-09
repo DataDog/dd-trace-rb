1b2986e234bc8ccdb114af008ee19930d8cc534d
quinna-h <quinna.halim@datadoghq.com>
2024-12-09 14:06:31 -0500

    rubocop lint

--- find_min_gemfile.rb
+++ find_min_gemfile.rb
@@ -3,95 +3,90 @@ require 'rubygems'
 require 'json'
 
 def parse_gemfiles(directory = 'gemfiles/')
-    parsed_gems_ruby = {}
-    parsed_gems_jruby = {}
-  
-    gemfiles = Dir.glob(File.join(directory, '*'))
-    gemfiles.each do |gemfile_name|
-      runtime = File.basename(gemfile_name).split('_').first # ruby or jruby
-      # puts "Runtime: #{runtime}"
-      File.foreach(gemfile_name) do |line|
-        if gem_details = parse_gemfile_entry(line)
-          gem_name, version = gem_details
-        elsif gem_details = parse_gemfile_lock_entry(line)
-          gem_name, version = gem_details
-        else
-          next
-        end
-  
-        # Validate and store the minimum version
-        if version_valid?(version)
-          if runtime == 'ruby'
-            if parsed_gems_ruby[gem_name].nil? || Gem::Version.new(version) < Gem::Version.new(parsed_gems_ruby[gem_name])
-              parsed_gems_ruby[gem_name] = version
-            end
+  parsed_gems_ruby = {}
+  parsed_gems_jruby = {}
+
+  gemfiles = Dir.glob(File.join(directory, '*'))
+  gemfiles.each do |gemfile_name|
+    runtime = File.basename(gemfile_name).split('_').first # ruby or jruby
+    # puts "Runtime: #{runtime}"
+    File.foreach(gemfile_name) do |line|
+      if (gem_details = parse_gemfile_entry(line))
+        gem_name, version = gem_details
+      elsif (gem_details = parse_gemfile_lock_entry(line))
+        gem_name, version = gem_details
+      else
+        next
+      end
+
+      # Validate and store the minimum version
+      if version_valid?(version)
+        if runtime == 'ruby'
+          if parsed_gems_ruby[gem_name].nil? || Gem::Version.new(version) < Gem::Version.new(parsed_gems_ruby[gem_name])
+            parsed_gems_ruby[gem_name] = version
           end
-          if runtime == 'jruby'
-            if parsed_gems_jruby[gem_name].nil? || Gem::Version.new(version) < Gem::Version.new(parsed_gems_jruby[gem_name])
-              parsed_gems_jruby[gem_name] = version
-            end
-          end
-        else
-          next
         end
+        if runtime == 'jruby'
+          if parsed_gems_jruby[gem_name].nil? || Gem::Version.new(version) < Gem::Version.new(parsed_gems_jruby[gem_name])
+            parsed_gems_jruby[gem_name] = version
+          end
+        end
+      else
+        next
       end
     end
-  
-    [parsed_gems_ruby, parsed_gems_jruby]
   end
-  
-  
-  # Helper: Parse a Gemfile-style gem declaration
-  # ex. gem 'ruby-kafka', '~> 5.0'
-  def parse_gemfile_entry(line)
-    if match = line.match(/^\s*gem\s+["']([^"']+)["']\s*,?\s*["']?([^"']*)["']?/)
-      gem_name, version_constraint = match[1], match[2]
-      version = extract_version(version_constraint)
-      [gem_name, version]
-    end
+
+  [parsed_gems_ruby, parsed_gems_jruby]
+end
+
+# Helper: Parse a Gemfile-style gem declaration
+# ex. gem 'ruby-kafka', '~> 5.0'
+def parse_gemfile_entry(line)
+  if (match = line.match(/^\s*gem\s+["']([^"']+)["']\s*,?\s*["']?([^"']*)["']?/))
+    gem_name, version_constraint = match[1], match[2]
+    version = extract_version(version_constraint)
+    [gem_name, version]
   end
-  
-  # Helper: Parse a Gemfile.lock-style entry
-  # matches on ex. actionmailer (= 6.0.6)
-  def parse_gemfile_lock_entry(line)
-    if match = line.match(/^\s*([a-z0-9_-]+)\s+\(([^)]+)\)/)
-      [match[1], match[2]]
-    end
+end
+
+# Helper: Parse a Gemfile.lock-style entry
+# matches on ex. actionmailer (= 6.0.6)
+def parse_gemfile_lock_entry(line)
+  if (match = line.match(/^\s*([a-z0-9_-]+)\s+\(([^)]+)\)/))
+    [match[1], match[2]]
   end
-  
-  # Helper: Validate the version format
-  def version_valid?(version)
-    version =~ /^\d+(\.\d+)*$/
+end
+
+# Helper: Validate the version format
+def version_valid?(version)
+  version =~ /^\d+(\.\d+)*$/
+end
+
+# Helper: Extract the actual version number from a constraint
+# Matches on the following version patterns:
+# 1. "pessimistic" versions, ex. '~> 1.2.3'
+# 2. '>= 1.2.3'
+# 3. 1.2.3
+def extract_version(constraint)
+  if constraint =~ /~>\s*([\d.]+(?:[-.\w]*))| # Handles ~> constraints
+                     >=\s*([\d.]+(?:[-.\w]*))| # Handles >= constraints
+                     ([\d.]+(?:[-.\w]*))       # Handles plain versions
+                    /x
+    Regexp.last_match(1) || Regexp.last_match(2) || Regexp.last_match(3)
   end
-  
-  # Helper: Extract the actual version number from a constraint
-  # Matches on the following version patterns:
-  # 1. "pessimistic" versions, ex. '~> 1.2.3'
-  # 2. '>= 1.2.3'
-  # 3. 1.2.3
-  def extract_version(constraint)
-    if constraint =~ /\~>\s*([\d\.]+(?:[-\.\w]*))| # Handles ~> constraints
-                       \>=\s*([\d\.]+(?:[-\.\w]*))| # Handles >= constraints
-                       ([\d\.]+(?:[-\.\w]*))       # Handles plain versions
-                      /x
-      Regexp.last_match(1) || Regexp.last_match(2) || Regexp.last_match(3)
-    else
-      nil
-    end
-  end  
+end
 
 def get_integration_names(directory = 'lib/datadog/tracing/contrib/')
-    unless Dir.exist?(directory)
-      puts "Directory '#{directory}' not found!"
-      return []
-    end
-  
-    # Get all subdirectories inside the specified directory
-    integrations = Dir.children(directory).select do |entry|
-      File.directory?(File.join(directory, entry))
-    end
-  
-    integrations
+  unless Dir.exist?(directory)
+    puts "Directory '#{directory}' not found!"
+    return []
+  end
+
+  # Get all subdirectories inside the specified directory
+  Dir.children(directory).select do |entry|
+    File.directory?(File.join(directory, entry))
+  end
 end
 
 mapping = {
@@ -138,10 +133,6 @@ integrations.each do |integration|
   # mapping jruby, ruby
   integration_json_mapping[integration] = [min_version_ruby, min_version_jruby]
   integration_json_mapping.replace(integration_json_mapping.sort.to_h)
-
 end
 
-
-File.open("minimum_gem_output.json", "w") do |f|
-  f.write(JSON.pretty_generate(integration_json_mapping))
-end
+File.write("minimum_gem_output.json", JSON.pretty_generate(integration_json_mapping))
