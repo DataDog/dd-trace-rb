4d554d5e9165547b171a08f65cfdcd8f2fa63dbe
Marco Costa <marco.costa@datadoghq.com>
2024-12-09 15:46:20 -0800

    Safely merge options

--- lib/datadog/tracing/contrib/faraday/patcher.rb
+++ lib/datadog/tracing/contrib/faraday/patcher.rb
@@ -30,20 +30,24 @@ module Datadog
             ::Faraday::Middleware.register_middleware(datadog_tracing: Middleware)
           end
 
+          # Patch the Faraday default connection (`Faraday.get`, `Faraday.post`, etc.)
+          # as well as add our middleware for when new connections are created.
           def add_default_middleware!
+            default_conn = ::Faraday.default_connection
+
             if target_version >= Gem::Version.new('1.0.0')
-              # Patch the default connection (e.g. +Faraday.get+)
-              ::Faraday.default_connection.use(:datadog_tracing)
+              # Patch the default connection
+              default_conn.use(:datadog_tracing, connection: default_conn)
 
               # Patch new connection instances (e.g. +Faraday.new+)
               ::Faraday::Connection.prepend(Connection)
             else
-              # Patch the default connection (e.g. +Faraday.get+)
+              # Patch the default connection
               #
               # We insert our middleware before the 'adapter', which is
               # always the last handler.
-              idx = ::Faraday.default_connection.builder.handlers.size - 1
-              ::Faraday.default_connection.builder.insert(idx, Middleware)
+              idx = default_conn.builder.handlers.size - 1
+              default_conn.builder.insert(idx, Middleware, connection: default_conn)
 
               # Patch new connection instances (e.g. +Faraday.new+)
               ::Faraday::RackBuilder.prepend(RackBuilder)
