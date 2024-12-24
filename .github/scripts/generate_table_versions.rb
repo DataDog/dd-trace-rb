3b8cff4a96614023e84f2d9d0449beb134c278b6
quinna-h <quinna.halim@datadoghq.com>
2024-12-24 09:42:01 -0500

    cleanup comments

--- .github/scripts/generate_table_versions.rb
+++ .github/scripts/generate_table_versions.rb
@@ -1,13 +1,10 @@
 require 'json'
 
-# Input and output file names
 input_file = 'gem_output.json'
 output_file = 'integration_versions.md'
 
-# Read JSON data from the input file
 data = JSON.parse(File.read(input_file))
 
-# Prepare the Markdown content
 comment = "# Integrations\n\n"
 header = "| Integration | Ruby Min |  Ruby Max | JRuby Min | JRuby Max |\n"
 separator = "|-------------|----------|-----------|----------|----------|\n"
@@ -16,7 +13,6 @@ rows = data.map do |integration_name, versions|
     "| #{integration_name} | #{ruby_min} | #{ruby_max} | #{jruby_min} | #{jruby_max} |"
 end
 
-# Write the Markdown file
 File.open(output_file, 'w') do |file|
   file.puts comment
   file.puts header
