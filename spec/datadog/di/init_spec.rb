d3b854c4d899fc6083aac23aff2782e5b6d86992
Oleg Pudeyev <code@olegp.name>
2024-12-25 12:09:18 -0500

    test that code tracker maintains mappings after datadog is loaded

--- spec/datadog/di/init_spec.rb
+++ spec/datadog/di/init_spec.rb
@@ -4,40 +4,64 @@ require 'open3'
 RSpec.describe 'DI initializer' do
   di_test
 
+  BOOTSTRAP_SCRIPT = <<-SCRIPT
+    if defined?(Datadog) && Datadog.constants != %i(VERSION)
+      raise "Datadog code loaded too early"
+    end
+
+    require 'datadog/di/init'
+
+    if Datadog.constants.sort != %i(DI VERSION)
+      raise "Too many datadog components loaded: \#{Datadog.constants}"
+    end
+
+    unless Datadog::DI.code_tracker
+      raise "Code tracker not instantiated"
+    end
+
+    unless Datadog::DI.code_tracker.send(:registry).empty?
+      raise "Code tracker registry is not empty"
+    end
+
+    # Test load something
+    require 'open3'
+
+    if Datadog::DI.code_tracker.send(:registry).empty?
+      raise "Code tracker did not add loaded file to registry"
+    end
+
+    unless Datadog::DI.code_tracker.send(:registry).detect { |key, value| key =~ /open3.rb\\z/ }
+      raise "Loaded script not found in code tracker registry"
+    end
+
+    if Datadog.constants.sort != %i(DI VERSION)
+      raise "Too many datadog components loaded at the end of execution: \#{Datadog.constants}"
+    end
+  SCRIPT
+
   context 'when loaded initially into a clean process' do
     it 'loads only DI code tracker' do
+      out, status = Open3.capture2e('ruby', stdin_data: BOOTSTRAP_SCRIPT)
+      unless status.exitstatus == 0
+        fail("Test script failed with exit status #{status.exitstatus}:\n#{out}")
+      end
+    end
+  end
+
+  context 'when entire library is loaded after di bootstrapper' do
+    it 'keeps the mappings in code tracker prior to datadog load' do
       script = <<-SCRIPT
-        if defined?(Datadog) && Datadog.constants != %i(VERSION)
-          raise "Datadog code loaded too early"
-        end
+        #{BOOTSTRAP_SCRIPT}
 
-        require 'datadog/di/init'
+        require 'datadog'
 
-        if Datadog.constants.sort != %i(DI VERSION)
-          raise "Too many datadog components loaded: \#{Datadog.constants}"
-        end
-
-        unless Datadog::DI.code_tracker
-          raise "Code tracker not instantiated"
-        end
-
-        unless Datadog::DI.code_tracker.send(:registry).empty?
-          raise "Code tracker registry is not empty"
-        end
-
-        # Test load something
-        require 'open3'
-
-        if Datadog::DI.code_tracker.send(:registry).empty?
-          raise "Code tracker did not add loaded file to registry"
-        end
-
+        # Should still have the open3 entry in code tracker
         unless Datadog::DI.code_tracker.send(:registry).detect { |key, value| key =~ /open3.rb\\z/ }
           raise "Loaded script not found in code tracker registry"
         end
 
-        if Datadog.constants.sort != %i(DI VERSION)
-          raise "Too many datadog components loaded at the end of execution: \#{Datadog.constants}"
+        unless defined?(Datadog::Tracing)
+          raise "Expected Datadog::Tracing to be defined after datadog was loaded"
         end
       SCRIPT
       out, status = Open3.capture2e('ruby', stdin_data: script)
