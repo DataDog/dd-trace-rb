f9c0bb90d8714ee12bae15bdc31cb2e250f94788
Oleg Pudeyev <code@olegp.name>
2025-01-09 17:26:35 -0500

    remove usage of OpenStruct in DI

--- lib/datadog/di/transport.rb
+++ lib/datadog/di/transport.rb
@@ -61,9 +61,7 @@ module Datadog
       attr_reader :client
 
       def send_request(desc, **options)
-        # steep:ignore:start
-        env = OpenStruct.new(**options)
-        # steep:ignore:end
+        env = Core::Transport::HTTP::Env.new(nil, options)
         response = client.post(env)
         unless response.ok?
           raise Error::AgentCommunicationError, "#{desc} failed: #{response.code}: #{response.payload}"
