cb26ad764d3f568dba00d361d6d07be0f4ce1c2d
Oleg Pudeyev <code@olegp.name>
2025-01-09 19:10:53 -0500

    load DI from auto_instrument

--- lib/datadog/auto_instrument.rb
+++ lib/datadog/auto_instrument.rb
@@ -6,6 +6,11 @@
 require_relative '../datadog'
 require_relative 'tracing/contrib/auto_instrument'
 
+# DI is not loaded on Ruby 2.5 and JRuby
+if defined?(Datadog::DI::Contrib)
+  Datadog::DI::Contrib.load_now_or_later
+end
+
 Datadog::Profiling.start_if_enabled
 
 module Datadog
