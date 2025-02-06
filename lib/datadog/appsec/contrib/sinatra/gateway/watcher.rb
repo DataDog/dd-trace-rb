baff5985e0e0d6d8092e66f891cb97ee17ff422b
Yury Lebedev <yury.lebedev@datadoghq.com>
2025-02-06 16:03:44 +0100

    Remove reactive engine from AppSec sinatra instrumentation

--- lib/datadog/appsec/contrib/sinatra/gateway/watcher.rb
+++ lib/datadog/appsec/contrib/sinatra/gateway/watcher.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 require_relative '../../../instrumentation/gateway'
-require_relative '../../../reactive/engine'
-require_relative '../reactive/routed'
 require_relative '../../../event'
 
 module Datadog
@@ -50,31 +48,27 @@ module Datadog
 
               def watch_request_routed(gateway = Instrumentation.gateway)
                 gateway.watch('sinatra.request.routed', :appsec) do |stack, (gateway_request, gateway_route_params)|
-                  event = nil
                   context = gateway_request.env[Datadog::AppSec::Ext::CONTEXT_KEY]
-                  engine = AppSec::Reactive::Engine.new
 
-                  Sinatra::Reactive::Routed.subscribe(engine, context) do |result|
-                    if result.match?
-                      # TODO: should this hash be an Event instance instead?
-                      event = {
-                        waf_result: result,
-                        trace: context.trace,
-                        span: context.span,
-                        request: gateway_request,
-                        actions: result.actions
-                      }
+                  persistent_data = {
+                    'server.request.path_params' => gateway_route_params.params
+                  }
 
-                      # We want to keep the trace in case of security event
-                      context.trace.keep! if context.trace
-                      Datadog::AppSec::Event.tag_and_keep!(context, result)
-                      context.events << event
+                  result = context.run_waf(persistent_data, {}, Datadog.configuration.appsec.waf_timeout)
 
-                      Datadog::AppSec::ActionsHandler.handle(result.actions)
-                    end
+                  if result.match?
+                    Datadog::AppSec::Event.tag_and_keep!(context, result)
+
+                    context.events << {
+                      waf_result: result,
+                      trace: context.trace,
+                      span: context.span,
+                      request: gateway_request,
+                      actions: result.actions
+                    }
+
+                    Datadog::AppSec::ActionsHandler.handle(result.actions)
                   end
-
-                  Sinatra::Reactive::Routed.publish(engine, [gateway_request, gateway_route_params])
 
                   stack.call(gateway_request.request)
                 end
