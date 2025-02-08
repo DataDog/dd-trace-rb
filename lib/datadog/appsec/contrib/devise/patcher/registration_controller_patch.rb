ed1b254241a69b3227a923e721905845bed7b525
Sergey Fedorov <sergey.fedorov@datadoghq.com>
2025-02-08 10:16:13 +0100

    Extract devise configuration into separate module

--- lib/datadog/appsec/contrib/devise/patcher/registration_controller_patch.rb
+++ lib/datadog/appsec/contrib/devise/patcher/registration_controller_patch.rb
@@ -46,26 +46,6 @@ module Datadog
                 yield resource if block_given?
               end
             end
-
-            private
-
-            # NOTE: DEV-3 replace method use with `auto_user_instrumentation.enabled?`
-            def auto_user_instrumentation_enabled?
-              Datadog.configuration.appsec.auto_user_instrumentation.enabled? &&
-                Datadog.configuration.appsec.track_user_events.enabled
-            end
-
-            # NOTE: DEV-3 replace method use with `auto_user_instrumentation.mode`
-            def auto_user_instrumentation_mode
-              case Datadog.configuration.appsec.track_user_events.mode
-              when Configuration::Settings::SAFE_TRACK_USER_EVENTS_MODE
-                Configuration::Settings::ANONYMIZATION_AUTO_USER_INSTRUMENTATION_MODE
-              when Configuration::Settings::EXTENDED_TRACK_USER_EVENTS_MODE
-                Configuration::Settings::IDENTIFICATION_AUTO_USER_INSTRUMENTATION_MODE
-              else
-                Datadog.configuration.appsec.auto_user_instrumentation.mode
-              end
-            end
           end
         end
       end
