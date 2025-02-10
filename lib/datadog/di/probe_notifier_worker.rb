da491947a9319e7a99c5c265ea1864e1595971ae
Oleg Pudeyev <code@olegp.name>
2025-02-10 08:15:40 -0500

    log queue runs

--- lib/datadog/di/probe_notifier_worker.rb
+++ lib/datadog/di/probe_notifier_worker.rb
@@ -235,6 +235,7 @@ module Datadog
             instance_variable_set("@#{event_type}_queue", [])
             @io_in_progress = batch.any? # steep:ignore
           end
+          logger.debug { "di: #{self.class.name}: checking #{event_type} queue - #{batch.length} entries" }
           if batch.any? # steep:ignore
             begin
               logger.debug { "di: sending #{batch.length} #{event_type} events to agent" }
