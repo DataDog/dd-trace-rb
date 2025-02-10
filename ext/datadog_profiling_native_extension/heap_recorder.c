78670f1c6445f51e462aba032d2c87e860e492e7
Ivo Anjo <ivo.anjo@datadoghq.com>
2025-02-10 10:41:53 +0000

    Add workaround for buggy gcc warnings in our Ruby 2.7 image
    Fixes these bogus warnings (that get turned into errors in our CI):
    
    ```
    ../../../../ext/datadog_profiling_native_extension/collectors_stack.c: In function ‘sample_thread’:
    ../../../../ext/datadog_profiling_native_extension/collectors_stack.c:303:7: error: missing initializer for field ‘build_id_id’ of ‘struct ddog_prof_Mapping’ [-Werror=missing-field-initializers]
      303 |       .mapping = {.filename = DDOG_CHARSLICE_C(""), .build_id = DDOG_CHARSLICE_C("")},
          |       ^
    In file included from /usr/local/bundle/gems/libdatadog-16.0.1.1.0-x86_64-linux/vendor/libdatadog-16.0.1/x86_64-linux/libdatadog-x86_64-unknown-linux-gnu/lib/pkgconfig/../../include/datadog/profiling.h:13,
                     from ../../../../ext/datadog_profiling_native_extension/libdatadog_helpers.h:3,
                     from ../../../../ext/datadog_profiling_native_extension/collectors_stack.c:5:
    /usr/local/bundle/gems/libdatadog-16.0.1.1.0-x86_64-linux/vendor/libdatadog-16.0.1/x86_64-linux/libdatadog-x86_64-unknown-linux-gnu/lib/pkgconfig/../../include/datadog/common.h:520:36: note: ‘build_id_id’ declared here
      520 |   struct ddog_prof_ManagedStringId build_id_id;
          |                                    ^~~~~~~~~~~
    ../../../../ext/datadog_profiling_native_extension/collectors_stack.c: In function ‘maybe_add_placeholder_frames_omitted’:
    ../../../../ext/datadog_profiling_native_extension/collectors_stack.c:382:5: error: missing initializer for field ‘build_id_id’ of ‘struct ddog_prof_Mapping’ [-Werror=missing-field-initializers]
      382 |     .mapping = {.filename = DDOG_CHARSLICE_C(""), .build_id = DDOG_CHARSLICE_C("")},
          |     ^
    In file included from /usr/local/bundle/gems/libdatadog-16.0.1.1.0-x86_64-linux/vendor/libdatadog-16.0.1/x86_64-linux/libdatadog-x86_64-unknown-linux-gnu/lib/pkgconfig/../../include/datadog/profiling.h:13,
                     from ../../../../ext/datadog_profiling_native_extension/libdatadog_helpers.h:3,
                     from ../../../../ext/datadog_profiling_native_extension/collectors_stack.c:5:
    /usr/local/bundle/gems/libdatadog-16.0.1.1.0-x86_64-linux/vendor/libdatadog-16.0.1/x86_64-linux/libdatadog-x86_64-unknown-linux-gnu/lib/pkgconfig/../../include/datadog/common.h:520:36: note: ‘build_id_id’ declared here
      520 |   struct ddog_prof_ManagedStringId build_id_id;
          |                                    ^~~~~~~~~~~
    ../../../../ext/datadog_profiling_native_extension/collectors_stack.c: In function ‘record_placeholder_stack’:
    ../../../../ext/datadog_profiling_native_extension/collectors_stack.c:429:5: error: missing initializer for field ‘build_id_id’ of ‘struct ddog_prof_Mapping’ [-Werror=missing-field-initializers]
      429 |     .mapping = {.filename = DDOG_CHARSLICE_C(""), .build_id = DDOG_CHARSLICE_C("")},
          |     ^
    In file included from /usr/local/bundle/gems/libdatadog-16.0.1.1.0-x86_64-linux/vendor/libdatadog-16.0.1/x86_64-linux/libdatadog-x86_64-unknown-linux-gnu/lib/pkgconfig/../../include/datadog/profiling.h:13,
                     from ../../../../ext/datadog_profiling_native_extension/libdatadog_helpers.h:3,
                     from ../../../../ext/datadog_profiling_native_extension/collectors_stack.c:5:
    /usr/local/bundle/gems/libdatadog-16.0.1.1.0-x86_64-linux/vendor/libdatadog-16.0.1/x86_64-linux/libdatadog-x86_64-unknown-linux-gnu/lib/pkgconfig/../../include/datadog/common.h:520:36: note: ‘build_id_id’ declared here
      520 |   struct ddog_prof_ManagedStringId build_id_id;
          |                                    ^~~~~~~~~~~
    cc1: all warnings being treated as errors
    ```

--- ext/datadog_profiling_native_extension/heap_recorder.c
+++ ext/datadog_profiling_native_extension/heap_recorder.c
@@ -678,7 +678,7 @@ static int st_object_records_iterate(DDTRACE_UNUSED st_data_t key, st_data_t val
   for (uint16_t i = 0; i < stack->frames_len; i++) {
     const heap_frame *frame = &stack->frames[i];
     locations[i] = (ddog_prof_Location) {
-      .mapping = {.filename = DDOG_CHARSLICE_C(""), .build_id = DDOG_CHARSLICE_C("")},
+      .mapping = {.filename = DDOG_CHARSLICE_C(""), .build_id = DDOG_CHARSLICE_C(""), .build_id_id = {}},
       .function = {
         .name = {.ptr = frame->name, .len = strlen(frame->name)},
         .filename = {.ptr = frame->filename, .len = strlen(frame->filename)},
