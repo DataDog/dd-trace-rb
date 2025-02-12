c29be53269ebe17db3f226b8c662200ad3f2f49a
Ivo Anjo <ivo.anjo@datadoghq.com>
2025-02-12 10:13:05 +0000

    Modify managed string storage benchmarking
    Rather than comparing intern with intern_all (less useful now that
    https://github.com/DataDog/libdatadog/pull/844 is merged), let's keep
    this around as a more generic measurement of managed string storage
    performance.

--- benchmarks/profiler_string_storage_intern.rb
+++ benchmarks/profiler_string_storage_intern.rb
@@ -5,7 +5,7 @@ return unless __FILE__ == $PROGRAM_NAME || VALIDATE_BENCHMARK_MODE
 
 require_relative 'benchmarks_helper'
 
-# This benchmark measures the performance of string storage intern/intern_all APIs
+# This benchmark measures the performance of string storage-related APIs
 
 class ProfilerStringStorageIntern
   def initialize
@@ -18,16 +18,37 @@ class ProfilerStringStorageIntern
       x.config(
         **benchmark_time,
       )
-
-      x.report('intern 1000 strings') do
-        Datadog::Profiling::StackRecorder::Testing._native_benchmark_intern(@recorder, "hello, world!", 1000, false)
-      end
-
-      x.report('intern_all 1000 strings') do
+      x.report('intern_all 1000 repeated strings') do
         Datadog::Profiling::StackRecorder::Testing._native_benchmark_intern(@recorder, "hello, world!", 1000, true)
       end
 
-      x.save! "#{File.basename(__FILE__)}-results.json" unless VALIDATE_BENCHMARK_MODE
+      x.save! "#{File.basename(__FILE__)}-1-results.json" unless VALIDATE_BENCHMARK_MODE
+      x.compare!
+    end
+
+    Benchmark.ips do |x|
+      benchmark_time = VALIDATE_BENCHMARK_MODE ? { time: 0.01, warmup: 0 } : { time: 10, warmup: 2 }
+      x.config(
+        **benchmark_time,
+      )
+
+      x.report('intern mixed existing and new') do
+        recorder = Datadog::Profiling::StackRecorder.for_testing(heap_samples_enabled: true)
+
+        strings_to_intern = 100_000
+        existing_strings = (strings_to_intern * 0.9).to_i
+        new_strings = strings_to_intern - existing_strings
+
+        new_strings.times do |i|
+          Datadog::Profiling::StackRecorder::Testing._native_benchmark_intern(recorder, ("%010d" % i), 1, false)
+        end
+
+        existing_strings.times do |i|
+          Datadog::Profiling::StackRecorder::Testing._native_benchmark_intern(recorder, "hello, world!", 1, false)
+        end
+      end
+
+      x.save! "#{File.basename(__FILE__)}-2-results.json" unless VALIDATE_BENCHMARK_MODE
       x.compare!
     end
   end
