37957540cdf920eeeefe0b6ed1e47b2eee301b2b
Sarah Chen <sarah.chen@datadoghq.com>
2025-02-19 17:11:25 -0500

    Print status.

--- spec/support/synchronization_helpers.rb
+++ spec/support/synchronization_helpers.rb
@@ -3,6 +3,8 @@ require 'English'
 module SynchronizationHelpers
   def expect_in_fork(fork_expectations: nil, timeout_seconds: 10)
     fork_expectations ||= proc { |status:, stdout:, stderr:|
+      puts "status: #{status}"
+      puts "status.success?: #{status.success?}"
       expect(status && status.success?).to be(true), "STDOUT:`#{stdout}` STDERR:`#{stderr}`"
     }
 
