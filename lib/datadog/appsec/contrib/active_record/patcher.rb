337993fe766540623e842f96b741ff6b5ea158fe
Yury Lebedev <yury.lebedev@datadoghq.com>
2025-02-21 17:56:12 +0100

    Fix AppSec ActiveRecord instrumentation

--- lib/datadog/appsec/contrib/active_record/patcher.rb
+++ lib/datadog/appsec/contrib/active_record/patcher.rb
@@ -19,27 +19,40 @@ module Datadog
           end
 
           def patch
-            ActiveSupport.on_load :active_record do
-              instrumentation_module = if ::ActiveRecord.gem_version >= Gem::Version.new('7.1')
-                                         Instrumentation::InternalExecQueryAdapterPatch
-                                       else
-                                         Instrumentation::ExecQueryAdapterPatch
-                                       end
+            if ::ActiveRecord.gem_version >= Gem::Version.new('7.1')
+              instrumentation_module = Instrumentation::InternalExecQueryAdapterPatch
 
-              if defined?(::ActiveRecord::ConnectionAdapters::SQLite3Adapter)
+              # Load Hooks for all adapters are present starting with ActiveRecord 7.1
+              ActiveSupport.on_load :active_record_sqlite3adapter do
                 ::ActiveRecord::ConnectionAdapters::SQLite3Adapter.prepend(instrumentation_module)
               end
 
-              if defined?(::ActiveRecord::ConnectionAdapters::Mysql2Adapter)
+              ActiveSupport.on_load :active_record_mysql2adapter do
                 ::ActiveRecord::ConnectionAdapters::Mysql2Adapter.prepend(instrumentation_module)
               end
 
-              if defined?(::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
-                unless defined?(::ActiveRecord::ConnectionAdapters::JdbcAdapter)
-                  instrumentation_module = Instrumentation::ExecuteAndClearAdapterPatch
+              ActiveSupport.on_load :active_record_postgresqladapter do
+                ::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.prepend(instrumentation_module)
+              end
+            else
+              instrumentation_module = Instrumentation::ExecQueryAdapterPatch
+
+              ActiveSupport.on_load :active_record do
+                if defined?(::ActiveRecord::ConnectionAdapters::SQLite3Adapter)
+                  ::ActiveRecord::ConnectionAdapters::SQLite3Adapter.prepend(instrumentation_module)
                 end
 
-                ::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.prepend(instrumentation_module)
+                if defined?(::ActiveRecord::ConnectionAdapters::Mysql2Adapter)
+                  ::ActiveRecord::ConnectionAdapters::Mysql2Adapter.prepend(instrumentation_module)
+                end
+
+                if defined?(::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+                  unless defined?(::ActiveRecord::ConnectionAdapters::JdbcAdapter)
+                    instrumentation_module = Instrumentation::ExecuteAndClearAdapterPatch
+                  end
+
+                  ::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.prepend(instrumentation_module)
+                end
               end
             end
           end
