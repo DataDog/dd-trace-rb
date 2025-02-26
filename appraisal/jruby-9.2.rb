b4166ccc69eef5b64f2b26e4783f93bd52de963e
Tony Hsu <tony.hsu@datadoghq.com>
2025-02-26 21:29:18 +0100

    Fix coverage matrix for karafka

--- appraisal/jruby-9.2.rb
+++ appraisal/jruby-9.2.rb
@@ -204,7 +204,10 @@ build_coverage_matrix('excon')
 build_coverage_matrix('rest-client')
 build_coverage_matrix('mongo', min: '2.1.0')
 build_coverage_matrix('dalli', [2])
-build_coverage_matrix('karafka', min: '2.0.41')
+
+appraise 'karafka-min' do
+  gem 'karafka', '= 2.0.41'
+end
 
 appraise 'relational_db' do
   gem 'activerecord', '~> 5'
