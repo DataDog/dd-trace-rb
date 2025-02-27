153fccf2afb5077dd786517a7cb9360873b2dc88
Sergey Fedorov <sergey.fedorov@datadoghq.com>
2025-02-27 17:34:26 +0100

    Refactor Devise patches and middleware

--- lib/datadog/appsec/contrib/devise/tracking_middleware.rb
+++ lib/datadog/appsec/contrib/devise/tracking_middleware.rb
@@ -1,51 +1,73 @@
 # frozen_string_literal: true
 
+require_relative '../../anonymizer'
+
 module Datadog
   module AppSec
     module Contrib
       module Devise
-        # TODO
+        # A Rack middleware capable tracking currently signed user
         class TrackingMiddleware
           def initialize(app)
             @app = app
           end
 
-          # TODO
           def call(env)
             return @app.call(env) unless AppSec.enabled?
             return @app.call(env) unless Configuration.auto_user_instrumentation_enabled?
             return @app.call(env) unless AppSec.active_context
 
+            # TODO: Add check for trace and span
             unless env.key?('warden')
               # TODO: puts debugging message
               return @app.call(env)
             end
 
             context = AppSec.active_context
-            session_serializer = env['warden'].session_serializer
+            id = transform(extract_id(env['warden']))
+
+            if id
+              context.span['usr.id'] ||= id.to_s
+              context.span['_dd.appsec.usr.id'] = id.to_s
+              context.span['_dd.appsec.user.collection_mode'] ||= Configuration.auto_user_instrumentation_mode
+            end
+
+            @app.call(env)
+          end
+
+          private
+
+          def extract_id(warden)
+            session_serializer = warden.session_serializer
 
             key = session_serializer.key_for(::Devise.default_scope)
             id = session_serializer.session[key]&.dig(0, 0)
 
-            if id.nil?
-              ::Devise.mappings.each_key do |scope|
-                next if scope == ::Devise.default_scope
+            return id if ::Devise.mappings.size == 1
+            return "#{::Devise.default_scope}:#{id}" if id
 
-                key = session_serializer.key_for(scope)
-                id = session_serializer.session[key]&.dig(0, 0)
-                break unless id.nil?
-              end
+            ::Devise.mappings.each_key do |scope|
+              next if scope == ::Devise.default_scope
+
+              key = session_serializer.key_for(scope)
+              id = session_serializer.session[key]&.dig(0, 0)
+
+              return "#{scope}:#{id}" unless id.nil?
             end
 
-            if id
-              context.span.set_tag('usr.id', id.to_s) unless context.span.has_tag?('usr.id')
-              context.span.set_tag('_dd.appsec.usr.id', id.to_s)
-              unless context.span.has_tag?('_dd.appsec.user.collection_mode')
-                context.span.set_tag('_dd.appsec.user.collection_mode', Configuration.auto_user_instrumentation_mode)
-              end
-            end
+            nil
+          end
 
-            @app.call(env)
+          def transform(value)
+            return if value.nil?
+            return value.to_s unless anonymize?
+
+            Anonymizer.anonimyze(value)
+          end
+
+          def anonymize?
+            Configuration.auto_user_instrumentation_mode ==
+              AppSec::Configuration::Settings::ANONYMIZATION_AUTO_USER_INSTRUMENTATION_MODE
           end
         end
       end
