a70a07ef7b6540b7f45792834dae1b82a7440140
Yury Lebedev <yury.lebedev@datadoghq.com>
2025-03-10 10:10:37 +0100

    Improve MetastructTagging module spec

--- spec/datadog/tracing/metadata/metastruct_tagging_spec.rb
+++ spec/datadog/tracing/metadata/metastruct_tagging_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 require 'datadog/tracing/metadata/metastruct_tagging'
 
 RSpec.describe Datadog::Tracing::Metadata::MetastructTagging do
-  subject(:test_object) { test_class.new }
+  subject(:object) { test_class.new }
 
   let(:test_class) do
     Class.new { include Datadog::Tracing::Metadata::MetastructTagging }
@@ -11,23 +11,21 @@ RSpec.describe Datadog::Tracing::Metadata::MetastructTagging do
 
   describe '#set_metastruct_tag' do
     it 'sets the metastruct to a hash with given key / value pair' do
-      expect do
-        test_object.set_metastruct_tag(:foo, [{ some: 'value' }])
-      end.to change { test_object.get_metastruct_tag(:foo) }.from(nil).to([{ some: 'value' }])
+      expect { object.set_metastruct_tag(:foo, [{ some: 'value' }]) }
+        .to change { object.get_metastruct_tag(:foo) }.from(nil).to([{ some: 'value' }])
     end
 
     it 'does not lose previous entries' do
-      test_object.set_metastruct_tag(:foo, [{ some: 'value' }])
+      object.set_metastruct_tag(:foo, [{ some: 'value' }])
 
-      expect do
-        test_object.set_metastruct_tag(:bar, [{ another: 'value' }])
-      end.not_to(change { test_object.get_metastruct_tag(:foo) })
+      expect { object.set_metastruct_tag(:bar, [{ another: 'value' }]) }
+        .not_to(change { object.get_metastruct_tag(:foo) })
     end
   end
 
   describe '#get_metastruct_tag' do
     it 'returns nil if the key does not exist' do
-      expect(test_object.get_metastruct_tag(:foo)).to be_nil
+      expect(object.get_metastruct_tag(:foo)).to be_nil
     end
   end
 end
