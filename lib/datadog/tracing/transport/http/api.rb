0828e5d370682515ab0e4594a4b2218bf37c7b94
Oleg Pudeyev <code@olegp.name>
2025-03-11 17:18:54 -0400

    tracing component can also use single inheritance

--- lib/datadog/tracing/transport/http/api.rb
+++ lib/datadog/tracing/transport/http/api.rb
@@ -22,28 +22,20 @@ module Datadog
 
           def defaults
             Core::Transport::HTTP::API::Map[
-              V4 => Spec.new do |s|
+              V4 => Traces::API::Spec.new do |s|
                 s.traces = Traces::API::Endpoint.new(
                   '/v0.4/traces',
                   Core::Encoding::MsgpackEncoder,
                   service_rates: true
                 )
               end,
-              V3 => Spec.new do |s|
+              V3 => Traces::API::Spec.new do |s|
                 s.traces = Traces::API::Endpoint.new(
                   '/v0.3/traces',
                   Core::Encoding::MsgpackEncoder
                 )
               end,
             ].with_fallbacks(V4 => V3)
-          end
-
-          class Instance < Core::Transport::HTTP::API::Instance
-            include Traces::API::Instance
-          end
-
-          class Spec < Core::Transport::HTTP::API::Spec
-            include Traces::API::Spec
           end
         end
       end
