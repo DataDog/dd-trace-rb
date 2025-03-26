79ee81284988dcff0590631e25bfea9a5d15e875
Oleg Pudeyev <code@olegp.name>
2025-03-26 10:32:40 -0400

    move

--- ext/libdatadog_api/init.c
+++ ext/libdatadog_api/init.c
@@ -2,6 +2,7 @@
 #include "datadog_ruby_common.h"
 
 void crashtracker_init(VALUE crashtracking_module);
+void di_init(VALUE di_module);
 
 // Used to report Ruby VM crashes.
 // Once initialized, segfaults will be reported automatically using libdatadog.
@@ -10,6 +11,8 @@ void DDTRACE_EXPORT Init_libdatadog_api(void) {
   VALUE datadog_module = rb_define_module("Datadog");
   VALUE core_module = rb_define_module_under(datadog_module, "Core");
   VALUE crashtracking_module = rb_define_module_under(core_module, "Crashtracking");
+  VALUE di_module = rb_define_module_under(datadog_module, "DI");
 
   crashtracker_init(crashtracking_module);
+  di_init(di_module);
 }
