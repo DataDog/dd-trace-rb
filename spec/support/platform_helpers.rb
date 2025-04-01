55063e790cb53753462dc77cc2596bc31515c8e6
Sergey Fedorov <sergey.fedorov@datadoghq.com>
2025-04-01 15:40:43 +0200

    Add PlatformHelper.engine_version_matches? helper
    It will allow us to have guard-clause for some specific cases. Example:
    
    ```ruby
    it 'runs only on Ruby > 3.0', ruby: '> 3.0' do
      expect(something).to be(true)
    end
    ```

--- spec/support/platform_helpers.rb
+++ spec/support/platform_helpers.rb
@@ -3,6 +3,9 @@
 require 'os'
 
 module PlatformHelpers
+  EQUALITY_OPERATOR = '=='
+  ALLOWED_COMPARISON_OPERATORS = %w[> >= == != < <=].freeze
+
   module_function
 
   # Ruby runtime engines
@@ -22,6 +25,28 @@ module PlatformHelpers
   def engine_version
     version = defined?(RUBY_ENGINE_VERSION) ? RUBY_ENGINE_VERSION : RUBY_VERSION
     Gem::Version.new(version)
+  end
+
+  def ruby_version_matches?(matcher_with_ruby_version)
+    ruby_version = Gem::Version.new(RUBY_VERSION)
+    operator, guard_version = matcher_with_ruby_version.split(' ', 2).tap { |array| array.unshift('==') if array.size == 1 }
+
+    unless ALLOWED_COMPARISON_OPERATORS.include?(operator)
+      message = "Unsupported operator: #{operator}. Supported operators: #{ALLOWED_COMPARISON_OPERATORS.join(', ')}"
+      raise ArgumentError, message
+    end
+
+    unless Gem::Version.correct?(guard_version)
+      message = "Invalid version: #{guard_version}. Make sure to add space between operator and version."
+      raise ArgumentError, message
+    end
+
+    if operator == EQUALITY_OPERATOR && guard_version.count('.') < 3
+      version = Gem::Version.new("#{guard_version}.0")
+      (version...version.bump).cover?(ruby_version)
+    else
+      ruby_version.send(operator, Gem::Version.new(guard_version))
+    end
   end
 
   # Operating systems
