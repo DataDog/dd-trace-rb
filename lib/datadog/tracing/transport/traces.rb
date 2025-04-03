60862b1706d101f0240e32cbd999f5547bfe1cf2
Oleg Pudeyev <code@olegp.name>
2025-04-03 10:02:56 -0400

    adddefaulting to tracing/transport

--- lib/datadog/tracing/transport/traces.rb
+++ lib/datadog/tracing/transport/traces.rb
@@ -51,7 +51,7 @@ module Datadog
           # @param encoder [Datadog::Core::Encoding::Encoder]
           # @param logger [Datadog::Core::Logger]
           # @param max_size [String] maximum acceptable payload size
-          def initialize(encoder, logger, native_events_supported:, max_size: DEFAULT_MAX_PAYLOAD_SIZE)
+          def initialize(encoder, logger:, native_events_supported:, max_size: DEFAULT_MAX_PAYLOAD_SIZE)
             @encoder = encoder
             @logger = logger
             @native_events_supported = native_events_supported
@@ -80,7 +80,7 @@ module Datadog
           private
 
           def encode_one(trace)
-            encoded = Encoder.encode_trace(encoder, trace, logger, native_events_supported: @native_events_supported)
+            encoded = Encoder.encode_trace(encoder, trace, logger: logger, native_events_supported: @native_events_supported)
 
             if encoded.size > max_size
               # This single trace is too large, we can't flush it
@@ -98,7 +98,7 @@ module Datadog
         module Encoder
           module_function
 
-          def encode_trace(encoder, trace, logger, native_events_supported:)
+          def encode_trace(encoder, trace, logger:, native_events_supported:)
             # Format the trace for transport
             TraceFormatter.format!(trace)
 
