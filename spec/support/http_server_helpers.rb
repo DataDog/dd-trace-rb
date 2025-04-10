0b0d8ac181cf83ab26caf231d459d6b358fe5b27
Oleg Pudeyev <code@olegp.name>
2025-04-10 15:05:24 -0400

    use queue to reduce wait time

--- spec/support/http_server_helpers.rb
+++ spec/support/http_server_helpers.rb
@@ -4,10 +4,12 @@ module HttpServerHelpers
   module ClassMethods
     def http_server(&block)
       let(:http_server_port) { http_server[:Port] }
+      let(:http_server_init_signal) { Queue.new }
 
       let(:http_server) do
         WEBrick::HTTPServer.new(
           Port: 0,
+          StartCallback: -> { http_server_init_signal.push(1) }
         ).tap do |http_server|
           instance_exec(http_server,&block)
         end
@@ -16,11 +18,11 @@ module HttpServerHelpers
       around do |example|
         @server_thread = Thread.new do
           http_server.start
+        rescue Exception
+          http_server_init_signal.push(1)
+          raise
         end
-        loop do
-          break if http_server.status == :Running || !@server_thread.alive?
-          sleep 0.5
-        end
+        http_server_init_signal.pop
         expect(@server_thread).to be_alive
         example.run
         @server_thread.kill
