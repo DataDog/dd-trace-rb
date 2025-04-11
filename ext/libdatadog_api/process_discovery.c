9419ec1e6a70efa78975d3bd4bcbbe21518c7b3b
Victor Pellan <victor.pellan@datadoghq.com>
2025-04-11 16:28:40 +0200

    -Add TracerMemfd native class to store file descriptor -Add get_and_store_metadata method -Remove default value for runtime_id

--- ext/libdatadog_api/process_discovery.c
+++ ext/libdatadog_api/process_discovery.c
@@ -1,4 +1,5 @@
 #include <errno.h>
+#include <stdlib.h>
 #include <ruby.h>
 #include <datadog/common.h>
 
@@ -7,14 +8,24 @@
 static VALUE _native_store_tracer_metadata(int argc, VALUE *argv, DDTRACE_UNUSED VALUE _self);
 static VALUE _native_close_tracer_memfd(DDTRACE_UNUSED VALUE _self, VALUE fd);
 
+static const rb_data_type_t tracer_memfd_type = {
+  .wrap_struct_name = "Datadog::Core::ProcessDiscovery::TracerMemfd",
+  .function = {
+    .dfree = RUBY_DEFAULT_FREE,
+    .dsize = NULL,
+  },
+  .flags = RUBY_TYPED_FREE_IMMEDIATELY
+};
+
 void process_discovery_init(VALUE core_module) {
   VALUE process_discovery_class = rb_define_class_under(core_module, "ProcessDiscovery", rb_cObject);
+  rb_define_class_under(process_discovery_class, "TracerMemfd", rb_cObject);
 
   rb_define_singleton_method(process_discovery_class, "_native_store_tracer_metadata", _native_store_tracer_metadata, -1);
   rb_define_singleton_method(process_discovery_class, "_native_close_tracer_memfd", _native_close_tracer_memfd, 1);
 }
 
-static VALUE _native_store_tracer_metadata(int argc, VALUE *argv, DDTRACE_UNUSED VALUE _self) {
+static VALUE _native_store_tracer_metadata(int argc, VALUE *argv, VALUE self) {
   VALUE options;
   rb_scan_args(argc, argv, "0:", &options);
   if (options == Qnil) options = rb_hash_new();
@@ -49,20 +60,34 @@ static VALUE _native_store_tracer_metadata(int argc, VALUE *argv, DDTRACE_UNUSED
   );
 
   if (result.tag == DDOG_RESULT_TRACER_MEMFD_HANDLE_ERR_TRACER_MEMFD_HANDLE) {
-    rb_raise(rb_eRuntimeError, "Failed to store the tracer configuration: %"PRIsVALUE, get_error_details_and_drop(&result.err));
+    rb_raise(rb_eRuntimeError, "Failed to store the tracer configuration in a memory file descriptor: %"PRIsVALUE, get_error_details_and_drop(&result.err));
   }
 
   // &result.ok is a ddog_TracerMemfdHandle, which is a struct only containing int fd, which is a file descriptor
-  // We should just return the fd to test everything is working
-  return INT2FIX(result.ok.fd);
+  // We should just return the fd
+  int *fd = malloc(sizeof(int));
+  if (!fd) {
+    rb_raise(rb_eRuntimeError, "Failed to allocate memory for file descriptor");
+  }
+
+  *fd = result.ok.fd;
+  VALUE tracer_memfd_class = rb_const_get(self, rb_intern("TracerMemfd"));
+  VALUE tracer_memfd = TypedData_Wrap_Struct(tracer_memfd_class, &tracer_memfd_type, fd);
+  return tracer_memfd;
 }
 
-static VALUE _native_close_tracer_memfd(DDTRACE_UNUSED VALUE _self, VALUE fd) {
-  ENFORCE_TYPE(fd, T_FIXNUM);
+static VALUE _native_close_tracer_memfd(DDTRACE_UNUSED VALUE _self, VALUE tracer_memfd) {
+  int *fd;
+  TypedData_Get_Struct(tracer_memfd, int, &tracer_memfd_type, fd);
+  if (fd == NULL) {
+    rb_raise(rb_eRuntimeError, "Failed to get the tracer configuration memory file descriptor: %s", strerror(errno));
+  }
 
-  int close_result = close(NUM2INT(fd));
+  int close_result = close(*fd);
+  free(fd);
+
   if (close_result == -1) {
-    rb_raise(rb_eRuntimeError, "Failed to close the tracer configuration: %s", strerror(errno));
+    rb_raise(rb_eRuntimeError, "Failed to close the tracer configuration memory file descriptor: %s", strerror(errno));
   }
 
   return Qnil;
