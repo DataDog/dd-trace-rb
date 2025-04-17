8a1edbde17df2f15dc72c8e6ad3137a33b554028
Victor Pellan <victor.pellan@datadoghq.com>
2025-04-17 17:39:14 +0200

    Add stable configuration C API

---
+++ ext/libdatadog_api/init.c
@@ -0,0 +1,12 @@
+#include <ruby.h>
+
+#include "datadog_ruby_common.h"
+#include "crashtracker.h"
+#include "library_config.h"
+void DDTRACE_EXPORT Init_libdatadog_api(void) {
+  VALUE datadog_module = rb_define_module("Datadog");
+  VALUE core_module = rb_define_module_under(datadog_module, "Core");
+
+  crashtracker_init(core_module);
+  library_config_init(core_module);
+}
