dd00409f48edec297c978af4ac374925b1aadb2d
Oleg Pudeyev <code@olegp.name>
2025-04-22 12:21:04 -0400

    warn when ignoring on_error argument due to it not being a proc

--- spec/datadog/tracing/span_operation_spec.rb
+++ spec/datadog/tracing/span_operation_spec.rb
@@ -147,7 +147,7 @@ RSpec.describe Datadog::Tracing::SpanOperation do
       end
 
       context ':on_error' do
-        let(:options) { { on_error: block } }
+        let(:options) { { on_error: on_error } }
 
         let(:block) { proc { raise error } }
         let(:error) { error_class.new('error message') }
@@ -162,6 +162,7 @@ RSpec.describe Datadog::Tracing::SpanOperation do
             before { allow(span_op).to receive(:set_error) }
 
             it 'propagates the error' do
+              expect(Datadog.logger).not_to receive(:warn)
               expect { measure }.to raise_error(error)
               expect(span_op).to have_received(:set_error).with(error)
             end
@@ -172,6 +173,7 @@ RSpec.describe Datadog::Tracing::SpanOperation do
           let(:on_error) { block }
 
           it 'yields to the error block and raises the error' do
+            expect(Datadog.logger).not_to receive(:warn)
             expect do
               expect do |b|
                 options[:on_error] = b.to_proc
@@ -191,7 +193,7 @@ RSpec.describe Datadog::Tracing::SpanOperation do
           let(:on_error) { 'not a proc' }
 
           it 'fallbacks to default error handler and log a debug message' do
-            expect(Datadog.logger).to receive(:debug).at_least(:once)
+            expect(Datadog.logger).to receive(:warn).with(/on_error argument to SpanOperation ignored because is not a Proc: not a proc/)
             expect do
               span_op.measure(&block)
             end.to raise_error(error)
