d5da508781a4ae7e47ebb6dfffe7d2426dd10779
Louis Tricot <louis.tricot@datadoghq.com>
2025-04-23 10:29:37 +0200

    feat: auto handled errors reporting on clean branch

---
+++ spec/datadog/core/errortracking/lib1.rb
@@ -0,0 +1,8 @@
+module Lib1
+  def self.rescue_error
+    begin
+      raise 'lib1 error'
+    rescue
+    end
+  end
+end
---
+++ spec/datadog/core/errortracking/sublib/sublib2.rb
@@ -0,0 +1,8 @@
+module SubLib2
+  def self.rescue_error
+    begin
+      raise 'sublib2 error'
+    rescue
+    end
+  end
+end
