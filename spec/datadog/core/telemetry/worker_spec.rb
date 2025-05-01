a8d7a61789ea2ffb628b3fcf76915d8b3d2ed62d
Oleg Pudeyev <code@olegp.name>
2025-05-01 16:31:09 -0400

    fix other test

--- spec/datadog/core/telemetry/worker_spec.rb
+++ spec/datadog/core/telemetry/worker_spec.rb
@@ -336,6 +336,11 @@ RSpec.describe Datadog::Core::Telemetry::Worker do
     let(:heartbeat_interval_seconds) { 60 }
     let(:metrics_aggregation_interval_seconds) { 30 }
 
+    before do
+      # This test expects the AppStarted event apparently
+      Datadog::Core::Telemetry::Worker::TELEMETRY_STARTED_ONCE.send(:reset_ran_once_state_for_tests)
+    end
+
     it 'flushes events and stops the worker' do
       worker.start
 
