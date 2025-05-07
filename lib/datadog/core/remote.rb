b6995eb12b5c744fb1602d316a3a77c6c4f25491
Oleg Pudeyev <code@olegp.name>
2025-05-07 18:28:54 -0400

    delete active_remote

--- lib/datadog/core/remote.rb
+++ lib/datadog/core/remote.rb
@@ -4,21 +4,6 @@ module Datadog
   module Core
     # Module to interact with core remote component
     module Remote
-      class << self
-        def active_remote
-          remote
-        end
-
-        private
-
-        def components
-          Datadog.send(:components)
-        end
-
-        def remote
-          components.remote
-        end
-      end
     end
   end
 end
