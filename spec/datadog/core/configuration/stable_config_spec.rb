45442740e2063c89a4b6fd9dde08f780c5991bfe
Victor Pellan <victor.pellan@datadoghq.com>
2025-05-12 16:08:17 +0200

    Add tests

---
+++ spec/datadog/core/configuration/stable_config_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+RSpec.describe Datadog::Core::Configuration::StableConfig do
+  # We cannot test the native lib as it calls libdatadog, which access /etc/datadog-agent
+  # System-tests is able to mock these files.
+  describe '#extract_configuration' do
+    context 'when libdatadog API is not available' do
+      it 'returns an empty hash' do
+        stub_const('Datadog::Core::LIBDATADOG_API_FAILURE', 'test')
+        expect(described_class.extract_configuration).to eq({})
+      end
+    end
+  end
+end
