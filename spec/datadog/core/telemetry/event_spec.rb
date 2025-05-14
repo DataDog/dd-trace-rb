262d3a3f0dd79f24d66b3f7b9bf545ea0ff49e2a
Oleg Pudeyev <code@olegp.name>
2025-05-14 16:00:25 -0400

    rubocop git/sci

--- spec/datadog/core/telemetry/event_spec.rb
+++ spec/datadog/core/telemetry/event_spec.rb
@@ -22,6 +22,20 @@ RSpec.describe Datadog::Core::Telemetry::Event do
     let(:logger) do
       stub_const('MyLogger', Class.new(::Logger)).new(nil)
     end
+    let(:expected_install_signature) do
+      { install_id: 'id', install_time: 'time', install_type: 'type' }
+    end
+    let(:expected_products) do
+      {
+        appsec: {
+          enabled: false,
+        },
+        dynamic_instrumentation: {
+          enabled: false,
+        },
+        profiler: hash_including(enabled: false),
+      }
+    end
     let(:event_class) { described_class::AppStarted }
     before do
       allow_any_instance_of(Datadog::Core::Utils::Sequence).to receive(:next).and_return(id)
@@ -52,51 +66,35 @@ RSpec.describe Datadog::Core::Telemetry::Event do
     end
 
     DEFAULT_CODE_CONFIGURATION = [
-          ['DD_AGENT_HOST', '1.2.3.4'],
-          ['DD_AGENT_TRANSPORT', 'TCP'],
-          ['DD_TRACE_SAMPLE_RATE', '0.5'],
-          ['DD_TRACE_REMOVE_INTEGRATION_SERVICE_NAMES_ENABLED', true],
-          ['DD_TRACE_PEER_SERVICE_DEFAULTS_ENABLED', false],
-          ['DD_TRACE_PEER_SERVICE_MAPPING', 'foo:bar'],
-          ['dynamic_instrumentation.enabled', false],
-          ['logger.level', 0],
-          ['profiling.advanced.code_provenance_enabled', true],
-          ['profiling.advanced.endpoint.collection.enabled', true],
-          ['profiling.enabled', false],
-          ['runtime_metrics.enabled', false],
-          ['tracing.analytics.enabled', true],
-          ['tracing.propagation_style_extract', '["datadog", "tracecontext", "baggage"]'],
-          ['tracing.propagation_style_inject', '["datadog", "tracecontext", "baggage"]'],
-          ['tracing.enabled', true],
-          ['tracing.log_injection', true],
-          ['tracing.partial_flush.enabled', false],
-          ['tracing.partial_flush.min_spans_threshold', 500],
-          ['tracing.report_hostname', false],
-          ['tracing.sampling.rate_limit', 100],
-          ['tracing.auto_instrument.enabled', false],
-          ['tracing.writer_options.buffer_size', 123],
-          ['tracing.writer_options.flush_interval', 456],
-          ['tracing.opentelemetry.enabled', false],
-          ['logger.instance', 'MyLogger'],
-          ['appsec.enabled', false],
-          ['appsec.sca_enabled', false]
-    ]
-
-    let(:expected_products) do
-      {
-          appsec: {
-            enabled: false,
-          },
-          dynamic_instrumentation: {
-            enabled: false,
-          },
-          profiler: hash_including(enabled: false),
-        }
-    end
-
-    let(:expected_install_signature) do
-      { install_id: 'id', install_time: 'time', install_type: 'type' }
-    end
+      ['DD_AGENT_HOST', '1.2.3.4'],
+      ['DD_AGENT_TRANSPORT', 'TCP'],
+      ['DD_TRACE_SAMPLE_RATE', '0.5'],
+      ['DD_TRACE_REMOVE_INTEGRATION_SERVICE_NAMES_ENABLED', true],
+      ['DD_TRACE_PEER_SERVICE_DEFAULTS_ENABLED', false],
+      ['DD_TRACE_PEER_SERVICE_MAPPING', 'foo:bar'],
+      ['dynamic_instrumentation.enabled', false],
+      ['logger.level', 0],
+      ['profiling.advanced.code_provenance_enabled', true],
+      ['profiling.advanced.endpoint.collection.enabled', true],
+      ['profiling.enabled', false],
+      ['runtime_metrics.enabled', false],
+      ['tracing.analytics.enabled', true],
+      ['tracing.propagation_style_extract', '["datadog", "tracecontext", "baggage"]'],
+      ['tracing.propagation_style_inject', '["datadog", "tracecontext", "baggage"]'],
+      ['tracing.enabled', true],
+      ['tracing.log_injection', true],
+      ['tracing.partial_flush.enabled', false],
+      ['tracing.partial_flush.min_spans_threshold', 500],
+      ['tracing.report_hostname', false],
+      ['tracing.sampling.rate_limit', 100],
+      ['tracing.auto_instrument.enabled', false],
+      ['tracing.writer_options.buffer_size', 123],
+      ['tracing.writer_options.flush_interval', 456],
+      ['tracing.opentelemetry.enabled', false],
+      ['logger.instance', 'MyLogger'],
+      ['appsec.enabled', false],
+      ['appsec.sca_enabled', false]
+    ].freeze
 
     it do
       is_expected.to match(
@@ -123,16 +121,16 @@ RSpec.describe Datadog::Core::Telemetry::Event do
       end
 
       it 'reports git/SCI values to telemetry' do
-          is_expected.to match(
-            products: expected_products,
-            configuration: contain_env_configuration(
-              ['DD_GIT_REPOSITORY_URL', 'https://github.com/datadog/hello'],
-              ['DD_GIT_COMMIT_SHA', '1234hash'],
-            ) + contain_code_configuration(
-          *DEFAULT_CODE_CONFIGURATION
-        ),
-            install_signature: expected_install_signature,
-          )
+        is_expected.to match(
+          products: expected_products,
+          configuration: contain_env_configuration(
+            ['DD_GIT_REPOSITORY_URL', 'https://github.com/datadog/hello'],
+            ['DD_GIT_COMMIT_SHA', '1234hash'],
+          ) + contain_code_configuration(
+            *DEFAULT_CODE_CONFIGURATION
+          ),
+          install_signature: expected_install_signature,
+        )
       end
     end
 
