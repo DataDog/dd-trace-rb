f48def49f4115fc1a01e5788adb229597468fd8f
Oleg Pudeyev <code@olegp.name>
2025-05-15 11:20:25 -0400

    go away

--- spec/datadog/core/telemetry/event_spec.rb
+++ spec/datadog/core/telemetry/event_spec.rb
@@ -22,6 +22,38 @@ RSpec.describe Datadog::Core::Telemetry::Event do
     let(:logger) do
       stub_const('MyLogger', Class.new(::Logger)).new(nil)
     end
+    let(:default_code_configuration) do
+      [
+        ['DD_AGENT_HOST', '1.2.3.4'],
+        ['DD_AGENT_TRANSPORT', 'TCP'],
+        ['DD_TRACE_SAMPLE_RATE', '0.5'],
+        ['DD_TRACE_REMOVE_INTEGRATION_SERVICE_NAMES_ENABLED', true],
+        ['DD_TRACE_PEER_SERVICE_DEFAULTS_ENABLED', false],
+        ['DD_TRACE_PEER_SERVICE_MAPPING', 'foo:bar'],
+        ['dynamic_instrumentation.enabled', false],
+        ['logger.level', 0],
+        ['profiling.advanced.code_provenance_enabled', true],
+        ['profiling.advanced.endpoint.collection.enabled', true],
+        ['profiling.enabled', false],
+        ['runtime_metrics.enabled', false],
+        ['tracing.analytics.enabled', true],
+        ['tracing.propagation_style_extract', '["datadog", "tracecontext", "baggage"]'],
+        ['tracing.propagation_style_inject', '["datadog", "tracecontext", "baggage"]'],
+        ['tracing.enabled', true],
+        ['tracing.log_injection', true],
+        ['tracing.partial_flush.enabled', false],
+        ['tracing.partial_flush.min_spans_threshold', 500],
+        ['tracing.report_hostname', false],
+        ['tracing.sampling.rate_limit', 100],
+        ['tracing.auto_instrument.enabled', false],
+        ['tracing.writer_options.buffer_size', 123],
+        ['tracing.writer_options.flush_interval', 456],
+        ['tracing.opentelemetry.enabled', false],
+        ['logger.instance', 'MyLogger'],
+        ['appsec.enabled', false],
+        ['appsec.sca_enabled', false]
+      ].freeze
+    end
     let(:expected_install_signature) do
       { install_id: 'id', install_time: 'time', install_type: 'type' }
     end
@@ -64,37 +96,6 @@ RSpec.describe Datadog::Core::Telemetry::Event do
     def contain_env_configuration(*array)
       array.map { |name, value| { name: name, origin: 'env_var', seq_id: id, value: value } }
     end
-
-    let(:default_code_configuration){ [
-      ['DD_AGENT_HOST', '1.2.3.4'],
-      ['DD_AGENT_TRANSPORT', 'TCP'],
-      ['DD_TRACE_SAMPLE_RATE', '0.5'],
-      ['DD_TRACE_REMOVE_INTEGRATION_SERVICE_NAMES_ENABLED', true],
-      ['DD_TRACE_PEER_SERVICE_DEFAULTS_ENABLED', false],
-      ['DD_TRACE_PEER_SERVICE_MAPPING', 'foo:bar'],
-      ['dynamic_instrumentation.enabled', false],
-      ['logger.level', 0],
-      ['profiling.advanced.code_provenance_enabled', true],
-      ['profiling.advanced.endpoint.collection.enabled', true],
-      ['profiling.enabled', false],
-      ['runtime_metrics.enabled', false],
-      ['tracing.analytics.enabled', true],
-      ['tracing.propagation_style_extract', '["datadog", "tracecontext", "baggage"]'],
-      ['tracing.propagation_style_inject', '["datadog", "tracecontext", "baggage"]'],
-      ['tracing.enabled', true],
-      ['tracing.log_injection', true],
-      ['tracing.partial_flush.enabled', false],
-      ['tracing.partial_flush.min_spans_threshold', 500],
-      ['tracing.report_hostname', false],
-      ['tracing.sampling.rate_limit', 100],
-      ['tracing.auto_instrument.enabled', false],
-      ['tracing.writer_options.buffer_size', 123],
-      ['tracing.writer_options.flush_interval', 456],
-      ['tracing.opentelemetry.enabled', false],
-      ['logger.instance', 'MyLogger'],
-      ['appsec.enabled', false],
-      ['appsec.sca_enabled', false]
-    ].freeze}
 
     it do
       is_expected.to match(
