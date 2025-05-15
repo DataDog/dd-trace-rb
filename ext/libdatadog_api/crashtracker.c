e27786fe65b76046238c6912c513c9cfea2d5c26
Ivo Anjo <ivo.anjo@datadoghq.com>
2025-05-15 09:17:28 +0100

    Clean up `_native_stop` for crashtracker

--- ext/libdatadog_api/crashtracker.c
+++ ext/libdatadog_api/crashtracker.c
@@ -108,12 +108,6 @@ static VALUE _native_start_or_update_on_fork(int argc, VALUE *argv, DDTRACE_UNUS
 }
 
 static VALUE _native_stop(DDTRACE_UNUSED VALUE _self) {
-  // FIXME: What should happen here now? Maybe just remove
-  // ddog_VoidResult result = ddog_crasht_shutdown();
-
-  // if (result.tag == DDOG_VOID_RESULT_ERR) {
-  //   rb_raise(rb_eRuntimeError, "Failed to stop the crash tracker: %"PRIsVALUE, get_error_details_and_drop(&result.err));
-  // }
-
+  // TODO: Waiting for libdatadog to re-add way to stop crashtracking
   return Qtrue;
 }
