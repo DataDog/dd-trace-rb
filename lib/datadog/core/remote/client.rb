1b8e9d70818e05d50a1a4ef303e7c4ce62f35a59
Oleg Pudeyev <156273877+p-datadog@users.noreply.github.com>
2025-05-16 11:40:50 -0400

    DEBUG-3573 Send source code integration tags in telemetry and remote config (#4653)

--- lib/datadog/core/remote/client.rb
+++ lib/datadog/core/remote/client.rb
@@ -140,6 +140,13 @@ module Datadog
             "ruby.gem.libdatadog.platform:#{gem_spec('libdatadog').platform}",
           ]
 
+          if (git_repository_url = Core::Environment::Git.git_repository_url)
+            client_tracer_tags << "git.repository_url:#{git_repository_url}"
+          end
+          if (git_commit_sha = Core::Environment::Git.git_commit_sha)
+            client_tracer_tags << "git.commit.sha:#{git_commit_sha}"
+          end
+
           client_tracer = {
             runtime_id: Core::Environment::Identity.id,
             language: Core::Environment::Identity.lang,
