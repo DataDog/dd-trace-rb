a2ddee4c034e6d92bebf0015aaf5730b3445e573
Oleg Pudeyev <code@olegp.name>
2025-05-21 11:56:06 -0400

    style

--- lib/datadog/core/utils/truncation.rb
+++ lib/datadog/core/utils/truncation.rb
@@ -9,11 +9,11 @@ module Datadog
 
         def truncate_in_middle(string, max_prefix_length, max_suffix_length)
           max_length = max_prefix_length + 3 + max_suffix_length
-                  if string.length > max_length
-                                     "#{string[0...max_prefix_length]}...#{string[-max_suffix_length..-1]}"
-                                   else
-                                     string
-                                   end
+          if string.length > max_length
+            "#{string[0...max_prefix_length]}...#{string[-max_suffix_length..-1]}"
+          else
+            string
+          end
         end
       end
     end
