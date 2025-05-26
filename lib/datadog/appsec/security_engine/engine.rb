167704cd8d586d09d3a660db1d289fa905bbde85
Yury Lebedev <yury.lebedev@datadoghq.com>
2025-05-26 16:25:12 +0200

    Add type signatures to AppSec::SecurityEngine::Engine

--- lib/datadog/appsec/security_engine/engine.rb
+++ lib/datadog/appsec/security_engine/engine.rb
@@ -11,22 +11,17 @@ module Datadog
         attr_reader :diagnostics, :addresses
 
         def initialize(settings:, telemetry:)
-          @telemetry = telemetry
+          # TODO: set WAF logger to Datadog.logger - in the component
 
-          require_libddwaf
-          # TODO: set WAF logger to Datadog.logger - maybe in the component
+          require_libddwaf(telemetry: telemetry)
 
-          @waf_builder = create_waf_builder(settings)
-          @diagnostics = load_default_config
+          @waf_builder = create_waf_builder(settings: settings, telemetry: telemetry)
+          @diagnostics = load_default_config(settings: settings, telemetry: telemetry)
 
-          @waf_handle = create_waf_handle
+          @waf_handle = create_waf_handle(telemetry: telemetry)
           @addresses = @handle.known_addresses
         rescue
           Datadog.logger.warn('AppSec is disabled, see logged errors above')
-        end
-
-        def active_waf_context
-          # TODO: maybe context can be initialized on request
         end
 
         def finalize
@@ -40,7 +35,7 @@ module Datadog
 
         private
 
-        def require_libddwaf
+        def require_libddwaf(telemetry:)
           require('libddwaf')
         rescue LoadError => e
           libddwaf_platform = Gem.loaded_specs['libddwaf']&.platform || 'unknown'
@@ -50,12 +45,12 @@ module Datadog
             "ruby platforms: #{ruby_platforms}"
 
           Datadog.logger.error("#{error_message}, error #{e.inspect}")
-          @telemetry.report(e, description: error_message)
+          telemetry.report(e, description: error_message)
 
           raise e
         end
 
-        def create_waf_builder(settings)
+        def create_waf_builder(settings:, telemetry:)
           WAF::HandleBuilder.new(
             obfuscator: {
               key_regex: settings.obfuscator_key_regex,
@@ -66,14 +61,14 @@ module Datadog
           error_message = "libddwaf handle builder failed to initialize"
 
           Datadog.logger.error("#{error_message}, error #{e.inspect}")
-          @telemetry.report(e, description: error_message)
+          telemetry.report(e, description: error_message)
 
           raise e
         end
 
-        def load_default_config
+        def load_default_config(settings:, telemetry:)
           # this might return nil if an error occurs
-          rules = AppSec::Processor::RuleLoader.load_rules(telemetry: @telemetry, ruleset: settings.ruleset)
+          rules = AppSec::Processor::RuleLoader.load_rules(telemetry: telemetry, ruleset: settings.ruleset)
 
           # TODO: this has nothing to do with rule loading
           data = AppSec::Processor::RuleLoader.load_data(
@@ -91,7 +86,7 @@ module Datadog
             scanners: rules['scanners'],
             processors: rules['processors'],
             exclusions: exclusions,
-            telemetry: @telemetry
+            telemetry: telemetry
           )
 
           @waf_builder.add_or_update_config(config, path: DEFAULT_CONFIG_PATH)
@@ -99,18 +94,18 @@ module Datadog
           error_message = "libddwaf handle builder failed to load default config"
 
           Datadog.logger.error("#{error_message}, error #{e.inspect}")
-          @telemetry.report(e, description: error_message)
+          telemetry.report(e, description: error_message)
 
           raise e
         end
 
-        def create_waf_handle
+        def create_waf_handle(telemetry:)
           @waf_handle = @waf_builder.build_handle
         rescue WAF::LibDDWAFError => e
           error_message = "libddwaf handle failed to initialize"
 
           Datadog.logger.error("#{error_message}, error #{e.inspect}")
-          @telemetry.report(e, description: error_message)
+          telemetry.report(e, description: error_message)
 
           raise e
         end
---
+++ spec/datadog/appsec/security_engine/engine_spec.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require 'datadog/appsec/spec_helper'
+
+RSpec.describe Datadog::AppSec::SecurityEngine::Engine do
+end
