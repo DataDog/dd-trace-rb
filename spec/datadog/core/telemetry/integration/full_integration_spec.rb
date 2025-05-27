5b3e780ddb3129a9924dd37fcb71c0b7563c79ff
Oleg Pudeyev <code@olegp.name>
2025-05-27 00:14:51 -0400

    test dependency collection event

--- spec/datadog/core/telemetry/integration/full_integration_spec.rb
+++ spec/datadog/core/telemetry/integration/full_integration_spec.rb
@@ -20,7 +20,11 @@ RSpec.describe 'Telemetry full integration tests' do
 
     it 'sends app-started followed by app-client-configuration-change' do
       expect(Datadog::Core::Telemetry::Worker).to receive(:new).and_return(worker1)
-      expect(worker1).to receive(:start).with(an_instance_of(Datadog::Core::Telemetry::Event::AppStarted))
+      expect(worker1).to receive(:start) do |event|
+        # SynthAppClientConfigurationChange derives from AppStarted
+        # therefore assertion must be on class matching exactly
+        expect(event.class).to eql(Datadog::Core::Telemetry::Event::AppStarted)
+      end
       allow(worker1).to receive(:enqueue)
       allow(worker1).to receive(:flush)
       allow(worker1).to receive(:stop)
@@ -32,7 +36,9 @@ RSpec.describe 'Telemetry full integration tests' do
       Datadog.send(:components).telemetry.flush
 
       expect(Datadog::Core::Telemetry::Worker).to receive(:new).and_return(worker2)
-      expect(worker2).to receive(:start).with(an_instance_of(Datadog::Core::Telemetry::Event::SynthAppClientConfigurationChange))
+      expect(worker2).to receive(:start) do |event|
+        expect(event.class).to eql(Datadog::Core::Telemetry::Event::SynthAppClientConfigurationChange)
+      end
       allow(worker2).to receive(:enqueue)
       allow(worker2).to receive(:flush)
       allow(worker2).to receive(:stop)
@@ -42,6 +48,65 @@ RSpec.describe 'Telemetry full integration tests' do
       end
 
       Datadog.send(:components).telemetry.flush
+    end
+
+    after do
+      Datadog.send(:reset!)
+    end
+  end
+
+  context 'when Datadog.configure is used and dependency collection is enabled' do
+
+    before do
+      Datadog.send(:reset!)
+    end
+
+    let(:ok_response) do
+      double(Datadog::Core::Transport::HTTP::Adapters::Net::Response).tap do |response|
+        expect(response).to receive(:ok?).and_return(true).at_least(:once)
+      end
+    end
+
+    it 'sends dependencies once' do
+      events = []
+      allow_any_instance_of(Datadog::Core::Telemetry::Worker).to receive(:send_event) do |_, event|
+        events << event
+        ok_response
+      end
+
+      Datadog.configure do |c|
+        c.telemetry.enabled = true
+        c.telemetry.dependency_collection = true
+      end
+
+      Datadog.send(:components).telemetry.flush
+
+      expect(events.map(&:class)).to eq([
+        Datadog::Core::Telemetry::Event::AppStarted,
+        Datadog::Core::Telemetry::Event::AppDependenciesLoaded,
+        # AppIntegrationsChange in MessageBatch
+        Datadog::Core::Telemetry::Event::MessageBatch,
+      ])
+
+      events = []
+      allow_any_instance_of(Datadog::Core::Telemetry::Worker).to receive(:send_event) do |_, event|
+        events << event
+        ok_response
+      end
+
+      Datadog.configure do |c|
+        c.telemetry.enabled = true
+        c.telemetry.dependency_collection = true
+      end
+
+      Datadog.send(:components).telemetry.flush
+
+      expect(events.map(&:class)).to eq([
+        Datadog::Core::Telemetry::Event::SynthAppClientConfigurationChange,
+        # AppDependenciesLoaded is NOT sent here
+        # AppIntegrationsChange in MessageBatch
+        Datadog::Core::Telemetry::Event::MessageBatch,
+      ])
     end
 
     after do
