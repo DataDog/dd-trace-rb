c854afa4c46b19a7881f4243416841d3f653edb5
Sergey Fedorov <sergey.fedorov@datadoghq.com>
2025-06-03 14:32:53 +0200

    Add API Security interface for Rack middleware

---
+++ spec/datadog/appsec/api_security_spec.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require 'datadog/appsec/spec_helper'
+require 'datadog/appsec/api_security'
+
+RSpec.describe Datadog::AppSec::APISecurity do
+  describe '.sample?' do
+    let(:sampler) { instance_double(Datadog::AppSec::APISecurity::Sampler) }
+    let(:request) { double('Rack::Request') }
+    let(:response) { double('Rack::Response') }
+
+    before { allow(Datadog::AppSec::APISecurity::Sampler).to receive(:thread_local).and_return(sampler) }
+
+    it 'delegates to thread local sampler' do
+      expect(sampler).to receive(:sample?).with(request, response).and_return(true)
+      expect(described_class.sample?(request, response)).to be(true)
+    end
+  end
+
+  describe '.sample_trace?' do
+    let(:trace) { instance_double(Datadog::Tracing::TraceOperation) }
+
+    context 'when running in standalone mode' do
+      around do |example|
+        Datadog.configure { |c| c.apm.tracing.enabled = false }
+        example.run
+      ensure
+        Datadog.configuration.reset!
+      end
+
+      it { expect(described_class.sample_trace?(trace)).to be(true) }
+    end
+
+    context 'when running in normal mode' do
+      around do |example|
+        Datadog.configure do |c|
+          c.apm.tracing.enabled = true
+          c.tracing.sampler = sampler
+        end
+
+        example.run
+      ensure
+        Datadog.configuration.reset!
+      end
+
+      let(:sampler) { instance_double(Datadog::Tracing::Sampling::PrioritySampler) }
+
+      context 'when sampler allows trace' do
+        before { allow(sampler).to receive(:sample!).with(trace).and_return(true) }
+
+        it { expect(described_class.sample_trace?(trace)).to be(true) }
+      end
+
+      context 'when sampler rejects trace' do
+        before { allow(sampler).to receive(:sample!).with(trace).and_return(false) }
+
+        it { expect(described_class.sample_trace?(trace)).to be(false) }
+      end
+    end
+  end
+end
