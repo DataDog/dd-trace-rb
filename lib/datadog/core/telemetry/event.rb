53b76f103e7b60fc059419fc70520fe0c17ba4e8
Munir Abdinur <munir_abdinur@hotmail.com>
2025-06-20 11:10:59 -0400

    to bool

--- lib/datadog/core/telemetry/event.rb
+++ lib/datadog/core/telemetry/event.rb
@@ -149,11 +149,12 @@ module Datadog
                                     else
                                       "unknown"
                                     end
+            inject_force = Core::Environment::VariableHelpers.env_to_bool('DD_INJECT_FORCE', false)
             # Track ssi configurations
             list.push(
               conf_value('instrumentation_source', instrumentation_source, seq_id),
+              conf_value('DD_INJECT_FORCE', inject_force, seq_id),
               conf_value('DD_INJECTION_ENABLED', ENV['DD_INJECTION_ENABLED'], seq_id),
-              conf_value('DD_INJECT_FORCE', ENV['DD_INJECT_FORCE'] , seq_id),
             )
             # Add some more custom additional payload values here
             list.push(
