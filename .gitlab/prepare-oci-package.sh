8108803c394b58606249386d8fa4e39af4746543
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2025-06-29 15:42:08 +0200

    Test against new branch

--- .gitlab/prepare-oci-package.sh
+++ .gitlab/prepare-oci-package.sh
@@ -14,7 +14,7 @@ fi
 ## Obtain injector source
 
 injector_repo="https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.ddbuild.io/DataDog/datadog-injector-rb.git"
-injector_ref="lloeki/testing"
+injector_ref="lloeki/injector" # TODO: pin to an injector release
 injector_path="${HOME}/datadog-injector-rb"
 
 git clone "${injector_repo}" --branch "${injector_ref}" "${injector_path}"
