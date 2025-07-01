f8a02dd7dcae5319bf0e86df9e758f0d57bfd8a5
yuandesu <yuan.chang@datadoghq.com>
2025-07-01 17:02:57 +0900

    Fix sampling rules and sample rate reporting in environment logger

--- lib/datadog/tracing/diagnostics/environment_logger.rb
+++ lib/datadog/tracing/diagnostics/environment_logger.rb
@@ -84,7 +84,9 @@ module Datadog
           # @return [Numeric, nil] tracer sample rate configured
           def sample_rate
             sampler = Datadog.configuration.tracing.sampler
-            return nil unless sampler
+            unless sampler
+              return Datadog.configuration.tracing.sampling.default_rate rescue nil
+            end
 
             sampler.sample_rate(nil) rescue nil
           end
@@ -97,6 +99,11 @@ module Datadog
           # @return [Hash, nil] sample rules configured
           def sampling_rules
             sampler = Datadog.configuration.tracing.sampler
+
+            unless sampler
+              return Datadog.configuration.tracing.sampling.rules rescue nil
+            end
+
             return nil unless sampler.is_a?(Tracing::Sampling::PrioritySampler) &&
               sampler.priority_sampler.is_a?(Tracing::Sampling::RuleSampler)
 
@@ -109,6 +116,8 @@ module Datadog
                 sample_rate: rule.sampler.sample_rate(nil)
               }
             end.compact
+          rescue
+            nil
           end
 
           # Concatenated list of integrations activated, with their gem version.
