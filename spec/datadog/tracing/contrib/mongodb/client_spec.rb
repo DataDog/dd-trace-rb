1c9086f2a3bede8f2332270cb41c42f436b05334
Victor Pellan <victor.pellan@datadoghq.com>
2025-07-10 10:55:26 +0200

    Fix mongodb tests when running them locally

--- spec/datadog/tracing/contrib/mongodb/client_spec.rb
+++ spec/datadog/tracing/contrib/mongodb/client_spec.rb
@@ -90,7 +90,7 @@ RSpec.describe 'Mongo::Client instrumentation' do
       let(:secondary_client) { Mongo::Client.new(["#{secondary_host}:#{port}"], client_options) }
       # TODO: This tests doesn't work if TEST_MONGODB_HOST is set to anything other than 127.0.0.1
       #       We should fix this... maybe do a more clever IP resolve or something?
-      let(:secondary_host) { 'localhost' }
+      let(:secondary_host) { ENV.fetch('TEST_MONGODB_SECONDARY_HOST', '127.0.0.1') }
 
       before do
         Datadog.configure do |c|
