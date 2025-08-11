c4a1ea93f9dfaaa6570e03f0609384455e1a44d6
Sergey Fedorov <sergey.fedorov@datadoghq.com>
2025-08-11 11:16:10 +0200

    Update test style

--- spec/datadog/appsec/utils/hash_coercion_spec.rb
+++ spec/datadog/appsec/utils/hash_coercion_spec.rb
@@ -7,27 +7,36 @@ RSpec.describe Datadog::AppSec::Utils::HashCoercion do
     context 'when object responds to as_json' do
       let(:obj) do
         Class.new do
-          def as_json; {foo: 'bar'}; end
+          def as_json
+            {foo: 'bar'}
+          end
         end
       end
+
       it { expect(described_class.coerce(obj.new)).to eq({foo: 'bar'}) }
     end
 
     context 'when object responds to to_hash' do
       let(:obj) do
         Class.new do
-          def to_hash; {bar: 'baz'}; end
+          def to_hash
+            {bar: 'baz'}
+          end
         end
       end
+
       it { expect(described_class.coerce(obj.new)).to eq({bar: 'baz'}) }
     end
 
     context 'when object responds to to_h' do
       let(:obj) do
         Class.new do
-          def to_h; {baz: 'qux'}; end
+          def to_h
+            {baz: 'qux'}
+          end
         end
       end
+
       it { expect(described_class.coerce(obj.new)).to eq({baz: 'qux'}) }
     end
 
