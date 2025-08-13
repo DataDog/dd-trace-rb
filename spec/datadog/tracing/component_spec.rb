192b0fb4394bf147972f2790e487d9c21751e7a7
Oleg Pudeyev <code@olegp.name>
2025-08-13 14:29:46 -0400

    rubocop

--- spec/datadog/tracing/component_spec.rb
+++ spec/datadog/tracing/component_spec.rb
@@ -12,30 +12,6 @@ require 'datadog/tracing/tracer'
 require 'datadog/tracing/writer'
 
 RSpec.describe Datadog::Tracing::Component do
-  describe 'writer event callbacks' do
-    describe Datadog::Tracing::Component::WRITER_RECORD_ENVIRONMENT_INFORMATION_CALLBACK do
-      subject(:call) { described_class.call(writer, responses) }
-      let(:writer) { double('writer') }
-      let(:responses) { [double('response')] }
-
-      before do
-        Datadog::Tracing::Component::WRITER_RECORD_ENVIRONMENT_INFORMATION_ONLY_ONCE.send(:reset_ran_once_state_for_tests)
-      end
-
-      it 'invokes the environment logger with responses' do
-        expect(Datadog::Tracing::Diagnostics::EnvironmentLogger).to receive(:collect_and_log!).with(responses: responses)
-        call
-      end
-
-      it 'invokes the environment logger only once' do
-        expect(Datadog::Tracing::Diagnostics::EnvironmentLogger).to receive(:collect_and_log!).once
-
-        described_class.call(writer, responses)
-        described_class.call(writer, responses)
-      end
-    end
-  end
-
   describe '::build_tracer' do
     subject(:build_tracer) { described_class.build_tracer(settings, agent_settings, logger: logger) }
 
@@ -553,6 +529,28 @@ RSpec.describe Datadog::Tracing::Component do
   end
 
   describe 'writer event callbacks' do
+    describe Datadog::Tracing::Component::WRITER_RECORD_ENVIRONMENT_INFORMATION_CALLBACK do
+      subject(:call) { described_class.call(writer, responses) }
+      let(:writer) { double('writer') }
+      let(:responses) { [double('response')] }
+
+      before do
+        Datadog::Tracing::Component::WRITER_RECORD_ENVIRONMENT_INFORMATION_ONLY_ONCE.send(:reset_ran_once_state_for_tests)
+      end
+
+      it 'invokes the environment logger with responses' do
+        expect(Datadog::Tracing::Diagnostics::EnvironmentLogger).to receive(:collect_and_log!).with(responses: responses)
+        call
+      end
+
+      it 'invokes the environment logger only once' do
+        expect(Datadog::Tracing::Diagnostics::EnvironmentLogger).to receive(:collect_and_log!).once
+
+        described_class.call(writer, responses)
+        described_class.call(writer, responses)
+      end
+    end
+
     describe '.writer_update_priority_sampler_rates_callback' do
       subject(:call) do
         described_class.writer_update_priority_sampler_rates_callback(sampler).call(writer, responses)
