51f32fd4e14396a6192286298aae4d2e8b92664b
Rafael Gibim <9031589+Drowze@users.noreply.github.com>
2025-08-29 11:40:50 -0300

    Fix thread leak warnings on Karafka tests

--- spec/datadog/tracing/contrib/karafka/patcher_spec.rb
+++ spec/datadog/tracing/contrib/karafka/patcher_spec.rb
@@ -1,7 +1,10 @@
 require 'datadog/tracing/contrib/support/spec_helper'
 require 'datadog/tracing/contrib/analytics_examples'
 
-require 'karafka'
+# FFI::Function background native thread
+ThreadHelpers.with_leaky_thread_creation(:rdkafka) do
+  require 'karafka'
+end
 require 'datadog'
 
 RSpec.describe 'Karafka patcher' do
