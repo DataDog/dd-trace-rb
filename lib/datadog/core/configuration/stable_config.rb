8fc0bb996f80418c3a7d3a6d29f244c73b23ac92
Victor Pellan <victor.pellan@datadoghq.com>
2025-09-19 14:15:01 +0200

    Update Stable Config native extension to support libdatadog 21 and add support for debug logs

--- lib/datadog/core/configuration/stable_config.rb
+++ lib/datadog/core/configuration/stable_config.rb
@@ -10,7 +10,18 @@ module Datadog
             Datadog.config_init_logger.debug("Cannot enable stable config: #{libdatadog_api_failure}")
             return {}
           end
-          Configurator.new.get
+          config = Configurator.new.get
+          # Take into account stable config values for DD_TRACE_DEBUG
+          if config[:logs]
+            # Priority : Fleet > Environment > Local
+            debug_source_value =
+              config.dig(:fleet, :config, Ext::Diagnostics::ENV_DEBUG_ENABLED) ||
+              DATADOG_ENV[Ext::Diagnostics::ENV_DEBUG_ENABLED] ||
+              config.dig(:local, :config, Ext::Diagnostics::ENV_DEBUG_ENABLED) ||
+              'false'
+            Datadog.config_init_logger(debug_source_value).debug(config[:logs])
+          end
+          config
         end
 
         def self.configuration
