8a64c7d8dc5992c5c6308938a448d6568bc06d11
Victor Pellan <victor.pellan@datadoghq.com>
2025-09-19 14:15:01 +0200

    Update Stable Config native extension to support libdatadog 21 and add support for debug logs

--- spec/datadog/core/configuration/stable_config_spec.rb
+++ spec/datadog/core/configuration/stable_config_spec.rb
@@ -18,32 +18,36 @@ RSpec.describe Datadog::Core::Configuration::StableConfig do
   describe '#configuration', skip: !LibdatadogHelpers.supported? do
     let(:tmpdir) { Dir.mktmpdir }
     before do
+      Datadog::Core::Configuration::StableConfig.instance_variable_set(:@configuration, nil)
+
       File.write(
         File.join(tmpdir, 'local_config.yaml'),
         local_config_content
-      )
+      ) if defined?(local_config_content)
       File.write(
         File.join(tmpdir, 'fleet_config.yaml'),
         fleet_config_content
-      )
+      ) if defined?(fleet_config_content)
 
       test_configurator = Datadog::Core::Configuration::StableConfig::Configurator.new
       Datadog::Core::Configuration::StableConfig::Testing.with_local_path(
         test_configurator,
         File.join(tmpdir, 'local_config.yaml')
-      )
+      ) if defined?(local_config_content)
       Datadog::Core::Configuration::StableConfig::Testing.with_fleet_path(
         test_configurator,
         File.join(tmpdir, 'fleet_config.yaml')
-      )
+      ) if defined?(fleet_config_content)
 
-      allow(Datadog::Core::Configuration::StableConfig).to receive(:extract_configuration).and_return(
+      allow_any_instance_of(Datadog::Core::Configuration::StableConfig::Configurator).to receive(:get).and_return(
         test_configurator.get
       )
     end
 
     after do
       FileUtils.remove_entry_secure(tmpdir)
+
+      Datadog::Core::Configuration::StableConfig.instance_variable_set(:@configuration, nil)
     end
 
     context 'when libdatadog API is available' do
@@ -65,12 +69,13 @@ RSpec.describe Datadog::Core::Configuration::StableConfig do
         end
 
         it 'returns the configuration' do
-          expect(described_class.configuration).to eq(
+          expect(described_class.configuration).to include(
             {
               local: {id: "12345", config: {"DD_LOGS_INJECTION" => "false"}},
               fleet: {id: "56789", config: {"DD_APPSEC_ENABLED" => "true"}}
             }
           )
+          expect(described_class.configuration[:logs]).to be_a(String)
         end
       end
 
@@ -90,12 +95,84 @@ RSpec.describe Datadog::Core::Configuration::StableConfig do
         end
 
         it 'returns the configuration' do
-          expect(described_class.configuration).to eq(
+          expect(described_class.configuration).to include(
             {
               local: {config: {"DD_LOGS_INJECTION" => "false"}},
               fleet: {config: {"DD_APPSEC_ENABLED" => "true"}}
             }
           )
+          expect(described_class.configuration[:logs]).to be_a(String)
+        end
+      end
+
+      context 'with DD_TRACE_DEBUG set during configuration initialization' do
+        # Global configuration has already been set up before the test, so we need to simulate that it was not set up
+        # Same for temporary logger
+        before do
+          allow(Datadog).to receive(:configuration?).and_return(false)
+          Datadog.instance_variable_set(:@temp_config_logger, nil)
+        end
+
+        after do
+          Datadog.instance_variable_set(:@temp_config_logger, nil)
+        end
+
+        context 'to true in fleet config' do
+          let(:fleet_config_content) do
+            <<~YAML
+            apm_configuration_default:
+              DD_TRACE_DEBUG: true
+            YAML
+          end
+
+          it 'prints debug logs' do
+            expect { described_class.configuration }.to output(/Reading stable configuration from files/).to_stdout
+          end
+        end
+
+        context 'to true in env' do
+          around do |example|
+            ClimateControl.modify(Datadog::Core::Configuration::Ext::Diagnostics::ENV_DEBUG_ENABLED => 'true') do
+              example.run
+            end
+          end
+
+          it 'prints debug logs' do
+            expect { described_class.configuration }.to output(/Reading stable configuration from files/).to_stdout
+          end
+        end
+
+        context 'to true in local config' do
+          let(:local_config_content) do
+            <<~YAML
+            apm_configuration_default:
+              DD_TRACE_DEBUG: true
+            YAML
+          end
+
+          it 'prints debug logs' do
+            expect { described_class.configuration }.to output(/Reading stable configuration from files/).to_stdout
+          end
+        end
+
+        context 'to true in local config and false in fleet config' do
+          let(:local_config_content) do
+            <<~YAML
+            apm_configuration_default:
+              DD_TRACE_DEBUG: true
+            YAML
+          end
+
+          let(:fleet_config_content) do
+            <<~YAML
+            apm_configuration_default:
+              DD_TRACE_DEBUG: false
+            YAML
+          end
+        end
+
+        it 'respects priority and does not print debug logs' do
+          expect { described_class.configuration }.not_to output(/Reading stable configuration from files/).to_stdout
         end
       end
     end
