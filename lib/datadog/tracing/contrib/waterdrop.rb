7b216e9922bcc4581ffa68700d1e517c91abd042
Eric Firth <eric.firth@datadoghq.com>
2025-09-23 11:42:11 -0400

    Add missing WaterDrop integration entry point
    - Add waterdrop.rb file in contrib directory
    - This is the main entry point that gets required when loading the integration
    - Fixes 'cannot load such file -- datadog/tracing/contrib/waterdrop' error

---
+++ lib/datadog/tracing/contrib/waterdrop.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative 'component'
+require_relative 'waterdrop/integration'
+
+module Datadog
+  module Tracing
+    module Contrib
+      # `WaterDrop` integration public API
+      module WaterDrop
+        # Public API methods can be added here if needed
+      end
+    end
+  end
+end
