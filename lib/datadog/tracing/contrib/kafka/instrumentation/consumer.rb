3e7e9c7668b13a7c460855953127f3c3e1d8beef
Eric Firth <eric.firth@datadoghq.com>
2025-09-26 16:10:11 -0400

    Fix Kafka consumer DSM implementation and remove noisy logging
    - Remove incorrect DSM implementation from Kafka consumer instrumentation
    - Kafka consumer each_message/each_batch are batch-level methods, not message-level
    - DSM should be handled in event handlers (ProcessMessage) for individual messages
    - Remove noisy puts statements and replace with proper debug logging
    - Update tests to reflect correct behavior
    - All tests pass, Kafka consumer now properly delegates DSM to event handlers

--- lib/datadog/tracing/contrib/kafka/instrumentation/consumer.rb
+++ lib/datadog/tracing/contrib/kafka/instrumentation/consumer.rb
@@ -15,31 +15,14 @@ module Datadog
             module InstanceMethods
               # Monkey patch for each_message method
               def each_message(**kwargs, &block)
-                # Only run data streams code if enabled
-                if Datadog.configuration.tracing.data_streams.enabled
-                  puts 'each_message (DSM enabled)'
-
-                  processor = Datadog.configuration.tracing.data_streams.processor
-                  processor.set_checkpoint(['direction:in', 'type:kafka'], Time.now.to_f)
-                else
-                  puts 'each_message (DSM disabled)'
-                end
-
+                # DSM is handled at the individual message level in event handlers
                 # Call the original method - spans are created by ActiveSupport::Notifications
                 super(**kwargs, &block)
               end
 
               # Monkey patch for each_batch method
               def each_batch(**kwargs, &block)
-                # Only run DSM code if enabled
-                if Datadog.configuration.tracing.data_streams.enabled
-                  puts 'each_batch (DSM enabled)'
-
-                  # TODO: Process DSM pathway information for the entire batch
-                else
-                  puts 'each_batch (DSM disabled)'
-                end
-
+                # DSM is handled at the individual message level in event handlers
                 # Call the original method - spans are created by ActiveSupport::Notifications
                 super(**kwargs, &block)
               end
