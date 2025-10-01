6e52a67fba1fafd268621428e0943eacd809dd79
Oleg Pudeyev <code@olegp.name>
2025-10-01 10:45:38 -0400

    use be_within

--- spec/datadog/di/integration/instrumentation_spec.rb
+++ spec/datadog/di/integration/instrumentation_spec.rb
@@ -526,8 +526,7 @@ RSpec.describe 'Instrumentation integration' do
               value = Float($1)
               # Actual execution time will change but it should be under
               # a second and it should be positive.
-              expect(value).to be > 0
-              expect(value).to be < 1
+              expect(value).to be_within(0.001, 0.9)
             end
             expect(InstrumentationSpecTestClass.new.test_method).to eq(42)
             component.probe_notifier_worker.flush
