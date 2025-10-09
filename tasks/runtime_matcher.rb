149b850a9538dbeb75c110b225eb86012ab18dd0
Sarah Chen <sarah.chen@datadoghq.com>
2025-10-09 15:55:18 -0500

    Skip mongo tests

--- tasks/runtime_matcher.rb
+++ tasks/runtime_matcher.rb
@@ -4,6 +4,8 @@ module RuntimeMatcher
     ruby_version = RUBY_VERSION[0..2]
 
     if RUBY_PLATFORM == 'java'
+      return false if ruby_version == '3.4' && rubies.include?('❌ jruby 10.0')
+
       rubies.include?("✅ #{ruby_version}") && rubies.include?('✅ jruby')
     else
       rubies.include?("✅ #{ruby_version}")
