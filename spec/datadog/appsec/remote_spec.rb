daeef94b61229d900959633fc0227a0a663d2681
Sergey Fedorov <sergey.fedorov@datadoghq.com>
2025-10-09 15:30:45 +0200

    Add RBS and fix tests

--- spec/datadog/appsec/remote_spec.rb
+++ spec/datadog/appsec/remote_spec.rb
@@ -20,7 +20,9 @@ RSpec.describe Datadog::AppSec::Remote do
       end
 
       it 'returns capabilities' do
-        expect(described_class.capabilities).to eq([4, 128, 16, 32, 64, 8, 256, 512, 1024, 8_388_608, 2_097_152])
+        expect(described_class.capabilities).to eq(
+          [4, 128, 16, 32, 64, 8, 256, 512, 1024, 8_388_608, 2_097_152, 8_796_093_022_208]
+        )
       end
     end
   end
