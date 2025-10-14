ddcf31b9555afaa204f2702be4ab94ca116b32f2
Eric Firth <eric.firth@datadoghq.com>
2025-10-14 08:46:32 -0400

    Fix standard, typecheck and frozen string literal CI issues

--- lib/datadog/core/configuration/supported_configurations.rb
+++ lib/datadog/core/configuration/supported_configurations.rb
@@ -33,6 +33,7 @@ module Datadog
          "DD_APPSEC_WAF_DEBUG" => {version: ["A"]},
          "DD_APPSEC_WAF_TIMEOUT" => {version: ["A"]},
          "DD_CRASHTRACKING_ENABLED" => {version: ["A"]},
+         "DD_DATA_STREAMS_ENABLED" => {version: ["A"]},
          "DD_DBM_PROPAGATION_MODE" => {version: ["A"]},
          "DD_DISABLE_DATADOG_RAILS" => {version: ["A"]},
          "DD_DYNAMIC_INSTRUMENTATION_ENABLED" => {version: ["A"]},
