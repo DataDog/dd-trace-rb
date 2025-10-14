02a59d0beb60d9b17bd054842fb3d745bb550991
Thomas Cannon <tcannon00@gmail.com>
2025-10-14 07:43:25 -0400

    WIP: Add `view_component` build matricies in `appraisal`
    * I was not able to get the `docker compose` setup running on my
    	machine due to the inability to pull the `ddapm-test-agent`
    	* Therefore, I was not able to spool up containers for each version
    		of Ruby to generate their `gemfiles/`
    * While I tested this locally with `ruby-3.3`, I did not commit the
    	changes since I was not able to follow the containerized setup

--- appraisal/ruby-3.5.rb
+++ appraisal/ruby-3.5.rb
@@ -94,6 +94,7 @@ build_coverage_matrix('mongo', min: '2.20.0')
 build_coverage_matrix('dalli', [2])
 build_coverage_matrix('karafka', min: '2.3.0')
 build_coverage_matrix('devise', min: '3.2.1')
+build_coverage_matrix('view_component', (3..4), min: '2.34.0')
 
 appraise 'relational_db' do
   # ActiveRecord locked because tests are failing with 7.1, which was attempted as a part of Ruby 3.4 testing in CI.
