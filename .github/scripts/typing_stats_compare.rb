5ccd161656d77611d347513b5ade069d74a52d48
Victor Pellan <victor.pellan@datadoghq.com>
2025-10-15 10:39:16 +0200

    Add ignored files part

--- .github/scripts/typing_stats_compare.rb
+++ .github/scripts/typing_stats_compare.rb
@@ -1,58 +1,49 @@
 #!/usr/bin/env ruby
 
-require 'json'
+# frozen_string_literal: true
 
-current_stats = JSON.parse(File.read(ENV['CURRENT_STATS_PATH']), symbolize_names: true)
-base_stats = JSON.parse(File.read(ENV['BASE_STATS_PATH']), symbolize_names: true)
+require "json"
+
+head_stats = JSON.parse(File.read(ENV["CURRENT_STATS_PATH"]), symbolize_names: true)
+base_stats = JSON.parse(File.read(ENV["BASE_STATS_PATH"]), symbolize_names: true)
 
 # If a file is added in contrib, currently the paths will have no diff.
-ignored_files_paths_added = current_stats[:ignored_files][:paths] - base_stats[:ignored_files][:paths]
-ignored_files_paths_removed = base_stats[:ignored_files][:paths] - current_stats[:ignored_files][:paths]
-
-steep_ignores_added = current_stats[:steep_ignore_comments] - base_stats[:steep_ignore_comments]
-steep_ignores_removed = base_stats[:steep_ignore_comments] - current_stats[:steep_ignore_comments]
-
-untyped_methods_added = current_stats[:untyped_methods] - base_stats[:untyped_methods]
-untyped_methods_removed = base_stats[:untyped_methods] - current_stats[:untyped_methods]
-
-partially_typed_methods_added = current_stats[:partially_typed_methods] - base_stats[:partially_typed_methods]
-partially_typed_methods_removed = base_stats[:partially_typed_methods] - current_stats[:partially_typed_methods]
-
-untyped_others_added = current_stats[:untyped_others] - base_stats[:untyped_others]
-untyped_others_removed = base_stats[:untyped_others] - current_stats[:untyped_others]
-
-partially_typed_others_added = current_stats[:partially_typed_others] - base_stats[:partially_typed_others]
-partially_typed_others_removed = base_stats[:partially_typed_others] - current_stats[:partially_typed_others]
-
-diff_stats = {
-  ignored_files: {
-    added: ignored_files_paths_added,
-    removed: ignored_files_paths_removed
-  },
-  steep_ignores: {
-    added: steep_ignores_added,
-    removed: steep_ignores_removed
-  },
-  methods: {
-    untyped: {
-      added: untyped_methods_added,
-      removed: untyped_methods_removed
-    },
-    partially_typed: {
-      added: partially_typed_methods_added,
-      removed: partially_typed_methods_removed
-    }
-  },
-  others: {
-    untyped: {
-      added: untyped_others_added,
-      removed: untyped_others_removed
-    },
-    partially_typed: {
-      added: partially_typed_others_added,
-      removed: partially_typed_others_removed
-    }
-  }
+ignored_files = {
+  added: head_stats[:ignored_files][:paths] - base_stats[:ignored_files][:paths],
+  removed: base_stats[:ignored_files][:paths] - head_stats[:ignored_files][:paths]
 }
 
-puts diff_stats.to_json
+def ignored_files_summary(head_stats, base_stats)
+  # This will skip the summary if files are added/removed from contrib folders for now.
+  ignored_files_added = head_stats[:ignored_files][:paths] - base_stats[:ignored_files][:paths]
+  ignored_files_removed = base_stats[:ignored_files][:paths] - head_stats[:ignored_files][:paths]
+  return nil if ignored_files_added.empty? && ignored_files_removed.empty?
+
+  typed_files_percentage_base = ((base_stats[:total_files_size] - base_stats[:ignored_files][:size]) / base_stats[:total_files_size].to_f * 100).round(2)
+  typed_files_percentage_head = ((head_stats[:total_files_size] - head_stats[:ignored_files][:size]) / head_stats[:total_files_size].to_f * 100).round(2)
+
+  summary = +"This PR "
+  summary << "adds **#{ignored_files_added.size}** ignored files " if ignored_files_added.any?
+  summary << "and " if ignored_files_added.any? && ignored_files_removed.any?
+  summary << "removes **#{ignored_files_removed.size}** ignored files " if ignored_files_removed.any?
+  if typed_files_percentage_base != typed_files_percentage_head
+    summary << "which #{(typed_files_percentage_base > typed_files_percentage_head) ? "decreases" : "increases"} the percentage of typed files from #{typed_files_percentage_base}% to #{typed_files_percentage_head}% (**#{(typed_files_percentage_head - typed_files_percentage_base).round(2)}**%)"
+  end
+  summary << "."
+
+  <<~IGNORED_FILES
+    ### Ignored files
+    #{summary}
+    <details><summary>Ignored files</summary>
+      #{"<em>Added:</em>" if ignored_files_added.any?}
+      #{"<pre><code>#{ignored_files_added.join("\n")}</code></pre>" if ignored_files_added.any?}
+      #{"<em>Removed:</em>" if ignored_files_removed.any?}
+      #{"<pre><code>#{ignored_files_removed.join("\n")}</code></pre>" if ignored_files_removed.any?}
+    </details>
+
+  IGNORED_FILES
+end
+
+result = +""
+result << ignored_files_summary(head_stats, base_stats)
+puts result
