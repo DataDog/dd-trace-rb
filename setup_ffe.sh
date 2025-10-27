9089e358636637f68307920e39f56dd337c6c91e
Sameeran Kunche <sameeran.kunche@datadoghq.com>
2025-10-26 23:09:28 -0700

    Update setup script to change feature flag ID from 'test-env' to '1'

--- setup_ffe.sh
+++ setup_ffe.sh
@@ -182,7 +182,7 @@ require './lib/datadog/core/feature_flags'
 config_json = '{
   \"data\": {
     \"type\": \"universal-flag-configuration\",
-    \"id\": \"test-env\",
+    \"id\": \"1\",
     \"attributes\": {
       \"createdAt\": \"2024-04-17T19:40:53.716Z\",
       \"format\": \"SERVER\",
