7585c4d73e3de0fc7897739e9d63d154a2de4557
Sergey Fedorov <sergey.fedorov@datadoghq.com>
2025-10-29 11:23:29 +0100

    Adjust evaluator specs

--- spec/datadog/open_feature/evaluator_spec.rb
+++ spec/datadog/open_feature/evaluator_spec.rb
@@ -6,6 +6,7 @@ require 'datadog/open_feature/evaluator'
 RSpec.describe Datadog::OpenFeature::Evaluator do
   let(:telemetry) { instance_double(Datadog::Core::Telemetry::Component) }
   let(:evaluator) { described_class.new(telemetry) }
+  let(:logger) { instance_double(Datadog::Core::Logger) }
   let(:ufc) do
     <<~JSON
       {
@@ -18,11 +19,11 @@ RSpec.describe Datadog::OpenFeature::Evaluator do
             "environment": { "name": "test" },
             "flags": {
               "test_flag": {
-                "key": "test_flag",
+                "key": "test",
                 "enabled": true,
                 "variationType": "STRING",
                 "variations": {
-                  "control": { "key": "control", "value": "control_value" }
+                  "control": { "key": "control", "value": "hello" }
                 },
                 "allocations": [
                   {
@@ -39,10 +40,10 @@ RSpec.describe Datadog::OpenFeature::Evaluator do
     JSON
   end
 
-  before { allow(telemetry).to receive(:report) }
+  before { allow(Datadog).to receive(:logger).and_return(logger) }
 
   describe '#fetch_value' do
-    let(:result) { evaluator.fetch_value(flag_key: 'test', expected_type: :boolean) }
+    let(:result) { evaluator.fetch_value(flag_key: 'test', expected_type: :string) }
 
     context 'when binding evaluator is not ready' do
       it 'returns evaluation error' do
@@ -75,6 +76,7 @@ RSpec.describe Datadog::OpenFeature::Evaluator do
         evaluator.ufc_json = ufc
         evaluator.reconfigure!
 
+        allow(telemetry).to receive(:report)
         allow_any_instance_of(described_class::Binding::Evaluator).to receive(:get_assignment)
           .and_raise(error)
       end
@@ -103,15 +105,93 @@ RSpec.describe Datadog::OpenFeature::Evaluator do
       end
     end
 
-    xcontext 'when binding evaluator returns resolution details' do
+    context 'when binding evaluator returns resolution details' do
+      before do
+        evaluator.ufc_json = ufc
+        evaluator.reconfigure!
+      end
+
+      let(:result) { evaluator.fetch_value(flag_key: 'test', expected_type: :string) }
+
+      it { expect(result.value).to eq('hello') }
     end
   end
 
-  xdescribe '#reconfigure' do
+  describe '#reconfigure!' do
     context 'when binding initialization fails with exception' do
+      before do
+        evaluator.ufc_json = ufc
+        evaluator.reconfigure!
+
+        allow(described_class::Binding::Evaluator).to receive(:new).and_raise(error)
+      end
+
+      let(:error) { StandardError.new('Ooops') }
+
+      it 'reports error to telemetry and logs it' do
+        expect(logger).to receive(:error).with(/Ooops/)
+        expect(telemetry).to receive(:report)
+          .with(error, description: match(/OpenFeature failed to reconfigure/))
+
+        evaluator.ufc_json = '{}'
+        expect { evaluator.reconfigure! }.not_to raise_error
+      end
+
+      it 'persists previouly configured evaluator' do
+        allow(logger).to receive(:error)
+        allow(telemetry).to receive(:report)
+
+        evaluator.ufc_json = '{}'
+        expect { evaluator.reconfigure! }.not_to change {
+          evaluator.fetch_value(flag_key: 'test', expected_type: :string).value
+        }.from('hello')
+      end
     end
 
     context 'when binding initialization succeeds' do
+      before do
+        evaluator.ufc_json = ufc
+        evaluator.reconfigure!
+      end
+
+      let(:new_ufc) do
+        <<~JSON
+          {
+            "data": {
+              "type": "universal-flag-configuration",
+              "id": "1",
+              "attributes": {
+                "createdAt": "2024-04-17T19:40:53.716Z",
+                "format": "SERVER",
+                "environment": { "name": "test" },
+                "flags": {
+                  "test_flag": {
+                    "key": "test",
+                    "enabled": true,
+                    "variationType": "STRING",
+                    "variations": {
+                      "control": { "key": "control", "value": "goodbye" }
+                    },
+                    "allocations": [
+                      {
+                        "key": "rollout",
+                        "splits": [{ "variationKey": "control", "shards": [] }],
+                        "doLog": false
+                      }
+                    ]
+                  }
+                }
+              }
+            }
+          }
+        JSON
+      end
+
+      xit 'reconfigures binding evaluator with new flags configuration' do
+        expect { evaluator.ufc_json = new_ufc; evaluator.reconfigure!}
+          .to change { evaluator.fetch_value(flag_key: 'test', expected_type: :string).value }
+          .from('hello').to('goodbye')
+      end
     end
   end
 
