ef9e7a27d1c39abbc9832304a2868268de31cd4f
Sergey Fedorov <sergey.fedorov@datadoghq.com>
2025-11-03 14:40:18 +0100

    Adjust buffer implementation and correct specs

--- lib/datadog/open_feature/exposures/buffer.rb
+++ lib/datadog/open_feature/exposures/buffer.rb
@@ -2,28 +2,33 @@
 
 require_relative '../../core/buffer/thread_safe'
 
+# TODO: Replace as module to have 2 implementations CRuby and ThreadSafe one
 module Datadog
   module OpenFeature
     module Exposures
-      class Buffer < Datadog::Core::Buffer::ThreadSafe
-        DEFAULT_LIMIT = 1000
+      class Buffer < Core::Buffer::ThreadSafe
+        DEFAULT_LIMIT = 1_000
 
         def initialize(limit = DEFAULT_LIMIT)
-          super(limit)
           @dropped = 0
+
+          super
         end
 
         protected
 
         def drain!
           drained = super
+
           dropped = @dropped
           @dropped = 0
+
           [drained, dropped]
         end
 
         def replace!(item)
           @dropped += 1
+
           super
         end
       end
