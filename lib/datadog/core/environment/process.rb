47efb900dfbe5d3fac579f176450020a8c89653d
wan <26727996+wantsui@users.noreply.github.com>
2025-11-18 13:57:41 -0500

    Change methods to private. Also add comments with examples

--- lib/datadog/core/environment/process.rb
+++ lib/datadog/core/environment/process.rb
@@ -10,25 +10,45 @@ module Datadog
       module Process
         module_function
 
+        # This method returns a key/value part of serialized tags in the format of k1:v1,k2:v2,k3:v3
+        # @return [String] comma-separated normalized key:value pairs
+        def serialized
+          return @serialized if defined?(@serialized)
+          tags = []
+          tags << serialized_kv_helper(Environment::Ext::TAG_ENTRYPOINT_WORKDIR, entrypoint_workdir) if entrypoint_workdir
+          tags << serialized_kv_helper(Environment::Ext::TAG_ENTRYPOINT_NAME, entrypoint_name) if entrypoint_name
+          tags << serialized_kv_helper(Environment::Ext::TAG_ENTRYPOINT_BASEDIR, entrypoint_basedir) if entrypoint_basedir
+          tags << serialized_kv_helper(Environment::Ext::TAG_ENTRYPOINT_TYPE, entrypoint_type) if entrypoint_type
+          @serialized = tags.join(',').freeze
+        end
+
+        private
+
         # Returns the last segment of the working directory of the process
+        # Example: /app/myapp -> myapp
         # @return [String] the last segment of the working directory
         def entrypoint_workdir
           File.basename(Dir.pwd)
         end
 
         # Returns the entrypoint type of the process
+        # In Ruby, the entrypoint type is always 'script'
         # @return [String] the type of the process, which is fixed in Ruby
         def entrypoint_type
           Environment::Ext::PROCESS_TYPE
         end
 
         # Returns the last segment of the base directory of the process
+        # Example 1: /bin/mybin -> mybin
+        # Example 2: ruby /test/myapp.rb -> myapp
         # @return [String] the last segment of base directory of the script
         def entrypoint_name
           File.basename($0)
         end
 
         # Returns the last segment of the base directory of the process
+        # Example 1: /bin/mybin -> bin
+        # Example 2: ruby /test/myapp.js -> test
         # @return [String] the last segment of the base directory of the script
         def entrypoint_basedir
           current_basedir = File.expand_path(File.dirname($0))
@@ -44,18 +64,6 @@ module Datadog
           key = Normalizer.normalize(key)
           value = Normalizer.normalize(value)
           "#{key}:#{value}"
-        end
-
-        # This method returns a key/value part of serialized tags in the format of k1:v1,k2:v2,k3:v3
-        # @return [String] comma-separated normalized key:value pairs
-        def serialized
-          return @serialized if defined?(@serialized)
-          tags = []
-          tags << serialized_kv_helper(Environment::Ext::TAG_ENTRYPOINT_WORKDIR, entrypoint_workdir) if entrypoint_workdir
-          tags << serialized_kv_helper(Environment::Ext::TAG_ENTRYPOINT_NAME, entrypoint_name) if entrypoint_name
-          tags << serialized_kv_helper(Environment::Ext::TAG_ENTRYPOINT_BASEDIR, entrypoint_basedir) if entrypoint_basedir
-          tags << serialized_kv_helper(Environment::Ext::TAG_ENTRYPOINT_TYPE, entrypoint_type) if entrypoint_type
-          @serialized = tags.join(',').freeze
         end
       end
     end
