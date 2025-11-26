4935cc343e5d482de0be28bc2e70a65bed1db492
Oleg Pudeyev <code@olegp.name>
2025-11-25 20:06:30 -0500

    add api private

--- lib/datadog/core/ddsketch.rb
+++ lib/datadog/core/ddsketch.rb
@@ -5,6 +5,8 @@ module Datadog
     # This class exists to wrap a ddsketch_Handle_DDSketch into a Ruby object.
     #
     # This class is not empty; all of this class is implemented as native code.
+    #
+    # @api private
     class DDSketch # rubocop:disable Lint/EmptyClass
     end
   end
