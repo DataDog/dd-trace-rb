e474defef3f701aaf7af419c549d43686a7c7de1
Oleg Pudeyev <code@olegp.name>
2025-11-25 20:04:16 -0500

    put in class stub as per profiling

---
+++ lib/datadog/core/ddsketch.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Datadog
+  module Core
+    # This class exists to wrap a ddsketch_Handle_DDSketch into a Ruby object.
+    #
+    # This class is not empty; all of this class is implemented as native code.
+    class DDSketch # rubocop:disable Lint/EmptyClass
+    end
+  end
+end
