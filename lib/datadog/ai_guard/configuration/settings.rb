25d67bc46e3f36236a72d3405a52239bfcd5831d
Yury Lebedev <yury.lebedev@datadoghq.com>
2025-12-01 16:40:08 +0100

    Add api_key setting for AI Guard component

--- lib/datadog/ai_guard/configuration/settings.rb
+++ lib/datadog/ai_guard/configuration/settings.rb
@@ -39,6 +39,15 @@ module Datadog
                 o.default "https://app.datadoghq.com/api/v2/ai-guard"
               end
 
+              # AI Guard API key.
+              #
+              # @default `DD_AI_GUARD_API_KEY`, otherwise nil
+              # @return [String, nil]
+              option :api_key do |o|
+                o.type :string, nilable: true
+                o.env Ext::ENV_AI_GUARD_API_KEY
+              end
+
               # Request timeout in milliseconds.
               #
               # @default `DD_AI_GUARD_TIMEOUT`, otherwise 10 000 ms
