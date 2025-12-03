5981b7c09684cddfda3f870f2b9e28cd2cf69c50
Marco Costa <marco.costa@datadoghq.com>
2025-12-03 14:18:04 -0800

    Add better init comments

--- ext/libdatadog_api/init.c
+++ ext/libdatadog_api/init.c
@@ -9,9 +9,12 @@ void ddsketch_init(VALUE core_module);
 
 void DDTRACE_EXPORT Init_libdatadog_api(void) {
   VALUE datadog_module = rb_define_module("Datadog");
+
+  // MUST be called before all other initialization
+  datadog_ruby_common_init(datadog_module);
+
   VALUE core_module = rb_define_module_under(datadog_module, "Core");
 
-  datadog_ruby_common_init(datadog_module);
   crashtracker_init(core_module);
   process_discovery_init(core_module);
   library_config_init(core_module);
