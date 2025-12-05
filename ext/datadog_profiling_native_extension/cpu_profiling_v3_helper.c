ad0a92bf71583d4db0ebb1c32b4c017863d29c57
Ivo Anjo <ivo.anjo@datadoghq.com>
2025-12-05 17:05:00 +0000

    Record state of timer in between activate/deactivate cycles

--- ext/datadog_profiling_native_extension/cpu_profiling_v3_helper.c
+++ ext/datadog_profiling_native_extension/cpu_profiling_v3_helper.c
@@ -1,5 +1,6 @@
 #include <ruby.h>
 #include <ruby/thread.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
 #include <time.h>
@@ -8,14 +9,18 @@
 #include "datadog_ruby_common.h"
 #include "cpu_profiling_v3_helper.h"
 #include "time_helpers.h"
-
 typedef struct {
   bool valid;
-  bool failed;
+  bool failed; // Once set, we'll never try to use the timer for this thread again
   timer_t timer;
-} per_thread_cpu_timer;
+  // Whenever we disarm the timer, we keep here "how far we were from the next sample". This makes sure that we correctly account for
+  // CPU time spent across a disarm/arm cycle (otherwise if we started always from the interval we'd be throwing CPU time away)
+  struct timespec last_suspend_leftover_time;
+} per_thread_cpu_timer_state;
 
-static __thread per_thread_cpu_timer current_thread_timer;
+static __thread per_thread_cpu_timer_state current_thread_timer;
+
+static const struct timespec CPU_TIMER_DEFAULT_INTERVAL = {.tv_nsec = MILLIS_AS_NS(10)};
 
 // TODO: According to the https://manpages.opensuse.org/Tumbleweed/man-pages/timer_create.2.en.html manpage, the first argument to
 // `timer_create` "can be specified as the clockid returned by a call to clock_getcpuclockid(3) or pthread_getcpuclockid(3)".
@@ -32,10 +37,16 @@ static void initialize_current_thread_cpu_timer(void) {
     .sigev_value.sival_int = 1234, // WIP
   };
 
-  int error = timer_create(CLOCK_PROCESS_CPUTIME_ID, &config, &current_thread_timer.timer);
+  timer_t new_timer;
+  int error = timer_create(CLOCK_PROCESS_CPUTIME_ID, &config, &new_timer);
 
   if (error == 0) {
-    current_thread_timer.valid = true;
+    // Fully reinitialize the state, to make sure there's no leftover counters
+    current_thread_timer = (per_thread_cpu_timer_state) {
+      .valid = true,
+      .timer = new_timer,
+      .last_suspend_leftover_time = CPU_TIMER_DEFAULT_INTERVAL,
+    };
   } else {
     current_thread_timer.failed = true;
     // TODO: Better logging
@@ -52,8 +63,8 @@ void cpu_profiling_v3_on_resume(void) {
 
   // Let's arm the timer
   struct itimerspec timer_config = {
-    .it_interval = {.tv_nsec = MILLIS_AS_NS(10)},
-    .it_value = {.tv_nsec = MILLIS_AS_NS(10)},
+    .it_interval = CPU_TIMER_DEFAULT_INTERVAL,
+    .it_value = current_thread_timer.last_suspend_leftover_time,
   };
   int error = timer_settime(current_thread_timer.timer, 0, &timer_config, NULL);
   if (error != 0) {
@@ -66,11 +77,25 @@ void cpu_profiling_v3_on_suspend(void) {
   if (!current_thread_timer.valid) return;
 
   // Let's disarm the timer
-  struct itimerspec disabled_timer = { 0 };
-  int error = timer_settime(current_thread_timer.timer, 0, &disabled_timer, NULL);
+  struct itimerspec disable_timer = { 0 };
+  struct itimerspec timer_state;
+
+  int error = timer_settime(current_thread_timer.timer, 0, &disable_timer, &timer_state);
   if (error != 0) {
     // TODO: Better logging
     fprintf(stderr, "Failure to disarm CPU timer on thread %d %s:%d:in `%s': %s\n",  gettid(), __FILE__, __LINE__, __func__, strerror(errno));
+  }
+
+  if (timer_state.it_value.tv_sec != 0 || timer_state.it_value.tv_nsec != 0) {
+    current_thread_timer.last_suspend_leftover_time = timer_state.it_value;
+  }
+
+  if (timer_state.it_interval.tv_sec == 0 && timer_state.it_interval.tv_nsec == 0) {
+    // TODO: I suspect this can actually happen sometimes -- I left a comment on gvl-tracing that indicated multiple
+    // suspends can show up for a Ruby thread in a row, and we're calling this code based also on SUSPEND.
+    // (TODO: We could make it an error based on some argument -- have the caller tell us if it's ok to double-suspend or not,
+    // since we know for which callers this would be expected to happen or not)
+    fprintf(stderr, "CPU timer on thread %d was disabled during suspension\n", gettid());
   }
 }
 
