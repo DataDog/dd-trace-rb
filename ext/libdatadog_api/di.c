2b0cd5b49b66a52aaad81b034216be27f68f5d68
Oleg Pudeyev <code@olegp.name>
2025-12-04 20:51:22 -0500

    indent

--- ext/libdatadog_api/di.c
+++ ext/libdatadog_api/di.c
@@ -8,23 +8,23 @@
 #endif
 
 struct ddtrace_di_os_each_struct {
-    VALUE array;
+  VALUE array;
 };
 
 static int ddtrace_di_os_obj_of_i(void *vstart, void *vend, size_t stride, void *data)
 {
-    struct ddtrace_di_os_each_struct *oes = (struct ddtrace_di_os_each_struct *)data;
-    VALUE array = oes->array;
+  struct ddtrace_di_os_each_struct *oes = (struct ddtrace_di_os_each_struct *)data;
+  VALUE array = oes->array;
 
-    VALUE v = (VALUE)vstart;
-    for (; v != (VALUE)vend; v += stride) {
-        if (ddtrace_imemo_iseq_p(v)) {
-            VALUE iseq = rb_iseqw_new((void *) v);
-            rb_ary_push(array, iseq);
-        }
+  VALUE v = (VALUE)vstart;
+  for (; v != (VALUE)vend; v += stride) {
+    if (ddtrace_imemo_iseq_p(v)) {
+      VALUE iseq = rb_iseqw_new((void *) v);
+      rb_ary_push(array, iseq);
     }
+  }
 
-    return 0;
+  return 0;
 }
 
 /* 
@@ -79,12 +79,12 @@ only has provision to how one code location as the instrumentation target.
 Thus, in this case, instrumentation should produce an error.
 */
 static VALUE all_iseqs(DDTRACE_UNUSED VALUE _self) {
-    struct ddtrace_di_os_each_struct oes;
+  struct ddtrace_di_os_each_struct oes;
 
-    oes.array = rb_ary_new();
-    rb_objspace_each_objects(ddtrace_di_os_obj_of_i, &oes);
-    RB_GC_GUARD(oes.array);
-    return oes.array;
+  oes.array = rb_ary_new();
+  rb_objspace_each_objects(ddtrace_di_os_obj_of_i, &oes);
+  RB_GC_GUARD(oes.array);
+  return oes.array;
 }
 
 void di_init(VALUE datadog_module) {
