43db64157ebf252f365bff710cd1b201f6234fb2
Sameeran Kunche <sameeran.kunche@datadoghq.com>
2025-12-05 13:09:56 -0800

    FFL-1482 refactor: clean up EvaluatorProxy comments and remove unnecessary default parameter

--- lib/datadog/open_feature/evaluator_proxy.rb
+++ lib/datadog/open_feature/evaluator_proxy.rb
@@ -5,16 +5,14 @@ module Datadog
     # A proxy that delegates to the current evaluator instance.
     # This prevents issues with cached references to old evaluators after reconfiguration.
     class EvaluatorProxy
-      def initialize(initial_evaluator = nil)
+      def initialize(initial_evaluator)
         @current_evaluator = initial_evaluator
       end
 
-      # Updates the evaluator that this proxy delegates to
       def update_evaluator!(new_evaluator)
         @current_evaluator = new_evaluator
       end
 
-      # Delegates to the current evaluator instance
       def get_assignment(flag_key, default_value:, expected_type:, context:)
         @current_evaluator.get_assignment(
           flag_key,
