af1ed125356ac1b80400410bac2ce416107d2911
Munir <munir.abdinur@datadoghq.com>
2025-12-22 08:47:36 -0500

    clean up config docs

--- lib/datadog/opentelemetry/configuration/settings.rb
+++ lib/datadog/opentelemetry/configuration/settings.rb
@@ -140,7 +140,7 @@ module Datadog
                 option :timeout_millis do |o|
                   o.type :int, nilable: true
                   o.env 'OTEL_EXPORTER_OTLP_METRICS_TIMEOUT'
-                  o.default 10000
+                  o.default nil
                 end
 
                 option :protocol do |o|
