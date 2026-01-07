f0c9cc0f7b4f2645d9d20ef65798107cd5dcb1e5
Marco Costa <marco.costa@datadoghq.com>
2026-01-07 14:27:17 -0800

    Apply suggestions from code review

--- lib/datadog/core/utils/fnv.rb
+++ lib/datadog/core/utils/fnv.rb
@@ -5,6 +5,8 @@ module Datadog
     module Utils
       module FNV
         # FNV-1a 64-bit hash function.
+        # @see https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function#FNV-1a_hash Algorithm
+        # @see https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function#FNV_hash_parameters Prime and Offset Basis
         def self.fnv1_64(data)
           fnv_offset_basis = 14695981039346656037
           fnv_prime = 1099511628211
