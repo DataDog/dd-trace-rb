9071b27bda086e7e74372b3c7143eeecc75c7140
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2026-01-19 17:51:38 +0100

    Update injector to v1.2.1

--- .gitlab/prepare-oci-package.sh
+++ .gitlab/prepare-oci-package.sh
@@ -14,7 +14,7 @@ fi
 ## Obtain injector source
 
 injector_repo="https://github.com/DataDog/datadog-injector-rb.git"
-injector_ref="lloeki/platform-mismatch"
+injector_ref="v1.2.1"
 injector_path="${HOME}/datadog-injector-rb"
 
 git clone "${injector_repo}" --branch "${injector_ref}" "${injector_path}"
