acd32535abe8fc49f94c68b3b3b192be6276d2d2
Yury Lebedev <yury.lebedev@datadoghq.com>
2026-01-19 17:14:37 +0100

    Separate tool call evaluation for Ruby LLM integration

--- lib/datadog/ai_guard/contrib/ruby_llm/chat_instrumentation.rb
+++ lib/datadog/ai_guard/contrib/ruby_llm/chat_instrumentation.rb
@@ -6,24 +6,38 @@ module Datadog
       module RubyLLM
         # module that gets prepended to RubyLLM::Chat
         module ChatInstrumentation
-          def complete(&block)
-            # FIXME: this gets called twice when there's a tool call
-            result = super
-
-            ai_guard_messages = messages.flat_map do |message|
-              if message.tool_call?
-                message.tool_calls.map do |tool_call_id, tool_call|
-                  AIGuard.assistant(id: tool_call_id, tool_name: tool_call.name, arguments: tool_call.arguments.to_s)
+          class << self
+            def evaluate(messages)
+              ai_guard_messages = messages.flat_map do |message|
+                if message.tool_call?
+                  message.tool_calls.map do |tool_call_id, tool_call|
+                    AIGuard.assistant(id: tool_call_id, tool_name: tool_call.name, arguments: tool_call.arguments.to_s)
+                  end
+                elsif message.tool_result?
+                  AIGuard.tool(tool_call_id: message.tool_call_id, content: message.content)
+                else
+                  AIGuard.message(role: message.role, content: message.content)
                 end
-              elsif message.tool_result?
-                AIGuard.tool(tool_call_id: message.tool_call_id, content: message.content)
-              else
-                AIGuard.message(role: message.role, content: message.content)
               end
-            end
-            evaluation_result = AIGuard.evaluate(*ai_guard_messages, allow_raise: false)
 
-            result
+              puts '=' * 80
+              puts ai_guard_messages.map(&:inspect)
+              puts '=' * 80
+
+              evaluation_result = AIGuard.evaluate(*ai_guard_messages, allow_raise: true)
+            end
+          end
+
+          def complete(&block)
+            evaluation_result = Datadog::AIGuard::Contrib::RubyLLM::ChatInstrumentation.evaluate(messages)
+
+            super
+          end
+
+          def handle_tool_calls(response, &block)
+            evaluation_result = Datadog::AIGuard::Contrib::RubyLLM::ChatInstrumentation.evaluate(messages)
+
+            super
           end
         end
       end
