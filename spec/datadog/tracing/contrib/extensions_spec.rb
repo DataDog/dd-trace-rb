69d1bdbff42893f65c4c93b4c8e477835a80fb7a
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2026-01-20 11:11:09 +0100

    [wip] try to fix test names

--- spec/datadog/tracing/contrib/extensions_spec.rb
+++ spec/datadog/tracing/contrib/extensions_spec.rb
@@ -8,10 +8,13 @@ RSpec.describe Datadog::Tracing::Contrib::Extensions do
     let(:integration_name) { :example }
     let(:integration) { integration_class.new(integration_name) }
     let(:integration_class) do
-      Class.new do
-        include Datadog::Tracing::Contrib::Integration
-        include Datadog::Tracing::Contrib::Configurable
-      end
+      stub_const(
+        'TestIntegrationClass',
+        Class.new do
+          include Datadog::Tracing::Contrib::Integration
+          include Datadog::Tracing::Contrib::Configurable
+        end
+      )
     end
 
     let(:configurable_module) do
