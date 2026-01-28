c4ca6aada439e6d427b0b4a0d1fcbbc807ae06cb
Yury Lebedev <yury.lebedev@datadoghq.com>
2026-01-28 11:48:21 +0100

    Improve patcher spec for AppSec rails contrib

--- spec/datadog/appsec/contrib/rails/patcher_spec.rb
+++ spec/datadog/appsec/contrib/rails/patcher_spec.rb
@@ -1,34 +1,41 @@
 # frozen_string_literal: true
 
-require 'active_support'
+require 'datadog/tracing/contrib/rails/rails_helper'
 require 'datadog/appsec'
 
 RSpec.describe Datadog::AppSec::Contrib::Rails::Patcher do
   describe '.patch' do
+    let(:telemetry) { instance_double(Datadog::Core::Telemetry::Component) }
+    let(:logger) { instance_double(Datadog::Core::Logger) }
+
     before do
-      allow(Datadog.logger).to receive(:error)
-      allow(Datadog::AppSec).to receive(:telemetry).and_return(double(report: nil))
+      allow(Datadog).to receive(:logger).and_return(logger)
+      allow(Datadog::AppSec).to receive(:telemetry).and_return(telemetry)
+
+      allow(logger).to receive(:error)
+      allow(logger).to receive(:debug)
+      allow(telemetry).to receive(:report)
 
       described_class.patch
     end
 
-    describe 'error handling in :after_routes_loaded load hook' do
-      context 'when unexpected argument causes error' do
-        it 'catches the error and logs it' do
-          ActiveSupport.run_load_hooks(:after_routes_loaded, Class.new)
+    context ':after_routes_loaded hook' do
+      context 'when error occurs while getting application routes' do
+        before do
+          allow(::Rails).to receive(:application).and_raise(StandardError)
+        end
+
+        it 'logs the error and reports it via telemetry' do
+          ActiveSupport.run_load_hooks(:after_routes_loaded)
 
           expect(Datadog.logger).to have_received(:error).with(
             /Failed to get application routes/
-          ).at_least(:once)
-        end
-
-        it 'reports the error to telemetry' do
-          ActiveSupport.run_load_hooks(:after_routes_loaded, Class.new)
+          ).once
 
           expect(Datadog::AppSec.telemetry).to have_received(:report).with(
-            an_instance_of(NoMethodError),
+            an_instance_of(StandardError),
             description: 'Failed to get application routes'
-          ).at_least(:once)
+          ).once
         end
       end
     end
