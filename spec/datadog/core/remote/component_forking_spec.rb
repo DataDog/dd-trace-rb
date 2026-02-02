dae1d0be6bf4141c788d36bbca4244dc4f5ac087
Unicorn Enterprises <une@narnia>
2026-02-02 17:05:39 -0500

    integration test with network requests

--- spec/datadog/core/remote/component_forking_spec.rb
+++ spec/datadog/core/remote/component_forking_spec.rb
@@ -2,6 +2,7 @@
 
 require 'spec_helper'
 require 'datadog/core/remote/component'
+require 'datadog/core/utils/base64'
 
 RSpec.describe Datadog::Core::Remote::Component do
   forking_platform_only
@@ -128,6 +129,167 @@ RSpec.describe Datadog::Core::Remote::Component do
           expect(child_client.settings).to equal(parent_settings)
           expect(child_component.logger).to equal(parent_logger)
         end
+      end
+    end
+  end
+
+  context 'network requests after fork', :integration do
+    let(:received_requests) { [] }
+    let(:request_mutex) { Mutex.new }
+
+    let(:info_handler) do
+      lambda do |req, res|
+        request_mutex.synchronize do
+          received_requests << { endpoint: '/info', method: req.request_method }
+        end
+        res.status = 200
+        res['Content-Type'] = 'application/json'
+        res.body = JSON.dump(
+          {
+            version: '1.0',
+            endpoints: ['/info', '/v0.7/config'],
+            config: {}
+          }
+        )
+      end
+    end
+
+    let(:config_handler) do
+      lambda do |req, res|
+        payload = JSON.parse(req.body) rescue {}
+        client_id = payload.dig('client', 'id')
+        runtime_id = payload.dig('client', 'client_tracer', 'runtime_id')
+
+        request_mutex.synchronize do
+          received_requests << {
+            endpoint: '/v0.7/config',
+            method: req.request_method,
+            client_id: client_id,
+            runtime_id: runtime_id,
+            pid: Process.pid
+          }
+        end
+
+        # Return an empty but valid response
+        jencode = proc do |obj|
+          Datadog::Core::Utils::Base64.strict_encode64(JSON.dump(obj)).chomp
+        end
+
+        res.status = 200
+        res['Content-Type'] = 'application/json'
+        res.body = JSON.dump(
+          {
+            roots: [jencode.call({})],
+            targets: jencode.call(
+              {
+                signed: {
+                  expires: '2099-12-31T23:59:59Z',
+                  targets: {}
+                }
+              }
+            ),
+            target_files: [],
+            client_configs: []
+          }
+        )
+      end
+    end
+
+    http_server do |http_server|
+      http_server.mount_proc('/info', &info_handler)
+      http_server.mount_proc('/v0.7/config', &config_handler)
+    end
+
+    let(:settings) do
+      Datadog::Core::Configuration::Settings.new.tap do |settings|
+        settings.remote.enabled = true
+        settings.remote.poll_interval_seconds = 0.1 # Short interval for testing
+        settings.remote.boot_timeout_seconds = 1
+        settings.agent.host = 'localhost'
+        settings.agent.port = http_server_port
+      end
+    end
+
+    before do
+      # after_fork handler goes through the global variable.
+      allow(Datadog).to receive(:components).and_return(components)
+    end
+
+    it 'sends requests with different client IDs from parent and child processes' do
+      # Start remote config and wait for first sync
+      component.barrier(:once)
+
+      # Wait for parent to make requests
+      try_wait_until(seconds: 2) { received_requests.any? { |r| r[:endpoint] == '/v0.7/config' } }
+
+      # Get parent requests
+      parent_requests = received_requests.select { |r| r[:endpoint] == '/v0.7/config' }
+      expect(parent_requests).not_to be_empty
+
+      parent_client_id = parent_requests.first[:client_id]
+      parent_runtime_id = parent_requests.first[:runtime_id]
+      expect(parent_client_id).to match(/^[0-9a-f-]+$/)
+      expect(parent_runtime_id).to match(/^[0-9a-f-]+$/)
+
+      # Record the parent PID
+      parent_pid = Process.pid
+
+      # Fork and verify child behavior
+      expect_in_fork do
+        child_pid = Process.pid
+        expect(child_pid).not_to eq(parent_pid)
+
+        # Get the remote component - after_fork should have run
+        child_component = components.remote
+        child_client_id = child_component.client.id
+        child_runtime_id = Datadog::Core::Environment::Identity.id
+
+        # Client ID should be different after fork
+        expect(child_client_id).not_to eq(parent_client_id)
+        expect(child_client_id).to match(/^[0-9a-f-]+$/)
+
+        # Runtime ID should also be different
+        expect(child_runtime_id).not_to eq(parent_runtime_id)
+        expect(child_runtime_id).to match(/^[0-9a-f-]+$/)
+
+        # Trigger a sync in the child to make a network request
+        child_component.barrier(:once)
+
+        # Give it a moment to complete the request
+        sleep 0.2
+
+        # Check if child made requests (server runs in parent, receives requests from child)
+        # Note: received_requests is modified by the parent process's HTTP server
+        # when it handles requests from the child
+        child_requests = received_requests.select do |r|
+          r[:endpoint] == '/v0.7/config' && r[:client_id] == child_client_id
+        end
+
+        # If we got requests, verify they have the right IDs
+        if child_requests.any?
+          expect(child_requests.first[:client_id]).to eq(child_client_id)
+          expect(child_requests.first[:runtime_id]).to eq(child_runtime_id)
+        end
+      end
+    end
+
+    it 'recreates client instance after fork with network verification' do
+      parent_client = component.client
+      parent_client_object_id = parent_client.object_id
+      parent_client_id = parent_client.id
+
+      expect_in_fork do
+        child_component = components.remote
+        child_client = child_component.client
+
+        # Should be a different object
+        expect(child_client.object_id).not_to eq(parent_client_object_id)
+
+        # Should have a different ID
+        expect(child_client.id).not_to eq(parent_client_id)
+
+        # Should have fresh state
+        expect(child_component.healthy).to be false
       end
     end
   end
