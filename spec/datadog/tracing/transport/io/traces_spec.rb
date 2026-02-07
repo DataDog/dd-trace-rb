7b8c7bcd4e1bfe957655b81cc1b90e2de57d5647
Unicorn Enterprises <une@narnia>
2026-02-07 01:03:06 -0500

    fix tests

--- spec/datadog/tracing/transport/io/traces_spec.rb
+++ spec/datadog/tracing/transport/io/traces_spec.rb
@@ -40,17 +40,13 @@ RSpec.describe Datadog::Tracing::Transport::IO::Client do
     context 'given traces' do
       subject(:send_traces) { client.send_traces(traces) }
 
-      let(:traces) { instance_double(Array) }
-      let(:encoded_traces) { double('encoded traces') }
+      let(:traces) { get_test_traces(2) }
       let(:result) { double('IO result') }
 
       before do
-        expect_any_instance_of(Datadog::Core::Transport::Parcel).to receive(:encode_with)
-          .with(encoder)
-          .and_return(encoded_traces)
-
+        # Mock only the IO operation - let encoding happen naturally
         expect(client.out).to receive(:puts)
-          .with(encoded_traces)
+          .with(kind_of(String))
           .and_return(result)
 
         expect(client).to receive(:update_stats_from_response!)
@@ -66,18 +62,14 @@ RSpec.describe Datadog::Tracing::Transport::IO::Client do
     context 'given traces and a block' do
       subject(:send_traces) { client.send_traces(traces) { |out, data| target.write(out, data) } }
 
-      let(:traces) { instance_double(Array) }
-      let(:encoded_traces) { double('encoded traces') }
+      let(:traces) { get_test_traces(2) }
       let(:result) { double('IO result') }
       let(:target) { double('target') }
 
       before do
-        expect_any_instance_of(Datadog::Core::Transport::Parcel).to receive(:encode_traces)
-          .with(encoder, traces)
-          .and_return(encoded_traces)
-
+        # Mock only the custom write operation - let encoding happen naturally
         expect(target).to receive(:write)
-          .with(client.out, encoded_traces)
+          .with(client.out, kind_of(String))
           .and_return(result)
 
         expect(client).to receive(:update_stats_from_response!)
