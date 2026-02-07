e07646cba6e3446c26d822d45a2ce37a286807b8
Unicorn Enterprises <une@narnia>
2026-02-07 03:34:41 -0500

    missing tests

---
+++ spec/datadog/tracing/contrib/active_storage/events/streaming_download_spec.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'datadog/tracing/contrib/active_storage/events/streaming_download'
+require 'datadog/tracing/span_operation'
+
+RSpec.describe Datadog::Tracing::Contrib::ActiveStorage::Events::StreamingDownload do
+  describe '.event_name' do
+    it 'returns the correct event name' do
+      expect(described_class.event_name).to eq('service_streaming_download.active_storage')
+    end
+  end
+
+  describe '.span_name' do
+    it 'returns the correct span name' do
+      expect(described_class.span_name).to eq('active_storage.streaming_download')
+    end
+  end
+
+  describe '.process' do
+    let(:span) { Datadog::Tracing::SpanOperation.new('active_storage.streaming_download') }
+    let(:event) { double('event') }
+    let(:id) { double('id') }
+    let(:payload) do
+      {
+        service: 'GCS',
+        key: 'media/stream.mp4'
+      }
+    end
+
+    before do
+      allow(Datadog.configuration.tracing).to receive(:[]).with(:active_storage).and_return(
+        double(
+          service_name: nil,
+          analytics_enabled: false,
+          analytics_sample_rate: 1.0
+        )
+      )
+    end
+
+    it 'sets the span resource' do
+      described_class.process(span, event, id, payload)
+      expect(span.resource).to eq('GCS: media/stream.mp4')
+    end
+
+    it 'sets the span type' do
+      described_class.process(span, event, id, payload)
+      expect(span.span_type).to eq('http')
+    end
+
+    it 'sets service and key tags' do
+      described_class.process(span, event, id, payload)
+      expect(span.get_tag('active_storage.service')).to eq('GCS')
+      expect(span.get_tag('active_storage.key')).to eq('media/stream.mp4')
+    end
+
+    it 'sets component and operation tags' do
+      described_class.process(span, event, id, payload)
+      expect(span.get_tag('component')).to eq('active_storage')
+      expect(span.get_tag('operation')).to eq('streaming_download')
+    end
+  end
+end
---
+++ spec/datadog/tracing/contrib/active_storage/events/transform_spec.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'datadog/tracing/contrib/active_storage/events/transform'
+require 'datadog/tracing/span_operation'
+
+RSpec.describe Datadog::Tracing::Contrib::ActiveStorage::Events::Transform do
+  describe '.event_name' do
+    it 'returns the correct event name' do
+      expect(described_class.event_name).to eq('transform.active_storage')
+    end
+  end
+
+  describe '.span_name' do
+    it 'returns the correct span name' do
+      expect(described_class.span_name).to eq('active_storage.transform')
+    end
+  end
+
+  describe '.process' do
+    let(:span) { Datadog::Tracing::SpanOperation.new('active_storage.transform') }
+    let(:event) { double('event') }
+    let(:id) { double('id') }
+    let(:payload) { {} }
+
+    before do
+      allow(Datadog.configuration.tracing).to receive(:[]).with(:active_storage).and_return(
+        double(
+          service_name: nil,
+          analytics_enabled: false,
+          analytics_sample_rate: 1.0
+        )
+      )
+    end
+
+    it 'sets the span type' do
+      described_class.process(span, event, id, payload)
+      expect(span.span_type).to eq('http')
+    end
+
+    it 'sets component and operation tags' do
+      described_class.process(span, event, id, payload)
+      expect(span.get_tag('component')).to eq('active_storage')
+      expect(span.get_tag('operation')).to eq('transform')
+    end
+
+    context 'when service_name is configured' do
+      before do
+        allow(Datadog.configuration.tracing).to receive(:[]).with(:active_storage).and_return(
+          double(
+            service_name: 'custom_storage',
+            analytics_enabled: false,
+            analytics_sample_rate: 1.0
+          )
+        )
+      end
+
+      it 'sets the custom service name' do
+        described_class.process(span, event, id, payload)
+        expect(span.service).to eq('custom_storage')
+      end
+    end
+  end
+end
