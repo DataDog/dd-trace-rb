a05e728cf5f2e3c6dd3a7235bc9b7816eafd2458
Unicorn Enterprises <une@narnia>
2026-02-07 02:05:15 -0500

    clean up

--- spec/datadog/tracing/transport/io/traces_spec.rb
+++ spec/datadog/tracing/transport/io/traces_spec.rb
@@ -31,72 +31,70 @@ RSpec.describe Datadog::Tracing::Transport::IO::Traces::Response do
 end
 
 RSpec.describe Datadog::Tracing::Transport::IO::Traces::Encoder do
-  describe '#encode_data' do
-    def compare_arrays(left = [], right = [])
-      left.zip(right).each { |tuple| yield(*tuple) }
-    end
+  def compare_arrays(left = [], right = [])
+    left.zip(right).each { |tuple| yield(*tuple) }
+  end
 
-    let(:trace_encoder) { Class.new { include Datadog::Tracing::Transport::IO::Traces::Encoder }.new }
-    let(:encoder) { Datadog::Core::Encoding::JSONEncoder }
+  let(:trace_encoder) { Class.new { include Datadog::Tracing::Transport::IO::Traces::Encoder }.new }
+  let(:encoder) { Datadog::Core::Encoding::JSONEncoder }
 
-    describe '.encode_traces' do
-      subject(:encode_traces) { trace_encoder.encode_traces(traces) }
+  describe '.encode_traces' do
+    subject(:encode_traces) { trace_encoder.encode_traces(traces) }
 
-      let(:traces) { get_test_traces(2) }
+    let(:traces) { get_test_traces(2) }
 
-      it { is_expected.to be_a_kind_of(Hash) }
+    it { is_expected.to be_a_kind_of(Hash) }
 
-      describe 'produces a hash' do
-        subject(:encoded) { encode_traces }
+    describe 'produces a hash' do
+      subject(:encoded) { encode_traces }
 
-        it 'which is wrapped' do
-          is_expected.to be_a_kind_of(Hash)
-          is_expected.to include(:traces => kind_of(Array))
+      it 'which is wrapped' do
+        is_expected.to be_a_kind_of(Hash)
+        is_expected.to include(:traces => kind_of(Array))
+      end
+
+      describe 'whose encoded traces' do
+        subject(:encoded_traces) { encoded[:traces] }
+
+        it 'contains the traces' do
+          is_expected.to have(traces.length).items
         end
 
-        describe 'whose encoded traces' do
-          subject(:encoded_traces) { encoded[:traces] }
-
-          it 'contains the traces' do
-            is_expected.to have(traces.length).items
-          end
-
-          it 'has IDs that are hex encoded' do
-            compare_arrays(traces, encoded_traces) do |trace, encoded_trace|
-              compare_arrays(trace.spans, encoded_trace) do |span, encoded_span|
-                expect(encoded_span).to include(
-                  :trace_id => span.trace_id.to_s(16),
-                  :span_id => span.id.to_s(16),
-                  :parent_id => span.parent_id.to_s(16)
-                )
-              end
+        it 'has IDs that are hex encoded' do
+          compare_arrays(traces, encoded_traces) do |trace, encoded_trace|
+            compare_arrays(trace.spans, encoded_trace) do |span, encoded_span|
+              expect(encoded_span).to include(
+                :trace_id => span.trace_id.to_s(16),
+                :span_id => span.id.to_s(16),
+                :parent_id => span.parent_id.to_s(16),
+              )
             end
           end
         end
       end
+    end
 
-      context 'when ID is missing' do
-        subject(:encoded_traces) { encode_traces[:traces] }
+    context 'when ID is missing' do
+      subject(:encoded_traces) { encode_traces[:traces] }
 
-        let(:missing_id) { :span_id }
+      let(:missing_id) { :span_id }
 
-        before do
-          # Delete ID from each Span
-          traces.each do |trace|
-            trace.spans.each do |span|
-              allow(span).to receive(:to_hash)
-                .and_wrap_original do |m, *_args|
-                  m.call.tap { |h| h.delete(missing_id) }
-                end
-            end
+      before do
+        # Delete ID from each Span
+        traces.each do |trace|
+          trace.spans.each do |span|
+            allow(span).to receive(:to_hash)
+              .and_wrap_original do |m, *_args|
+                m.call.tap { |h| h.delete(missing_id) }
+              end
           end
         end
+      end
 
-        it 'does not include the missing ID' do
-          compare_arrays(traces, encoded_traces) do |trace, encoded_trace|
-            compare_arrays(trace.spans, encoded_trace) do |_span, encoded_span|
-              expect(encoded_span).to_not include(missing_id.to_s)
-            end
+      it 'does not include the missing ID' do
+        compare_arrays(traces, encoded_traces) do |trace, encoded_trace|
+          compare_arrays(trace.spans, encoded_trace) do |_span, encoded_span|
+            expect(encoded_span).to_not include(missing_id.to_s)
           end
         end
       end
