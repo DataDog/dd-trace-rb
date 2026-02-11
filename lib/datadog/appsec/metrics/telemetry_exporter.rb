f018562ba6ed7cc192be94550281ff23bcb2f81e
Yury Lebedev <yury.lebedev@datadoghq.com>
2026-02-11 16:46:04 +0100

    Add API Security telemetry to AppSec telemetry exporter

--- lib/datadog/appsec/metrics/telemetry_exporter.rb
+++ lib/datadog/appsec/metrics/telemetry_exporter.rb
@@ -23,6 +23,17 @@ module Datadog
             }
           )
         end
+
+        def export_api_security_metrics(context)
+          return unless context.state[:instrumented_web_framework]
+
+          metric_name = context.state[:schema_extracted] ? 'schema' : 'no_schema'
+
+          AppSec.telemetry.inc(
+            AppSec::Ext::TELEMETRY_METRICS_NAMESPACE, "api_security.request.#{metric_name}", 1,
+            tags: {framework: context.state[:instrumented_web_framework]}
+          )
+        end
       end
     end
   end
