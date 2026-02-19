d8459b9881f69c6d171d9999ddeec4e1ebaa2ee7
Augusto de Oliveira <augusto.deoliveira@datadoghq.com>
2026-02-19 18:08:43 +0100

    tweak: remove ".rb" from benchmark results files

--- benchmarks/profiling_sample_serialize.rb
+++ benchmarks/profiling_sample_serialize.rb
@@ -42,7 +42,7 @@ class ProfilerSampleSerializeBenchmark
         nil
       end
 
-      x.save! "#{File.basename(__FILE__)}-results.json" unless VALIDATE_BENCHMARK_MODE
+      x.save! "#{File.basename(__FILE__, '.rb')}-results.json" unless VALIDATE_BENCHMARK_MODE
       x.compare!
     end
 
