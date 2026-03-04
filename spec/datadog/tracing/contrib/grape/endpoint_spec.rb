5f6e26b4be6f4b1aa489f47fcd57cd60c450d46a
Victor Pellan <37553749+vpellan@users.noreply.github.com>
2026-03-04 14:02:27 +0100

    Apply suggestions from code review
    Co-authored-by: Sergey Fedorov <oni.strech@gmail.com>

--- spec/datadog/tracing/contrib/grape/endpoint_spec.rb
+++ spec/datadog/tracing/contrib/grape/endpoint_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Datadog::Tracing::Contrib::Grape::Endpoint do
   describe '.api_view' do
     subject(:api_view) { described_class.send(:api_view, api) }
 
-    context 'when api inherits from Grape::API::Instance and responds to base (Grape >= 1.2.0, < 2.3.0)' do
+    context 'when api inherits from Grape::API::Instance and responds to base' do
       let(:api) do
         stub_const('Grape::API::Instance', Class.new)
         stub_const(
@@ -26,7 +26,7 @@ RSpec.describe Datadog::Tracing::Contrib::Grape::Endpoint do
     # This test covers Grape < 1.2.0 where the API is an Grape::API::Instance and not a class,
     # as well as Grape >= 2.3.0 where the base attr_reader was removed
     # See: https://github.com/ruby-grape/grape/commit/98214705fb61e3e90583bd3ad2b9889daa1bc794
-    context 'when api inherits from Grape::API::Instance but does not respond to base (Grape < 1.2.0 or Grape >= 2.3.0)' do
+    context 'when api inherits from Grape::API::Instance but does not respond to base' do
       let(:api) do
         stub_const('Grape::API::Instance', Class.new)
         stub_const('TestAPIWithoutBase', Class.new(Grape::API::Instance))
