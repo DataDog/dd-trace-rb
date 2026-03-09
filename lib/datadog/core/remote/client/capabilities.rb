fc28746232f297ad56d05e32c1f372589121530c
Unicorn Enterprises <une@narnia>
2026-03-09 16:10:39 -0400

    Register symbol_database with remote config capabilities

--- lib/datadog/core/remote/client/capabilities.rb
+++ lib/datadog/core/remote/client/capabilities.rb
@@ -37,6 +37,13 @@ module Datadog
               register_capabilities(Datadog::DI::Remote.capabilities)
               register_products(Datadog::DI::Remote.products)
               register_receivers(Datadog::DI::Remote.receivers(@telemetry))
+
+              # Symbol Database (requires DI)
+              if settings.respond_to?(:symbol_database) && settings.symbol_database.enabled
+                register_capabilities(Datadog::SymbolDatabase::Remote.capabilities)
+                register_products(Datadog::SymbolDatabase::Remote.products)
+                register_receivers(Datadog::SymbolDatabase::Remote.receivers(@telemetry))
+              end
             end
 
             if settings.respond_to?(:open_feature) && settings.open_feature.enabled
