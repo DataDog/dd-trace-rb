e7d485f406781d7dff77a5f007b1cef7483a8853
Unicorn Enterprises <une@narnia>
2026-03-09 16:10:21 -0400

    [SECTION START: Lifecycle Management] Wire symbol_database into tracer

--- lib/datadog/core/configuration/components.rb
+++ lib/datadog/core/configuration/components.rb
@@ -166,6 +166,9 @@ module Datadog
           @ai_guard = Datadog::AIGuard::Component.build(settings, logger: @logger, telemetry: telemetry)
           @open_feature = OpenFeature::Component.build(settings, agent_settings, logger: @logger, telemetry: telemetry)
           @dynamic_instrumentation = Datadog::DI::Component.build(settings, agent_settings, @logger, telemetry: telemetry)
+
+        # Symbol Database (requires DI)
+        @symbol_database = Datadog::SymbolDatabase::Component.build(settings, agent_settings, @logger, telemetry: telemetry)
           @error_tracking = Datadog::ErrorTracking::Component.build(settings, @tracer, @logger)
           @data_streams = self.class.build_data_streams(settings, agent_settings, @logger, @agent_info)
           @environment_logger_extra[:dynamic_instrumentation_enabled] = !!@dynamic_instrumentation
