b2a67b0fb8dc4c0daba554bd453f9d5aeaf5c08d
wan <26727996+wantsui@users.noreply.github.com>
2026-03-24 17:05:57 -0400

    Fix undefined method after_initialize' for Datadog::Core::Contrib::Rails::Railtie:Class error

--- lib/datadog/core/contrib/rails/railtie.rb
+++ lib/datadog/core/contrib/rails/railtie.rb
@@ -9,15 +9,17 @@ module Datadog
       module Rails
         # Railtie that registers the Rails application name on the process tag module.
         class Railtie < ::Rails::Railtie
-          ::ActiveSupport.on_load(:after_initialize) do
-            Datadog::Core::Contrib::Rails::Railtie.after_initialize
-          end
-
           def self.after_initialize
             return unless Datadog.configuration.experimental_propagate_process_tags_enabled
 
             Datadog::Core::Environment::Process.rails_application_name =
               Datadog::Core::Contrib::Rails::Utils.app_name
+          end
+
+          # Registered after the method definition so the method exists if on_load fires immediately
+          # (which happens when the Railtie is loaded into an already-initialized Rails app).
+          ::ActiveSupport.on_load(:after_initialize) do
+            Datadog::Core::Contrib::Rails::Railtie.after_initialize
           end
         end
       end
