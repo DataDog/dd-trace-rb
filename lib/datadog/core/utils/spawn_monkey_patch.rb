30561aa371548561542d83d6f3a3cc334e6f0cbe
Marco Costa <marco.costa@datadoghq.com>
2026-04-22 17:30:29 -0700

    Fix spawn monkey patch env handling

--- lib/datadog/core/utils/spawn_monkey_patch.rb
+++ lib/datadog/core/utils/spawn_monkey_patch.rb
@@ -3,32 +3,46 @@
 module Datadog
   module Core
     module Utils
+      # Applies the Process.spawn wrapper used to merge additional environment variables
+      # into child processes.
       module SpawnMonkeyPatch
         # @param env_provider [#call] returns a Hash of env vars to merge into the child process
         def self.apply!(env_provider:)
           @env_provider = env_provider
+
+          return if ::Process.singleton_class.ancestors.include?(ProcessSpawnPatch)
+
           ::Process.singleton_class.prepend(ProcessSpawnPatch)
-          true
         end
 
+        # Vessel for env_provider propagation.
         module ProcessSpawnPatch
-          def spawn(*args, **opts)
-            args.replace(SpawnMonkeyPatch.inject_envs(args))
-            super
+          def spawn(*args)
+            super(*SpawnMonkeyPatch.inject_envs(args))
           end
         end
 
-        # Process.spawn(env?, cmd, ...): env is optional first arg (Hash). When present, merge
-        # runtime_ids into it; when absent, prepend full ENV + runtime_ids so the child inherits both.
+        # Merge the env vars from `env_provider` with the `env` argument from {Process.spawn}.
+        #
+        # `env` is the first argument to {Process.spawn}, which is an optional {Hash}
+        # (https://docs.ruby-lang.org/en/4.0/Process.html#method-c-spawn):
+        # `Process.spawn([env, ] *args, options = {})`
+        #
+        # One thing to note is that parent process' (this process') environment variables are
+        # inherited by default by the spawned process. They are merged with (and possibly overwritten by)
+        # the env vars from the argument `env`.
+        # (https://docs.ruby-lang.org/en/4.0/Process.html#module-Process-label-Environment+Variables+-28-3Aunsetenv_others-29)
+        # This doesn't affect the current implementation.
         def self.inject_envs(args)
-          runtime_ids = @env_provider.call
-          env_provided = Hash === args.first
+          provided_env = @env_provider.call
 
-          base_env = env_provided ? args.first : DATADOG_ENV.to_h
-          env = base_env.merge(runtime_ids)
-          rest = env_provided ? args.drop(1) : args
+          if ::Hash === args.first
+            args[0] = args.first.merge(provided_env)
+          else
+            args.unshift(provided_env)
+          end
 
-          [env, *rest]
+          args
         end
       end
     end
--- spec/datadog/core/utils/spawn_monkey_patch_spec.rb
+++ spec/datadog/core/utils/spawn_monkey_patch_spec.rb
@@ -3,21 +3,16 @@
 require 'datadog/core/utils/spawn_monkey_patch'
 require 'datadog/core/configuration/components'
 require 'datadog/core/configuration/settings'
-require 'rbconfig'
 
 RSpec.describe Datadog::Core::Utils::SpawnMonkeyPatch do
-  let(:lineage_envs) do
+  let(:envs) do
     {
-      Datadog::Core::Environment::Identity::ENV_ROOT_SESSION_ID => 'root-runtime-id',
-      Datadog::Core::Environment::Identity::ENV_PARENT_SESSION_ID => 'parent-runtime-id',
+      'ENV1' => 'val1',
+      'ENV2' => 'val2',
     }
   end
-  let(:lineage_script) do
-    'print ENV.values_at("SPAWN_PATCH_PARENT_TEST", "_DD_ROOT_RB_SESSION_ID", ' \
-      '"_DD_PARENT_RB_SESSION_ID").join(":")'
-  end
 
-  def spawn_output(*spawn_args)
+  def process_spawn(*spawn_args)
     IO.pipe do |read_io, write_io|
       process_options = {in: File::NULL, out: write_io, err: write_io}
       process_options.merge!(spawn_args.pop) if ::Hash === spawn_args.last
@@ -26,12 +21,12 @@ RSpec.describe Datadog::Core::Utils::SpawnMonkeyPatch do
       write_io.close
       Process.wait(pid)
 
-      read_io.read
+      Hash[read_io.read.lines.map { |line| line.chomp.split('=', 2) }]
     end
   end
 
   describe '::apply!' do
-    subject(:apply!) { described_class.apply!(env_provider: -> { lineage_envs }) }
+    subject(:apply!) { described_class.apply!(env_provider: -> { envs }) }
 
     context 'when Process.spawn is supported' do
       before do
@@ -40,140 +35,86 @@ RSpec.describe Datadog::Core::Utils::SpawnMonkeyPatch do
       end
 
       it 'prepends the spawn monkey patch' do
-        expect_in_fork do
-          apply!
-          expect(Process.singleton_class.ancestors).to include(described_class::ProcessSpawnPatch)
-          expect(Process.method(:spawn).source_location.first).to match(/spawn_monkey_patch\.rb/)
-        end
+        apply!
+        expect(Process.singleton_class.ancestors).to include(described_class::ProcessSpawnPatch)
+        expect(Process.method(:spawn).source_location.first).to match(/spawn_monkey_patch\.rb/)
       end
+
+      it 'does not patch twice' do
+        described_class.apply!(env_provider: -> { {'ENV1' => 'val1'} })
+        described_class.apply!(env_provider: -> { {'ENV1' => 'val2'} })
+
+        expect(Process.singleton_class.ancestors.count(described_class::ProcessSpawnPatch)).to eq(1)
+        expect(process_spawn('/usr/bin/env')).to include('ENV1' => 'val2')
+      end
+    end
+  end
+
+  describe 'on Process.spawn' do
+    subject(:apply!) { described_class.apply!(env_provider: -> { envs }) }
+
+    around do |example|
+      ClimateControl.modify('PARENT1' => 'parent_val') { example.run }
+    end
+
+    before do
+      skip 'Process.spawn not supported' unless Process.respond_to?(:spawn)
+      apply!
+    end
+
+    it 'merges env_provider, parent envs, and env argument' do
+      output = process_spawn({'ARG' => 'arg_val'}, '/usr/bin/env', pgroup: true)
+
+      expect(output).to include('PARENT1' => 'parent_val', 'ENV1' => 'val1', 'ENV2' => 'val2', 'ARG' => 'arg_val')
+    end
+
+    it 'merges env_provider and parent envs when no env argument is provided' do
+      output = process_spawn('/usr/bin/env', pgroup: true)
+
+      expect(output).to include('PARENT1' => 'parent_val', 'ENV1' => 'val1', 'ENV2' => 'val2')
+    end
+
+    it 'respects parent env removal through the value `nil`' do
+      output = process_spawn({'PARENT1' => nil}, '/usr/bin/env')
+
+      expect(output).not_to include('PARENT1')
+      expect(output).to include('ENV1' => 'val1', 'ENV2' => 'val2')
+    end
+
+    it 'respects array-form command variant' do
+      command = 'printf %s "$0:$ARG:$PARENT1::$ENV1:$ENV2"'
+
+      output = IO.pipe do |read_io, write_io|
+        pid = Process.spawn(
+          {'ARG' => 'arg_val'},
+          ['/bin/sh', 'cmd-name'],
+          '-c',
+          command,
+          in: File::NULL,
+          out: write_io,
+          err: write_io,
+        )
+        write_io.close
+        Process.wait(pid)
+
+        read_io.read
+      end
+
+      expect(output).to eq('cmd-name:arg_val:parent_val:val1:val2')
     end
   end
 
   describe '::inject_envs' do
-    subject(:inject_envs) { described_class.inject_envs(args) }
+    subject(:inject_envs) { described_class.inject_envs(args.dup) }
+    let(:args) { [env_argument, '/bin/ls', '.', { pgroup: 0 }] }
+    let(:env_argument) { {'TZ' => 'UTC'} }
 
     before do
-      described_class.apply!(env_provider: -> { lineage_envs })
+      described_class.apply!(env_provider: -> { envs })
     end
 
-    context 'when Process.spawn already has an env hash' do
-      let(:args) { [existing_env, '/bin/sh', '-lc', 'exit 0', {pgroup: true}] }
-      let(:existing_env) { {'BROWSER_PATH' => '/tmp/chrome'} }
-
-      it 'merges the lineage envs into the existing env hash' do
-        expect(inject_envs).to eq(
-          [
-            existing_env.merge(lineage_envs),
-            '/bin/sh',
-            '-lc',
-            'exit 0',
-            {pgroup: true},
-          ],
-        )
-      end
-
-      it 'does not mutate the existing env hash' do
-        expect { inject_envs }.not_to change { existing_env }
-      end
-    end
-
-    context 'when Process.spawn does not have an env hash' do
-      let(:args) { ['/bin/sh', '-lc', 'exit 0', {pgroup: true}] }
-
-      it 'prepends only the lineage envs' do
-        expect(inject_envs).to eq(
-          [
-            lineage_envs,
-            '/bin/sh',
-            '-lc',
-            'exit 0',
-            {pgroup: true},
-          ],
-        )
-      end
-    end
-  end
-
-  describe 'patched Process.spawn' do
-    subject(:apply!) { described_class.apply!(env_provider: -> { lineage_envs }) }
-
-    before do
-      skip 'Fork not supported' unless Process.respond_to?(:fork)
-      skip 'Process.spawn not supported' unless Process.respond_to?(:spawn)
-    end
-
-    it 'preserves Ferrum-style positional process options when an env hash is provided' do
-      expect_in_fork do
-        ENV['SPAWN_PATCH_PARENT_TEST'] = 'parent'
-        apply!
-
-        expect(spawn_output({}, RbConfig.ruby, '-e', lineage_script, pgroup: true))
-          .to eq('parent:root-runtime-id:parent-runtime-id')
-      end
-    end
-
-    it 'preserves keyword-style spawn options when no env hash is provided' do
-      expect_in_fork do
-        ENV['SPAWN_PATCH_PARENT_TEST'] = 'parent'
-        apply!
-
-        expect(spawn_output(RbConfig.ruby, '-e', lineage_script, pgroup: true))
-          .to eq('parent:root-runtime-id:parent-runtime-id')
-      end
-    end
-
-    it 'respects unsetenv_others when an env hash is provided' do
-      expect_in_fork do
-        ENV['SPAWN_PATCH_PARENT_TEST'] = 'parent'
-        script = 'print [ENV.fetch("SPAWN_PATCH_PARENT_TEST", "missing"), ' \
-          '*ENV.values_at("EXPLICIT_CHILD_ENV", "_DD_ROOT_RB_SESSION_ID", ' \
-          '"_DD_PARENT_RB_SESSION_ID")].join(":")'
-
-        apply!
-
-        expect(
-          spawn_output(
-            {'EXPLICIT_CHILD_ENV' => 'explicit'},
-            RbConfig.ruby,
-            '-e',
-            script,
-            unsetenv_others: true,
-          ),
-        ).to eq('missing:explicit:root-runtime-id:parent-runtime-id')
-      end
-    end
-
-    it 'preserves explicit nil env values that unset variables in the child' do
-      expect_in_fork do
-        ENV['SPAWN_PATCH_DELETE_ME'] = 'parent'
-        script = 'print [(ENV.key?("SPAWN_PATCH_DELETE_ME") ? "present" : "missing"), ' \
-          '*ENV.values_at("_DD_ROOT_RB_SESSION_ID", "_DD_PARENT_RB_SESSION_ID")].join(":")'
-
-        apply!
-
-        expect(
-          spawn_output({'SPAWN_PATCH_DELETE_ME' => nil}, RbConfig.ruby, '-e', script)
-        ).to eq('missing:root-runtime-id:parent-runtime-id')
-      end
-    end
-
-    it 'preserves array-form command variants when an env hash is provided' do
-      expect_in_fork do
-        ENV['SPAWN_PATCH_PARENT_TEST'] = 'parent'
-        command = 'printf %s "$0:$SPAWN_PATCH_PARENT_TEST:$EXPLICIT_CHILD_ENV:' \
-          '$_DD_ROOT_RB_SESSION_ID:$_DD_PARENT_RB_SESSION_ID"'
-
-        apply!
-
-        expect(
-          spawn_output(
-            {'EXPLICIT_CHILD_ENV' => 'explicit'},
-            ['/bin/sh', 'custom-sh'],
-            '-c',
-            command,
-          ),
-        ).to eq('custom-sh:parent:explicit:root-runtime-id:parent-runtime-id')
-      end
+    it 'does not mutate the provided env argument Hash' do
+      expect { inject_envs }.not_to change { env_argument }
     end
   end
 
@@ -186,16 +127,14 @@ RSpec.describe Datadog::Core::Utils::SpawnMonkeyPatch do
     end
 
     it 'applies both fork and spawn patches when Components is initialized' do
-      expect_in_fork do
-        Datadog::Core::Configuration::Components.new(Datadog::Core::Configuration::Settings.new)
+      Datadog::Core::Configuration::Components.new(Datadog::Core::Configuration::Settings.new)
 
-        expect(Process.singleton_class.ancestors).to include(
-          Datadog::Core::Utils::AtForkMonkeyPatch::ProcessMonkeyPatch,
-        )
-        expect(Process.singleton_class.ancestors).to include(
-          Datadog::Core::Utils::SpawnMonkeyPatch::ProcessSpawnPatch,
-        )
-      end
+      expect(Process.singleton_class.ancestors).to include(
+        Datadog::Core::Utils::AtForkMonkeyPatch::ProcessMonkeyPatch,
+      )
+      expect(Process.singleton_class.ancestors).to include(
+        Datadog::Core::Utils::SpawnMonkeyPatch::ProcessSpawnPatch,
+      )
     end
   end
 end
