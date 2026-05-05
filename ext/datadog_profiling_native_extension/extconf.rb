8b86aabfee47ee29d780d2663a83c7c6dfe2bd42
Benoit Daloze <benoit.daloze@datadoghq.com>
2026-05-05 14:11:00 +0200

    Remove incorrect append_cflags "-Wall"/"-Wextra" which cause compilation failures on 3.3.11
    * See https://github.com/DataDog/dd-trace-rb/pull/5676#discussion_r3188224498 for details.

--- ext/datadog_profiling_native_extension/extconf.rb
+++ ext/datadog_profiling_native_extension/extconf.rb
@@ -75,6 +75,10 @@ Logging.message("[datadog] Using compiler:\n")
 xsystem("#{CONFIG["CC"]} -v")
 Logging.message("[datadog] End of compiler information\n")
 
+# We must *never* `append_cflags "-Wall"` or `append_cflags "-Wextra"`, because those add the flag at the end,
+# which then overrides earlier -Wno-* flags from RbConfig::CONFIG["warnflags"], and causes errors if -Werror is added.
+# There is no need to add them anyway, Ruby since 1.9 has RbConfig::CONFIG["warnflags"] starting with "-Wall -Wextra ".
+
 # Because we can't control what compiler versions our customers use, shipping with -Werror by default is a no-go.
 # But we can enable it in CI, so that we quickly spot any new warnings that just got introduced.
 append_cflags "-Werror" if ENV["DATADOG_GEM_CI"] == "true"
@@ -106,10 +110,6 @@ append_cflags "-fvisibility=hidden"
 
 # Avoid legacy C definitions
 append_cflags "-Wold-style-definition"
-
-# Enable all other compiler warnings
-append_cflags "-Wall"
-append_cflags "-Wextra"
 
 if ENV["DDTRACE_DEBUG"] == "true"
   $defs << "-DDD_DEBUG"
