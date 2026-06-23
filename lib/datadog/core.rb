fee28b9075c853a3a5a33969ad8adfafc5b47a82
ddsign <ddsign@bsdpower.com>
2026-06-23 14:18:14 -0400

    core: add FATAL_EXCEPTION_CLASSES and reraise_if_fatal helper
    Shared by DI and symbol database catch-all rescue handlers to re-raise
    process-fatal exceptions (SystemExit, SignalException, NoMemoryError)
    that must never be swallowed.

--- lib/datadog/core.rb
+++ lib/datadog/core.rb
@@ -16,6 +16,19 @@ module Datadog
   module Core
     extend Core::Deprecations
 
+    # Exception classes that catch-all rescues must never swallow: they signal
+    # that the process is being torn down or has run out of memory and have to
+    # propagate. SignalException covers Interrupt (Ctrl+C) and every other
+    # signal delivered as an exception.
+    FATAL_EXCEPTION_CLASSES = [SystemExit, SignalException, NoMemoryError].freeze
+
+    # Re-raise +exc+ when it is fatal (see FATAL_EXCEPTION_CLASSES). Call this as
+    # the first statement of a `rescue Exception` handler so that fatal
+    # conditions are not accidentally swallowed by a broad rescue.
+    def self.reraise_if_fatal(exc)
+      raise exc if FATAL_EXCEPTION_CLASSES.any? { |klass| exc.is_a?(klass) }
+    end
+
     LIBDATADOG_API_FAILURE =
       begin
         require "libdatadog_api.#{RUBY_VERSION[/\d+.\d+/]}_#{RUBY_PLATFORM}"
