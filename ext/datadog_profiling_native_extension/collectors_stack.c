1bbeb3d2e29bfbd981777cb7d74338565278a670
Ivo Anjo <ivo.anjo@datadoghq.com>
2026-06-30 10:02:58 +0100

    Remove raising of exception in situation where we can't raise exceptions
    I saw this blowing up in CI due to some other errors on the branch,
    and in practice raising exceptions here is never correct and it was
    good that Ruby complained.

--- ext/datadog_profiling_native_extension/collectors_stack.c
+++ ext/datadog_profiling_native_extension/collectors_stack.c
@@ -646,7 +646,11 @@ void sampling_buffer_initialize(sampling_buffer *buffer, uint16_t max_frames) {
 
 void sampling_buffer_free(sampling_buffer *buffer) {
   if (buffer->max_frames == 0 || buffer->stack_buffer == NULL) {
-    raise_error(rb_eArgError, "sampling_buffer_free called with invalid buffer");
+    // This buffer was not initialized. This can happen if `sampling_buffer_check_max_frames` fails during initialize,
+    // although in practice that shouldn't happen either. @ivoanjo: We can't raise an exception here, this gets called
+    // by the Ruby GC, but I hesitated on dropping an `rb_bug` since it is possible to trigger this if we pass a wrong
+    // `max_frames`.
+    return;
   }
 
   ruby_xfree(buffer->stack_buffer);
