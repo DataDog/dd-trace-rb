6986aa0aa6b519f5b873b3162b55cc78659b5561
Ivo Anjo <ivo.anjo@datadoghq.com>
2026-07-23 11:38:24 +0000

    Remove `_native_filenames_available?` and checks for it
    We can support this feature on Linux and macOS on the expected platforms
    we run on, so as a first step of simplifying this code, let's remove the
    `_native_filenames_available?` checking, we won't need that anymore.

--- lib/datadog/profiling/collectors/thread_context.rb
+++ lib/datadog/profiling/collectors/thread_context.rb
@@ -32,7 +32,7 @@ module Datadog
             endpoint_collection_enabled: endpoint_collection_enabled,
             waiting_for_gvl_threshold_ns: waiting_for_gvl_threshold_ns,
             otel_context_enabled: otel_context_enabled,
-            native_filenames_enabled: validate_native_filenames(native_filenames_enabled),
+            native_filenames_enabled: native_filenames_enabled,
             overhead_filename: __FILE__,
           )
         end
@@ -89,17 +89,6 @@ module Datadog
 
           context = provider.instance_variable_get(:@context)
           context&.instance_variable_get(:@key)
-        end
-
-        def validate_native_filenames(native_filenames_enabled)
-          if native_filenames_enabled && !Datadog::Profiling::Collectors::Stack._native_filenames_available?
-            Datadog.logger.debug(
-              "Native filenames are enabled, but the required dladdr API was not available. Disabling native filenames."
-            )
-            false
-          else
-            native_filenames_enabled
-          end
         end
       end
     end
