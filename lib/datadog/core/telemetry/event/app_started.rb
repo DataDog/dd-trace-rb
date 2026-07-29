39d3466c74913edd209a5df3dbe88f72850204d2
Brian Marks <brian.marks@datadoghq.com>
2026-07-29 19:38:40 -0400

    refactor: share telemetry configuration value serialization

--- lib/datadog/core/telemetry/event/app_started.rb
+++ lib/datadog/core/telemetry/event/app_started.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "base"
+require_relative "configuration_value"
 
 module Datadog
   module Core
@@ -214,23 +215,9 @@ module Datadog
           end
 
           def to_telemetry_value(value)
-            # TODO: Add float if telemetry starts accepting it
-            case value
-            when Integer, String, true, false, nil
-              value
-            when Hash
-              value.map { |key, entry_value| "#{key}:#{entry_value}" }.join(",")
-            when Array
-              value.join(",")
-            when Module
-              value.name.to_s
-            else
-              if implements_to_s?(value)
-                value.to_s
-              else
-                value.class.to_s
-              end
-            end
+            return value.to_s if value.is_a?(Float)
+
+            ConfigurationValue.convert(value)
           end
 
           def install_signature(settings)
