3928129992901cef6e8be0a56ad127a37743098a
ddsign <ddsign@bsdpower.com>
2026-08-17 15:37:17 -0400

    Revert "Remove NONE constant from SamplingUnit"
    This reverts commit 7dcf0f04bcd128f51af954cca195494e36887be8.

--- lib/datadog/di/sampling_unit.rb
+++ lib/datadog/di/sampling_unit.rb
@@ -9,6 +9,8 @@ module Datadog
     #
     # @api private
     class SamplingUnit
+      NONE = new(nil)
+
       # Resolves the unit enclosing the current probe hit.
       #
       # @return [SamplingUnit]
@@ -20,7 +22,7 @@ module Datadog
           end
         end
 
-        new(nil)
+        NONE
       end
 
       # @param key [String, Integer, nil] the trace id, or nil when no trace is
