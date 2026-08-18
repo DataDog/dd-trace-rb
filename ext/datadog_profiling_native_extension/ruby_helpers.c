80b64e12e49f6b29a23ac7b5f674334d078a447b
Benoit Daloze <benoit.daloze@datadoghq.com>
2026-08-18 16:31:15 +0200

    Remove ruby_is_obj_with_class() since it's completely redundant with rb_objspace_internal_object_p()
    * rb_objspace_internal_object_p() already checks for `RBASIC_CLASS(obj) == 0`.

--- ext/datadog_profiling_native_extension/ruby_helpers.c
+++ ext/datadog_profiling_native_extension/ruby_helpers.c
@@ -198,16 +198,6 @@ size_t ruby_obj_memsize_of(VALUE obj) {
   }
 }
 
-// Inspired by rb_class_of but without actually returning classes or potentially doing assertions
-static bool ruby_is_obj_with_class(VALUE obj) {
-  if (!RB_SPECIAL_CONST_P(obj)) {
-    return RBASIC_CLASS(obj) != 0;
-  } else {
-    // Immediates always have a class
-    return true;
-  }
-}
-
 #ifdef NO_RB_OBJ_INFO
   const char* safe_object_info(DDTRACE_UNUSED VALUE obj) { return "(No rb_obj_info for current Ruby)"; }
 #else
@@ -219,7 +209,6 @@ static bool ruby_is_obj_with_class(VALUE obj) {
 #endif
 
 VALUE ruby_safe_inspect(VALUE obj) {
-  if (!ruby_is_obj_with_class(obj))       return rb_str_new_cstr("(Not an object)");
   if (ddtrace_is_internal_object_p(obj))  return rb_sprintf("(VM Internal, %s)", safe_object_info(obj));
   // @ivoanjo: I saw crashes on Ruby 3.1.4 when trying to #inspect matchdata objects. I'm not entirely sure why this
   // is needed, but since we only use this method for debug purposes I put in this alternative and decided not to
