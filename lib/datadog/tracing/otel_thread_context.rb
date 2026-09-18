113ca4c8fe7269691edf56fb5144e3af01bb236d
Yury Lebedev <yury.lebedev@datadoghq.com>
2026-09-18 18:07:52 +0200

    Add guard for zero trace and span IDs for OTEP

--- lib/datadog/tracing/otel_thread_context.rb
+++ lib/datadog/tracing/otel_thread_context.rb
@@ -44,6 +44,13 @@ module Datadog
       end
 
       def set(trace_id:, span_id:, local_root_span_id:)
+        # OTEP requires trace-id and span-id to be either both set or both unset.
+        # Zeroes can be used to indicate that no trace and span are active.
+        # https://github.com/open-telemetry/opentelemetry-specification/blob/main/oteps/profiles/4947-thread-ctx.md#thread-local-context-record
+        if !trace_id || trace_id.zero? || !span_id || span_id.zero?
+          return clear
+        end
+
         _native_set(trace_id, span_id, local_root_span_id)
       end
 
