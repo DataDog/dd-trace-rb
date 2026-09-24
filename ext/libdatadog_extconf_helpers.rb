74030a18920ce5ee671fc94eddb7a36cf58c1858
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2026-09-24 16:18:35 +0200

    Fix native extension builds with spaces in libdatadog paths

--- ext/libdatadog_extconf_helpers.rb
+++ ext/libdatadog_extconf_helpers.rb
@@ -2,6 +2,7 @@
 
 require "rubygems"
 require "pathname"
+require "shellwords"
 
 module Datadog
   # Contains a bunch of shared helpers that get used during building of extensions that link to libdatadog
@@ -125,8 +126,8 @@ module Datadog
       includedir = "#{libdatadog_pkgconfig_folder}/../../include"
 
       # Set mkmf global variables
-      $INCFLAGS << " -I#{includedir}"
-      $LDFLAGS << " -L#{libdir} -Wl,-rpath,#{libdir}"
+      $INCFLAGS << " -I#{Shellwords.escape(includedir)}"
+      $LDFLAGS << " -L#{Shellwords.escape(libdir)} -Wl,-rpath,#{Shellwords.escape(libdir)}"
       $libs << " -ldatadog_profiling"
 
       # Add extra relative rpaths using $ORIGIN to handle environments where gems are moved after installation.
@@ -141,7 +142,7 @@ module Datadog
           libdatadog_pkgconfig_folder: libdatadog_pkgconfig_folder,
         ),
       ]
-      extra_relative_rpaths.each { |folder| $LDFLAGS << " -Wl,-rpath,$$$\\\\{ORIGIN\\}/#{folder}" }
+      extra_relative_rpaths.each { |folder| $LDFLAGS << " -Wl,-rpath,$$$\\\\{ORIGIN\\}/#{Shellwords.escape(folder)}" }
 
       logger.message("linking with libdatadog (include=#{includedir}, lib=#{libdir})\n")
       logger.message("[datadog] $LDFLAGS were set to: #{$LDFLAGS.inspect}\n")
