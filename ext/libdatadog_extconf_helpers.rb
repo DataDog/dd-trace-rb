b6629844d059a6d16dc03c7c87935bfa0f7e3ca4
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2026-09-25 09:51:22 +0200

    Temporarily disable path escaping to verify build-gem regression

--- ext/libdatadog_extconf_helpers.rb
+++ ext/libdatadog_extconf_helpers.rb
@@ -126,8 +126,8 @@ module Datadog
       includedir = "#{libdatadog_pkgconfig_folder}/../../include"
 
       # Set mkmf global variables
-      $INCFLAGS << " -I#{Shellwords.escape(includedir)}"
-      $LDFLAGS << " -L#{Shellwords.escape(libdir)} -Wl,-rpath,#{Shellwords.escape(libdir)}"
+      $INCFLAGS << " -I#{includedir}"
+      $LDFLAGS << " -L#{libdir} -Wl,-rpath,#{libdir}"
       $libs << " -ldatadog_profiling"
 
       # Add extra relative rpaths using $ORIGIN to handle environments where gems are moved after installation.
@@ -142,7 +142,7 @@ module Datadog
           libdatadog_pkgconfig_folder: libdatadog_pkgconfig_folder,
         ),
       ]
-      extra_relative_rpaths.each { |folder| $LDFLAGS << " -Wl,-rpath,$$$\\\\{ORIGIN\\}/#{Shellwords.escape(folder)}" }
+      extra_relative_rpaths.each { |folder| $LDFLAGS << " -Wl,-rpath,$$$\\\\{ORIGIN\\}/#{folder}" }
 
       logger.message("linking with libdatadog (include=#{includedir}, lib=#{libdir})\n")
       logger.message("[datadog] $LDFLAGS were set to: #{$LDFLAGS.inspect}\n")
