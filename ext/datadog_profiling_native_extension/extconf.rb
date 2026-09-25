5f7b9300e823b0a73729c0730ee315178c4bc79b
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2026-09-25 09:56:03 +0200

    Escape Ruby core header paths when building the profiler

--- ext/datadog_profiling_native_extension/extconf.rb
+++ ext/datadog_profiling_native_extension/extconf.rb
@@ -260,7 +260,8 @@ dir_config("ruby") # allow user to pass in non-standard core include directory
 #
 # To workaround this issue, we override `with_cppflags` for datadog-ruby_core_source to still include `$CPPFLAGS`.
 Datadog::RubyCoreSource.define_singleton_method(:with_cppflags) do |newflags, &block|
-  super("#{newflags} #{$CPPFLAGS}", &block)
+  # ruby_core_source supplies one unescaped -I argument containing the complete header path.
+  super("#{Shellwords.escape(newflags)} #{$CPPFLAGS}", &block)
 end
 
 Datadog::RubyCoreSource
